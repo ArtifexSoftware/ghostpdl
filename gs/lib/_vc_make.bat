c7971ed5f0e5774935166e41971e720d1874d307
Henry Stiles <henry.stiles@artifex.com>
1998-07-28 06:13:21 +0000

    This commit was generated by cvs2svn to compensate for changes in r253, which included commits to RCS files with non-trunk default branches.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@254 06663e23-700e-0410-b217-a244a6096597

--- gs/lib/_vc_make.bat
+++
@@ -1,5 +0,0 @@
-cd ..\gs 
-c:\progra~1\devstudio\VC\bin\nmake /F _vc_temp.mak CONFIG=5 gsargs.obj gsnogc.obj echogs.exe 
-c:\progra~1\devstudio\VC\bin\nmake /F _vc_temp.mak CONFIG=5 ld5.tr gconfig5.obj gscdefs5.obj 
-..\gs\echogs.exe -w _wm_cdir.bat cd -s -r _vc_dir.bat 
-_wm_cdir.bat 
