4b377f2c06ab31100cf703b04de2b819b3648d10
Ray Johnston <ray.johnston@artifex.com>
1998-11-25 17:31:20 +0000

    Change to make this pseudo "cp" run silently and get rid of annoying and misleading "File not found." message.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@557 06663e23-700e-0410-b217-a244a6096597

--- gs/lib/cp.bat
+++ gs/lib/cp.bat
@@ -1,11 +1,10 @@
-
 @echo off
 if "%2"=="." goto ne
-erase _.tmp
+if exist _.tmp erase _.tmp > nul:
 rem >_.tmp
-copy /B %1+_.tmp %2
-erase _.tmp
+copy /B %1+_.tmp %2 > nul:
+erase _.tmp > nul:
 goto x
 :ne
-copy /B %1 %2
+copy /B %1 %2 > nul:
 :x
