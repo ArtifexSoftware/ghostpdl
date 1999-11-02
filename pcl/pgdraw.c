ad387be7c84ac058070b336758f292da4c969888
Henry Stiles <henry.stiles@artifex.com>
1999-11-02 04:26:52 +0000

    It seems that HP would prefer 6 digits of accuracy while scaling.  The test file cd60bw22.bin actually requires this lesser accuracy to print correctly.  This fixes the stray lines on the baseball players face on page 3.  The truncation was empirically derived.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@1018 06663e23-700e-0410-b217-a244a6096597

--- pcl/pgdraw.c
+++ pcl/pgdraw.c
@@ -169,6 +169,10 @@ hpgl_compute_user_units_to_plu_ctm(const hpgl_state_t *pgls, gs_matrix *pmat)
 		      scale_y = scale_x;
 		    }
 		}
+	      /* looks like HP keeps 6 digits of accuracy here.  This
+                 was derived empirically */
+	      scale_x -= fmod(scale_x, 1.0E-6);
+	      scale_y -= fmod(scale_y, 1.0E-6);
 	      hpgl_call(gs_make_translation(origin_x, origin_y, pmat));
 	      hpgl_call(gs_matrix_scale(pmat, scale_x, scale_y, pmat));
 	      hpgl_call(gs_matrix_translate(pmat, -pgls->g.scaling_params.pmin.x,
