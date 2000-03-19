f09eb63770d272d2b24f56befd0d01f749fa295e
L Peter Deutsch <lpd@ghostscript.com>
2000-03-19 16:53:50 +0000

    Fix: .buildfont0 should not check CIDSystemInfo compatibility.  The PLRM3 says PostScript interpreters do not make this check, even though the PLRM2 says compatibility is required (!).
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@157 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/zfcmap.c
+++ gs/src/zfcmap.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1997, 1998, 1999 Aladdin Enterprises.  All rights reserved.
+/* Copyright (C) 1997, 2000 Aladdin Enterprises.  All rights reserved.
 
    This file is part of Aladdin Ghostscript.
 
@@ -31,6 +31,13 @@
 #include "ifont.h"		/* for zfont_mark_glyph_name */
 #include "iname.h"
 #include "store.h"
+  
+/*
+ * Define whether to check the compatibility of CIDSystemInfo between the
+ * CMap and the composite font.  PLRM2 says compatibility is required, but
+ * PLRM3 says the interpreter doesn't check it.
+ */
+/*#define CHECK_CID_SYSTEM_INFO_COMPATIBILITY*/
 
 /* ---------------- Internal procedures ---------------- */
 
@@ -281,6 +288,8 @@ get_cid_system_info(gs_cid_system_info * pcidsi, const ref * psia, uint index)
     return (code < 0 ? code : 0);
 }
 
+#ifdef CHECK_CID_SYSTEM_INFO_COMPATIBILITY
+
 /* Check compatibility of CIDSystemInfo. */
 private bool
 bytes_eq(const gs_const_string *pcs1, const gs_const_string *pcs2)
@@ -295,6 +304,8 @@ cid_system_info_compatible(const gs_cid_system_info * psi1,
     return bytes_eq(&psi1->Registry, &psi2->Registry) &&
 	bytes_eq(&psi1->Ordering, &psi2->Ordering);
 }
+
+#endif /* CHECK_CID_SYSTEM_INFO_COMPATIBILITY */
 
 /* ---------------- (Semi-)public procedures ---------------- */
 
@@ -321,7 +332,6 @@ ztype0_get_cmap(const gs_cmap **ppcmap, const ref *pfdepvector, const ref *op,
     num_fonts = r_size(pfdepvector);
     for (i = 0; i < num_fonts; ++i) {
 	ref rfdep, rfsi;
-	gs_cid_system_info cidsi;
 
 	array_get(pfdepvector, (long)i, &rfdep);
 	code = acquire_cid_system_info(&rfsi, &rfdep);
@@ -330,9 +340,16 @@ ztype0_get_cmap(const gs_cmap **ppcmap, const ref *pfdepvector, const ref *op,
 	if (code == 0) {
 	    if (r_size(&rfsi) != 1)
 		return_error(e_rangecheck);
-	    get_cid_system_info(&cidsi, &rfsi, 0);
-	    if (!cid_system_info_compatible(&cidsi, pcmap->CIDSystemInfo + i))
-		return_error(e_rangecheck);
+#ifdef CHECK_CID_SYSTEM_INFO_COMPATIBILITY
+	    {
+		gs_cid_system_info cidsi;
+
+		get_cid_system_info(&cidsi, &rfsi, 0);
+		if (!cid_system_info_compatible(&cidsi,
+						pcmap->CIDSystemInfo + i))
+		    return_error(e_rangecheck);
+	    }
+#endif
 	}
     }
     *ppcmap = pcmap;
