dcf231c3dc8b7d521654b4bc2c2cb370ce24149e
L Peter Deutsch <lpd@ghostscript.com>
2000-06-08 02:33:09 +0000

    Fix: The ps2pdf.bat script didn't set the default PDF level, so it normally produced PDF 1.3 rather than 1.2, which was contrary to the documentation and different from the Unix ps2pdf script.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@499 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2pdf.bat
+++ gs/lib/ps2pdf.bat
@@ -1,12 +1,12 @@
 @echo off
 @rem $Id$
 
-rem Convert PostScript to PDF 1.3 (Acrobat 4-and-later compatible).
-rem The PDF compatibility level may change in the future:
+rem Convert PostScript to PDF 1.2 (Acrobat 3-and-later compatible).
+rem The default PDF compatibility level may change in the future:
 rem use ps2pdf12 or ps2pdf13 if you want a specific level.
 
-rem Create an empty file.
-type NUL: >_.at
+rem The current default compatibility level is PDF 1.2.
+echo -dCompatibilityLevel#1.2 >_.at
 goto bot
 
 rem Pass arguments through a file to avoid overflowing the command line.
