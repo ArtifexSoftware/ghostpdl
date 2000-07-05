c73a052894d5e7ad0ab270192c166e6b4e61cb9b
L Peter Deutsch <lpd@ghostscript.com>
2000-07-05 16:21:13 +0000

    Adds a number of (user-maintained) command files for OS/2.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@558 a1074d23-0009-0410-80fe-cf8c14f379e6

---
+++ gs/lib/pf2afm.cmd
@@ -0,0 +1,7 @@
+/* $Id$ */
+/*
+ * This file is maintained by a user: if you have any questions about it,
+ * please contact Mark Hale (mark.hale@physics.org).
+ */
+
+@gsos2 -q -dNODISPLAY -sDEVICE=nullpage -- pf2afm.ps %1
