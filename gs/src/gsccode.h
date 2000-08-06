2c72b985b583b9aa372491180999aa28256921b6
Ray Johnston <ray.johnston@artifex.com>
2000-08-06 16:48:19 +0000

    Fix problem with gs_cff.ps causing rangecheck in put -- move minimal files for CFF StandardStrings Encoding over from Aladdin 6.22.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@1177 06663e23-700e-0410-b217-a244a6096597

--- gs/src/gsccode.h
+++ gs/src/gsccode.h
@@ -60,8 +60,9 @@ typedef enum {
 	/* Pseudo-encodings (glyph sets). */
     ENCODING_INDEX_MACGLYPH,	/* a pseudo-encoding */
     ENCODING_INDEX_ALOGLYPH,	/* ditto */
-    ENCODING_INDEX_ALXGLYPH	/* ditto */
-#define NUM_KNOWN_ENCODINGS 10
+    ENCODING_INDEX_ALXGLYPH,	/* ditto */
+    ENCODING_INDEX_CFFSTRINGS	/* CFF StandardStrings */
+#define NUM_KNOWN_ENCODINGS 11
 } gs_encoding_index_t;
 #define KNOWN_REAL_ENCODING_NAMES\
   "StandardEncoding", "ISOLatin1Encoding", "SymbolEncoding",\
