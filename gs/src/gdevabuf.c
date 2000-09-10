13e900d0435423dead9e3b790f99fb0f16f0741d
L Peter Deutsch <lpd@ghostscript.com>
2000-09-10 05:23:10 +0000

    Changes the -Zv switch to trace alpha/transparency-related features.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@694 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gdevabuf.c
+++ gs/src/gdevabuf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1994, 1995, 1996, 1998, 1999 Aladdin Enterprises.  All rights reserved.
+/* Copyright (C) 1994, 2000 Aladdin Enterprises.  All rights reserved.
 
    This file is part of Aladdin Ghostscript.
 
@@ -317,7 +317,8 @@ y_transfer_next(y_transfer * pyt, gx_device * dev)
 	tby -= mdev->height;
 	tbh = ms + mh - dev->height - tby;
     }
-    if_debug7('v', "[v]my=%d, mh=%d, ms=%d, ty=%d, th=%d, tby=%d, tbh=%d\n",
+    if_debug7('V',
+	      "[V]abuf: my=%d, mh=%d, ms=%d, ty=%d, th=%d, tby=%d, tbh=%d\n",
 	      my, mh, ms, ty, th, tby, tbh);
     if (tbh > th)
 	tbh = th;
