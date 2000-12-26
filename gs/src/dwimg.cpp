8f701bda88aedf09bb394903a8739ae51c0623c8
Igor Melichev <igor.melichev@artifex.com>
2000-12-26 13:41:40 +0000

    The old code caused gswin crash when console window closes.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@1121 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/dwimg.cpp
+++ gs/src/dwimg.cpp
@@ -60,8 +60,8 @@ FindImageWindow(char FAR *dev)
 {
     ImageWindow *iw;
     for (iw = ImageWindow::first; iw!=0; iw=iw->next) {
-	if (iw->device == dev)
-	    return iw;
+        if (iw->device == dev)
+            return iw;
     }
     return NULL;
 }
@@ -73,11 +73,10 @@ ImageWindow::open(char FAR *dev)
 {
     // register class if first window
     if (first == (ImageWindow *)NULL)
-	register_class();
+        register_class();
    
     // add to list
-    if (first)
-	next = first;
+    next = first;
     first = this;
 
     // remember device handle
@@ -95,14 +94,14 @@ ImageWindow::close(void)
 
     // remove from list
     if (this == first) {
-	first = this->next;
+        first = this->next;
     }
     else {
-	ImageWindow *tmp;
-	for (tmp = first; tmp!=0; tmp=tmp->next) {
-	    if (this == tmp->next)
-		tmp->next = this->next;
-	}
+        ImageWindow *tmp;
+        for (tmp = first; tmp!=0; tmp=tmp->next) {
+            if (this == tmp->next)
+                tmp->next = this->next;
+        }
     }
     
 }
@@ -111,55 +110,55 @@ ImageWindow::close(void)
 void
 ImageWindow::register_class(void)
 {
-	WNDCLASS wndclass;
+        WNDCLASS wndclass;
 
-	/* register the window class for graphics */
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;
-	wndclass.lpfnWndProc = WndImgProc;
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = sizeof(LONG);
-	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIcon(hInstance,(LPSTR)MAKEINTRESOURCE(GSIMAGE_ICON));
-	wndclass.hCursor = LoadCursor((HINSTANCE)NULL, IDC_ARROW);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndclass.lpszMenuName = NULL;
-	wndclass.lpszClassName = szImgName;
-	RegisterClass(&wndclass);
+        /* register the window class for graphics */
+        wndclass.style = CS_HREDRAW | CS_VREDRAW;
+        wndclass.lpfnWndProc = WndImgProc;
+        wndclass.cbClsExtra = 0;
+        wndclass.cbWndExtra = sizeof(LONG);
+        wndclass.hInstance = hInstance;
+        wndclass.hIcon = LoadIcon(hInstance,(LPSTR)MAKEINTRESOURCE(GSIMAGE_ICON));
+        wndclass.hCursor = LoadCursor((HINSTANCE)NULL, IDC_ARROW);
+        wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+        wndclass.lpszMenuName = NULL;
+        wndclass.lpszClassName = szImgName;
+        RegisterClass(&wndclass);
 }
 
 void
 ImageWindow::create_window(void)
 {
-	HMENU sysmenu;
+        HMENU sysmenu;
 
-	cxClient = cyClient = 0;
-	nVscrollPos = nVscrollMax = 0;
-	nHscrollPos = nHscrollMax = 0;
+        cxClient = cyClient = 0;
+        nVscrollPos = nVscrollMax = 0;
+        nHscrollPos = nHscrollMax = 0;
 
-	// create window
-	hwnd = CreateWindow(szImgName, (LPSTR)szImgName,
-		  WS_OVERLAPPEDWINDOW,
-		  CW_USEDEFAULT, CW_USEDEFAULT, 
-		  CW_USEDEFAULT, CW_USEDEFAULT, 
-		  NULL, NULL, hInstance, (void FAR *)this);
-	ShowWindow(hwnd, SW_SHOWMINNOACTIVE);
+        // create window
+        hwnd = CreateWindow(szImgName, (LPSTR)szImgName,
+                  WS_OVERLAPPEDWINDOW,
+                  CW_USEDEFAULT, CW_USEDEFAULT, 
+                  CW_USEDEFAULT, CW_USEDEFAULT, 
+                  NULL, NULL, hInstance, (void FAR *)this);
+        ShowWindow(hwnd, SW_SHOWMINNOACTIVE);
 
-	// modify the menu to have the new items we want
-	sysmenu = GetSystemMenu(hwnd, 0);	// get the sysmenu
-	AppendMenu(sysmenu, MF_SEPARATOR, 0, NULL);
-	AppendMenu(sysmenu, MF_STRING, M_COPY_CLIP, "Copy to Clip&board");
+        // modify the menu to have the new items we want
+        sysmenu = GetSystemMenu(hwnd, 0);       // get the sysmenu
+        AppendMenu(sysmenu, MF_SEPARATOR, 0, NULL);
+        AppendMenu(sysmenu, MF_STRING, M_COPY_CLIP, "Copy to Clip&board");
 }
 
-	
+        
 void
 ImageWindow::sync(void)
 {
-    if ( !IsWindow(hwnd) )	// some clod closed the window
-	create_window();
+    if ( !IsWindow(hwnd) )      // some clod closed the window
+        create_window();
 
     if ( !IsIconic(hwnd) ) {  /* redraw window */
-	InvalidateRect(hwnd, NULL, 1);
-	UpdateWindow(hwnd);
+        InvalidateRect(hwnd, NULL, 1);
+        UpdateWindow(hwnd);
     }
 }
 
@@ -168,7 +167,7 @@ void
 ImageWindow::page(void)
 {
     if (IsIconic(hwnd))    // useless as an Icon so fix it
-	ShowWindow(hwnd, SW_SHOWNORMAL);
+        ShowWindow(hwnd, SW_SHOWNORMAL);
     BringWindowToTop(hwnd);
 
     sync();
@@ -189,11 +188,11 @@ WndImgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     ImageWindow *iw;
     if (message == WM_CREATE) {
-	// Object is stored in window extra data.
-	// Nothing must try to use the object before WM_CREATE
-	// initializes it here.
-	iw = (ImageWindow *)(((CREATESTRUCT FAR *)lParam)->lpCreateParams);
-	SetWindowLong(hwnd, 0, (LONG)iw);
+        // Object is stored in window extra data.
+        // Nothing must try to use the object before WM_CREATE
+        // initializes it here.
+        iw = (ImageWindow *)(((CREATESTRUCT FAR *)lParam)->lpCreateParams);
+        SetWindowLong(hwnd, 0, (LONG)iw);
     }
 
     // call the object window procedure
@@ -204,218 +203,218 @@ WndImgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 /* graphics window */
 LRESULT 
 ImageWindow::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{	
+{       
     HDC hdc;
     PAINTSTRUCT ps;
     RECT rect;
     int nVscrollInc, nHscrollInc;
 
     switch(message) {
-	case WM_SYSCOMMAND:
-	    // copy to clipboard
-	    if (LOWORD(wParam) == M_COPY_CLIP) {
-		HGLOBAL hglobal;
-		HPALETTE hpalette;
-		gsdll.lock_device(device, 1);
-		hglobal = gsdll.copy_dib(device);
-		if (hglobal == (HGLOBAL)NULL) {
-		    MessageBox(hwnd, "Not enough memory to Copy to Clipboard", 
-			szImgName, MB_OK | MB_ICONEXCLAMATION);
-		    gsdll.lock_device(device, 0);
-		    return 0;
-		}
-		OpenClipboard(hwnd);
-		EmptyClipboard();
-		SetClipboardData(CF_DIB, hglobal);
-		hpalette = gsdll.copy_palette(device);
-		if (hpalette)
-		    SetClipboardData(CF_PALETTE, hpalette);
-		CloseClipboard();
-		gsdll.lock_device(device, 0);
-		return 0;
-	    }
-	    break;
-	case WM_CREATE:
-	    // enable drag-drop
-	    DragAcceptFiles(hwnd, TRUE);
-	    break;
-	case WM_SIZE:
-	    if (wParam == SIZE_MINIMIZED)
-		    return(0);
-	    cyClient = HIWORD(lParam);
-	    cxClient = LOWORD(lParam);
+        case WM_SYSCOMMAND:
+            // copy to clipboard
+            if (LOWORD(wParam) == M_COPY_CLIP) {
+                HGLOBAL hglobal;
+                HPALETTE hpalette;
+                gsdll.lock_device(device, 1);
+                hglobal = gsdll.copy_dib(device);
+                if (hglobal == (HGLOBAL)NULL) {
+                    MessageBox(hwnd, "Not enough memory to Copy to Clipboard", 
+                        szImgName, MB_OK | MB_ICONEXCLAMATION);
+                    gsdll.lock_device(device, 0);
+                    return 0;
+                }
+                OpenClipboard(hwnd);
+                EmptyClipboard();
+                SetClipboardData(CF_DIB, hglobal);
+                hpalette = gsdll.copy_palette(device);
+                if (hpalette)
+                    SetClipboardData(CF_PALETTE, hpalette);
+                CloseClipboard();
+                gsdll.lock_device(device, 0);
+                return 0;
+            }
+            break;
+        case WM_CREATE:
+            // enable drag-drop
+            DragAcceptFiles(hwnd, TRUE);
+            break;
+        case WM_SIZE:
+            if (wParam == SIZE_MINIMIZED)
+                    return(0);
+            cyClient = HIWORD(lParam);
+            cxClient = LOWORD(lParam);
 
-	    cyAdjust = min(height, cyClient) - cyClient;
-	    cyClient += cyAdjust;
+            cyAdjust = min(height, cyClient) - cyClient;
+            cyClient += cyAdjust;
 
-	    nVscrollMax = max(0, height - cyClient);
-	    nVscrollPos = min(nVscrollPos, nVscrollMax);
+            nVscrollMax = max(0, height - cyClient);
+            nVscrollPos = min(nVscrollPos, nVscrollMax);
 
-	    SetScrollRange(hwnd, SB_VERT, 0, nVscrollMax, FALSE);
-	    SetScrollPos(hwnd, SB_VERT, nVscrollPos, TRUE);
+            SetScrollRange(hwnd, SB_VERT, 0, nVscrollMax, FALSE);
+            SetScrollPos(hwnd, SB_VERT, nVscrollPos, TRUE);
 
-	    cxAdjust = min(width,  cxClient) - cxClient;
-	    cxClient += cxAdjust;
+            cxAdjust = min(width,  cxClient) - cxClient;
+            cxClient += cxAdjust;
 
-	    nHscrollMax = max(0, width - cxClient);
-	    nHscrollPos = min(nHscrollPos, nHscrollMax);
+            nHscrollMax = max(0, width - cxClient);
+            nHscrollPos = min(nHscrollPos, nHscrollMax);
 
-	    SetScrollRange(hwnd, SB_HORZ, 0, nHscrollMax, FALSE);
-	    SetScrollPos(hwnd, SB_HORZ, nHscrollPos, TRUE);
+            SetScrollRange(hwnd, SB_HORZ, 0, nHscrollMax, FALSE);
+            SetScrollPos(hwnd, SB_HORZ, nHscrollPos, TRUE);
 
-	    if ((wParam==SIZENORMAL) && (cxAdjust!=0 || cyAdjust!=0)) {
-		GetWindowRect(GetParent(hwnd),&rect);
-		MoveWindow(GetParent(hwnd),rect.left,rect.top,
-		    rect.right-rect.left+cxAdjust,
-		    rect.bottom-rect.top+cyAdjust, TRUE);
-		cxAdjust = cyAdjust = 0;
-	    }
-	    return(0);
-	case WM_VSCROLL:
-	    switch(LOWORD(wParam)) {
-		case SB_TOP:
-		    nVscrollInc = -nVscrollPos;
-		    break;
-		case SB_BOTTOM:
-		    nVscrollInc = nVscrollMax - nVscrollPos;
-		    break;
-		case SB_LINEUP:
-		    nVscrollInc = -cyClient/16;
-		    break;
-		case SB_LINEDOWN:
-		    nVscrollInc = cyClient/16;
-		    break;
-		case SB_PAGEUP:
-		    nVscrollInc = min(-1,-cyClient);
-		    break;
-		case SB_PAGEDOWN:
-		    nVscrollInc = max(1,cyClient);
-		    break;
-		case SB_THUMBTRACK:
-		case SB_THUMBPOSITION:
+            if ((wParam==SIZENORMAL) && (cxAdjust!=0 || cyAdjust!=0)) {
+                GetWindowRect(GetParent(hwnd),&rect);
+                MoveWindow(GetParent(hwnd),rect.left,rect.top,
+                    rect.right-rect.left+cxAdjust,
+                    rect.bottom-rect.top+cyAdjust, TRUE);
+                cxAdjust = cyAdjust = 0;
+            }
+            return(0);
+        case WM_VSCROLL:
+            switch(LOWORD(wParam)) {
+                case SB_TOP:
+                    nVscrollInc = -nVscrollPos;
+                    break;
+                case SB_BOTTOM:
+                    nVscrollInc = nVscrollMax - nVscrollPos;
+                    break;
+                case SB_LINEUP:
+                    nVscrollInc = -cyClient/16;
+                    break;
+                case SB_LINEDOWN:
+                    nVscrollInc = cyClient/16;
+                    break;
+                case SB_PAGEUP:
+                    nVscrollInc = min(-1,-cyClient);
+                    break;
+                case SB_PAGEDOWN:
+                    nVscrollInc = max(1,cyClient);
+                    break;
+                case SB_THUMBTRACK:
+                case SB_THUMBPOSITION:
 #ifdef __WIN32__
-		    nVscrollInc = HIWORD(wParam) - nVscrollPos;
+                    nVscrollInc = HIWORD(wParam) - nVscrollPos;
 #else
-		    nVscrollInc = LOWORD(lParam) - nVscrollPos;
+                    nVscrollInc = LOWORD(lParam) - nVscrollPos;
 #endif
-		    break;
-		default:
-		    nVscrollInc = 0;
-	    }
-	    if ((nVscrollInc = max(-nVscrollPos, 
-		min(nVscrollInc, nVscrollMax - nVscrollPos)))!=0) {
-		nVscrollPos += nVscrollInc;
-		ScrollWindow(hwnd,0,-nVscrollInc,NULL,NULL);
-		SetScrollPos(hwnd,SB_VERT,nVscrollPos,TRUE);
-		UpdateWindow(hwnd);
-	    }
-	    return(0);
-	case WM_HSCROLL:
-	    switch(LOWORD(wParam)) {
-		case SB_LINEUP:
-		    nHscrollInc = -cxClient/16;
-		    break;
-		case SB_LINEDOWN:
-		    nHscrollInc = cyClient/16;
-		    break;
-		case SB_PAGEUP:
-		    nHscrollInc = min(-1,-cxClient);
-		    break;
-		case SB_PAGEDOWN:
-		    nHscrollInc = max(1,cxClient);
-		    break;
-		case SB_THUMBTRACK:
-		case SB_THUMBPOSITION:
+                    break;
+                default:
+                    nVscrollInc = 0;
+            }
+            if ((nVscrollInc = max(-nVscrollPos, 
+                min(nVscrollInc, nVscrollMax - nVscrollPos)))!=0) {
+                nVscrollPos += nVscrollInc;
+                ScrollWindow(hwnd,0,-nVscrollInc,NULL,NULL);
+                SetScrollPos(hwnd,SB_VERT,nVscrollPos,TRUE);
+                UpdateWindow(hwnd);
+            }
+            return(0);
+        case WM_HSCROLL:
+            switch(LOWORD(wParam)) {
+                case SB_LINEUP:
+                    nHscrollInc = -cxClient/16;
+                    break;
+                case SB_LINEDOWN:
+                    nHscrollInc = cyClient/16;
+                    break;
+                case SB_PAGEUP:
+                    nHscrollInc = min(-1,-cxClient);
+                    break;
+                case SB_PAGEDOWN:
+                    nHscrollInc = max(1,cxClient);
+                    break;
+                case SB_THUMBTRACK:
+                case SB_THUMBPOSITION:
 #ifdef __WIN32__
-		    nHscrollInc = HIWORD(wParam) - nHscrollPos;
+                    nHscrollInc = HIWORD(wParam) - nHscrollPos;
 #else
-		    nHscrollInc = LOWORD(lParam) - nHscrollPos;
+                    nHscrollInc = LOWORD(lParam) - nHscrollPos;
 #endif
-		    break;
-		default:
-		    nHscrollInc = 0;
-	    }
-	    if ((nHscrollInc = max(-nHscrollPos, 
-		min(nHscrollInc, nHscrollMax - nHscrollPos)))!=0) {
-		nHscrollPos += nHscrollInc;
-		ScrollWindow(hwnd,-nHscrollInc,0,NULL,NULL);
-		SetScrollPos(hwnd,SB_HORZ,nHscrollPos,TRUE);
-		UpdateWindow(hwnd);
-	    }
-	    return(0);
-	case WM_KEYDOWN:
-	    switch(LOWORD(wParam)) {
-		case VK_HOME:
-		    SendMessage(hwnd,WM_VSCROLL,SB_TOP,0L);
-		    break;
-		case VK_END:
-		    SendMessage(hwnd,WM_VSCROLL,SB_BOTTOM,0L);
-		    break;
-		case VK_PRIOR:
-		    SendMessage(hwnd,WM_VSCROLL,SB_PAGEUP,0L);
-		    break;
-		case VK_NEXT:
-		    SendMessage(hwnd,WM_VSCROLL,SB_PAGEDOWN,0L);
-		    break;
-		case VK_UP:
-		    SendMessage(hwnd,WM_VSCROLL,SB_LINEUP,0L);
-		    break;
-		case VK_DOWN:
-		    SendMessage(hwnd,WM_VSCROLL,SB_LINEDOWN,0L);
-		    break;
-		case VK_LEFT:
-		    SendMessage(hwnd,WM_HSCROLL,SB_PAGEUP,0L);
-		    break;
-		case VK_RIGHT:
-		    SendMessage(hwnd,WM_HSCROLL,SB_PAGEDOWN,0L);
-		    break;
-		case VK_RETURN:
-		    BringWindowToTop(hwndtext);
-		    break;
-	    }
-	    return(0);
-	case WM_CHAR:
-	    // send on all characters to text window
-	    if (hwndtext)
-		SendMessage(hwndtext, message, wParam, lParam);
-	    return 0;
-	case WM_PAINT:
-	    {
-	    int sx,sy,wx,wy,dx,dy;
-	    hdc = BeginPaint(hwnd, &ps);
-	    SetMapMode(hdc, MM_TEXT);
-	    SetBkMode(hdc,OPAQUE);
-	    gsdll.lock_device(device, 1);
-	    rect = ps.rcPaint;
-	    dx = rect.left;	/* destination */
-	    dy = rect.top;
-	    wx = rect.right-rect.left; /* width */
-	    wy = rect.bottom-rect.top;
-	    sx = rect.left;	/* source */
-	    sy = rect.top;
-	    sx += nHscrollPos; /* scrollbars */
-	    sy += nVscrollPos;	
-	    if (sx+wx > width)
-		    wx = width - sx;
-	    if (sy+wy > height)
-		    wy = height - sy;
+                    break;
+                default:
+                    nHscrollInc = 0;
+            }
+            if ((nHscrollInc = max(-nHscrollPos, 
+                min(nHscrollInc, nHscrollMax - nHscrollPos)))!=0) {
+                nHscrollPos += nHscrollInc;
+                ScrollWindow(hwnd,-nHscrollInc,0,NULL,NULL);
+                SetScrollPos(hwnd,SB_HORZ,nHscrollPos,TRUE);
+                UpdateWindow(hwnd);
+            }
+            return(0);
+        case WM_KEYDOWN:
+            switch(LOWORD(wParam)) {
+                case VK_HOME:
+                    SendMessage(hwnd,WM_VSCROLL,SB_TOP,0L);
+                    break;
+                case VK_END:
+                    SendMessage(hwnd,WM_VSCROLL,SB_BOTTOM,0L);
+                    break;
+                case VK_PRIOR:
+                    SendMessage(hwnd,WM_VSCROLL,SB_PAGEUP,0L);
+                    break;
+                case VK_NEXT:
+                    SendMessage(hwnd,WM_VSCROLL,SB_PAGEDOWN,0L);
+                    break;
+                case VK_UP:
+                    SendMessage(hwnd,WM_VSCROLL,SB_LINEUP,0L);
+                    break;
+                case VK_DOWN:
+                    SendMessage(hwnd,WM_VSCROLL,SB_LINEDOWN,0L);
+                    break;
+                case VK_LEFT:
+                    SendMessage(hwnd,WM_HSCROLL,SB_PAGEUP,0L);
+                    break;
+                case VK_RIGHT:
+                    SendMessage(hwnd,WM_HSCROLL,SB_PAGEDOWN,0L);
+                    break;
+                case VK_RETURN:
+                    BringWindowToTop(hwndtext);
+                    break;
+            }
+            return(0);
+        case WM_CHAR:
+            // send on all characters to text window
+            if (hwndtext)
+                SendMessage(hwndtext, message, wParam, lParam);
+            return 0;
+        case WM_PAINT:
+            {
+            int sx,sy,wx,wy,dx,dy;
+            hdc = BeginPaint(hwnd, &ps);
+            SetMapMode(hdc, MM_TEXT);
+            SetBkMode(hdc,OPAQUE);
+            gsdll.lock_device(device, 1);
+            rect = ps.rcPaint;
+            dx = rect.left;     /* destination */
+            dy = rect.top;
+            wx = rect.right-rect.left; /* width */
+            wy = rect.bottom-rect.top;
+            sx = rect.left;     /* source */
+            sy = rect.top;
+            sx += nHscrollPos; /* scrollbars */
+            sy += nVscrollPos;  
+            if (sx+wx > width)
+                    wx = width - sx;
+            if (sy+wy > height)
+                    wy = height - sy;
 
-	    gsdll.draw(device, hdc, dx, dy, wx, wy, sx, sy);
-	    gsdll.lock_device(device, 0);
+            gsdll.draw(device, hdc, dx, dy, wx, wy, sx, sy);
+            gsdll.lock_device(device, 0);
 
-	    EndPaint(hwnd, &ps);
-	    return 0;
-	    }
-	case WM_DROPFILES:
-	    SendMessage(hwndtext, message, wParam, lParam);
-	    break;
-	case WM_DESTROY:
-	    DragAcceptFiles(hwnd, FALSE);
-	    break;
+            EndPaint(hwnd, &ps);
+            return 0;
+            }
+        case WM_DROPFILES:
+            SendMessage(hwndtext, message, wParam, lParam);
+            break;
+        case WM_DESTROY:
+            DragAcceptFiles(hwnd, FALSE);
+            break;
 
     }
 
 not_ours:
-	return DefWindowProc(hwnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, message, wParam, lParam);
 }
 
