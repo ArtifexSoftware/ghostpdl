8a90724441674ce04784b5859a0523db730f44cf
Alex Cherepanov <alex.cherepanov@artifex.com>
2001-04-08 14:12:56 +0000

    Fix: AR4 doesn't like fonts with empty FontBBox which happens when the font contains only space characters. Small BBox causes AR 4 to display a hairline. So we use the full BBox.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@1412 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gdevpdfw.c
+++ gs/src/gdevpdfw.c
@@ -72,9 +72,16 @@ private int
 pdf_write_font_bbox(gx_device_pdf *pdev, const gs_int_rect *pbox)
 {
     stream *s = pdev->strm;
+    /* AR 4 doesn't like fonts with empty FontBBox which
+     * happens when the font contains only space characters.
+     * Small bbox causes AR 4 to display a hairline. So we use
+     * the full BBox.
+     */ 
+    int x= pbox->q.x + ((pbox->p.x == pbox->q.x) ? 1000 : 0);
+    int y= pbox->q.y + ((pbox->p.y == pbox->q.y) ? 1000 : 0);
 
     pprintd4(s, "/FontBBox[%d %d %d %d]",
-	     pbox->p.x, pbox->p.y, pbox->q.x, pbox->q.y);
+	     pbox->p.x, pbox->p.y, x, y);
     return 0;
 }
 
