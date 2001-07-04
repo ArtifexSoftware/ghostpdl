5892fc9df7051aa8586212d6cff9fc39442bf242
L Peter Deutsch <lpd@ghostscript.com>
2001-07-04 18:06:39 +0000

    Fix: When producing text using a Type 3 (bitmap) font, the TL command was used (incorrectly) when the new text position was directly below the current text position, not (correctly) when the new text position was directly below the beginning of the current text line.  Fixes SourceForge bug #228180.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@1574 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gdevpdft.c
+++ gs/src/gdevpdft.c
@@ -291,6 +291,7 @@ pdf_set_text_matrix(gx_device_pdf * pdev, const gs_matrix * pmat)
 	    }
 	    pdf_append_chars(pdev, &space_char, 1);
 	    pdev->text.current.x += dx * pmat->xx;
+	    pdev->text.current.y += dx * pmat->xy;
 	    /* Don't change use_leading -- it only affects Y placement. */
 	    return 0;
 	}
@@ -298,6 +299,7 @@ pdf_set_text_matrix(gx_device_pdf * pdev, const gs_matrix * pmat)
 	code = pdf_open_page(pdev, PDF_IN_TEXT);
 	if (code < 0)
 	    return code;
+	set_text_distance(&dist, &pdev->text.line_start, pmat);
 	if (dist.x == 0 && dist.y < 0) {
 	    /* Use TL, if needed, + '. */
 	    float dist_y = (float)-dist.y;
@@ -309,7 +311,6 @@ pdf_set_text_matrix(gx_device_pdf * pdev, const gs_matrix * pmat)
 	    pdev->text.use_leading = true;
 	} else {
 	    /* Use Td. */
-	    set_text_distance(&dist, &pdev->text.line_start, pmat);
 	    pprintg2(s, "%g %g Td\n", dist.x, dist.y);
 	    pdev->text.use_leading = false;
 	}
