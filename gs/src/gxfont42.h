0c60d8acd1cbf1866bfda773dcfe449daf0a08f6
Stefan Kemper <stefan.kemper@artifex.com>
2001-08-04 03:38:37 +0000

    commented proto for gs_type42_default_get_metrics it is the default implementation for font->get_metrics Direct access is needed so that it can be called prior to complete font constuction.  pxl does this to get the width of the space character during the read/construction of truetype fonts from disk.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@1451 06663e23-700e-0410-b217-a244a6096597

--- gs/src/gxfont42.h
+++ gs/src/gxfont42.h
@@ -77,10 +77,14 @@ int gs_type42_append(P7(uint glyph_index, gs_imager_state * pis,
 /* Get the metrics of a TrueType character. */
 int gs_type42_get_metrics(P3(gs_font_type42 * pfont, uint glyph_index,
 			     float psbw[4]));
-int gs_type42_default_get_metrics(P4(gs_font_type42 * pfont, uint glyph_index,
-                                     int wmode, float sbw[4]));
 int gs_type42_wmode_metrics(P4(gs_font_type42 * pfont, uint glyph_index,
 			       int wmode, float psbw[4]));
+
+/* Get the metrics of a TrueType glyph. 
+ * default for overrideable function pfont->data.get_metrics()
+ */
+int gs_type42_default_get_metrics(P4(gs_font_type42 * pfont, uint glyph_index,
+				     int wmode, float sbw[4]));
 
 /* Export the font procedures so they can be called from the interpreter. */
 font_proc_enumerate_glyph(gs_type42_enumerate_glyph);
