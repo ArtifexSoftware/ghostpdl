84114a7da5861ebba005f336807114efbc5162f7
Ralph Giles <ralph.giles@artifex.com>
2001-08-28 03:28:08 +0000

    Improves documentation of the op_def array elements and removes the obsolete BEGIN/END_OP_DEFS macros, deprecated since 5.25.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@1687 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/opdef.h
+++ gs/src/opdef.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991, 1995, 1998, 1999 Aladdin Enterprises.  All rights reserved.
+/* Copyright (C) 1991, 1995, 1998, 1999 artofcode LLC.  All rights reserved.
   
   This file is part of AFPL Ghostscript.
   
@@ -26,26 +26,24 @@
  * Define the structure for initializing the operator table.  Each operator
  * file zxxx.c declares an array of these as follows:
 
- const op_def * const zxxx_op_defs[] = {
-    {"1name", zname},
-    ...
-    op_def_end(iproc)
- };
+   const op_def * const zxxx_op_defs[] = {
+      {"1name", zname},
+      ...
+      op_def_end(iproc)
+   };
 
- * where iproc is an initialization procedure for the file, or 0.  This
- * definition always appears at the END of the file, to avoid the need for
- * forward declarations for all the operator procedures.  For backward
- * compatibility with an older convention, we also allow (deprecated)
-
- BEGIN_OP_DEFS(my_defs) {
-    {"1name", zname},
-    ...
- END_OP_DEFS(iproc) }
-
+ * where iproc is an initialization procedure for the file or 0, and, for
+ * each operator defined, the initial digit of the name string indicates
+ * the number of arguments and zname is the address of the associated C
+ * function to invoke.
+ *
+ * The array definition always appears at the END of the file, to avoid
+ * the need for forward declarations for all the operator procedures.
+ *
  * Operators may be stored in dictionaries other than systemdict.
  * We support this with op_def entries of a special form:
 
- op_def_begin_dict("dictname"),
+   op_def_begin_dict("dictname"),
 
  */
 typedef struct {
@@ -74,25 +72,6 @@ typedef struct {
  * Define the table of pointers to all operator definition tables.
  */
 extern const op_def *const op_defs_all[];
-
-/*
- * Formerly, we needed to define each op_defs table as a procedure that
- * returns the actual table, because of cross-segment linking restrictions
- * in the 16-bit Borland C compiler for MS Windows.  This is no longer
- * relevant, but for backward compatibility, we need to retain
- * BEGIN/END_OP_DEFS with the same syntax.  This involves a kludge to create
- * a dummy procedure to match up with a closing brace, and another kludge to
- * prevent a "defined but not used" warning.
- */
-
-#define BEGIN_OP_DEFS(xx_op_defs)\
-const op_def xx_op_defs[] =
-
-#define END_OP_DEFS(iproc)\
-    op_def_end(iproc)\
-};\
-static int op_defs_dummy(void)\
-{	return 1 || op_defs_dummy();	/* generates the least wasted code */
 
 /*
  * Internal operators whose names begin with %, such as continuation
