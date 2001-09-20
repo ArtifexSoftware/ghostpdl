f881decb75447998e15dfad19a3e0454ee0299a1
Stefan Kemper <stefan.kemper@artifex.com>
2001-09-20 01:51:47 +0000

    Changed comments from // to /** for java doc
    Changed run string to use text alpha bits so text is more readable.
    
    Pulled java.lang imports, not needed.
    
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@1488 06663e23-700e-0410-b217-a244a6096597

--- tools/viewer/Gpickle.java
+++ tools/viewer/Gpickle.java
@@ -2,42 +2,54 @@ import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
 import com.sun.image.codec.jpeg.*;
-import java.lang.Process;
-import java.lang.Runtime;
 
-// Interface to pcl/pxl interpreter
+/** Interface to pcl/pxl interpreter
+ * Uses Process to interpret and pipe the results back.
+ * @author Henry Stiles
+ * @version $Revision$
+*/
 
 public class Gpickle {
-    // here are some defaults that might be overriden will probably be
-    // overriden.
+    /** here are some defaults that might be overriden will probably be
+    * overriden.
+    */
     private int xRes = 100;
     private int yRes = 100;
     private String jobList = "startpage.pcl";
     private int pageToDisplay = 1;
 
-    // we always use jpeg for output because java has good internal
-    // support for jpeg.
+    /** We always use jpeg for output because java has good internal support for jpeg.
+     */
     private String deviceName = "jpeg";
 
-    // height and width of the generated image
+    /** height of the generated image
+     */
     private int height;
+    /** width of the generated image
+     */
     private int width;
-    // private methods...
+
+    /**
+     * "command line" used to run interpreter.
+     * NB Client should be able to modify settings.
+     */
     private String runString()
     {
 	return "pcl6 " +  " -dFirstPage=" + pageToDisplay + " -dLastPage=" + pageToDisplay + " -r" +
-	    xRes + "x" + yRes + " -sDEVICE=" + deviceName + " -dNOPAUSE " + " -sOutputFile=- " + jobList + "\n";
+	    xRes + "x" + yRes + " -dTextAlphaBits=2" +" -sDEVICE=" + deviceName + " -dNOPAUSE " +
+	    " -sOutputFile=- " + jobList + "\n";
     }
 
     // public methods.
 
-    // run the pcl interpreter with the current settings and return a
-    // buffered image for the page.  Sets height and width as a side
-    // effect.
+    /**
+    * run the pcl interpreter with the current settings and return a
+    * buffered image for the page.  Sets height and width as a side
+    * effect.
+    */
 
     public BufferedImage getPrinterOutputPage()
     {
-        int ch;
 	try {
 	    Process p = Runtime.getRuntime().exec(runString());
 
@@ -51,40 +63,47 @@ public class Gpickle {
 	    System.out.println(e);
 	    return null;
 	}
-	    
     }
 
-    // a list of file names that comprise a job. NB should be an array
-    // of String.
+    /** A list of file names that comprise a job.
+     * NB should be an array of String.
+     */
     public void setJob(String jobList) {
 	this.jobList = jobList;
     }
 
-    // NB needs error handling.
-    // set x and y resolution.
+    /** NB needs error handling.
+     * set x and y resolution.
+     */
     public void setRes(int xRes, int yRes)
     {
 	this.xRes = xRes;
 	this.yRes = yRes;
     }
 
-    public void setPageNumber(int page) 
+    /**
+     * Page number to display.
+     * Previous pages are "skipped".
+     */
+    public void setPageNumber(int page)
     {
 	this.pageToDisplay = page;
     }
 
-    // accessors for height and width.
+    /** Accessor for image height.
+     */
     public int getHeight()
     {
 	return height;
     }
-	
+    /** Accessor for image width.
+     */ 	 
     public int getWidth()
     {
 	return width;
     }
 
-    // test
+    /** main for test purposes */
     public static void main( String[] args ) {
 	Gpickle pcl = new Gpickle();
 	pcl.setRes(72, 72);
--- tools/viewer/Gview.java
+++ tools/viewer/Gview.java
@@ -3,33 +3,42 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 
-
+/**
+ * Simple Viewer for PCL and PXL files.
+ * PageUp and PageDown move between pages of a document.
+ * Usage:
+ * java Gview ../frs96.pxl
+ * @version $Revision$
+ * @author Henry Stiles 
+ */
 public class Gview extends JFrame implements KeyListener {
-    
-    static BufferedImage currentPage;
-    static int pageNumber = 1;
-    static Gpickle page = new Gpickle();
+
+    // NB why static ?  I wouldn't mind two Frames in one application.
+    private static BufferedImage currentPage;
+    private static int pageNumber = 1;
+    private static Gpickle page = new Gpickle();
     // constructor
-    public Gview() 
+    public Gview()
     {
 	super( "Ghost Pickle Viewer" );
 	addKeyListener(this);
     }
 
-    public void keyTyped( KeyEvent e )
-    {
-    }
-
+    /**
+     * Page Up and Page Down Keys
+     * Increment and Decrement the current page respectively.
+     * Currently does't prevent keys strokes while a page is being generated,  this give poor feedback when a key is pressed repeatedly while a long job runs.
+     */
     public void keyPressed( KeyEvent e )
     {
 	int key = e.getKeyCode();
 	// page down NB - increment past last page - BufferedImage
 	// will be null and no repaint operation
 	if ( key == 34 )
-	    pageNumber = pageNumber + 1;
+	    ++pageNumber;
 	// page up
 	else if ( key == 33 ) {
-	    pageNumber = pageNumber - 1;
+	    --pageNumber;
 	    if ( pageNumber < 1 )
 		pageNumber = 1;
 	}
@@ -39,17 +48,29 @@ public class Gview extends JFrame implements KeyListener {
 	    repaint();
     }
 
+    /**
+     * Unused required by KeyListener
+     */
+    public void keyTyped( KeyEvent e )
+    {
+    }
+
+    /**
+     * Unused required by KeyListener
+     */
     public void keyReleased( KeyEvent e )
     {
     }
 
-    
+
     public void paint( Graphics g )
     {
 	g.drawImage(currentPage, 0, 0, this);
-    }
-	    
-    
+    }	
+
+    /**
+     * Usage:
+     */
     public static void main( String[] args )
     {
 	Gview view = new Gview();
