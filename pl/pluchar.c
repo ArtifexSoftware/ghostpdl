cfd2db6bcd7eb9122d516751b2ababf21f2ab5de
Henry Stiles <henry.stiles@artifex.com>
2001-10-26 03:47:45 +0000

    UFST set size and point size were truncated now they are rounded.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@1511 06663e23-700e-0410-b217-a244a6096597

--- pl/pluchar.c
+++ pl/pluchar.c
@@ -371,11 +371,8 @@ pl_init_fc(
         /* fixed point scaling */
         floatp mscale = 1L << 16;
         pfc->s.m2.matrix_scale = 16;
-        /* NB shouldn't be 72.0 points per inch - font technologies
-           differ, for example 72.307 points per inch for AGFA
-           intellifonts */
-        pfc->s.m2.point_size = hy * 72.0 / xres * 8; /* 1/8ths */
-        pfc->s.m2.set_size = hx * 72.0 / yres * 8;
+        pfc->s.m2.point_size = (int)((hy * plfont->pts_per_inch / xres) + 0.5) * 8; /* 1/8ths */
+        pfc->s.m2.set_size = (int)((hx * plfont->pts_per_inch / yres) + 0.5) * 8;
         pfc->s.m2.m[0] = mscale * mat.xx / hx;
         pfc->s.m2.m[1] = mscale * -mat.xy / hx;
         pfc->s.m2.m[2] = mscale * mat.yx / hy;
