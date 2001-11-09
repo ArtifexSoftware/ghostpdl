fd8a064b6206ce8d04e2428177e5a2b3b494b32d
Jack Moffitt <jack.moffitt@artifex.com>
2001-11-09 18:04:01 +0000

    Remove hardcoded paths from run_regression and eliminate need for external command 'md5sum'.  We now use the 'md5' python module which does the same thing.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@1869 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/toolbin/regression/gssum.py
+++ gs/toolbin/regression/gssum.py
@@ -31,14 +31,19 @@ def get_sum(file):
     return sum
 
 def make_sum(file):
-    import os, string
+    import os, string, md5
     from stat import *
 
     mode = os.stat(file)[ST_MODE]
     if S_ISREG(mode):
-        f = os.popen('md5sum %s' % (file,))
-        line = string.split(f.readline(), ' ')[0]
-        f.close()
-        return line
+	sum = md5.new()
+	f = open(file, "r")
+	data = f.read(1024)
+	while data:
+		sum.update(data)
+		data = f.read(1024)
+	f.close()
+
+        return sum.hexdigest()
     
     return 0
