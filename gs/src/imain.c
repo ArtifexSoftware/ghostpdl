719fad664006f0c3918cec67959b0da731b21bfd
Stefan Kemper <stefan.kemper@artifex.com>
2001-11-14 00:41:00 +0000

    Don't double define outwrite, errwrite, ... for systems that include both pcl and ps.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@1549 06663e23-700e-0410-b217-a244a6096597

--- gs/src/imain.c
+++ gs/src/imain.c
@@ -862,6 +862,8 @@ gs_debug_dump_stack(int code, ref * perror_object)
     gs_main_dump_stack(gs_main_instance_default(), code, perror_object);
 }
 
+#ifndef PSI_INCLUDED 
+
 /* Provide a single point for all "C" stdout and stderr.
  * Eventually these will always be referenced through an instance structure. 
  * We don't know which instance is running (and currently only one
@@ -898,3 +900,4 @@ void errflush()
         fflush(minst->fstderr);
 }
 
+#endif
