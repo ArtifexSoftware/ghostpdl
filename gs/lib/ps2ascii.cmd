f37b2c3244da607d78f58e85e523e9675501e997
L Peter Deutsch <lpd@ghostscript.com>
2001-12-08 15:43:07 +0000

    Fix: ps2ascii was broken (probably for a long time) because it still used NOBIND rather than DELAYBIND.  Fixes SourceForge #485029.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@1944 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2ascii.cmd
+++ gs/lib/ps2ascii.cmd
@@ -7,12 +7,12 @@
 @echo off
 if '%1'=='' goto a0
 if '%2'=='' goto a1
-gsos2 -q -dNODISPLAY -dNOBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps %1 -c quit >%2
+gsos2 -q -dNODISPLAY -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps %1 -c quit >%2
 goto x
 :a0
-gsos2 -q -dNODISPLAY -dNOBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps - -c quit
+gsos2 -q -dNODISPLAY -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps - -c quit
 goto x
 :a1
-gsos2 -q -dNODISPLAY -dNOBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps %1 -c quit
+gsos2 -q -dNODISPLAY -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps %1 -c quit
 goto x
 :x
