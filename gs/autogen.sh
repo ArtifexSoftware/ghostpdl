0340b4c6462056d6f4a257959ad8f4f81037c099
Ralph Giles <ralph.giles@artifex.com>
2002-01-04 05:47:50 +0000

    Adds basic support for GNU autoconf to the build system.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@1982 a1074d23-0009-0410-80fe-cf8c14f379e6

---
+++ gs/autogen.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Run this to set up the build system: configure, makefiles, etc.
+
+package="ghostscript"
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+cd "$srcdir"
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have autoconf installed to compile $package."
+        echo "Download the appropriate package for your distribution,"
+        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	exit 1
+}
+
+echo "Generating configuration files for $package, please wait...."
+
+echo "  creating links to configuration sources"
+ln -sf src/configure.ac .
+ln -sf src/Makefile.in .
+
+#echo "  aclocal $ACLOCAL_FLAGS"
+#aclocal $ACLOCAL_FLAGS
+#echo "  autoheader"
+#autoheader
+#echo "  automake --add-missing $AUTOMAKE_FLAGS"
+#automake --add-missing $AUTOMAKE_FLAGS 
+echo "  running autoconf"
+autoconf
+
+if test -z "$*"; then
+        echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+else
+	echo "running ./configure $@"
+fi
+
+$srcdir/configure "$@" && echo
