1e3fe288997be71cba85fe48e08f7e2b1cead3b2
Ray Johnston <ray.johnston@artifex.com>
2002-03-15 07:43:34 +0000

    Fix: The file_cleanup routine used the wrong offset in the estack for the enumerator causing a SEGV if the procedure of filenameforall stopped due to error. Bug was introduced with diskn changes.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@2323 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/zfile.c
+++ gs/src/zfile.c
@@ -384,7 +384,7 @@ file_cleanup(i_ctx_t *i_ctx_p)
 {
     gx_io_device *iodev = r_ptr(esp + 2, gx_io_device);
 
-    iodev->procs.enumerate_close(r_ptr(esp + 4, file_enum));
+    iodev->procs.enumerate_close(r_ptr(esp + 5, file_enum));
     return 0;
 }
 
