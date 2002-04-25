74223f7be5f763dc8eecfeb627940d0537e244f4
Russell Lang <gsview@ghostgum.com.au>
2002-04-25 21:29:08 +0000

    Fix: Correctly embed the original EPS file in the output of ps2epsi on Windows and OS/2.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@2591 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2epsi.cmd
+++ gs/lib/ps2epsi.cmd
@@ -15,11 +15,14 @@ rem Ghostscript uses %outfile% to define the output file
 gsos2 -q -dNOPAUSE -dSAFER -dDELAYSAFER -sDEVICE=bit -sOutputFile=NUL ps2epsi.ps < %infile%
 
 rem We bracket the actual file with a few commands to help encapsulation
+echo %%%%Page: 1 1 >> %outfile%
+echo %%%%BeginDocument: %outfile% >> %outfile%
 echo /InitDictCount countdictstack def gsave save mark newpath >> %outfile%
 
 rem Append the original onto the preview header
 copy %outfile% + %infile%
 
+echo %%%%EndDocument >> %outfile%
 echo countdictstack InitDictCount sub { end } repeat >> %outfile%
 echo cleartomark restore grestore >> %outfile%
 
