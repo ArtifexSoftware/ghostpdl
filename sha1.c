4d306e9ed969ff29bbceb2092ca67d9671e04a6a
giles <giles@ded80894-8fb9-0310-811b-c03f3676ab4d>
2002-07-20 16:09:08 +0000

    document endian worry
    git-svn-id: http://svn.ghostscript.com/jbig2dec/trunk@150 ded80894-8fb9-0310-811b-c03f3676ab4d

--- sha1.c
+++ sha1.c
@@ -100,6 +100,7 @@ void SHA1_Transform(uint32_t state[5], const uint8_t buffer[64]);
 
 /* blk0() and blk() perform the initial expand. */
 /* I got the idea of expanding during the round function from SSLeay */
+/* FIXME: can we do this in an endian-proof way? */
 #ifndef WORDS_BIGENDIAN
 #define blk0(i) (block->l[i] = (rol(block->l[i],24)&0xFF00FF00) \
     |(rol(block->l[i],8)&0x00FF00FF))
