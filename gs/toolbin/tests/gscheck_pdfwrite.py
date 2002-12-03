089187a98986c96b1c6d26275643f8fc00b8bb52
Jack Moffitt <jack.moffitt@artifex.com>
2002-12-03 16:43:39 +0000

    Fix: Turn logging on where it was overlooked.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@3403 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/toolbin/tests/gscheck_pdfwrite.py
+++ gs/toolbin/tests/gscheck_pdfwrite.py
@@ -32,68 +32,78 @@ import gsconf, gstestgs, gsparamsets, gssum
 
 
 class GSPDFWriteCompareTestCase(gstestgs.GhostscriptTestCase):
-	def shortDescription(self):
-		if self.band:
-			return "Checking pdfwrite of %s (%s/%ddpi/banded) against baseline" % (self.file[string.rindex(self.file, '/') + 1:], self.device, self.dpi)
-		else:
-			return "Checking pdfwrite of %s (%s/%ddpi/noband) against baseline" % (self.file[string.rindex(self.file, '/') + 1:], self.device, self.dpi)
+    def shortDescription(self):
+        if self.band:
+	    return "Checking pdfwrite of %s (%s/%ddpi/banded)" +
+	           "against baseline" %
+		   (self.file[string.rindex(self.file, '/') + 1:],
+		    self.device, self.dpi)
+	else:
+	    return "Checking pdfwrite of %s (%s/%ddpi/noband) against baseline" % (self.file[string.rindex(self.file, '/') + 1:], self.device, self.dpi)
 	
-	def runTest(self):
-		file1 = '%s.%s.%d.%d.pdf' % (self.file[string.rindex(self.file, '/') + 1:], 'pdf', self.dpi, self.band)
-		file2 = '%s.pdf.%s.%d.%d' % (self.file[string.rindex(self.file, '/') + 1:], self.device, self.dpi, self.band)
+    def runTest(self):
+        file1 = '%s.%s.%d.%d.pdf' % (self.file[string.rindex(self.file, '/') + 1:], 'pdf', self.dpi, self.band)
+	file2 = '%s.pdf.%s.%d.%d' % (self.file[string.rindex(self.file, '/') + 1:], self.device, self.dpi, self.band)
 
-		gs = gstestgs.Ghostscript()
-		gs.command = self.gs
-		gs.dpi = self.dpi
-		gs.band = self.band
-		gs.infile = self.file
-		gs.gsoptions = self.gsoptions
-		if self.log_stdout:
-			gs.log_stdout = self.log_stdout
-		if self.log_stderr:
-			gs.log_stderr = self.log_stderr
+	gs = gstestgs.Ghostscript()
+	gs.command = self.gs
+	gs.dpi = self.dpi
+	gs.band = self.band
+	gs.infile = self.file
+	gs.gsoptions = self.gsoptions
+	if self.log_stdout:
+	    gs.log_stdout = self.log_stdout
+	if self.log_stderr:
+	    gs.log_stderr = self.log_stderr
 
-		# do file->PDF conversion
+	# do file->PDF conversion
 
-		gs.device = 'pdfwrite'
-		gs.outfile = file1
-		if not gs.process():
-			self.fail("non-zero exit code trying to create pdf file from " + self.file)
+	gs.device = 'pdfwrite'
+	gs.outfile = file1
+	if not gs.process():
+	    self.fail("non-zero exit code trying to create pdf file from " + self.file)
 
-		# do PDF->device (pbmraw, pgmraw, ppmraw, pkmraw)
+	# do PDF->device (pbmraw, pgmraw, ppmraw, pkmraw)
 		
-		gs.device = self.device
-		gs.infile = file1
-		gs.outfile = file2
-		if not gs.process():
-			self.fail("non-zero exit code trying to"\
-				  " rasterize " + file1)
+	gs.device = self.device
+	gs.infile = file1
+	gs.outfile = file2
+	if not gs.process():
+	    self.fail("non-zero exit code trying to"\
+		      " rasterize " + file1)
 
-		# compare baseline
+	# compare baseline
 		
-		sum = gssum.make_sum(file2)
-		os.unlink(file1)
-		os.unlink(file2)
+	sum = gssum.make_sum(file2)
+	os.unlink(file1)
+	os.unlink(file2)
+	
+	# add test result to daily database
+	if self.track_daily:
+	    gssum.add_file(file2, dbname=gsconf.dailydb, sum=sum)
 
-		# add test result to daily database
-		if self.track_daily:
-			gssum.add_file(file2, dbname=gsconf.dailydb, sum=sum)
-
-		self.assertEqual(sum, gssum.get_sum(file2), "md5sum did not match baseline (" + file2 + ") for file: " + self.file)
+	self.assertEqual(sum, gssum.get_sum(file2), "md5sum did not match baseline (" + file2 + ") for file: " + self.file)
 
 # Add the tests defined in this file to a suite
 
 def add_compare_test(suite, f, device, dpi, band):
-        suite.addTest(GSPDFWriteCompareTestCase(gs=gsconf.comparegs, file=gsconf.comparefiledir + f, device=device, dpi=dpi, band=band, track_daily=1))
+    suite.addTest(GSPDFWriteCompareTestCase(gs=gsconf.comparegs,
+					    file=gsconf.comparefiledir + f,
+					    device=device, dpi=dpi,
+					    band=band, track_daily=1,
+					    log_stdout=gsconf.log_stdout,
+					    log_stderr=gsconf.log_stderr))
 
 def addTests(suite, gsroot, **args):
-	# get a list of test files
-	comparefiles = os.listdir(gsconf.comparefiledir)
+    # get a list of test files
+    comparefiles = os.listdir(gsconf.comparefiledir)
 
-	for f in comparefiles:
-		if f[-3:] == '.ps' or f[-4:] == '.pdf' or f[-4:] == '.eps':
-			for params in gsparamsets.pdftestparamsets:
-				add_compare_test(suite, f, params.device, params.resolution, params.banding)
+    for f in comparefiles:
+        if f[-3:] == '.ps' or f[-4:] == '.pdf' or f[-4:] == '.eps':
+	    for params in gsparamsets.pdftestparamsets:
+	        add_compare_test(suite, f, params.device,
+				 params.resolution,
+				 params.banding)
 
 if __name__ == "__main__":
-	gstestutils.gsRunTestsMain(addTests)
+    gstestutils.gsRunTestsMain(addTests)
