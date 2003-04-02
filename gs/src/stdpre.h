9a9ca48a5485b4446f2e74990fe4e1bf86e2be74
Ralph Giles <ralph.giles@artifex.com>
2003-04-02 21:43:36 +0000

    Remove the non-HAVE_STDINT_H non-win32 attempts to define stdint types and related sys/types redefine hacks. Also warn in the configure script if stdint.h is not found.
    DETAILS:
    
    Compiling libjbig2dec requires the size-specific types in stdint.h, as
    does compiling in the headers. Therefore these types must be made
    available in ghostscript if jbig2.dev is included in FEATURE_DEVS.
    
    The best way to integrate this is to create a stdint_.h for ghostscript
    that contains discovered and/or platform-specific typedefs for the
    types. This is still the plan, but to speed integration we just through
    some defines into stdpre.h. If -DHAVE_CONFIG_H is passed by the makefile
    (as configure does) then the C99 header is used, and we make specific
    defines for MSVC which doesn't yet ship the header. We also had some
    fallbacks for other systems (vendor unix mostly) but these caused
    collisions with partial definitions of what became the C99 types in
    sys/types.h and the redirection hack we do with ulong et al. did not
    work on all platforms (notably macosx).
    
    It is the fallback defines that have been removed; users building
    on older platforms will probably have to patch the types in to use JBIG2
    support.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@3770 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/stdpre.h
+++ gs/src/stdpre.h
@@ -265,13 +265,8 @@ typedef unsigned long ulong;
    typedef unsigned short int      uint16_t;
    typedef unsigned int            uint32_t;
    /* no uint64_t */
-# else /* hope generic types are adequate */
-   typedef short int               int16_t;
-   typedef int                     int32_t;
-   typedef unsigned char           uint8_t;
-   typedef unsigned short int      uint16_t;
-   typedef unsigned int            uint32_t;
 # endif
+   /* other archs may want to add defines here */
 #endif /* STDINT_H */
 
 /* Since sys/types.h may define one or more of these (depending on
@@ -288,24 +283,12 @@ typedef unsigned long ulong;
 #define uint uint_
 #define ushort ushort_
 #define ulong ulong_
-#define int8_t int8_t_
-#define int16_t int16_t_
-#define int32_t int32_t_
-#define uint8_t uint8_t_
-#define uint16_t uint16_t_
-#define uint32_t uint32_t_
 #include <sys/types.h>
 #undef bool
 #undef uchar
 #undef uint
 #undef ushort
 #undef ulong
-#undef int8_t
-#undef int16_t
-#undef int32_t
-#undef uint8_t
-#undef uint16_t
-#undef uint32_t
 
 /*
  * Define a Boolean type.  Even though we would like it to be
