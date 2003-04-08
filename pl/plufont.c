e44997f0815772604bb8c22c90224aaf69fc5070
Stefan Kemper <stefan.kemper@artifex.com>
2003-04-08 15:18:19 +0000

    Changed set pitch in pl_load_tt_font to not fetch metrics for gs_no_glyph or galley character glyph.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@1878 06663e23-700e-0410-b217-a244a6096597

--- pl/plufont.c
+++ pl/plufont.c
@@ -636,13 +636,14 @@ pl_load_tt_font(FILE *in, gs_font_dir *pdir, gs_memory_t *mem,
 		       * design width arbitrarily at 0.6 em.
 		       */
 		      gs_char space = ' ';
+		      float sbw[4];
 		      uint glyph_index =
 			(*pfont->procs.encode_char)
 			  ((gs_font *)pfont, space, gs_no_glyph);
-		      float sbw[4];
 
-		      if ( gs_type42_get_metrics(pfont, 
-				glyph_index == 0xffff ? 0 : glyph_index, sbw) < 0 )
+		      if (glyph_index == gs_no_glyph || glyph_index == 0xffff)
+			glyph_index = 0;
+		      if ( gs_type42_get_metrics(pfont, glyph_index, sbw) < 0 )
 			sbw[2] = 0.6;
 		      pl_fp_set_pitch_cp(&plfont->params, sbw[2] * 100);
 		    }
