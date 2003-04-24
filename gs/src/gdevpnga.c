fa55e07390c7ca56c71676c030e27e5eb9c89046
Ray Johnston <ray.johnston@artifex.com>
2003-04-24 06:20:54 +0000

    Get rid of some dead code (left over from cut+paste)
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@3857 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gdevpnga.c
+++ gs/src/gdevpnga.c
@@ -682,29 +682,6 @@ pnga_output_page(gx_device *dev, int num_copies, int flush)
     info_ptr->bit_depth = 8;
     info_ptr->color_type = PNG_COLOR_TYPE_RGB_ALPHA;
 
-    /* set the palette if there is one */
-    if (info_ptr->color_type == PNG_COLOR_TYPE_PALETTE) {
-	int i;
-	int num_colors = 1 << depth;
-	gx_color_value rgb[3];
-
-	info_ptr->palette =
-	    (void *)gs_alloc_bytes(mem, 256 * sizeof(png_color),
-				   "png palette");
-	if (info_ptr->palette == 0) {
-	    code = gs_note_error(gs_error_VMerror);
-	    goto done;
-	}
-	info_ptr->num_palette = num_colors;
-	info_ptr->valid |= PNG_INFO_PLTE;
-	for (i = 0; i < num_colors; i++) {
-	    (*dev_proc(dev, map_color_rgb)) ((gx_device *) dev,
-					      (gx_color_index) i, rgb);
-	    info_ptr->palette[i].red = gx_color_value_to_byte(rgb[0]);
-	    info_ptr->palette[i].green = gx_color_value_to_byte(rgb[1]);
-	    info_ptr->palette[i].blue = gx_color_value_to_byte(rgb[2]);
-	}
-    }
     /* add comment */
     sprintf(software_text, "%s %d.%02d", gs_product,
 	    (int)(gs_revision / 100), (int)(gs_revision % 100));
@@ -738,9 +715,6 @@ pnga_output_page(gx_device *dev, int num_copies, int flush)
 
     /* write the rest of the file */
     png_write_end(png_ptr, info_ptr);
-
-    /* if you alloced the palette, free it here */
-    gs_free_object(mem, info_ptr->palette, "png palette");
 
   done:
     /* free the structures */
