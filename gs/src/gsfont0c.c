5dc3e05e0d88fb706fd93bc9d09c479f6b56dc93
Stefan Kemper <stefan.kemper@artifex.com>
2003-05-06 23:58:54 +0000

    Changing license header.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@1901 06663e23-700e-0410-b217-a244a6096597

--- gs/src/gsfont0c.c
+++ gs/src/gsfont0c.c
@@ -1,18 +1,10 @@
 /* Copyright (C) 2002 Aladdin Enterprises.  All rights reserved.
   
-  This software is provided AS-IS with no warranty, either express or
-  implied.
-  
-  This software is distributed under license and may not be copied,
-  modified or distributed except as expressly authorized under the terms
-  of the license contained in the file LICENSE in this distribution.
-  
-  For more information about licensing, please refer to
-  http://www.ghostscript.com/licensing/. For information on
-  commercial licensing, go to http://www.artifex.com/licensing/ or
-  contact Artifex Software, Inc., 101 Lucas Valley Road #110,
-  San Rafael, CA  94903, U.S.A., +1(415)492-9861.
-*/
+   This software is distributed under license and may not be copied, modified
+   or distributed except as expressly authorized under the terms of that
+   license.  Refer to licensing information at http://www.artifex.com/ or
+   contact Artifex Software, Inc., 101 Lucas Valley Road #110,
+   San Rafael, CA  94903, (415)492-9861, for further information. */
 
 /* $Id$ */
 /* Create a FontType 0 wrapper for a CIDFont. */
--- gs/src/gsserial.c
+++ gs/src/gsserial.c
@@ -1,18 +1,11 @@
 /* Copyright (C) 2002 artofcode LLC.  All rights reserved.
 
-  This software is provided AS-IS with no warranty, either express or
-  implied.
+   This software is distributed under license and may not be copied, modified
+   or distributed except as expressly authorized under the terms of that
+   license.  Refer to licensing information at http://www.artifex.com/ or
+   contact Artifex Software, Inc., 101 Lucas Valley Road #110,
+   San Rafael, CA  94903, (415)492-9861, for further information. */
 
-  This software is distributed under license and may not be copied,
-  modified or distributed except as expressly authorized under the terms
-  of the license contained in the file LICENSE in this distribution.
-
-  For more information about licensing, please refer to
-  http://www.ghostscript.com/licensing/. For information on
-  commercial licensing, go to http://www.artifex.com/licensing/ or
-  contact Artifex Software, Inc., 101 Lucas Valley Road #110,
-  San Rafael, CA  94903, U.S.A., +1(415)492-9861.
-*/
 /* $Id$ */
 /* some utilities useful for converting objects to serial form */
 
