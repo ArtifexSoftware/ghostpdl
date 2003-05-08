3a315a5ba3c51240b7c8683bdd588ded646e0d19
Ralph Giles <ralph.giles@artifex.com>
2003-05-08 18:32:44 +0000

    Add the standard copyright banner and $Id line to the new changelog script.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@3901 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/toolbin/split_changelog.py
+++ gs/toolbin/split_changelog.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+#    Copyright (C) 2003 artofcode LLC.  All rights reserved.
+# 
+# This software is provided AS-IS with no warranty, either express or
+# implied.
+# 
+# This software is distributed under license and may not be copied,
+# modified or distributed except as expressly authorized under the terms
+# of the license contained in the file LICENSE in this distribution.
+# 
+# For more information about licensing, please refer to
+# http://www.ghostscript.com/licensing/. For information on
+# commercial licensing, go to http://www.artifex.com/licensing/ or
+# contact Artifex Software, Inc., 101 Lucas Valley Road #110,
+# San Rafael, CA  94903, U.S.A., +1(415)492-9861.
+
+# $Id$
+
+# script to generate the split Changes/Details html changelogs
+# for Ghostscript from the output of 'cvs2cl.pl --xml'
+
 import string, re
 import xml.parsers.expat
 import sys
