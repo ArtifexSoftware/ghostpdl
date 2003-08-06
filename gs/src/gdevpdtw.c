e5c467092b21300aa08703641b68da06fdc72b5a
Ralph Giles <ralph.giles@artifex.com>
2003-08-06 16:13:49 +0000

    Remove an unused local variable
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@4111 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gdevpdtw.c
+++ gs/src/gdevpdtw.c
@@ -104,7 +104,7 @@ pdf_different_encoding_index(const pdf_font_resource_t *pdfont, int ch0)
 private bool
 pdf_simple_font_needs_ToUnicode(const pdf_font_resource_t *pdfont)
 {
-    int ch, ch1, i;
+    int ch, i;
     unsigned char mask = (pdfont->FontType == ft_encrypted || pdfont->FontType == ft_encrypted2 
 		? GS_C_PDF_GOOD_GLYPH_MASK : GS_C_PDF_GOOD_NON_SYMBOL_MASK);
 
