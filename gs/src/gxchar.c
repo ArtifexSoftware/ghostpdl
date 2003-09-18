93b8db78f53551b160ce71ca0c392d1ab0ec57ab
Igor Melichev <igor.melichev@artifex.com>
2003-09-18 13:20:49 +0000

    Fix : Fonts with zero FontBBox were not anti-aliased.
    DETAILS :
    
    Bug 687036 "gs 8.11 does not anti-alias type 3 bitmap fonts".
    The bug appeared in gxchar.c revision 1.21.
    
    EXPECTED DIFFERENCES :
    
    None, because the test tool doesn't test TextAlphaBits > 1.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@4219 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gxchar.c
+++ gs/src/gxchar.c
@@ -1383,13 +1383,11 @@ show_set_scale(const gs_show_enum * penum, gs_log2_scale_point *log2_scale)
 
 	if (code >= 0) {
 	    int sx =
-	    (extent.x == 0 ? 0 :
-	     any_abs(extent.x) < int2fixed(60) ? 2 :
+	    (any_abs(extent.x) < int2fixed(60) ? 2 :
 	     any_abs(extent.x) < int2fixed(200) ? 1 :
 	     0);
 	    int sy =
-	    (extent.y == 0 ? 0 :
-	     any_abs(extent.y) < int2fixed(60) ? 2 :
+	    (any_abs(extent.y) < int2fixed(60) ? 2 :
 	     any_abs(extent.y) < int2fixed(200) ? 1 :
 	     0);
 
