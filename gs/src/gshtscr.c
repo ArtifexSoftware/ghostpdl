cb255467d6dac05da9910db30586db46549c128b
Dan Coby <dan.coby@artifex.com>
2003-11-04 01:25:31 +0000

    Fix for 686796 tiffg4 device and incorrect halftone frequency. This eliminates a problem with not choosing the best halftone cell size when more than one possible cell size has the exact angle requested.
    DETAILS:
    
    Previously the logic for determing the size of the halftone cells used
    the product of the error in frequency times the error in the angle as
    the metric for determing the best cell size.  For many common angles,
    0, 45, 90, etc., this results in an angle error of zero.  This then
    gave a value of zero for the metric no matter what the frequency error.
    The result was that in many cases we ignored cell sizes that gave a
    better frequency match.  The fix revies the metric to choose a cell
    size which is closest (geometrically) to the ideal cell size.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@4342 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gshtscr.c
+++ gs/src/gshtscr.c
@@ -363,6 +363,8 @@ pick_cell_size(gs_screen_halftone * ph, const gs_matrix * pmat, ulong max_size,
   try_size:
     better = false;
     {
+        double fm0 = u0 * rt;
+        double fn0 = v0 * rt;
         int m0 = (int)floor(u0 * rt + 0.0001);
         int n0 = (int)floor(v0 * rt + 0.0001);
         gx_ht_cell_params_t p;
@@ -415,17 +417,23 @@ pick_cell_size(gs_screen_halftone * ph, const gs_matrix * pmat, ulong max_size,
                 if_debug5('h', " ==> d=%d, wt=%ld, wt_size=%ld, f=%g, a=%g\n",
                           p.D, wt, bitmap_raster(wt) * wt, ft, at);
 
-                /*
-                 * Minimize angle and frequency error within the
-                 * permitted maximum super-cell size.
-                 */
-
                 {
-                    double err = f_err * a_err;
+                    /*
+		     * Compute the error in position between ideal location.
+		     * and the current integer location.
+		     */
 
-                    if (err > e_best)
+		    double error =
+			(fn0 - p.N) * (fn0 - p.N) + (fm0 - p.M) * (fm0 - p.M);
+		    /*
+		     * Adjust the error by the length of the vector.  This gives
+		     * a slight bias toward larger cell sizzes.
+		     */
+		    error /= p.N * p.N + p.M * p.M;
+		    error = sqrt(error); /* The previous calcs. gave value squared */
+                    if (error > e_best)
                         continue;
-                    e_best = err;
+                    e_best = error;
                 }
                 *phcp = p;
                 f = ft, a = at;
