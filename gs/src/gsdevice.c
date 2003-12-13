2fe30fef7a2ae36b34e5ca1de3426b813e717736
Ray Johnston <ray.johnston@artifex.com>
2003-12-13 18:07:56 +0000

    Fix a premature closedevice leading to a SEGV when a setpagedevice follows a setdevice with the only reference to the device in the graphics state. Bug #687204 detected with ps2epsi.ps.
    DETAILS:
    
    the ps2epsi.ps sets an image device, then does a .setsafe which uses
    setpagedevice to set .LockSafetyParams. Eventually setpagedevice results
    in a call to gs_setdevice_no_init, but the reference count to the current
    device is 1 so even though the device is not being changed, the device
    was being closed (which freed the buffer and line pointers). Usually
    the currentdevice reference count is > 1 since there is more than one
    gstate containing a pointer to the device.
    
    This has been broken since 8.10 when version 1.19 was released with
    some changes to device finalization.
    
    Note that ps2epsi.ps is extremely fragile and still doesn't work with
    examples/alphabet.ps after the patch (due to the "clear cleardictstack
    alphabetsave restore" logic at the end. This patch is ONLY for the SEGV.
    IMO, ps2epsi really is a mess and needs to be scrapped and designed
    properly.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@4459 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gsdevice.c
+++ gs/src/gsdevice.c
@@ -400,8 +400,17 @@ gs_setdevice_no_init(gs_state * pgs, gx_device * dev)
     /*
      * Just set the device, possibly changing color space but no other
      * device parameters.
+     * 
+     * Make sure we don't close the device if dev == pgs->device
+     * This could be done by allowing the rc_assign to close the
+     * old 'dev' if the rc goes to 0 (via the device structure's
+     * finalization procedure), but then the 'code' from the dev
+     * closedevice would not be propagated up. We want to allow
+     * the code to be handled, particularly for the pdfwrite
+     * device.
      */
-    if (pgs->device != NULL && pgs->device->rc.ref_count == 1) {
+    if (pgs->device != NULL && pgs->device->rc.ref_count == 1 &&
+	pgs->device != dev) {
 	int code = gs_closedevice(pgs->device);
 
 	if (code < 0)
