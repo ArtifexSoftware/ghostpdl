24b0237f1a1a4b5a14548db63008a8e4875d0b83
Stefan Kemper <stefan.kemper@artifex.com>
2004-02-18 18:27:51 +0000

    Return a reasonable result in the case where no locked target exists (No locking allocator)
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@2068 06663e23-700e-0410-b217-a244a6096597

--- gs/src/gsmalloc.c
+++ gs/src/gsmalloc.c
@@ -466,8 +466,9 @@ gs_malloc_wrapped_contents(gs_memory_t *wrapped)
     gs_memory_retrying_t *rmem = (gs_memory_retrying_t *)wrapped;
     gs_memory_locked_t *lmem =
 	(gs_memory_locked_t *)gs_memory_retrying_target(rmem);
-
-    return (gs_malloc_memory_t *)gs_memory_locked_target(lmem);
+    if (lmem) 
+	return (gs_malloc_memory_t *)gs_memory_locked_target(lmem);
+    return (gs_malloc_memory_t *) wrapped;
 }
 
 /* Free the wrapper, and return the wrapped contents. */
