da420b02de6412f053b759cd04d498762ac62fbe
giles <giles@ded80894-8fb9-0310-811b-c03f3676ab4d>
2004-03-24 07:49:53 +0000

    Check for defined(HAVE_CONFIG_H) instead of just a non-false value. Corrects a warning on the Intel compiler. Ghostscript bug 687320.
    git-svn-id: http://svn.ghostscript.com/jbig2dec/trunk@309 ded80894-8fb9-0310-811b-c03f3676ab4d

--- os_types.h
+++ os_types.h
@@ -37,7 +37,7 @@
    typedef u_int8_t uint8_t;
    typedef u_int16_t uint16_t;
    typedef u_int32_t uint32_t;
-#elif HAVE_CONFIG_H
+#elif defined(HAVE_CONFIG_H)
 # include "config_types.h"
 #elif defined(_WIN32)
 # include "config_win32.h"
