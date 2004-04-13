a2010016cc135538ddbdc4cf05bde2bdee2f4adf
Stefan Kemper <stefan.kemper@artifex.com>
2004-04-13 21:10:47 +0000

    Fixed cut and paste typo.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@2114 06663e23-700e-0410-b217-a244a6096597

--- pl/plfont.c
+++ pl/plfont.c
@@ -516,7 +516,7 @@ pl_clone_font(const pl_font_t *src, gs_memory_t *mem, client_name_t cname)
 	plfont->storage = src->storage;
 	plfont->header_size = src->header_size;
 	plfont->scaling_technology = src->scaling_technology;
-        plfont->is_xl_format = plfont->is_xl_format;
+        plfont->is_xl_format = src->is_xl_format;
 	plfont->font_type = src->font_type;
 	plfont->char_width = src->char_width;
 	plfont->char_metrics = src->char_metrics;
