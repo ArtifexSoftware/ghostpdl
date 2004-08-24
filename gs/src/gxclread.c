3b3206a8771a3379e1cce9784fb392e3cd6b3941
Igor Melichev <igor.melichev@artifex.com>
2004-08-24 15:36:08 +0000

    Fix : gs_lib_ctx was not accessible while clist_playback_band.
    DETAILS :
    
    Bug 687624 "A crash in gp_check_interrupts".
    
    EXPECTED DIFFERENCES :
    
    Fixes crash with re-distilled files :
    Svd.pdf
    test.pdf
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5256 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gxclread.c
+++ gs/src/gxclread.c
@@ -510,6 +510,8 @@ clist_playback_file_bands(clist_playback_action action,
 	s_std_init(&s, sbuf, cbuf_size, &no_procs, s_mode_read);
 	s.foreign = 1;
 	s.state = (stream_state *)&rs;
+	s.memory = mem; /* The stream doesn't need a memory,
+	                   but we'll need to access s.memory->gs_lib_ctx. */
 	code = clist_playback_band(action, cdev, &s, target, x0, y0, mem);
     }
 
