077573fd5e092387b8407f0563bb06816df56b64
Igor Melichev <igor.melichev@artifex.com>
2004-08-24 10:58:38 +0000

    Fix (TT interpreter) : A minor portability problem.
    DETAILS :
    
    A preprocessor statement didn't start in the first column.
    Occasionally discovered readin the code.
    Some time ago it was claimed as non-portable.
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5254 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/ttload.c
+++ gs/src/ttload.c
@@ -133,10 +133,10 @@
 
     face->cvtSize = font->t_cvt_.nLen / 2;
 
-    #if 0
+#   if 0
     if(face->cvtSize < 300)
       face->cvtSize = 300; /* Work around DynaLab bug in DingBat1. */
-    #endif
+#   endif
 
     if(face->cvtSize > 0) {  /* allow fonts with a CVT table */
 	face->cvt = mem->alloc_bytes(mem, face->cvtSize * sizeof(Short), "Load_TrueType_CVT");
