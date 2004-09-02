71f6f8caa48b65458eb483d4505b363c41d304fa
Ralph Giles <ralph.giles@artifex.com>
2004-09-02 21:49:05 +0000

    Use the MTIME define from the stat module rather than a numerical index to specify the modification time in python. Propagates a similar change from gscheck_raster.py.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5292 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/toolbin/tests/gscheck_pdfwrite.py
+++ gs/toolbin/tests/gscheck_pdfwrite.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#    Copyright (C) 2001 Artifex Software Inc.
+#    Copyright (C) 2001-2004 Artifex Software Inc.
 # 
 # This file is part of AFPL Ghostscript.
 # 
@@ -25,7 +25,8 @@
 # compares Ghostscript against a baseline made from file->pdf->raster->md5sum.
 # this test tries to detect Ghostscript changes that affect the pdfwrite driver.
 
-import os, calendar, string, time
+import os, stat
+import calendar, string, time
 import gstestutils
 import gsconf, gstestgs, gsparamsets, gssum
 
@@ -36,7 +37,7 @@ class GSPDFWriteCompareTestCase(gstestgs.GhostscriptTestCase):
 	rasterfilename = gsconf.rasterdbdir + file + ".gz"
 	if not os.access(rasterfilename, os.F_OK):
 		os.system(gsconf.codedir + "update_pdfbaseline " + os.path.basename(self.file))	
-	ct = time.localtime(os.stat(rasterfilename)[9])
+	ct = time.localtime(os.stat(rasterfilename)[stat.ST_MTIME]])
 	baseline_date = "%s %d, %4d %02d:%02d" % ( calendar.month_abbr[ct[1]], ct[2], ct[0], ct[3], ct[4] )
 
 	if self.band:
