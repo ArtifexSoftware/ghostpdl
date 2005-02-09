26de39db6eb811e91f6894b13dbdf899dbde9cf7
Igor Melichev <igor.melichev@artifex.com>
2005-02-09 19:42:01 +0000

    ps2write : Fix problems with heh encodings.
    DETAILS :
    
    The old code failed when pdfwrite applies /ASCII85EncodePages true .
    This patch fixes that.
    
    When reading the generated PS file with Ghostscript,
    we observe a problem with ASCII85Decode filter :
    images and fonts appear corrupt,
    and embedded fonts cause an access violation on Windows.
    In same time Adobe Reader reads them fine.
    This secondary problem shouldn't be important for the Postscript generation.
    It will be fixed separately.
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5675 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2write.bat
+++ gs/lib/ps2write.bat
@@ -1,5 +1,5 @@
 @rem $Id$
 @rem Converting Postscript 3 or PDF to PostScript 2.
 
-%GSC% -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=temp.pdf %more_param% -c mark /CompatibilityLevel 1.2 /OrderResources true /PSVersion 2017 /MaxInlineImage 2147483647 /CompressPages false /CompressFonts false .dicttomark setpagedevice -f %1
+%GSC% -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=temp.pdf %more_param% -c mark /CompatibilityLevel 1.2 /OrderResources true /PSVersion 2017 /MaxInlineImage 2147483647 /CompressPages false /CompressFonts false /ASCII85EncodePages true .dicttomark setpagedevice -f %1
 copy /b %GS_LIBPATH%opdfread.ps+%GS_LIBPATH%gs_agl.ps+%GS_LIBPATH%gs_mro_e.ps+%GS_LIBPATH%gs_mex_e.ps+%GS_LIBPATH%gs_mgl_e.ps+%GS_LIBPATH%gs_wan_e.ps+temp.pdf %2
