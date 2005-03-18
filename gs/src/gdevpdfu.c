58436190a4a6ed8174544778e3aca8aecd3ddf8a
Igor Melichev <igor.melichev@artifex.com>
2005-03-18 14:48:32 +0000

    Fix (pdfwrite) : Don't apply ASCII85Encode to page contents streams which appear to be already ASCII.
    DETAILS :
    
    It's a bug introduced when implementing PDFW_DELAYED_STREAMS.
    Restoring the old behavior now.
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5774 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gdevpdfu.c
+++ gs/src/gdevpdfu.c
@@ -1437,9 +1437,15 @@ pdf_begin_data_stream(gx_device_pdf *pdev, pdf_data_writer_t *pdw,
 	pdw->encrypted = true;
     } else
     	pdw->encrypted = false;
-    code = psdf_begin_binary((gx_device_psdf *)pdev, &pdw->binary);
-    if (code < 0)
-	return code;
+    if ((options & DATA_STREAM_BINARY) || !PDFW_DELAYED_STREAMS) 
+	code = psdf_begin_binary((gx_device_psdf *)pdev, &pdw->binary);
+	if (code < 0)
+	    return code;
+    else {
+	pdw->binary.target = pdev->strm;
+	pdw->binary.dev = (gx_device_psdf *)pdev;
+	pdw->binary.strm = pdev->strm;
+    }
     pdw->start = stell(s);
     if (filters & USE_FLATE)
 	code = pdf_flate_binary(pdev, &pdw->binary);
