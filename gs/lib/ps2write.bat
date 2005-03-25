a979358b784367470c834a3bca265df75feee796
Igor Melichev <igor.melichev@artifex.com>
2005-03-25 10:11:29 +0000

    ps2write : Improve the handling of encodings.
    DETAILS :
    
    1. Fixed a bug in GetMacRomanEncodingInverse.
    2. Dropped some unused encodings.
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5789 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2write.bat
+++ gs/lib/ps2write.bat
@@ -2,4 +2,4 @@
 @rem Converting Postscript 3 or PDF to PostScript 2.
 
 %GSC% -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=temp.pdf %more_param% -c mark /CompatibilityLevel 1.2 /OrderResources true /PSVersion 2017 /MaxInlineImage 2147483647 /CompressPages false /CompressFonts false /ASCII85EncodePages true .dicttomark setpagedevice -f %1
-copy /b %GS_LIBPATH%opdfread.ps+%GS_LIBPATH%gs_agl.ps+%GS_LIBPATH%gs_mro_e.ps+%GS_LIBPATH%gs_mex_e.ps+%GS_LIBPATH%gs_mgl_e.ps+%GS_LIBPATH%gs_wan_e.ps+temp.pdf+%GS_LIBPATH%EndOfTask.ps %2
+copy /b %GS_LIBPATH%opdfread.ps+%GS_LIBPATH%gs_agl.ps+%GS_LIBPATH%gs_mro_e.ps+%GS_LIBPATH%gs_mgl_e.ps+temp.pdf+%GS_LIBPATH%EndOfTask.ps %2
