8364d28a3a63aa6b2f6299f0dd052f1c02eef361
Igor Melichev <igor.melichev@artifex.com>
2005-04-05 11:29:16 +0000

    ps2write : Implementing a viewer's resource memory control, part 2.
    DETAILS :
    
    1. opdfread.ps :
    Defined the new procedure CleanAllResources and called it after each page,
    which defines /.ClearResources /All.
    Currently it cleans all objects and all non-standard fonts.
    Daemons, if they are stored in the Registry, are not cleaned.
    (BTW, daemons may contain a reference to an object,
    but we believe that currently unresolved daemons never appear
    in Registry when ClearAllResources is being called).
    
    2. ps2write.bat : Updated to involve the resource memory control.
    See the log message of the previous patch for more information.
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5827 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2write.bat
+++ gs/lib/ps2write.bat
@@ -1,5 +1,5 @@
 @rem $Id$
 @rem Converting Postscript 3 or PDF to PostScript 2.
 
-%GSC% -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=temp.pdf %more_param% -c mark  /ForOPDFRead true /CompressPages false /CompressFonts false /ASCII85EncodePages true .dicttomark setpagedevice -f %1
+%GSC% -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=temp.pdf %more_param% -c mark /ForOPDFRead true /MaxViewerMemorySize 8000000 /CompressPages false /CompressFonts false /ASCII85EncodePages true .dicttomark setpagedevice -f %1
 copy /b %GS_LIBPATH%opdfread.ps+%GS_LIBPATH%gs_agl.ps+%GS_LIBPATH%gs_mro_e.ps+%GS_LIBPATH%gs_mgl_e.ps+temp.pdf+%GS_LIBPATH%EndOfTask.ps %2
