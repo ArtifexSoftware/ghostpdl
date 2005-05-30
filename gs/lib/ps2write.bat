37e37c6ff17f8e68ff63d745044d9cbb6ea4ced3
Igor Melichev <igor.melichev@artifex.com>
2005-05-30 21:00:33 +0000

    ps2write : Implementing the SetPageSize mode.
    DETAILS :
    
    1. SetPageSize mode enables changing a page size in the
       target printer. This mode is implemented mainly to simplify testing
       with Ghostscript|ppmraw. We need same page size of 'normal' and
       'ps2write' rasters to simplify the automated comparison.
    
    2. (A minor change) Apply immediate binding to FitPages, RotatePages.
    
    3. Allow more parameterization in ps2write.bat .
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5930 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2write.bat
+++ gs/lib/ps2write.bat
@@ -4,7 +4,7 @@
 set invoke0=%GSC% -dBATCH -dNOPAUSE -dNOOUTERSAVE -sDEVICE=pdfwrite -sOutputFile=temp.pdf %more_param% 
 set invoke1=-c mark /ForOPDFRead true /PreserveHalftoneInfo true /TransferFunctionInfo /Preserve 
 set invoke2=/MaxViewerMemorySize 8000000 /CompressPages false /CompressFonts false /ASCII85EncodePages true 
-set invoke3=.dicttomark setpagedevice -f %more_param1%
+set invoke3=%more_deviceparam% .dicttomark setpagedevice -f %more_param1%
 set invoke=%invoke0% %invoke1% %invoke2% %invoke3%
 
 set procsets=%GS_LIBPATH%opdfread.ps+%GS_LIBPATH%gs_agl.ps+%GS_LIBPATH%gs_mro_e.ps+%GS_LIBPATH%gs_mgl_e.ps
