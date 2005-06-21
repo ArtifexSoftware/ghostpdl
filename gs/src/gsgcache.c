f5998b9627da22207819083f7f9a14b66c045773
Igor Melichev <igor.melichev@artifex.com>
2005-06-21 16:25:50 +0000

    Fix : A wrong allocator was used to release the glyph cache.
    DETAILS :
    
    Bug 688146 "heap corruption with ppmraw -r72 -Z?$@ 055-01.ps".
    
    A structure was allocated in a non-stable memory and
    released to a stable memory.
    
    EXPECTED DIFFERENCES :
    
    May fix some of pkmraw, pgmraw regressions appeared on June 16.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@5974 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gsgcache.c
+++ gs/src/gsgcache.c
@@ -84,7 +84,8 @@ gs_glyph_cache *
 gs_glyph_cache__alloc(gs_font_type42 *pfont, stream *s,
 			get_glyph_data_from_file read_data)
 { 
-    gs_glyph_cache *gdcache = (gs_glyph_cache *)gs_alloc_struct(pfont->memory,
+    gs_memory_t *mem = pfont->memory->stable_memory;
+    gs_glyph_cache *gdcache = (gs_glyph_cache *)gs_alloc_struct(mem,
 	    gs_glyph_cache, &st_glyph_cache, "gs_glyph_cache");
     if (gdcache == 0)
 	return 0;
@@ -97,7 +98,7 @@ gs_glyph_cache__alloc(gs_font_type42 *pfont, stream *s,
     * get removed by 'restore' (elements can be created at a different
     * save level than the current level)
     */
-    gdcache->memory = pfont->memory->stable_memory;
+    gdcache->memory = mem;
     gdcache->read_data = read_data;
     gs_font_notify_register((gs_font *)pfont, gs_glyph_cache__release, (void *)gdcache);
     return gdcache;
