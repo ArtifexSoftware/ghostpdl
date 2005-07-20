021fcd99da65a268f70f35507d3c1e4bb8e7a5e6
Igor Melichev <igor.melichev@artifex.com>
2005-07-20 06:00:54 +0000

    Fix (ps2write) : Bring the script to the traditional form.
    DETAILS :
    
    Bug 688190 "ps2write : improve the ps2ps2 script".
    
    Since ps2write actually is a clone of pdfwrite,
    we define special distiller settings named PSPrinter.
    After this device parameters are gone from the script,
    and the ps2ps2 script looks similarly to ps2ps .
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@6016 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2ps2.bat
+++ gs/lib/ps2ps2.bat
@@ -1,15 +1,24 @@
-@rem $Id$
+#!/bin/sh
+# $Id$
 @rem Converting Postscript 3 or PDF into PostScript 2.
 
-set invoke0=%GSC% -dBATCH -dNOPAUSE -dNOOUTERSAVE -sDEVICE=ps2write -sOutputFile=%2 %more_param% 
-set invoke1=-c mark /PreserveHalftoneInfo true /TransferFunctionInfo /Preserve 
-set invoke2=/MaxViewerMemorySize 8000000 /CompressPages false /CompressFonts false /ASCII85EncodePages true 
-set invoke3=%more_deviceparam% .dicttomark setpagedevice -f %more_param1%
-set invoke=%invoke0% %invoke1% %invoke2% %invoke3%
+if %1/==/ goto usage
+if %2/==/ goto usage
+call gssetgs.bat
+echo -dNOPAUSE -dSAFER -dBATCH >_.at
+:cp
+if %3/==/ goto doit
+echo %1 >>_.at
+shift
+goto cp
 
-if %jobserver%. == yes. goto s
-%invoke%  %1
-goto j
-:s
-%invoke% -c false 0 startjob pop -f - < %1
-:j
+:doit
+rem Watcom C deletes = signs, so use # instead.
+%GSC% -q -sDEVICE#ps2write -sOutputFile#%2 @_.at %1
+goto end
+
+:usage
+echo "Usage: ps2ps [options] input.ps output.ps"
+echo "  e.g. ps2ps -sPAPERSIZE=a4 input.ps output.ps
+
+:end
