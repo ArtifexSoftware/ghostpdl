2ef6014a26731abec9225dc60801c9485d4406db
Ralph Giles <ralph.giles@artifex.com>
2006-01-06 19:01:15 +0000

    Remove the check for files not listed in CVS.
    DETAILS:
    
    This no longer works since we've switched to subversion for version control. The
    check could of course be ported, but the object was the detect files that had
    either accidentally been left out of a commit, or files that might slip into a
    release unintentionally. The 'svn status' command is an effective and more
    commonly used check for the former, and our policy of building releases from
    a clean export of the tag eliminates the other.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@6441 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/toolbin/tests/check_dirs.py
+++ gs/toolbin/tests/check_dirs.py
@@ -42,83 +42,14 @@ class GSCheckEmptyTopDirectory(GSTestCase):
 	messages.sort()
         self.failIfMessages(messages)
 
-################ Check that the set of files in the directories is the
-################ same as the set of files registered with CVS.
-
-class GSCheckDirectoryMatchesCVS(GSTestCase):
-
-    def __init__(self, root, dirName):
-        self.root = root
-        self.dirName = dirName
-        GSTestCase.__init__(self)
-
-    def shortDescription(self):
-        return "The contents of %s/ must match its CVS/Entries." % self.dirName
-
-    def runTest(self):
-        import re, glob, os.path
-        pattern = re.compile("^/([^/]+)/")
-        Entries = {}
-        Files = {}
-        d = os.path.join(self.root, self.dirName)
-        # Skip files matching patterns in .cvsignore.
-        fp = None
-        skip = []
-        try:
-            fp = open(os.path.join(d, '.cvsignore'), 'r')
-        except:
-            pass
-        if fp != None:
-            while 1:
-                line = fp.readline()
-                if line == '': break
-                skip += glob.glob(os.path.join(d, line.rstrip()))
-            fp.close()
-        try:
-            fp = open(os.path.join(d, 'CVS', 'Entries'), 'r')
-        except:
-            self.fail("Cannot find %s/CVS/Root" % d)
-            return
-        while 1:
-            line = fp.readline()
-            if line == '': break
-            found = pattern.match(line)
-            if found != None:
-                Entries[found.group(1)] = line
-        fp.close()
-        for f in glob.glob(os.path.join(d, "*")) + glob.glob(os.path.join(d, ".[a-zA-Z0-9_-]*")):
-            if f not in skip and not os.path.isdir(f):
-                Files[os.path.basename(f)] = 1
-        m1 = []
-        for f in Entries.keys():
-            if not Files.has_key(f):
-                m1.append(os.path.join(d, f))
-        if m1:
-            m1.sort()
-            m1 = ['These %d files are registered with CVS, but do not exist:' % len(m1)] + m1
-        m2 = []
-        for f in Files.keys():
-            if not Entries.has_key(f):
-                m2.append(os.path.join(d, f))
-        if m2:
-            m2.sort()
-            m2 = ['These %d files are not registered with CVS:' % len(m2)] + m2
-        self.failIfMessages(m1 + m2)
-
 ################ Main program
 
 gsFilesInTopDirectory = ['LICENSE', 'autogen.sh', 'Makefile',
 			 'configure', 'config.log', 'config.status']
-gsDirectories = [
-    'doc', 'examples', 'lib', 'src', 'toolbin', 'toolbin/tests'
-    ]
-
 # Add the tests defined in this file to a suite.
 
 def addTests(suite, gsroot, **args):
     suite.addTest(GSCheckEmptyTopDirectory(gsroot, gsFilesInTopDirectory))
-    for dir in gsDirectories:
-        suite.addTest(GSCheckDirectoryMatchesCVS(gsroot, dir))
 
 if __name__ == "__main__":
     gsRunTestsMain(addTests)
