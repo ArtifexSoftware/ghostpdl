972780457b79558f40ce97ecf1615629c762fa6f
Ralph Giles <ralph.giles@artifex.com>
2006-01-29 01:04:43 +0000

    Rearrange code for clarity.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@6506 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/sjbig2.h
+++ gs/src/sjbig2.h
@@ -37,7 +37,12 @@ typedef struct stream_jbig2decode_state_s
 }
 stream_jbig2decode_state;
 
-/* call in to process the JBIG2Globals parameter */
+#define private_st_jbig2decode_state()	\
+  gs_private_st_simple(st_jbig2decode_state, stream_jbig2decode_state,\
+    "jbig2decode filter state")
+extern const stream_template s_jbig2decode_template;
+
+/* call ins to process the JBIG2Globals parameter */
 public int
 s_jbig2decode_make_global_data(byte *data, uint length, void **result);
 public int
@@ -45,9 +50,4 @@ s_jbig2decode_set_global_data(stream_state *ss, void *data);
 public void
 s_jbig2decode_free_global_data(void *data);
 	
-#define private_st_jbig2decode_state()	\
-  gs_private_st_simple(st_jbig2decode_state, stream_jbig2decode_state,\
-    "jbig2decode filter state")
-extern const stream_template s_jbig2decode_template;
-
 #endif /* sjbig2_INCLUDED */
