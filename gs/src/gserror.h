37fa4b79b5a67bfa23547ec7161c9ef34c1efd08
Stefan Kemper <stefan.kemper@artifex.com>
2006-02-02 22:47:21 +0000

    Adding gs_throw macros to gserror.h Implementation in gsmisc.c
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@2322 06663e23-700e-0410-b217-a244a6096597

--- gs/src/gserror.h
+++ gs/src/gserror.h
@@ -23,4 +23,130 @@ int gs_log_error(const gs_memory_t *mem, int, const char *, int);
 #define gs_note_error(mem, err) gs_log_error(mem, err, __FILE__, __LINE__)
 #define return_error(mem, err) return gs_note_error(mem, err)
 
+
+
+/*
+ * Error reporting macros
+ * 
+ */
+
+#ifndef __printflike
+#if __GNUC__ > 2 || __GNUC__ == 2 && __GNUC_MINOR__ >= 7
+#define __printflike(fmtarg, firstvararg) \
+    __attribute__((__format__ (__printf__, fmtarg, firstvararg)))
+#else
+#define __printflike(fmtarg, firstvararg)
+#endif
+#endif
+
+const char *gs_errstr(int code);
+
+int gs_throw_imp(const char *func, const char *file, int line,
+        int op, int code, const char *fmt, ...) __printflike(6, 7);
+
+
+
+/* Use throw at origin of error 
+*/
+#define gs_throw(code, fmt) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt)
+#define gs_throw1(code, fmt, arg1) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1)
+#define gs_throw2(code, fmt, arg1, arg2) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1, arg2)
+#define gs_throw3(code, fmt, arg1, arg2, arg3) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1, arg2, arg3)
+#define gs_throw4(code, fmt, arg1, arg2, arg3, arg4) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1, arg2, arg3, arg4)
+#define gs_throw5(code, fmt, arg1, arg2, arg3, arg4, arg5) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1, arg2, arg3, arg4, arg5)
+#define gs_throw6(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6)
+#define gs_throw7(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
+#define gs_throw8(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
+#define gs_throw9(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 0, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
+
+
+/* Bubble the code up the stack 
+*/
+#define gs_rethrow(code, fmt) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt)
+#define gs_rethrow1(code, fmt, arg1) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1)
+#define gs_rethrow2(code, fmt, arg1, arg2) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1, arg2)
+#define gs_rethrow3(code, fmt, arg1, arg2, arg3) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1, arg2, arg3)
+#define gs_rethrow4(code, fmt, arg1, arg2, arg3, arg4) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1, arg2, arg3, arg4)
+#define gs_rethrow5(code, fmt, arg1, arg2, arg3, arg4, arg5) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1, arg2, arg3, arg4, arg5)
+#define gs_rethrow6(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6)
+#define gs_rethrow7(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
+#define gs_rethrow8(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
+#define gs_rethrow9(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 1, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
+
+
+
+/* This will cause trouble, as it implies you are fixing an error
+ * the system will spew messages 
+ */
+#define gs_catch(code, fmt) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt)
+#define gs_catch1(code, fmt, arg1) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1)
+#define gs_catch2(code, fmt, arg1, arg2) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1, arg2)
+#define gs_catch3(code, fmt, arg1, arg2, arg3) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1, arg2, arg3)
+#define gs_catch4(code, fmt, arg1, arg2, arg3, arg4) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1, arg2, arg3, arg4)
+#define gs_catch5(code, fmt, arg1, arg2, arg3, arg4, arg5) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1, arg2, arg3, arg4, arg5)
+#define gs_catch6(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6)
+#define gs_catch7(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
+#define gs_catch8(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
+#define gs_catch9(code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) \
+    gs_throw_imp(__func__, __FILE__, __LINE__, 2, code, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
+
+
+/* gs_warn is a printf
+ */
+#define gs_warn(fmt) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt)
+#define gs_warn1(fmt, arg1) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1)
+#define gs_warn2(fmt, arg1, arg2) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1, arg2)
+#define gs_warn3(fmt, arg1, arg2, arg3) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1, arg2, arg3)
+#define gs_warn4(fmt, arg1, arg2, arg3, arg4) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1, arg2, arg3, arg4)
+#define gs_warn5(fmt, arg1, arg2, arg3, arg4, arg5) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1, arg2, arg3, arg4, arg5)
+#define gs_warn6(fmt, arg1, arg2, arg3, arg4, arg5, arg6) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1, arg2, arg3, arg4, arg5, arg6)
+#define gs_warn7(fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
+#define gs_warn8(fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
+#define gs_warn9(fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) \
+    (void)gs_throw_imp(__func__, __FILE__, __LINE__, 3, 0, fmt, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
+
+
+/* just in case you don't know 0 means no error
+ * other return codes are errors.
+ */
+#define gs_okay 0
+
 #endif /* gserror_INCLUDED */
--- gs/src/gsmisc.c
+++ gs/src/gsmisc.c
@@ -216,6 +216,69 @@ gs_return_check_interrupt(const gs_memory_t *mem, int code)
     }
 }
 
+
+int gs_throw_imp(const char *func, const char *file, int line, int op, int code, const char *fmt, ...)
+{
+    char msg[1024];
+    va_list ap;
+
+    va_start(ap, fmt);
+    vsnprintf(msg, sizeof msg, fmt, ap);
+    msg[sizeof(msg) - 1] = 0;
+    va_end(ap);
+
+    if (!gs_debug_c('#')) {
+	; /* NB: gs_log_errors  
+	   * we could disable these printfs, and probably will when, 
+	   * the code becomes more stable: 
+	   * return code;
+	   */
+    }
+       
+
+    /* throw */
+    if (op == 0)
+	errprintf(NULL, "+ %s:%d: %s(): %s\n", file, line, func, msg);
+
+    /* rethrow */
+    if (op == 1)
+	errprintf(NULL, "| %s:%d: %s(): %s\n", file, line, func, msg);
+
+    /* catch */
+    if (op == 2)
+	errprintf(NULL, "- %s:%d: %s(): %s\n", file, line, func, msg);
+
+    /* warn */
+    if (op == 3)
+	errprintf(NULL, "  %s:%d: %s(): %s\n", file, line, func, msg);
+
+    return code;
+}
+
+const char *gs_errstr(int code)
+{
+    switch (code) {
+    default:
+    case gs_error_unknownerror: return "unknownerror";
+    case gs_error_interrupt: return "interrupt";
+    case gs_error_invalidaccess: return "invalidaccess";
+    case gs_error_invalidfileaccess: return "invalidfileaccess";
+    case gs_error_invalidfont: return "invalidfont";
+    case gs_error_ioerror: return "ioerror";
+    case gs_error_limitcheck: return "limitcheck";
+    case gs_error_nocurrentpoint: return "nocurrentpoint";
+    case gs_error_rangecheck: return "rangecheck";
+    case gs_error_typecheck: return "typecheck";
+    case gs_error_undefined: return "undefined";
+    case gs_error_undefinedfilename: return "undefinedfilename";
+    case gs_error_undefinedresult: return "undefinedresult";
+    case gs_error_VMerror: return "vmerror";
+    case gs_error_unregistered: return "unregistered";
+    case gs_error_hit_detected: return "hit_detected";
+    case gs_error_Fatal: return "Fatal";
+    }
+}
+
 /* ------ Substitutes for missing C library functions ------ */
 
 #ifdef MEMORY__NEED_MEMMOVE	/* see memory_.h */
