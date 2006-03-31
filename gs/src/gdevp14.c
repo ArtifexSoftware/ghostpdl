6005a4e67b4f51e5a61f9ef6b0a5d1b82f735036
Dan Coby <dan.coby@artifex.com>
2006-03-31 01:07:31 +0000

    Fix for 688608 image rendered black in PDF-file
    DETAILS:
    
    The test file for this problem has several 1 bit DeviceGray images.  The file also uses
    the 'Darken' blending mode.  This result of this blending mode is the darker of the
    background and source colors.
    
    The problem was that 1 bit DeviceGray images are handled by the copy_mon device procedure.
    The PDF 1.4 transparency compositor devices were using the gx_default_copy_mono procedure.
    However this routine first sets the specified area to black and then sets the white regions.
    Since the entire area is first set to black, the blending operation always produced a
    black result.
    
    The fix consists of creating a new copy_mono procedure for the PDF 1.4 transparency
    compositor devices.  The new routine does not set the entire area to black first.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@6692 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gdevp14.c
+++ gs/src/gdevp14.c
@@ -95,6 +95,7 @@ private	dev_proc_fill_rectangle(pdf14_fill_rectangle);
 private	dev_proc_fill_rectangle(pdf14_mark_fill_rectangle);
 private	dev_proc_fill_rectangle(pdf14_mark_fill_rectangle_ko_simple);
 private	dev_proc_fill_path(pdf14_fill_path);
+private dev_proc_copy_mono(pdf14_copy_mono);
 private	dev_proc_stroke_path(pdf14_stroke_path);
 private	dev_proc_begin_typed_image(pdf14_begin_typed_image);
 private	dev_proc_text_begin(pdf14_text_begin);
@@ -124,7 +125,7 @@ private	const gx_color_map_procs *
 	pdf14_decode_color,		/* gx_default_rgb_map_color_rgb */\
 	pdf14_fill_rectangle,		/* fill_rectangle */\
 	NULL,				/* tile_rectangle */\
-	NULL,				/* copy_mono */\
+	pdf14_copy_mono,		/* copy_mono */\
 	NULL,				/* copy_color */\
 	NULL,				/* draw_line */\
 	NULL,				/* get_bits */\
@@ -1366,6 +1367,81 @@ pdf14_text_begin(gx_device * dev, gs_imager_state * pis,
 	return code;
     *ppenum = (gs_text_enum_t *)penum;
     return code;
+}
+
+/*
+ * Implement copy_mono by filling lots of small rectangles.
+ */
+private int
+pdf14_copy_mono(gx_device * dev,
+	       const byte * base, int sourcex, int sraster, gx_bitmap_id id,
+	int x, int y, int w, int h, gx_color_index zero, gx_color_index one)
+{
+    const byte *sptr;
+    const byte *line;
+    int sbit, first_bit;
+    int code, sbyte, bit, count;
+    int run_length, startx, current_bit, bit_value;
+    gx_color_index current_color;
+
+    fit_copy(dev, base, sourcex, sraster, id, x, y, w, h);
+    line = base + (sourcex >> 3);
+    sbit = sourcex & 7;
+    first_bit = 7 - sbit;
+
+    /* Loop through the height of the specfied area. */
+    while (h-- > 0) {
+	/* Set up for the start of each line of the area. */
+	sptr = line;
+	sbyte = *sptr++;
+	bit = first_bit;
+	count = w;
+        run_length = 0;
+	startx = x;
+	current_bit = 0;
+        current_color = zero;
+
+	/* Loop across each pixel of a line. */
+	do {
+	    bit_value = (sbyte >> bit) & 1;
+	    if (bit_value == current_bit) {
+		/* The value did not change, simply increment our run length */
+		run_length++;
+	    } else {
+		/* The value changed, fill the current rectangle. */
+		if (run_length != 0) {
+		    if (current_color != gx_no_color_index) {
+			code = (*dev_proc(dev, fill_rectangle))
+				(dev, startx, y, run_length, 1, current_color);
+	                if (code < 0)
+			    return code;
+		    }
+		    startx += run_length;
+		}
+		run_length = 1;
+		current_color = bit_value ? one : zero;
+		current_bit = bit_value;
+	    }
+	    /* Move to the next input bit. */
+	    if (bit == 0) {
+		bit = 7;
+		sbyte = *sptr++;
+	    }
+	    else
+		bit--;
+	} while (--count > 0);
+	/* Fill the last rectangle in the line. */
+	if (run_length != 0 && current_color != gx_no_color_index) {
+	    code = (*dev_proc(dev, fill_rectangle))
+			(dev, startx, y, run_length, 1, current_color);
+	    if (code < 0)
+		return code;
+	}
+	/* Move to the next line */
+	line += sraster;
+	y++;
+    }
+    return 0;
 }
 
 private	int
