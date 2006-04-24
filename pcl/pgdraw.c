698708909965d0b127c1f45a5893bbc6bdde488f
Stefan Kemper <stefan.kemper@artifex.com>
2006-04-24 09:10:38 +0000

    Increase SC scaling from 5 to 7 decimal digits of accuracy. Looks like older plotter had a 2^23 limit and newer system allow up 2^30 scale factors.  5 digits wasn't enough to make an invertable 2^30 ctm causing errors with these large scalefactors.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@2366 06663e23-700e-0410-b217-a244a6096597

--- pcl/pgdraw.c
+++ pcl/pgdraw.c
@@ -201,9 +201,11 @@ hpgl_compute_user_units_to_plu_ctm(const hpgl_state_t *pgls, gs_matrix *pmat)
 		  }
 	      }
 	      /* looks like HP keeps 5 decimal digits of accuracy
-                 here.  This was derived empirically */
-	      scale_x -= fmod(scale_x, 1.0E-5);
-	      scale_y -= fmod(scale_y, 1.0E-5);
+               * here.  This was derived empirically 
+	       * Increased to 7 digits to allow 2^30 scale factors to be invertable */
+	      scale_x -= fmod(scale_x, 1.0E-7);
+	      scale_y -= fmod(scale_y, 1.0E-7);
+
 	      hpgl_call(gs_make_translation(origin_x, origin_y, pmat));
 	      hpgl_call(gs_matrix_scale(pmat, scale_x, scale_y, pmat));
 	      hpgl_call(gs_matrix_translate(pgls->memory, pmat, 
