9aee6b944d384c5f5ebd4c8d9cc8d0603773b08b
Alex Cherepanov <alex.cherepanov@artifex.com>
2006-05-30 13:56:04 +0000

    Fix incorrect element size argument of qsort(), that causes SEGV on platforns with sizeof(void *) != sizeof(int) since rev. 6669. Bug 688721.
    DIFFERENCES:
    None
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@6818 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gxfcopy.c
+++ gs/src/gxfcopy.c
@@ -2391,7 +2391,7 @@ order_font_data(gs_copied_font_data_t *cfdata, gs_memory_t *memory)
 	    a[j++] = &cfdata->names[i];
 	}
     }
-    qsort(a, cfdata->num_glyphs, sizeof(int), compare_glyph_names);
+    qsort(a, cfdata->num_glyphs, sizeof(*a), compare_glyph_names);
     for (; j >= 0; j--)
 	cfdata->glyphs[j].order_index = a[j] - cfdata->names;    
     gs_free_object(memory, a, "order_font_data");
