65ee3c7be6efff8688c322c9edb3635160ae7e4d
Ralph Giles <ralph.giles@artifex.com>
2006-06-07 15:26:51 +0000

    Correct the copyright holder and Id keyword after the licese header reversion.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@6841 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/md5.c
+++ gs/src/md5.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 1999, 2000 Aladdin Enterprises.  All rights reserved.
+  Copyright (C) 1999, 2000 artofcode LLC.  All rights reserved.
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -21,7 +21,7 @@
   ghost@aladdin.com
 
  */
-/*$RCSfile$ $Revision: 1899 $ */
+/* $Id:$ */
 /*
   Independent implementation of MD5 (RFC 1321).
 
