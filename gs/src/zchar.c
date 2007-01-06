751c98b750e0167faded61e3e4a63c7c4bb3df15
Igor Melichev <igor.melichev@artifex.com>
2007-01-06 20:35:32 +0000

    Fix : 'show' must throw error when the current font is a CID font resource.
    DETAILS :
    
    Only 'glyphshow' is allowed when the current font is a CID font resource,
    except for a cshow procedure - see comment in op_show_finish_setup.
    
    Debugged with CET 32_all.PS page 14.
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@7574 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/zchar.c
+++ gs/src/zchar.c
@@ -466,6 +466,14 @@ op_show_finish_setup(i_ctx_t *i_ctx_p, gs_text_enum_t * penum, int npop,
 	/* A special behavior defined in PLRM3 section 5.11 page 389. */
 	penum->outer_CID = osenum->returned.current_glyph;
     }
+    if (osenum == NULL && !(penum->text.operation & (TEXT_FROM_GLYPHS | TEXT_FROM_SINGLE_GLYPH))) {
+	gs_font *pfont = igs->root_font;
+
+	if (pfont->FontType == ft_CID_encrypted || 
+	    pfont->FontType == ft_CID_user_defined ||
+	    pfont->FontType == ft_CID_TrueType)
+	    return_error(e_typecheck);
+    }
     make_mark_estack(ep - (snumpush - 1), es_show, op_show_cleanup);
     if (endproc == NULL)
 	endproc = finish_show;
