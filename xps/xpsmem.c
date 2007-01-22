6d249aafcbfd612398274300cccda4f1028ac63b
Tor Andersson <tor.andersson@artifex.com>
2007-01-22 18:41:23 +0000

    Add a function to normalize a path name in-place by stripping out extra slashes and resolving dotdots.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@2740 06663e23-700e-0410-b217-a244a6096597

--- xps/xpsmem.c
+++ xps/xpsmem.c
@@ -1,6 +1,5 @@
 #include "ghostxps.h"
 
-
 char *
 xps_strdup_imp(xps_context_t *ctx, const char *str, const char *cname)
 {
@@ -10,5 +9,62 @@ xps_strdup_imp(xps_context_t *ctx, const char *str, const char *cname)
     if (cpy)
 	strcpy(cpy, str);
     return cpy;
+}
+
+#define SEP(x)  ((x)=='/' || (x) == 0)
+
+char *
+xps_clean_path(char *name)
+{
+    char *p, *q, *dotdot;
+    int rooted;
+
+    rooted = name[0] == '/';
+
+    /*
+     * invariants:
+     *      p points at beginning of path element we're considering.
+     *      q points just past the last path element we wrote (no slash).
+     *      dotdot points just past the point where .. cannot backtrack
+     *              any further (no slash).
+     */
+    p = q = dotdot = name + rooted;
+    while (*p)
+    {
+	if(p[0] == '/') /* null element */
+	    p++;
+	else if (p[0] == '.' && SEP(p[1]))
+	    p += 1; /* don't count the separator in case it is nul */
+	else if (p[0] == '.' && p[1] == '.' && SEP(p[2]))
+	{
+	    p += 2;
+	    if (q > dotdot) /* can backtrack */
+	    {
+		while(--q > dotdot && *q != '/')
+		    ;
+	    }
+	    else if (!rooted) /* /.. is / but ./../ is .. */
+	    {
+		if (q != name)
+		    *q++ = '/';
+		*q++ = '.';
+		*q++ = '.';
+		dotdot = q;
+	    }
+	}
+	else /* real path element */
+	{
+	    if (q != name+rooted)
+		*q++ = '/';
+	    while ((*q = *p) != '/' && *q != 0)
+		p++, q++;
+	}
+    }
+
+    if (q == name) /* empty string is really "." */
+	*q++ = '.';
+    *q = '\0';
+
+    return name;
 }
 
