6adc018dfbf52479778e29b8566409dc6ecea122
Ralph Giles <ralph.giles@artifex.com>
2007-03-06 19:46:12 +0000

    Resolve version control Id line issues flagged by the regression run.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@7769 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gxhintn1.c
+++ gs/src/gxhintn1.c
@@ -11,7 +11,7 @@
    San Rafael, CA  94903, U.S.A., +1(415)492-9861, for further information.
 */
 
-/* $Id: gxhintn.c 7583 2007-01-08 15:04:53Z leonardo $ */
+/* $Id$ */
 /* A stuff for reconnizing and fixing wrong contour signs. */
 
 #include "memory_.h"
