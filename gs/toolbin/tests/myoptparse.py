bd651168c1aece93a64ee2fbeaa6d470d275ab89
Ray Johnston <ray.johnston@artifex.com>
2007-04-26 23:46:30 +0000

    Fix regression tools to remove dependency on PYTHONPATH since the environment variable method isn't very convenient for those running local regressions and add those python files from peeves:/home/regression/python to this directory (under svn).
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@7884 a1074d23-0009-0410-80fe-cf8c14f379e6

---
+++ gs/toolbin/tests/myoptparse.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- Mode: python -*-
+
+#    Copyright (C) 2007 Artifex Software Inc.
+# 
+# This software is provided AS-IS with no warranty, either express or
+# implied.
+# 
+# This software is distributed under license and may not be copied,
+# modified or distributed except as expressly authorized under the terms
+# of the license contained in the file LICENSE in this distribution.
+# 
+# For more information about licensing, please refer to
+# http://www.ghostscript.com/licensing/. For information on
+# commercial licensing, go to http://www.artifex.com/licensing/ or
+# contact Artifex Software, Inc., 101 Lucas Valley Road #110,
+# San Rafael, CA  94903, U.S.A., +1(415)492-9861.
+
+
+import optparse
+import sys, os
+import types
+
+def parseCommandLineBasic(optionsParser=None,args=sys.argv):
+    optionsParser.add_option('--verbose','-v',action='store_true',help="noisy execution")
+
+    myself=os.path.basename(args[0])
+    options,arguments=optionsParser.parse_args()
+    options.myself=myself
+    return (options,arguments)
+
+def parseCommandLine(optionsParser=None,args=sys.argv,revisionSkip=False,testfileSkip=False,listfileSkip=False,deviceSkip=False):
+    if not optionsParser:
+        optionsParser=optparse.OptionParser()
+
+    if not testfileSkip:
+        optionsParser.add_option('--testfile','-t',action='store',help="testfile:\"test\"",default="test")
+
+    if not listfileSkip:
+        optionsParser.add_option('--listfile','-l',action='store',help="listfile:\"list\"",default="list")
+
+    if not revisionSkip:
+        optionsParser.add_option('--revision','-e',action='store',help="revision:HEAD",default="HEAD")
+
+    if not deviceSkip:
+        optionsParser.add_option('--device','-d',action='store',help="output device:ppmraw",default="ppmraw")
+        optionsParser.add_option('--resolution','-r',action='store',help="output resolution:300",default="300")
+        optionsParser.add_option('--banding','-b',action='store',help="output banding:False",default=False)
+
+    optionsParser.add_option('--quiet','-q',action='store_true',help="quiet execution")
+    optionsParser.add_option('--verbose','-v',action='store_true',help="noisy execution")
+
+    optionsParser.add_option('--nocleanup','-k',action='store_true',help="do not delete intermediate files")
+
+    myself=os.path.basename(args[0])
+    options,arguments=optionsParser.parse_args()
+    options.myself=myself
+    return (options,arguments)
+
+if __name__ == "__main__":
+
+    optionsParser=optparse.OptionParser()
+    optionsParser.add_option('--option','-o',action='store_true',help="sample additional option")
+    optionsParser.add_option('--nosvn','-s',action='store_true',help="no not update from svn")
+    optionsParser.add_option('--nomake','-m',action='store_true',help="no not make")
+
+    (options,arguments)=myoptparse.parseCommandLine(optionsParser)
+    print options.revision
+    print options.testfile
+    print arguments
+
+#   (options,arguments)=parseCommandLine(optionsParser,revisionSkip=True)
+#   (options,arguments)=parseCommandLine(optionsParser,revisionSkip=True,testfileSkip=True,listfileSkip=True,deviceSkip=True):
