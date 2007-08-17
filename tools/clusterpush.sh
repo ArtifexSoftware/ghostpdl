3fdeaa3602af3495e5db5dfc4b042fa9ee5bf38e
Ralph Giles <ralph.giles@artifex.com>
2007-08-17 17:48:43 +0000

    Make clusterpush slightly more verbose.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@2894 06663e23-700e-0410-b217-a244a6096597

--- tools/clusterpush.sh
+++ tools/clusterpush.sh
@@ -2,4 +2,10 @@
 
 # helper script to syncronize a working tree with the regression cluster
 
-rsync -avz --exclude .svn --exclude obj --exclude ufst --exclude ufst-obj ./* atfxsw01@tticluster.com:$USER/ghostpcl/
+HOST=atfxsw01@tticluster.com
+DEST=$USER/ghostpcl
+
+echo "Pushing to $DEST on the cluster..."
+rsync -avz \
+  --exclude .svn --exclude obj --exclude ufst --exclude ufst-obj \
+  ./* $HOST:$DEST/
