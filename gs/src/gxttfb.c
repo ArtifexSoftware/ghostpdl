ec528869bf508b680719e24aeed68dc33741eec9
Igor Melichev <igor.melichev@artifex.com>
2007-08-29 09:36:29 +0000

    Fix (TT font handler) : Place thr True Type interpreter and its data into stable memory (continued).
    DETAILS :
    
    Bug 688429 "Crash on vmreclaim".
    
    The revision 8209 change is incorrect
    because it effectively frees a memory allocator instance.
    Thanks to Henry for pointing this out.
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@8214 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gxttfb.c
+++ gs/src/gxttfb.c
@@ -402,8 +402,8 @@ void ttfFont__destroy(ttfFont *this, gs_font_dir *dir)
     ttfInterpreter__release(&dir->tti);
     gx_san__release(&dir->san);
     if (dir->tti == NULL && dir->ttm != NULL) {
+	gs_free_object(dir->memory, dir->ttm, "ttfFont__destroy(gx_ttfMemory)");
 	dir->ttm = NULL;
-	gs_free_object(dir->memory, mem, "ttfFont__destroy(gx_ttfMemory)");
     }
 }
 
