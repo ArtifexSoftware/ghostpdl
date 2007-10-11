e3d11753afcb7701c8a2963f3a1ef9ec26d36bf5
Tor Andersson <tor.andersson@artifex.com>
2007-10-11 14:45:58 +0000

    Add bounds check for accessing font file data.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@2929 06663e23-700e-0410-b217-a244a6096597

--- xps/xpsttf.c
+++ xps/xpsttf.c
@@ -66,8 +66,12 @@ static const char *pl_mac_names[258] = {
 static int
 xps_true_callback_string_proc(gs_font_type42 *p42, ulong offset, uint length, const byte **pdata)
 {
-    /* NB bounds check offset + length */
     xps_font_t *font = p42->client_data;
+    if (offset < 0 || offset + length > font->length)
+    {
+	*pdata = NULL;
+	return gs_throw2(-1, "font data access out of bounds (offset=%d size=%d)", offset, length);
+    }
     *pdata = font->data + offset;
     return 0;
 }
