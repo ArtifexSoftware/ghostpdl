1ed1a30dc9f2f3881221912b0501559b53459fcf
Igor Melichev <igor.melichev@artifex.com>
2007-11-01 17:22:45 +0000

    PS interpreter : Trace the executed operators' names (continued).
    DETAILS :
    
    Fix an inaccurate coding of the last patch.
    
    EXPECTED DIFFERENCES :
    
    None.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@8343 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/iinit.c
+++ gs/src/iinit.c
@@ -522,8 +522,6 @@ op_get_name_string(op_proc_t opproc)
     const op_def *const *tptr;
     int code;
 
-    /* Enter each operator into the appropriate dictionary. */
-
     for (tptr = op_defs_all; *tptr != 0; tptr++) {
 	const op_def *def;
 
