6f756850f91a04f89bc41e3d621edc2e9df5029e
Ralph Giles <ralph.giles@artifex.com>
2008-05-26 19:04:05 +0000

    Work around broken JBIG2Decode streams created by several versions of Xerox WorkCentre. Bugs 689841 and 689852.
    In these streams, a segment data length field is written as -1
    expecting the decoder to infer it from the length of the remaining
    data.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@8778 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/jbig2dec/jbig2_page.c
+++ gs/jbig2dec/jbig2_page.c
@@ -194,12 +194,30 @@ jbig2_parse_end_of_stripe(Jbig2Ctx *ctx, Jbig2Segment *segment, const uint8_t *s
  *
  * called upon seeing an 'end of page' segment, this routine
  * marks a page as completed so it can be returned.
- * compositing will have already happened in the previous 
+ * compositing will have already happened in the previous
  * segment handlers.
  **/
 int
 jbig2_complete_page (Jbig2Ctx *ctx)
 {
+
+    /* check for unfinished segments */
+    if (ctx->segment_index != ctx->n_segments) {
+      Jbig2Segment *segment = ctx->segments[ctx->segment_index];
+      int code = 0;
+      /* Some versions of Xerox Workcentre generate PDF files
+         with the segment data length field of the last segment
+         set to -1. Try to cope with this here. */
+      if ((segment->data_length & 0xffffffff) == 0xffffffff) {
+        jbig2_error(ctx, JBIG2_SEVERITY_WARNING, segment->number,
+          "File has an invalid segment data length!"
+          " Trying to decode using the available data.");
+        segment->data_length = ctx->buf_wr_ix - ctx->buf_rd_ix;
+        code = jbig2_parse_segment(ctx, segment, ctx->buf + ctx->buf_rd_ix);
+        ctx->buf_rd_ix += segment->data_length;
+        ctx->segment_index++;
+      }
+    }
     ctx->pages[ctx->current_page].state = JBIG2_PAGE_COMPLETE;
 
     return 0;
