84857636f6c5b6af732df52b4a73fa787fdf57ad
Ralph Giles <ralph.giles@artifex.com>
2008-06-02 20:38:14 +0000

    Update the clusterpush script to also run a regression against the latest baseline. There are some problems with output ordering, but otherwise it works.
    git-svn-id: http://svn.ghostscript.com/ghostpcl/trunk/ghostpcl@3078 06663e23-700e-0410-b217-a244a6096597

--- tools/clusterpush.sh
+++ tools/clusterpush.sh
@@ -11,9 +11,28 @@ if test -z "$TARGET"; then
   TARGET='ghostpcl'
 fi
 
+# try get the current revision
+REV=`svn info | grep Revision | cut -d ' ' -f 2`
+if test -z "$REV"; then
+  REV='unknown'
+fi
+
 echo "Pushing to $DEST/$TARGET on the cluster..."
 rsync -avz \
   --exclude .svn \
   --exclude bin --exclude obj --exclude debugobj \
+  --exclude main/obj --exclude main/debugobj \
+  --exclude language_switch/obj --exclude language_switch/obj \
+  --exclude xps/obj --exclude xps/debugobj \
+  --exclude svg/obj --exclude xps/debugobj \
   --exclude ufst --exclude ufst-obj \
   ./* $HOST:$DEST/$TARGET
+
+echo -n "Copying regression baseline"
+LATEST=`ssh $HOST ls regression | \
+ egrep 'ghostpcl-r[0-9]+\+[0-9]+' | sort -r | head -1`
+echo " from $LATEST..."
+ssh $HOST "cp regression/$LATEST/reg_baseline.txt $DEST/$TARGET/"
+
+echo "Queuing regression test..."
+echo "cd $DEST/$TARGET && run_regression" | ssh $HOST
