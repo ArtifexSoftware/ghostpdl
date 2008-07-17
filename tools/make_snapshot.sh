4048634547fb1db895dafb635bed472adf0b0195
Ralph Giles <ralph.giles@artifex.com>
2008-07-17 08:38:37 +0000

    Update make_snapshot for the new repository location.
    We no longer sample the external gs revision number, so things are labelled
    just by the revision number of the enclosing ghostpdl tree. The name of the
    tarball is also changed from 'ghostpcl' to 'ghostpdl'.
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@8848 a1074d23-0009-0410-80fe-cf8c14f379e6

--- tools/make_snapshot.sh
+++ tools/make_snapshot.sh
@@ -1,39 +1,32 @@
 #!/bin/sh
 
-# this is a script to make a snapshot of ghostpcl+gs trunk and upload
-# it casper. This is mostly because casper has an old svn installation
-# which can't be conveniently upgraded, so we have to do this manually.
+# this is a script makes a snapshot of the ghostpdl/gs trunk and 
+# uploads it casper. 
 # Note that all the code gets pulled to wherever you run it and then
 # pushed back to casper, so it takes a few minutes.
 
-# it constructs a snapshot from a clean export from the repository,
+# It constructs the snapshot from a clean export from the repository,
 # but BASED ON THE REVISIONS IN THE CURRENT CHECKOUT it's run from.
-# a little confusing, but it gives you some control over the version
+# A little confusing, but it gives you some control over the version
 # posted. Run 'svn up' first if you want to do the latest.
 
-# this script must be run from the top level of a ghostpcl working copy
-# requires svn export --ignore-externals, which is svn 1.2 or later I think
+# this script must be run from the top level of a ghostpdl working copy
 
 # cut off revision numbers for the changelogs
-ghostpcl_NEWSREV=3034
-ghostscript_NEWSREV=8567
+NEWSREV=8840
 
 # svn urls
-ghostpcl_SVNROOT=svn+ssh://svn.ghostscript.com/var/lib/svn-private/ghostpcl/trunk/ghostpcl
-ghostscript_SVNROOT=svn+ssh://svn.ghostscript.com/svn/ghostscript/trunk/gs
+SVNROOT=http://svn.ghostscript.com/ghostscript/trunk
+ghostpdl_SVNROOT=${SVNROOT}/ghostpdl
+gs_SVNROOT=${SVNROOT}/gs
 
-ghostpcl_REV=`svn info | grep Revision: | cut -f 2 -d ' '`
-ghostscript_REV=`svn info gs | grep Revision: | cut -f 2 -d ' '`
+REV=`svn info | grep Revision: | cut -f 2 -d ' '`
 
-echo "creating changelogs ..."
-echo "  ghostscript-r${ghostscript_REV}_NEWS.txt"
-svn log -r${ghostscript_REV}:${ghostscript_NEWSREV} $ghostscript_SVNROOT \
-	> ghostscript-r${ghostscript_REV}_NEWS.txt
-echo "  ghostpcl-r${ghostpcl_REV}_NEWS.txt"
-svn log -r${ghostpcl_REV}:${ghostpcl_NEWSREV} $ghostpcl_SVNROOT \
-	> ghostpcl-r${ghostpcl_REV}_NEWS.txt
+CHANGELOG="ghostpdl-r${REV}_NEWS.txt"
+echo "creating changelog... ${CHANGELOG}"
+svn log -r${REV}:${NEWSREV} $ghostpdl_SVNROOT > ${CHANGELOG}
 
-exportdir="ghostpcl-r${ghostpcl_REV}+${ghostscript_REV}"
+exportdir="ghostpdl-r${REV}"
 if test -d "$exportdir"; then
   echo "export target directory $exportdir already exists!"
   exit 1
@@ -41,15 +34,10 @@ fi
 
 echo "creating ${exportdir}.tar.gz ..."
 
-# export the ghostpcl code
-svn export -q --ignore-externals -r ${ghostpcl_REV} $ghostpcl_SVNROOT \
-	$exportdir
-# export ghostscript code
-cd $exportdir
-svn export -q -r ${ghostscript_REV} $ghostscript_SVNROOT gs
-cd ..
+# export the source code
+svn export -q -r ${REV} $ghostpdl_SVNROOT ${exportdir}
 
-# remove proprietary subdirectories
+# remove proprietary subdirectories that might exist
 echo "removing proprietary code ... "
 for verboten in ufst tools/metro_tests; do
   if test -d $exportdir/$verboten; then
@@ -59,26 +47,28 @@ for verboten in ufst tools/metro_tests; do
 done
 
 # copy in the changelogs
-cp ghostpcl-r${ghostpcl_REV}_NEWS.txt ghostscript-r${ghostscript_REV}_NEWS.txt \
-	$exportdir
+cp ${CHANGELOG} ${exportdir}
 tar czf $exportdir.tar.gz $exportdir/*
 rm -rf $exportdir
 
 #echo $exportdir.tar.gz ready for upload
 echo "uploading changelogs and $exportdir.tar.gz ..."
-scp ghostpcl-r${ghostpcl_REV}_NEWS.txt \
-	ghostscript-r${ghostscript_REV}_NEWS.txt \
-	$exportdir.tar.gz \
+scp ${CHANGELOG} $exportdir.tar.gz \
   www.ghostscript.com:/www/ghostscript.com/snapshots/
 
 echo "updating links ..."
 echo "cd /www/ghostscript.com/snapshots/ && \
+  if test -w ghostpdl-current.tar.gz; then \
+   rm ghostpdl-current.tar.gz; \
+   ln -s ${exportdir}.tar.gz ghostpdl-current.tar.gz; \
+  fi; \
+  if test -w ghostpdl-current_NEWS.txt; then \
+   rm ghostpdl-current_NEWS.txt; \
+   ln -s ${CHANGELOG} ghostpdl-current_NEWS.txt; \
+  fi;" \
+  # maintain the legacy current tarball link \
   if test -w ghostpcl-current.tar.gz; then \
    rm ghostpcl-current.tar.gz; \
    ln -s ${exportdir}.tar.gz ghostpcl-current.tar.gz; \
-  fi; \
-  if test -w ghostpcl-current_NEWS.txt; then \
-   rm ghostpcl-current_NEWS.txt; \
-   ln -s ghostpcl-r${ghostpcl_REV}_NEWS.txt ghostpcl-current_NEWS.txt; \
-  fi" \
+  fi \
 	| ssh ghostscript.com
