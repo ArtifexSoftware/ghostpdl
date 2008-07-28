f89fadf7c1df5e0c064515f178120a02284669d2
Ralph Giles <ralph.giles@artifex.com>
2008-07-28 18:58:41 +0000

    Update release date and product name for 8.63rc1.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@8891 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/src/gscdef.c
+++ gs/src/gscdef.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 2001-2008 Artifex Software, Inc.
    All Rights Reserved.
-  
+
    This software is provided AS-IS with no warranty, either express or
    implied.
 
@@ -43,7 +43,7 @@ const char *const gs_productfamily = GS_PRODUCTFAMILY;
 
 #ifndef GS_PRODUCT
 #  define GS_PRODUCT\
-	GS_PRODUCTFAMILY " SVN PRE-RELEASE"
+	GS_PRODUCTFAMILY " RELEASE CANDIDATE"
 #endif
 const char *const gs_product = GS_PRODUCT;
 
