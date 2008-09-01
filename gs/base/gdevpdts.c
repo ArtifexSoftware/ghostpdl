45c4349db9722de7a51bc12b9828af8b52058c09
Ken Sharp <ken.sharp@artifex.com>
2008-09-01 16:53:43 +0000

    Resolve another gcc compiler warning. The test was incorrect, now it is correctly taking the text render mode into account.
    Local regression testing shows no differences.
    
    Expected differences:
    None
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@9058 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/gdevpdts.c
+++ gs/base/gdevpdts.c
@@ -520,8 +520,8 @@ pdf_render_mode_uses_stroke(const gx_device_pdf *pdev,
 			    const pdf_text_state_values_t *ptsv)
 {
     return (pdev->text->text_state->in.render_mode != ptsv->render_mode &&
-	    ptsv->render_mode == 1 || ptsv->render_mode == 2 ||
-	    ptsv->render_mode == 5 || ptsv->render_mode == 6);
+	    (ptsv->render_mode == 1 || ptsv->render_mode == 2 ||
+	    ptsv->render_mode == 5 || ptsv->render_mode == 6));
 }
 
 /*
