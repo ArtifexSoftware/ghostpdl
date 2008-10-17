4bc3f64b73af0fe8ebfa960970d478acbcf91da6
Till Kamppeter <till.kamppeter@gmail.com>
2008-10-17 21:46:27 +0000

    Put new files under MIT license,
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@9169 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/cups/pdftoraster.c
+++ gs/cups/pdftoraster.c
@@ -1,17 +1,32 @@
-/* Copyright (C) 2008 Artifex Software, Inc.
-   All Rights Reserved.
-  
-   This software is provided AS-IS with no warranty, either express or
-   implied.
+/*
+Copyright (c) 2008, Till Kamppeter
+Copyright (c) 2008, BBR Inc.  All rights reserved.
 
-   This software is distributed under license and may not be copied, modified
-   or distributed except as expressly authorized under the terms of that
-   license.  Refer to licensing information at http://www.artifex.com/
-   or contact Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134,
-   San Rafael, CA  94903, U.S.A., +1(415)492-9861, for further information.
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+MIT Open Source License  -  http://www.opensource.org/
+
 */
 
 /* $Id: pdftoraster.c 8803 2008-06-24 14:16:29Z till $ */
+
 /* PDF to CUPS Raster filter based on Ghostscript */
 
 #include <stdio.h>
