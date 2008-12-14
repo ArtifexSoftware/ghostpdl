e1b7b750be5ec0be057a71d5f8c101fc6c2b67c2
Michael Vrhel <michael.vrhel@artifex.com>
2008-12-14 08:01:20 +0000

    Fixed wrong byte count in memory initialization.
    git-svn-id: http://svn.ghostscript.com/ghostscript/branches/smask_work@9289 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/gdevp14.c
+++ gs/base/gdevp14.c
@@ -731,7 +731,8 @@ pdf14_pop_transparency_group(pdf14_ctx *ctx,
             /* i.e. copy over those planes that exist beyond the count
                of the number of color components */
 
-			memset(new_data_buf, 0, tos->planestride*nos->n_planes);
+			/* memset(new_data_buf, 0, tos->planestride*nos->n_planes);  */
+			memset(new_data_buf, 0, new_num_planes); 
 
             /* Go ahead and do the conversion on the buffer */
 
