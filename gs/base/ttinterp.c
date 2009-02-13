42d11881f7b4231e162ea856058cef6c53c92468
Ken Sharp <ken.sharp@artifex.com>
2009-02-13 09:22:34 +0000

    Fix (TrueType interpreter): Ignore a 'faulty font' condition
    Details:
    bug #689760 "Missing characters: Failed to interpret TT instructions"
    
    The font in question is technically invalid. The problem occurs in function 1 of the
    FPGM table:
    
    	00069: PUSHB[2]              0    21
    	00072: JMPR
    	00073: ABS
    	00074: DUP
    	00075: ROLL
    	00076: RCVT
    	00077: DUP
    	00078: ROLL
    	00079: SUB
    	00080: ABS
    	00081: PUSHB[1]             50
    	00083: LT
    	00084: IF
    	00085: SWAP
    	00086: EIF
    	00087: POP
    	00088: ROUND[Black]
    	00089: PUSHB[1]             64
    	00091: MAX
    	00092: ENDF
    
    The JMPR (jump relative) instruction is intended to resume execution at the ENDF
    instruction, but the number of bytes is miscounted, falling of the end of the function.
    I suspect some TT interpreters catch this by spotting the fact that the IP is past the
    end of the function.
    
    We don't seem to be able to check if we've fallen off a function, because we don't store
    function lengths, so this code works around the problem by checking the opcode after we
    apply the JMPR but befopre resuming execution. If the opcode is *not* ENDF, but the
    preceding opcode was, assume that we have a counting problem, and move the IP back one
    instruction.
    
    Also applied the same work around to JROT and JROF instructions.
    
    Expected Differences
    None
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@9474 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/ttinterp.c
+++ gs/base/ttinterp.c
@@ -1836,6 +1836,10 @@ static int nInstrCount=0;
     {
       CUR.IP      += (Int)(args[0]);
       CUR.step_ins = FALSE;
+
+      /* See JMPR below */
+      if(CUR.code[CUR.IP] != 0x2D && CUR.code[CUR.IP - 1] == 0x2D)
+        CUR.IP -= 1;
     }
   }
 
@@ -1848,6 +1852,16 @@ static int nInstrCount=0;
   {
     CUR.IP      += (Int)(args[0]);
     CUR.step_ins = FALSE;
+
+    if(CUR.code[CUR.IP] != 0x2D && CUR.code[CUR.IP - 1] == 0x2D)
+    /* The JPMR is meant to stop at the ENDF instruction to finish
+     * the function. However the programmer made a mistake, and ended
+     * up one byte too far. I suspect that some TT interpreters handle this
+     * by detecting that the IP has gone off the end of the function. We can 
+     * allow for simple cases here by just checking the preceding byte.
+     * Fonts with this problem are not uncommon.
+     */
+      CUR.IP -= 1;
   }
 
 
@@ -1861,6 +1875,10 @@ static int nInstrCount=0;
     {
       CUR.IP      += (Int)(args[0]);
       CUR.step_ins = FALSE;
+
+      /* See JMPR above */
+      if(CUR.code[CUR.IP] != 0x2D && CUR.code[CUR.IP - 1] == 0x2D)
+        CUR.IP -= 1;
     }
   }
 
