fdefccb83e29b399d3c3935ed2ada533fb57bdd0
Ralph Giles <giles@ghostscript.com>
2009-03-31 21:29:09 -0700

    Remove unnecessary header includes.

--- jbig2.c
+++ jbig2.c
@@ -28,9 +28,6 @@
 
 #include "jbig2.h"
 #include "jbig2_priv.h"
-#include "jbig2_arith.h"
-#include "jbig2_generic.h"
-#include "jbig2_symbol_dict.h"
 
 static void *
 jbig2_default_alloc (Jbig2Allocator *allocator, size_t size)
