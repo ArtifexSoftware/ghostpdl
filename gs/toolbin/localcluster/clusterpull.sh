661723e33ccbd249276066350cb7fe4e8d082e58
Marcos H. Woehrmann <marcos.woehrmann@artifex.com>
2009-12-03 19:21:12 +0000

    I missed a file that should have been part of the r10440 commit.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@10444 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/toolbin/localcluster/clusterpull.sh
+++ gs/toolbin/localcluster/clusterpull.sh
@@ -2,33 +2,20 @@
 
 set machine=REPLACE_WITH_YOUR_MACHINE_NAME
 
+if (-e run.pid) then
 
-ssh -i ~/.ssh/cluster_key regression@casper.ghostscript.com touch /home/regression/cluster/$machine.up
+find . -maxdepth 1 -name run.pid -mmin +120 -type f -print | xargs /bin/rm -f
 
-if (-e $machine.start) then
 else
 
-touch $machine.start
-rm -f $machine.start
-
+ssh -i ~/.ssh/cluster_key regression@casper.ghostscript.com touch /home/regression/cluster/$machine.up
 scp -i ~/.ssh/cluster_key  -q regression@casper.ghostscript.com:/home/regression/cluster/$machine.start . >&/dev/null
-
 if (-e $machine.start) then
   ssh -i ~/.ssh/cluster_key regression@casper.ghostscript.com rm /home/regression/cluster/$machine.start
-
-  scp -i ~/.ssh/cluster_key -q regression@casper.ghostscript.com:/home/regression/cluster/$machine.jobs.gz . >&/dev/null
-  ssh -i ~/.ssh/cluster_key regression@casper.ghostscript.com rm /home/regression/cluster/$machine.jobs.gz
-
   scp -i ~/.ssh/cluster_key -q regression@casper.ghostscript.com:/home/regression/cluster/run.pl . >&/dev/null
   chmod +x run.pl
-
-  touch $machine.jobs
-  rm $machine.jobs
-  gunzip $machine.jobs.gz
-
   perl run.pl $machine >&$machine.out
-
-
 endif
+
 endif
 
