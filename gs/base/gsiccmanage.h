06ec06a0829e56d8278c64f9b1f06077776d110c
Michael Vrhel <michael.vrhel@artifex.com>
2009-12-19 02:23:42 +0000

    White space clean up.
    git-svn-id: http://svn.ghostscript.com/ghostscript/branches/icc_work@10529 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/gsiccmanage.h
+++ gs/base/gsiccmanage.h
@@ -11,9 +11,7 @@
    San Rafael, CA  94903, U.S.A., +1(415)492-9861, for further information.
 */
 
-
 /*  Header for the ICC Manager */
-
 
 #ifndef gsiccmanage_INCLUDED
 #  define gsiccmanage_INCLUDED
@@ -21,7 +19,6 @@
 #define ICC_DUMP 0
 
 /* Define the default ICC profiles in the file system */
-
 #define DEFAULT_GRAY_ICC  "iccprofiles/default_gray.icc"
 #define DEFAULT_RGB_ICC   "iccprofiles/default_rgb.icc"
 #define DEFAULT_CMYK_ICC  "iccprofiles/default_cmyk.icc"
@@ -33,55 +30,35 @@
 
 #define GSICC_STANDARD_PROFILES_KEYS\
   "srgb", "sgray"
-
 #define GSICC_STANDARD_PROFILES\
   "iccprofiles/srgb.icc", "iccprofiles/sgray.icc"
-
 #define GSICC_NUMBER_STANDARD_PROFILES 2
-
 
 #include "gsicc_littlecms.h"
 #include "gxclist.h"
 
 /* Prototypes */
-
-void  gsicc_profile_serialize(gsicc_serialized_profile_t *profile_data, cmm_profile_t *iccprofile);
-
+void  gsicc_profile_serialize(gsicc_serialized_profile_t *profile_data, 
+                              cmm_profile_t *iccprofile);
 int gsicc_init_device_profile(const gs_imager_state * pis, gx_device * dev);
-
-int gsicc_set_profile(const gs_imager_state * pis, const char *pname, int namelen, gsicc_profile_t defaulttype);
-
+int gsicc_set_profile(gsicc_manager_t *icc_manager, const char *pname, 
+                      int namelen, gsicc_profile_t defaulttype);
 cmm_profile_t* gsicc_get_profile_handle_file(const char* pname, int namelen, gs_memory_t *mem);
-
 gsicc_manager_t* gsicc_manager_new(gs_memory_t *memory);
-
 cmm_profile_t* gsicc_profile_new(stream *s, gs_memory_t *memory, const char* pname, int namelen);
-
 int gsicc_set_gscs_profile(gs_color_space *pcs, cmm_profile_t *icc_profile, gs_memory_t * mem);
-
 cmm_profile_t* gsicc_get_gscs_profile(const gs_color_space *gs_colorspace, gsicc_manager_t *icc_manager);
-
 void gsicc_init_hash_cs(cmm_profile_t *picc_profile, gs_imager_state *pis);
-
 gcmmhprofile_t gsicc_get_profile_handle_clist(cmm_profile_t *picc_profile, gs_memory_t *memory);
-
 gcmmhprofile_t gsicc_get_profile_handle_buffer(unsigned char *buffer);
-
 void gsicc_set_icc_directory(const gs_imager_state *pis, const char* pname, int namelen);
-
 unsigned int gsicc_getprofilesize(unsigned char *buffer);
-
 cmm_profile_t* gsicc_read_serial_icc(gx_device_clist_reader *pcrdev, int64_t icc_hashcode);
-
 cmm_profile_t* gsicc_finddevicen(const gs_color_space *pcs, gsicc_manager_t *icc_manager);
-
 int gsicc_profile_clist_read(cmm_profile_t *icc_profile, const gs_imager_state * pis,
     uint offset, const byte *data, uint size, gs_memory_t *mem);
-
-
 #if ICC_DUMP
 static void dump_icc_buffer(int buffersize, char filename[],byte *Buffer);
 #endif
-
 #endif
 
