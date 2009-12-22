3d1802a08be5e3c15126761b368fb17c46f29b67
Michael Vrhel <michael.vrhel@artifex.com>
2009-12-22 20:55:04 +0000

    White space clean up
    git-svn-id: http://svn.ghostscript.com/ghostscript/branches/icc_work@10545 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/gsicccache.h
+++ gs/base/gsicccache.h
@@ -13,35 +13,24 @@
 
 
 /*  Header for the ICC profile link cache */
-
-
 #ifndef gsicccache_INCLUDED
 #  define gsicccache_INCLUDED
 
 #include "gx.h"
 
 gsicc_link_cache_t* gsicc_cache_new(gs_memory_t *memory);
-
 void
 gsicc_init_buffer(gsicc_bufferdesc_t *buffer_desc, unsigned char num_chan, unsigned char bytes_per_chan,
                   bool has_alpha, bool alpha_first, bool is_planar, int plane_stride, int row_stride,
                   int num_rows, int pixels_per_row);
-
-
 gsicc_link_t* gsicc_get_link(const gs_imager_state * pis, const gs_color_space  *input_colorspace, 
                     gs_color_space *output_colorspace, 
                     gsicc_rendering_param_t *rendering_params, gs_memory_t *memory, bool include_softproof);
-
 gsicc_link_t* gsicc_get_link_profile(gs_imager_state *pis, cmm_profile_t *gs_input_profile, 
                     cmm_profile_t *gs_output_profile, 
                     gsicc_rendering_param_t *rendering_params, gs_memory_t *memory, bool include_softproof);
-
-
 void gsicc_release_link(gsicc_link_t *icclink);
-
-
 void gsicc_get_icc_buff_hash(unsigned char *buffer, int64_t *hash);
-
 
 #endif
 
