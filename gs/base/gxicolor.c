d58d217961d4b8aba65bd4498cfae82f5a1fd375
Michael Vrhel <michael.vrhel@artifex.com>
2010-02-06 07:38:44 +0000

    white space clean up
    git-svn-id: http://svn.ghostscript.com/ghostscript/branches/icc_work@10720 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/gxicolor.c
+++ gs/base/gxicolor.c
@@ -148,53 +148,36 @@ image_render_color(gx_image_enum *penum_orig, const byte *buffer, int data_x,
 
     pdevc = &devc1;
     pdevc_next = &devc2;
-
     /* These used to be set by init clues */
     pdevc->type = gx_dc_type_none;
     pdevc_next->type = gx_dc_type_none;
-
     /* Define the rendering intents */
     rendering_params.black_point_comp = BP_ON;
     rendering_params.object_type = GS_IMAGE_TAG;
     rendering_params.rendering_intent = pis->renderingintent;
-
     if (h == 0)
 	return 0;
-
     /* Request the ICC link for the transform that we will need to use */
-    
     icc_link = gsicc_get_link(pis, pcs, NULL, &rendering_params, pis->memory, false);
-
     if (icc_link == NULL) {
-
         return gs_rethrow(-1, "ICC Link not created during image render color");
-
     }
-
     /* If the link is the identity, then we don't need to do any color conversions */
-
     if (icc_link->is_identity ) {
-
         psrc_cm = (unsigned char *) psrc;
         spp_cm = spp;
         bufend = psrc_cm +  w;
         psrc_cm_start = NULL;
-
     } else {
-
         /* Set up the buffer descriptors. */
-
         spp_cm = pis->icc_manager->device_profile->num_comps;
         num_pixels = w/spp;
-
         gsicc_init_buffer(&input_buff_desc, spp, 1,
                       false, false, false, 0, w,
                       1, num_pixels);
-
         gsicc_init_buffer(&output_buff_desc, spp_cm, 1,
                       false, false, false, 0, num_pixels * spp_cm,
                       1, num_pixels);
-
         /* For now, just blast it all through the link. If we had a significant reduction 
            we will want to repack the data first and then do this.  That will be 
            an optimization shortly.  Also we are going to have to do something here
@@ -202,89 +185,58 @@ image_render_color(gx_image_enum *penum_orig, const byte *buffer, int data_x,
            others will need to be considered.  Also, for now just allocate a new output
            buffer.  We can reuse the old one if the number of channels in the output is
            less than or equal to the new one.  We will do that soon. */
-
         psrc_cm = gs_alloc_bytes(pis->memory,  w * spp_cm/spp, "image_render_color");
         psrc_cm_start = psrc_cm;
         bufend = psrc_cm +  w * dev->color_info.num_components/spp;
-
         gscms_transform_color_buffer(icc_link, &input_buff_desc, &output_buff_desc, 
                                  psrc,psrc_cm);
-
-
     }
-
     /* Release the link */
-
     gsicc_release_link(icc_link);
-
     pnext = penum->dda.pixel0;
     xrun = xprev = dda_current(pnext.x);
     yrun = yprev = dda_current(pnext.y);
     pdyx = dda_current(penum->dda.row.x) - penum->cur.x;
     pdyy = dda_current(penum->dda.row.y) - penum->cur.y;
-
     switch (posture) {
-
 	case image_portrait:
 	    vci = penum->yci, vdi = penum->hci;
 	    irun = fixed2int_var_rounded(xrun);
 	    break;
-
 	case image_landscape:
 	default:    /* we don't handle skew -- treat as landscape */
 	    vci = penum->xci, vdi = penum->wci;
 	    irun = fixed2int_var_rounded(yrun);
 	    break;
-
     }
-
     if_debug5('b', "[b]y=%d data_x=%d w=%d xt=%f yt=%f\n",
 	      penum->y, data_x, w, fixed2float(xprev), fixed2float(yprev));
-
     memset(&run, 0, sizeof(run));
     memset(&next, 0, sizeof(next));
-
     cs_full_init_color(&cc, pcs);
-
     run.v[0] = ~psrc_cm[0];	/* Force intial setting */
-
     while (psrc_cm < bufend) {
-
         dda_next(pnext.x);
 	dda_next(pnext.y);
-
         if ( penum->alpha ) {
-        
             /* If the pixels are different, then take care of the alpha now */
             /* will need to adjust spp below.... */
-            
-
-             
-
         } else {
-
             memcpy(&(next.v[0]),psrc_cm, spp_cm);
             psrc_cm += spp_cm;
-
         }
-
         /* Compare to previous.  If same then move on */
-
         if (posture != image_skewed && next.all[0] == run.all[0])
             goto inc;
-         
         /* This needs to be sped up */
          for ( k = 0; k < spp_cm; k++ ) {
             conc[k] = gx_color_value_from_byte(next.v[k]);
         }
-
         /* encode as a color index */
         color = dev_proc(dev, encode_color)(dev, &(conc[0]));
-
     /* check if the encoding was successful; we presume failure is rare */
     if (color != gx_no_color_index)
         color_set_pure(pdevc_next, color);
-
         /* Fill the region between */
 	/* xrun/irun and xprev */
         /*
@@ -331,14 +283,11 @@ image_render_color(gx_image_enum *penum_orig, const byte *buffer, int data_x,
 	    goto err;
 	rsrc = psrc;
 	if ((code = mcode) < 0) goto err;
-
         /* Swap around the colors due to a change */
         ptemp = pdevc;
         pdevc = pdevc_next;
         pdevc_next = ptemp;
-
  	run = next;
-
 inc:	xprev = dda_current(pnext.x);
 	yprev = dda_current(pnext.y);	/* harmless if no skew */
     }
@@ -381,17 +330,13 @@ inc:	xprev = dda_current(pnext.x);
 		     pdevc, lop);
 	    }
     }
-
     /* Free cm buffer, if it was used */
-
     if (psrc_cm_start != NULL) {
         gs_free_object(pis->memory, (byte *)psrc_cm_start, "image_render_color");
     }
-
     return (code < 0 ? code : 1);
     /* Save position if error, in case we resume. */
 err:
-
     gs_free_object(pis->memory, (byte *)psrc_cm_start, "image_render_color");
     penum_orig->used.x = (rsrc - spp - psrc_initial) / spp;
     penum_orig->used.y = 0;
