ArtifexSoftware/ghostpdl

commit b3372e0c1fb55e3ec089dd60e08bc29340afc811

process the VUDataLength optional payload of the VendorUnique operator; part of fix to 690083
git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@10859 a1074d23-0009-0410-80fe-cf8c14f379e6

tools/pxldis.py
@@ -368,6 +368,12 @@ class pxl_dis:
         # the n'th operator in the stream
         self.operator_position = 0
 
+        # The VendorUnique operator can have an optional embedded payload
+        # where the length is stored in the VUDataLength attribute.
+        # We need to cache the value of that atttribute to skip over
+        # that many number of bytes when it is encountered.
+        self.VUDataLength = 0 
+
         # true if we get UEL
         self.endjob = 0
         
@@ -699,7 +705,7 @@ class pxl_dis:
 
     # check for embedded tags.
     def isEmbedded(self, name):
-        return ( name == 'embedded_data' or name == 'embedded_data_byte' )
+        return ( name == 'embedded_data' or name == 'embedded_data_byte' or name == 'VendorUnique')
 
     def process_EmbeddedInfo(self, name):
         if ( name == 'embedded_data' ):
@@ -708,6 +714,9 @@ class pxl_dis:
         if ( name == 'embedded_data_byte' ):
             length = int(self.unpack('B', self.data[self.index:self.index+1])[0])
             self.index = self.index + 1
+        if ( name == 'VendorUnique' ):
+            length = self.VUDataLength
+            self.VUDataLength = 0
         print "length:",
         print length
         print "["
@@ -726,6 +735,17 @@ class pxl_dis:
             for k in pxl_attribute_name_to_attribute_number_dict.keys():
                 if ( pxl_attribute_name_to_attribute_number_dict[k] == tag ):
                     tag_not_listed = 0
+                    if (k == 'VUDataLength'):
+                        # The VUDataLength is only ever seen with the CLJ3500/3550/3600
+                        # as part of an optional VendorUnque payload; it is possible
+                        # that its value type can be something else other than uint32.
+                        # We'll just backtrack to get a uint32 value, and raise an error 
+                        # if it isn't uint32.
+                        vu_tag = unpack('B', self.data[self.index-6])[0]
+                        if ( vu_tag == pxl_tags_dict['uint32'] ):
+                            self.VUDataLength = int(self.unpack('I', self.data[self.index-5:self.index-1])[0])
+                        else:
+                            raise(SyntaxError), "VUDataLength not uint32"
                     print k,
                     if pxl_enumerations_dict.has_key(k):
                         print "//",

commit 6200084f8c3143929b6591007ec057e2f85753dc

fix small bug introduced in r10859 where the operator pos in the output comment becomes off by 1
git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@10865 a1074d23-0009-0410-80fe-cf8c14f379e6

tools/pxldis.py
@@ -427,7 +427,7 @@ class pxl_dis:
         print key
         self.index = self.index + 1
                 # handle special cases
-        if (self.isEmbedded(key)):
+        if (self.isEmbedded(key) or (key == 'VendorUnique')):
             self.process_EmbeddedInfo(key)
         return 1
 
@@ -715,7 +715,7 @@ class pxl_dis:
 
     # check for embedded tags.
     def isEmbedded(self, name):
-        return ( name == 'embedded_data' or name == 'embedded_data_byte' or name == 'VendorUnique')
+        return ( name == 'embedded_data' or name == 'embedded_data_byte')
 
     def process_EmbeddedInfo(self, name):
         if ( name == 'embedded_data' ):