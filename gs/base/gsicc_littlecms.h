435c06a27186f9e840cd05fc364c4016b4da8c96
Michael Vrhel <michael.vrhel@artifex.com>
2010-03-17 18:57:33 +0000

    White space clean up
    git-svn-id: http://svn.ghostscript.com/ghostscript/branches/icc_work@10934 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/gsicc_littlecms.h
+++ gs/base/gsicc_littlecms.h
@@ -33,56 +33,40 @@
 
 /* Prototypes */
 
-gcmmhprofile_t gscms_get_profile_handle_mem(unsigned char *buffer, unsigned int input_size);
-
+gcmmhprofile_t gscms_get_profile_handle_mem(unsigned char *buffer, 
+                                            unsigned int input_size);
 gcmmhprofile_t gscms_get_profile_handle_file(const char *filename);
-
-void gscms_transform_color_buffer(gsicc_link_t *icclink, gsicc_bufferdesc_t *input_buff_desc,
+void gscms_transform_color_buffer(gsicc_link_t *icclink, 
+                                  gsicc_bufferdesc_t *input_buff_desc,
                              gsicc_bufferdesc_t *output_buff_desc, 
                              void *inputbuffer,
                              void *outputbuffer);
-
 int gscms_get_channel_count(gcmmhprofile_t profile);
-
 int gscms_get_pcs_channel_count(gcmmhprofile_t profile);
-
 char* gscms_get_clrtname(gcmmhprofile_t profile, int colorcount);
-
 int gscms_get_numberclrtnames(gcmmhprofile_t profile);
-
 gsicc_colorbuffer_t gscms_get_profile_data_space(gcmmhprofile_t profile);
-
 void gscms_transform_color(gsicc_link_t *icclink,  
                              void *inputcolor,
                              void *outputcolor,
                              int num_bytes,
                              void **contextptr);
-
 gcmmhlink_t gscms_get_link(gcmmhprofile_t  lcms_srchandle, 
                     gcmmhprofile_t lcms_deshandle, 
                     gsicc_rendering_param_t *rendering_params);
-
-
-
 gcmmhlink_t gscms_get_link_proof(gcmmhprofile_t  lcms_srchandle, 
-                    gcmmhprofile_t lcms_deshandle, gcmmhprofile_t lcms_proofhandle, 
+                    gcmmhprofile_t lcms_deshandle, 
+                    gcmmhprofile_t lcms_proofhandle, 
                     gsicc_rendering_param_t *rendering_params);
-
 void gscms_create(void **contextptr);
-
 void gscms_destroy(void **contextptr);
-
 void gscms_release_link(gsicc_link_t *icclink);
-
 void gscms_release_profile(void *profile);
-
-int gscms_transform_named_color(gsicc_link_t *icclink,  float tint_value, const char* ColorName, 
+int gscms_transform_named_color(gsicc_link_t *icclink,  float tint_value, 
+                                const char* ColorName, 
                         gx_color_value device_values[] ); 
-
 void gscms_get_name2device_link(gsicc_link_t *icclink, gcmmhprofile_t  lcms_srchandle, 
                     gcmmhprofile_t lcms_deshandle, gcmmhprofile_t lcms_proofhandle, 
                     gsicc_rendering_param_t *rendering_params);
-
-
 #endif
 
