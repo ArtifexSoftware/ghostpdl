1b6f4b4c05feea1199105f6465cfd85614ba21b4
Chris Liddell <chris.liddell@artifex.com>
2010-03-31 14:40:31 +0000

    Correct a logical error/typo in the fallback to non-hinted condition.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@11001 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/psi/fapi_ft.c
+++ gs/psi/fapi_ft.c
@@ -351,7 +351,7 @@ load_glyph(FAPI_font *a_fapi_font, const FAPI_char_ref *a_char_ref,
 	ft_error = FT_Load_Glyph(ft_face, index, a_bitmap ? FT_LOAD_MONOCHROME | FT_LOAD_RENDER: FT_LOAD_MONOCHROME);
     }
 
-    if (ft_error == FT_Err_Too_Many_Hints || ft_error == FT_Err_Invalid_Argument || ft_error == FT_Err_Too_Many_Function_Defs || FT_Err_Invalid_Glyph_Index) {
+    if (ft_error == FT_Err_Too_Many_Hints || ft_error == FT_Err_Invalid_Argument || ft_error == FT_Err_Too_Many_Function_Defs || ft_error == FT_Err_Invalid_Glyph_Index) {
         a_fapi_font->char_data = saved_char_data;
         ft_error = FT_Load_Glyph(ft_face, index, a_bitmap ? FT_LOAD_MONOCHROME | FT_LOAD_RENDER | FT_LOAD_NO_HINTING: FT_LOAD_MONOCHROME | FT_LOAD_NO_HINTING);
     }
