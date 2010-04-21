f6a18e4e08447aa5e721302dee6a10ce252ac129
Robin Watts <robin.watts@artifex.com>
2010-04-21 19:26:13 +0000

    Revert macro magic from gserror.h introduced in r11097, due to it being broken when used as an expression. Sorry.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@11098 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/gserror.h
+++ gs/base/gserror.h
@@ -19,7 +19,7 @@
 
 int gs_log_error(int, const char *, int);
 #ifndef DEBUG
-#  define gs_log_error(err, file, line) do {} while ((err) && (0==1))
+#  define gs_log_error(err, file, line) (err)
 #endif
 #define gs_note_error(err) gs_log_error(err, __FILE__, __LINE__)
 #define return_error(err) return gs_note_error(err)
