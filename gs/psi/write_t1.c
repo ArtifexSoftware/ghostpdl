e2620fa4234f3681bff97ea915b1510fb4a142d7
Ken Sharp <ken.sharp@artifex.com>
2010-05-17 08:25:06 +0000

    white space change only, committed to make a regression run as the test for the previous revision was interrupted.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@11269 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/psi/write_t1.c
+++ gs/psi/write_t1.c
@@ -349,7 +349,7 @@ long FF_serialize_type1_font(FAPI_font* a_fapi_font,unsigned char* a_buffer,long
 
 long FF_serialize_type1_font_complete(FAPI_font* a_fapi_font,unsigned char* a_buffer,long a_buffer_size)
 	{
-	WRF_output output;
+        WRF_output output;
 	WRF_init(&output,a_buffer,a_buffer_size);
 	
 	/* Leading comment identifying a Type 1 font. */
