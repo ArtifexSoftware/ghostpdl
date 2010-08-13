6c3f25b808a4b7c8505720563e5cfe1a87d1e920
Ken Sharp <ken.sharp@artifex.com>
2010-08-13 19:46:28 +0000

    Fix (pdfwrite( : Invalid compression filter emitted.
    Bug #691556 "Images compressed with the RunLengthDecode filter are invalid" A typo in
    gdevpdfx.h caused the /Filter entry of an image dictionary to be written with a
    trailing comma if the filter was RunLength.
    
    No differences expected
    
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@11632 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/base/gdevpdfx.h
+++ gs/base/gdevpdfx.h
@@ -1005,7 +1005,7 @@ typedef struct pdf_filter_names_s {
 #define PDF_FILTER_NAMES\
   "/ASCII85Decode", "/ASCIIHexDecode", "/CCITTFaxDecode",\
   "/DCTDecode",  "/DecodeParms", "/Filter", "/FlateDecode",\
-  "/LZWDecode", "/RunLengthDecode,", "/JBIG2Decode", "/JPXDecode"
+  "/LZWDecode", "/RunLengthDecode", "/JBIG2Decode", "/JPXDecode"
 #define PDF_FILTER_NAMES_SHORT\
   "/A85", "/AHx", "/CCF", "/DCT", "/DP", "/F", "/Fl", "/LZW", "/RL", "/???", "/???"
 
