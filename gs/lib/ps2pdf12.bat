f610841cbabd0345ef181fa79826635104394d3a
Alex Cherepanov <alex.cherepanov@artifex.com>
2010-09-25 02:08:22 +0000

    For long time ps2pdf*.bat files failed when they were used with some options and a single argument. findstr is now used to identify options by the leading '-' and handle this case. Bug 691622.
    git-svn-id: http://svn.ghostscript.com/ghostscript/trunk@11743 a1074d23-0009-0410-80fe-cf8c14f379e6

--- gs/lib/ps2pdf12.bat
+++ gs/lib/ps2pdf12.bat
@@ -13,5 +13,9 @@ rem Pass arguments through a file to avoid overflowing the command line.
 echo %1 >>"%TEMP%\_.at"
 shift
 :bot
-if not %3/==/ goto top
+rem Search for leading '-'
+echo %1 | findstr /b /C:- >nul 2>&1
+if ERRORLEVEL 1 goto proc
+goto top
+:proc
 call "%LIBDIR%ps2pdfxx.bat" %1 %2
