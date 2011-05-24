8151aa54b8a71e5c9929bb43390a9be1b4b8d042
Robin Watts <Robin.Watts@artifex.com>
2011-05-24 16:23:32 +0100

    Add gitsetup.sh to gs/toolbin.
    Simple script for doing the 'obvious' configuration to a freshly checked
    out git clone of ghostscript.
    
    CLUSTER_UNTESTED.

---
+++ gs/toolbin/gitsetup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git config core.whitespace "space-before-tab,tab-in-indent,trailing-space"
+git config branch.autosetuprebase always
+git config branch.master.rebase true
+git config push.default tracking
+git config alias.update 'pull --rebase'
+git config alias.up 'pull --rebase'
+git config alias.logg "log --graph --oneline --decorate --all"
+git config ui.color true
+
+echo "Git configuration (local):"
+git config --local -l
+
+echo
+echo "Git configuraton (global):"
+git config --global -l
+
+echo
+echo You may want to consider the following if they are not correct above:
+echo   git config --global user.name "Your Name"
+echo   git config --global user.email "your.name@artifex.com"
