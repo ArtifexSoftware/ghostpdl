998fc31cc9bd717190b0313eb246c14fe6303602
Marcos H. Woehrmann <marcos.woehrmann@artifex.com>
2011-05-26 11:20:10 -0700

    Restore Makefile to clusterpush.pl file list.
    Removed Makefile from the list of files excluded from the rsync
    operation in clusterpush.pl since the ghostpdl Makefile is not
    built by a configure/autogen.sh operation.

--- gs/toolbin/localcluster/clusterpush.pl
+++ gs/toolbin/localcluster/clusterpush.pl
@@ -131,7 +131,7 @@ my $cmd="rsync -avxcz ".
 " --exclude bin --exclude obj --exclude debugobj --exclude pgobj".
 " --exclude sobin --exclude soobj".
 " --exclude ufst --exclude ufst-obj".
-" --exclude Makefile --exclude config.log".
+" --exclude config.log".
 " --exclude .ppm --exclude .pkm --exclude .pgm --exclude .pbm".
 " -e \"$ssh\" ".
 " .".
