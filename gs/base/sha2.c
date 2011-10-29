4c00d4bc5eb87191674463af9444c457ac361c07
Tim Waugh <twaugh@redhat.com>
2011-10-29 18:14:14 -0400

    Bug 692599: fix bad sizeof() usage in base/sha2.c
    sizeof() was being used incorrectly in base/sha2.c,
    found using Coverity.

--- gs/base/sha2.c
+++ gs/base/sha2.c
@@ -642,7 +642,7 @@ void SHA256_Final(sha2_byte digest[], SHA256_CTX* context) {
         }
 
         /* Clean up state data: */
-        MEMSET_BZERO(context, sizeof(context));
+        MEMSET_BZERO(context, sizeof(*context));
         usedspace = 0;
 }
 
@@ -663,7 +663,7 @@ char *SHA256_End(SHA256_CTX* context, char buffer[]) {
                 }
                 *buffer = (char)0;
         } else {
-                MEMSET_BZERO(context, sizeof(context));
+                MEMSET_BZERO(context, sizeof(*context));
         }
         MEMSET_BZERO(digest, SHA256_DIGEST_LENGTH);
         return buffer;
@@ -970,7 +970,7 @@ void SHA512_Final(sha2_byte digest[], SHA512_CTX* context) {
         }
 
         /* Zero out state data */
-        MEMSET_BZERO(context, sizeof(context));
+        MEMSET_BZERO(context, sizeof(*context));
 }
 
 char *SHA512_End(SHA512_CTX* context, char buffer[]) {
@@ -990,7 +990,7 @@ char *SHA512_End(SHA512_CTX* context, char buffer[]) {
                 }
                 *buffer = (char)0;
         } else {
-                MEMSET_BZERO(context, sizeof(context));
+                MEMSET_BZERO(context, sizeof(*context));
         }
         MEMSET_BZERO(digest, SHA512_DIGEST_LENGTH);
         return buffer;
@@ -1044,7 +1044,7 @@ void SHA384_Final(sha2_byte digest[], SHA384_CTX* context) {
         }
 
         /* Zero out state data */
-        MEMSET_BZERO(context, sizeof(context));
+        MEMSET_BZERO(context, sizeof(*context));
 }
 
 char *SHA384_End(SHA384_CTX* context, char buffer[]) {
@@ -1064,7 +1064,7 @@ char *SHA384_End(SHA384_CTX* context, char buffer[]) {
                 }
                 *buffer = (char)0;
         } else {
-                MEMSET_BZERO(context, sizeof(context));
+                MEMSET_BZERO(context, sizeof(*context));
         }
         MEMSET_BZERO(digest, SHA384_DIGEST_LENGTH);
         return buffer;
