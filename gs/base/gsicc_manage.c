044b6ca010ecb8f264fcd4b3515051c037659967
Michael Vrhel <michael.vrhel@artifex.com>
2012-03-15 12:07:51 -0700

    Fix for broken setting of Default ICC profiles
    A recent change http://git.ghostscript.com/?p=ghostpdl.git;a=commit;h=d6f504174bd51d22cc43b2f87bee8c275c004cc1
    broke the ability to set the profiles.  Previously there was a check in gsicc_set_profile to
    avoid reseting the profiles.  Once removed, the call from gsicc_init_manager was always blowing
    away any non default settings for the defaults.  A check was added into gsicc_init_manager to
    avoid this.

--- gs/base/gsicc_manage.c
+++ gs/base/gsicc_manage.c
@@ -1370,19 +1370,36 @@ gsicc_init_gs_colors(gs_state *pgs)
     return code;
 }
 
+/* Only set those that have not already been set. */
 int
 gsicc_init_iccmanager(gs_state * pgs)
 {
-    int code, k;
+    int code = 0, k;
     const char *pname;
     int namelen;
     gsicc_manager_t *iccmanager = pgs->icc_manager;
+    cmm_profile_t *profile; 
 
     for (k = 0; k < 4; k++) {
         pname = default_profile_params[k].path;
         namelen = strlen(pname);
-        code = gsicc_set_profile(iccmanager, pname, namelen+1,
-            default_profile_params[k].default_type);
+
+        switch(default_profile_params[k].default_type) {
+            case DEFAULT_GRAY:
+                profile = iccmanager->default_gray;
+                break;
+            case DEFAULT_RGB:
+                profile = iccmanager->default_rgb;
+                break;
+            case DEFAULT_CMYK:
+                 profile = iccmanager->default_cmyk;
+                 break;
+            default:
+                profile = NULL;
+        }
+        if (profile == NULL)
+            code = gsicc_set_profile(iccmanager, pname, namelen+1,
+                                     default_profile_params[k].default_type);
         if (code < 0)
             return gs_rethrow(code, "cannot find default icc profile");
     }
