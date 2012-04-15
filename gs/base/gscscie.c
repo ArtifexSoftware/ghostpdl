ffedf4d9c97b1f502e47bab7aed8ee2531c3de5c
Ray Johnston <ray.johnston@artifex.com>
2012-04-15 16:53:58 -0700

    Fix ref counting of CIEBased 'params' during alloc_restore_all with 11-19.PS CET.
    The 'const' had to be broken to allow the pointer to be set to NULL when the object is freed
    due to reference count going to zero. Debug build showed an error message due to ref_count
    going below zero, but since this is referencing freed memory, can cause a segfault.

--- gs/base/gscscie.c
+++ gs/base/gscscie.c
@@ -147,49 +147,57 @@ gx_install_CIE(gs_color_space * pcs, gs_state * pgs)
 static void
 gx_final_CIEDEFG(const gs_color_space * pcs)
 {
+    gs_color_space *pcs_noconst = (gs_color_space *)pcs;
+
     if (pcs->icc_equivalent != NULL) {
         rc_decrement_only(pcs->icc_equivalent, "gx_final_CIEDEFG");
     }
     if (pcs->cmm_icc_profile_data != NULL) {
         rc_decrement_only(pcs->cmm_icc_profile_data, "gx_final_CIEDEFG");
     }
-    rc_decrement_only(pcs->params.defg, "gx_final_CIEDEFG");
+    rc_decrement(pcs_noconst->params.defg, "gx_final_CIEDEFG");
 }
 
 static void
 gx_final_CIEDEF(const gs_color_space * pcs)
 {
+    gs_color_space *pcs_noconst = (gs_color_space *)pcs;
+
     if (pcs->icc_equivalent != NULL) {
         rc_decrement_only(pcs->icc_equivalent,"gx_final_CIEDEF");
     }
     if (pcs->cmm_icc_profile_data != NULL) {
         rc_decrement_only(pcs->cmm_icc_profile_data, "gx_final_CIEDEF");
     }
-    rc_decrement_only(pcs->params.def, "gx_final_CIEDEF");
+    rc_decrement(pcs_noconst->params.def, "gx_final_CIEDEF");
 }
 
 static void
 gx_final_CIEABC(const gs_color_space * pcs)
 {
+    gs_color_space *pcs_noconst = (gs_color_space *)pcs;
+
     if (pcs->icc_equivalent != NULL) {
         rc_decrement_only(pcs->icc_equivalent,"gx_final_CIEABC");
     }
     if (pcs->cmm_icc_profile_data != NULL) {
         rc_decrement_only(pcs->cmm_icc_profile_data, "gx_final_CIEABC");
     }
-    rc_decrement_only(pcs->params.abc, "gx_final_CIEABC");
+    rc_decrement(pcs_noconst->params.abc, "gx_final_CIEABC");
 }
 
 static void
 gx_final_CIEA(const gs_color_space * pcs)
 {
+    gs_color_space *pcs_noconst = (gs_color_space *)pcs;
+
     if (pcs->icc_equivalent != NULL) {
         rc_decrement_only(pcs->icc_equivalent,"gx_final_CIEA");
     }
     if (pcs->cmm_icc_profile_data != NULL) {
         rc_decrement_only(pcs->cmm_icc_profile_data, "gx_final_CIEA");
     }
-    rc_decrement_only(pcs->params.a, "gx_adjust_cspace_CIEA");
+    rc_decrement(pcs_noconst->params.a, "gx_adjust_cspace_CIEA");
 }
 
 /* ---------------- Procedures ---------------- */
