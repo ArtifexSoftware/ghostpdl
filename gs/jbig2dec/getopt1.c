7c9a1a772cef2339c110238b40c73e8039dca2eb
Chris Liddell <chris.liddell@artifex.com>
2012-06-15 14:01:27 +0100

    Revert some copyrights accidentally changed in the recent revamp
    Three files which are copyright FSF.
    
    Thanks to zeniko for spotting the mistake.
    
    No cluster differences.

--- gs/jbig2dec/getopt1.c
+++ gs/jbig2dec/getopt1.c
@@ -1,20 +1,23 @@
-/* Copyright (C) 2001-2012 Artifex Software, Inc.
-   All Rights Reserved.
-
-   This software is provided AS-IS with no warranty, either express or
-   implied.
-
-   This software is distributed under license and may not be copied,
-   modified or distributed except as expressly authorized under the terms
-   of the license contained in the file LICENSE in this distribution.
-
-   Refer to licensing information at http://www.artifex.com or contact
-   Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134, San Rafael,
-   CA  94903, U.S.A., +1(415)492-9861, for further information.
-*/
-
 /* getopt_long and getopt_long_only entry points for GNU getopt.
-*/
+   Copyright (C) 1987,88,89,90,91,92,93,94,96,97,98
+     Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
