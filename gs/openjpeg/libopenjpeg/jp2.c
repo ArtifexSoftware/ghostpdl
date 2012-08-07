291872308da215e1d7e12c8375fcf10dd5e651d2
Alex Cherepanov <alex.cherepanov@artifex.com>
2012-08-06 23:37:19 -0400

    Backport patches CVE problems.
    CVE-2009-5030 http://code.google.com/p/openjpeg/source/detail?r=1703
    CVE-2012-1499 http://code.google.com/p/openjpeg/source/detail?r=1330
    CVE-2012-3358 http://code.google.com/p/openjpeg/source/detail?r=1727

--- gs/openjpeg/libopenjpeg/jp2.c
+++ gs/openjpeg/libopenjpeg/jp2.c
@@ -93,7 +93,7 @@ Apply collected palette data
 @param color Collector for profile, cdef and pclr data
 @param image 
 */
-static void jp2_apply_pclr(opj_jp2_color_t *color, opj_image_t *image);
+static void jp2_apply_pclr(opj_jp2_color_t *color, opj_image_t *image, opj_common_ptr cinfo);
 /**
 Collect palette data
 @param jp2 JP2 handle
@@ -318,7 +318,7 @@ static void free_color_data(opj_jp2_color_t *color)
 	if(color->icc_profile_buf) opj_free(color->icc_profile_buf);
 }
 
-static void jp2_apply_pclr(opj_jp2_color_t *color, opj_image_t *image)
+static void jp2_apply_pclr(opj_jp2_color_t *color, opj_image_t *image, opj_common_ptr cinfo)
 {
 	opj_image_comp_t *old_comps, *new_comps;
 	unsigned char *channel_size, *channel_sign;
@@ -343,7 +343,10 @@ static void jp2_apply_pclr(opj_jp2_color_t *color, opj_image_t *image)
    {
 	pcol = cmap[i].pcol; cmp = cmap[i].cmp;
 
-	new_comps[pcol] = old_comps[cmp];
+	 if( pcol < nr_channels )
+	    new_comps[pcol] = old_comps[cmp];
+	 else
+	    opj_event_msg(cinfo, EVT_ERROR, "Error with pcol value. skipping\n");
 
 	if(cmap[i].mtyp == 0) /* Direct use */
   {
@@ -748,7 +751,7 @@ opj_image_t* jp2_decode(opj_jp2_t *jp2, opj_cio_t *cio,
                 image->has_palette = true;
             }
             else
-                jp2_apply_pclr(&color, image);
+                jp2_apply_pclr(&color, image, cinfo);
     }
 
 	if(color.icc_profile_buf)
