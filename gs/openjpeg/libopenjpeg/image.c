4e3808ff733ed0d485b67211ad5bcf7aa992c739
Alex Cherepanov <alex.cherepanov@artifex.com>
2012-08-08 20:36:38 -0400

    Import original OpenJpeg 1.5
    This revision doesn't compile. It's here to establish a reference
    for the diffs.

--- gs/openjpeg/libopenjpeg/image.c
+++ gs/openjpeg/libopenjpeg/image.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, Hervé Drolon, FreeImage Team
+ * Copyright (c) 2005, Herve Drolon, FreeImage Team
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -58,7 +58,6 @@ opj_image_t* OPJ_CALLCONV opj_image_create(int numcmpts, opj_image_cmptparm_t *c
 			comp->prec = cmptparms[compno].prec;
 			comp->bpp = cmptparms[compno].bpp;
 			comp->sgnd = cmptparms[compno].sgnd;
-			comp->typ = 0;
 			comp->data = (int*) opj_calloc(comp->w * comp->h, sizeof(int));
 			if(!comp->data) {
 				fprintf(stderr,"Unable to allocate memory for image.\n");
