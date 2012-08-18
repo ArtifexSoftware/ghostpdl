32e1d6b480017d0655b8926b69bb3b7617730665
Alex Cherepanov <alex.cherepanov@artifex.com>
2012-08-17 22:32:25 -0400

    Bug 692910: fix quoted arguments in ps2epsi.bat
    Strip the quotes before assigning the values to environment variables.
    Use the original quoted arguments elsewhere.

--- gs/lib/ps2epsi.bat
+++ gs/lib/ps2epsi.bat
@@ -4,30 +4,30 @@ if %1/==/ goto usage
 if %2/==/ goto usage
 
 call "%~dp0gssetgs.bat"
-set infile=%1
-set outfile=%2
+set infile=%~1
+set outfile=%~2
 
 rem First we need to determine the bounding box. ps2epsi.ps below will pick
 rem the result up from %outfile%
-%GSC% -q -dNOPAUSE -dBATCH -P- -dSAFER -dDELAYSAFER -sDEVICE=bbox -sOutputFile=NUL %infile% 2> %outfile%
+%GSC% -q -dNOPAUSE -dBATCH -P- -dSAFER -dDELAYSAFER -sDEVICE=bbox -sOutputFile=NUL %1 2> %2
 
 rem Ghostscript uses %outfile% to define the output file
-%GSC% -q -dNOPAUSE -P- -dSAFER -dDELAYSAFER -sDEVICE=bit -sOutputFile=NUL ps2epsi.ps < %infile%
+%GSC% -q -dNOPAUSE -P- -dSAFER -dDELAYSAFER -sDEVICE=bit -sOutputFile=NUL ps2epsi.ps < %1
 
 rem We bracket the actual file with a few commands to help encapsulation
-echo %%%%Page: 1 1 >> %outfile%
-echo %%%%BeginDocument: %outfile% >> %outfile%
-echo /InitDictCount countdictstack def gsave save mark newpath >> %outfile%
-echo userdict /setpagedevice /pop load put >> %outfile%
+echo %%%%Page: 1 1 >> %2
+echo %%%%BeginDocument: %2 >> %2
+echo /InitDictCount countdictstack def gsave save mark newpath >> %2
+echo userdict /setpagedevice /pop load put >> %2
 
 rem Append the original onto the preview header
 rem cat.ps uses the %infile% and %outfile% environment variables for the filenames
 %GSC% -q -dNOPAUSE -dBATCH -P- -dSAFER -dDELAYSAFER -sDEVICE=bit -sOutputFile=NUL cat.ps
 
 
-echo %%%%EndDocument >> %outfile%
-echo countdictstack InitDictCount sub { end } repeat >> %outfile%
-echo cleartomark restore grestore >> %outfile%
+echo %%%%EndDocument >> %2
+echo countdictstack InitDictCount sub { end } repeat >> %2
+echo cleartomark restore grestore >> %2
 
 goto end
 
