2346369bb7536062946d409f940e865791afc34a
Robin Watts <robin.watts@artifex.com>
2012-09-21 19:07:48 +0100

    Fix typo in previous commit; return NULL in void function.
    MSVC and gcc both accept it with a warning, hence I didn't spot it.
    Sorry.

--- gs/base/gslibctx.c
+++ gs/base/gslibctx.c
@@ -151,7 +151,7 @@ void *gs_lib_ctx_get_cms_context( const gs_memory_t *mem )
 void gs_lib_ctx_set_cms_context( const gs_memory_t *mem, void *cms_context )
 {
     if (mem == NULL)
-        return NULL;
+        return;
     mem->gs_lib_ctx->cms_context = cms_context;
 }
 
