389fbbbc82693094357d95276a4362d701e380b1
Michael Vrhel <michael.vrhel@artifex.com>
2013-02-12 09:08:03 -0800

    Update header in screen generation code.

--- gs/toolbin/halftone/gen_ordered/gen_ordered.c
+++ gs/toolbin/halftone/gen_ordered/gen_ordered.c
@@ -1,6 +1,21 @@
 /**
- * Ordered Dither Screen Creation Tool. Copyright 2010-2012 Artifex Software, Inc.
+ * Ordered Dither Screen Creation Tool.
  **/
+
+/* Copyright (C) 2001-2013 Artifex Software, Inc.
+   All Rights Reserved.
+
+   This software is provided AS-IS with no warranty, either express or
+   implied.
+
+   This software is distributed under license and may not be copied,
+   modified or distributed except as expressly authorized under the terms
+   of the license contained in the file LICENSE in this distribution.
+
+   Refer to licensing information at http://www.artifex.com or contact
+   Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134, San Rafael,
+   CA  94903, U.S.A., +1(415)492-9861, for further information.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
