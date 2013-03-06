ArtifexSoftware/ghostpdl

commit 70ba7029bc563bab7a24bff27decad5feceba4f8

Fix indeterminate page background with plank and comparefiles/104-01.ps
This file uses the PS erasepage operator to paint the background to
a non-white value. The mem_planar_fill_rectangle_hl_color used the
'devn' colors without checking the type. With the plank device these
colors were 'pure' or 'ht_binary' so the devn values were bogus.
If the color type was binary, an address was used as color values.
A pure color was OK as long as the number of planes did not exceed
the size of the gx_color_index and the bytes in the color index
corresponded to the planes.

Also add warning (DEBUG build) if mem_planar_strip_tile_rect_devn
ever uses non-devn colors.

Progressions with 104-01.ps, 12-05.PS, 12-07B.PS and 29-07A.PS

gs/base/gdevmpla.c
@@ -19,6 +19,7 @@
 #include "gserrors.h"
 #include "gsbitops.h"
 #include "gxdevice.h"
+#include "gxdcolor.h"		/* for gx_fill_rectangle_device_rop */
 #include "gxdevmem.h"           /* semi-public definitions */
 #include "gxgetbit.h"
 #include "gdevmem.h"            /* private definitions */
@@ -256,6 +257,10 @@ mem_planar_fill_rectangle_hl_color(gx_device *dev, const gs_fixed_rect *rect,
     int w = fixed2int(rect->q.x) - x;
     int h = fixed2int(rect->q.y) - y;
 
+    /* We can only handle devn cases, so use the default if not */
+    if (pdcolor->type != gx_dc_type_devn) {
+        return gx_fill_rectangle_device_rop( x, y, w, h, pdcolor, dev, lop_default);
+    }
     MEM_SAVE_PARAMS(mdev, save);
     for (pi = 0; pi < mdev->num_planes; ++pi) {
         int plane_depth = mdev->planes[pi].depth;
@@ -954,16 +959,21 @@ mem_planar_strip_tile_rect_devn(gx_device * dev, const gx_strip_bitmap * tiles,
             gdev_mem_device_for_bits(plane_depth);
         gx_color_index c1, c0;
 
-        if (pdcolor0->type == gx_dc_type_pure) {
-            c0 = gx_no_color_index;
-        } else {
+        if (pdcolor0->type == gx_dc_type_devn) {
             c0 = (pdcolor0->colors.devn.values[pi]) >> shift & mask;
-        }
-        if (pdcolor1->type == gx_dc_type_pure) {
-            c1 = gx_no_color_index;
         } else {
-            c1 = (pdcolor1->colors.devn.values[pi]) >> shift & mask;
+            c0 = gx_no_color_index;
         }
+        if (pdcolor1->type == gx_dc_type_devn) {
+            c1 = (pdcolor1->colors.devn.values[pi]) >> shift & mask;
+        } else {
+            c1 = gx_no_color_index;
+        }
+#ifdef DEBUG
+        if (c0 == gx_no_color_index && c1 == gx_no_color_index) {
+            dprintf("mem_planar_strip_tile_rect_dev called with two non-devn colors\n");
+        }
+#endif
         MEM_SET_PARAMS(mdev, plane_depth);
         if (c0 == c1)
             dev_proc(mdproto, fill_rectangle)(dev, x, y, w, h, c0);

gs/base/lib.mak
@@ -1093,7 +1093,7 @@ $(GLOBJ)gdevmx.$(OBJ) : $(GLSRC)gdevmx.c $(AK) $(gx_h) $(memory__h)\
 	$(GLCC) $(GLO_)gdevmx.$(OBJ) $(C_) $(GLSRC)gdevmx.c
 	
 $(GLOBJ)gdevmpla.$(OBJ) : $(GLSRC)gdevmpla.c $(AK) $(gx_h)\
- $(gserrors_h) $(memory__h) $(gsbitops_h)\
+ $(gserrors_h) $(memory__h) $(gsbitops_h) $(gxdcolor_h)\
  $(gxdevice_h) $(gxdevmem_h) $(gxgetbit_h) $(gdevmem_h) $(gdevmpla_h)\
  $(gxdevsop_h)\
  $(MAKEDIRS)

commit d3d66fddadc345aab1139397e10b1ab200af2c99

Fix infinite recursion (caused segfault) introduced with commit 70ba7029.
The fix to mem_planar_fill_rectangle_hl_color did not account for the
devn_masked color type which can also be handled.

gs/base/gdevmpla.c
@@ -20,6 +20,7 @@
 #include "gsbitops.h"
 #include "gxdevice.h"
 #include "gxdcolor.h"		/* for gx_fill_rectangle_device_rop */
+#include "gxpcolor.h"           /* for gx_dc_devn_masked */
 #include "gxdevmem.h"           /* semi-public definitions */
 #include "gxgetbit.h"
 #include "gdevmem.h"            /* private definitions */
@@ -258,7 +259,8 @@ mem_planar_fill_rectangle_hl_color(gx_device *dev, const gs_fixed_rect *rect,
     int h = fixed2int(rect->q.y) - y;
 
     /* We can only handle devn cases, so use the default if not */
-    if (pdcolor->type != gx_dc_type_devn) {
+    /* We can get called here from gx_dc_devn_masked_fill_rectangle */
+    if (pdcolor->type != gx_dc_type_devn && pdcolor->type != &gx_dc_devn_masked) {
         return gx_fill_rectangle_device_rop( x, y, w, h, pdcolor, dev, lop_default);
     }
     MEM_SAVE_PARAMS(mdev, save);