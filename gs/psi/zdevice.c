f560b73066ee787678a31973bca411bedc6ca0f6
Ken Sharp <ken.sharp@artifex.com>
2013-06-29 09:38:53 +0100

    fix a minor compiler warning

--- gs/psi/zdevice.c
+++ gs/psi/zdevice.c
@@ -325,7 +325,7 @@ zmakewordimagedevice(i_ctx_t *i_ctx_p)
 
 static void invalidate_stack_devices(i_ctx_t *i_ctx_p)
 {
-    os_ptr op = op = osbot;
+    os_ptr op = osbot;
     while (op != ostop) {
         if (r_has_type(op, t_device))
             op->value.pdevice = 0;
