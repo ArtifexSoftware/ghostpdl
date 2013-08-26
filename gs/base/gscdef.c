466d8f67a5b1ada99d05777ba33fab48a7d74bc9
Chris Liddell <chris.liddell@artifex.com>
2013-08-26 14:29:57 +0100

    Change product string for release candidate

--- gs/base/gscdef.c
+++ gs/base/gscdef.c
@@ -45,7 +45,7 @@ const char *const gs_productfamily = GS_PRODUCTFAMILY;
 
 #ifndef GS_PRODUCT
 #  define GS_PRODUCT\
-        GS_PRODUCTFAMILY " GIT PRERELEASE"
+        GS_PRODUCTFAMILY " RELEASE CANDIDATE 1"
 #endif
 const char *const gs_product = GS_PRODUCT;
 
