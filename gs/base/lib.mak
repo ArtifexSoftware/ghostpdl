ArtifexSoftware/ghostpdl

commit b5472ea6bf6925023febdeab12be9dbd83e811f1

update ramfs code

gs/base/lib.mak
@@ -3166,17 +3166,20 @@ MKROMFS_COMMON_DEPS=$(stdpre_h) $(stdint__h) $(gsiorom_h) $(arch_h)\
 	$(gsmemret_h) $(gsmalloc_h) $(gsstype_h) $(gp_h) $(time__h)
 
 # ---------------- Support for %ram% IODevice ----------------- #
-gsioram_h=$(GLSRC)gsioram.h $(GLSRC)ramfs.h
+gsioram_h=$(GLSRC)gsioram.h
 ramfs_=$(GLOBJ)gsioram.$(OBJ) $(GLOBJ)ramfs.$(OBJ)
 $(GLD)ramfs.dev : $(LIB_MAK) $(ECHOGS_XE) $(ramfs_)
 	$(SETMOD) $(GLD)ramfs $(ramfs_)
 	$(ADDMOD) $(GLD)ramfs -iodev ram
 	$(ADDMOD) $(GLD)ramfs -obj $(GLOBJ)ramfs.$(OBJ)
 
-$(GLOBJ)ramfs.$(OBJ) : $(GLSRC)ramfs.c
+$(GLOBJ)ramfs.$(OBJ) : $(GLSRC)ramfs.c $(gp_h) $(gscdefs_h) $(gserrors_h)\
+  $(gsparam_h) $(gsstruct_h) $(gx_h) $(ramfs_h)$(string__h) $(unistd__h)
 	$(GLCC) $(GLO_)ramfs.$(OBJ) $(C_) $(GLSRC)ramfs.c
 
-$(GLOBJ)gsioram.$(OBJ) : $(GLSRC)gsioram.c
+$(GLOBJ)gsioram.$(OBJ) : $(GLSRC)gsioram.c $(gp_h) $(gscdefs_h) $(gserrors_h)\
+  $(gsparam_h) $(gsstruct_h) $(gsutil_h) $(gx_h) $(gxiodev_h) $(ramfs_h)\
+  $(stream_h) $(string__h) $(unistd__h)
 	$(GLCC) $(GLO_)gsioram.$(OBJ) $(C_) $(GLSRC)gsioram.c
 
 

gs/base/gsioram.c
@@ -31,7 +31,6 @@
 #include "gsutil.h"
 #include "stream.h"
 #include "ramfs.h"
-#include <malloc.h>
 
 /* Function prototypes */
 static iodev_proc_init(iodev_ram_init);
@@ -43,7 +42,6 @@ static iodev_proc_enumerate_files(ram_enumerate_init);
 static iodev_proc_enumerate_next(ram_enumerate_next);
 static iodev_proc_enumerate_close(ram_enumerate_close);
 static iodev_proc_get_params(ram_get_params);
-static iodev_proc_put_params(ram_put_params);
 static void ram_finalize(const gs_memory_t *memory, void * vptr);
 
 const gx_io_device gs_iodev_ram = {
@@ -130,18 +128,24 @@ ram_open_file(gx_io_device * iodev, const char *fname, uint len,
     char * namestr = NULL;
 
     /* Is there a more efficient way to do this? */
-    namestr = malloc(len+1);
-    if(!namestr) return_error(gs_error_VMerror);
+    namestr = (char *)gs_alloc_bytes(mem, len + 1, "temporary filename string");
+    if(!namestr)
+        return_error(gs_error_VMerror);
     strncpy(namestr,fname,len);
     namestr[len] = 0;
 
-    if (!iodev) /*iodev = iodev_default;*/
+    if (!iodev) {/*iodev = iodev_default;*/
+        gs_free_object(mem, namestr, "free temporary filename string");
         return gs_note_error(gs_error_invalidaccess);
+    }
     code = file_prepare_stream(fname, len, file_access, DEFAULT_BUFFER_SIZE,
     ps, fmode, mem
     );
     if (code < 0) goto error;
-    if (fname == 0) return 0;
+    if (fname == 0) {
+        gs_free_object(mem, namestr, "free temporary filename string");
+        return 0;
+    }
 
     if (fmode[0] != 'r' || fmode[1] == '+') openmode |= RAMFS_WRITE;
     if (fmode[0] != 'r') openmode |= RAMFS_CREATE;
@@ -168,7 +172,7 @@ ram_open_file(gx_io_device * iodev, const char *fname, uint len,
     (*ps)->save_close = (*ps)->procs.close;
     (*ps)->procs.close = file_close_file;
  error:
-    free(namestr);
+    gs_free_object(mem, namestr, "free temporary filename string");
     /* XXX free stream stuff? */
     return code;
 }
@@ -424,7 +428,6 @@ ram_status(gx_io_device * iodev, const char *fname, struct stat *pstat)
 {
     ramhandle * f;
     ramfs* fs = GETRAMFS(iodev->state);
-    int blocksize = ramfs_blocksize(fs);
 
     f = ramfs_open(((ramfs_state *)iodev->state)->memory, fs,fname,RAMFS_READ);
     if(!f) return_error(ramfs_errno_to_code(ramfs_error(fs)));
@@ -483,7 +486,7 @@ ram_enumerate_next(file_enum *pfen, char *ptr, uint maxlen)
     gsram_enum *penum = (gsram_enum *)pfen;
 
     char * filename;
-    while (filename = ramfs_enum_next(penum->e)) {
+    while ((filename = ramfs_enum_next(penum->e))) {
     if (string_match((byte *)filename, strlen(filename),
         (byte *)penum->pattern,
         strlen(penum->pattern), 0)) {

gs/base/ramfs.c
@@ -7,7 +7,6 @@
 
 #include "unistd_.h"
 #include "string_.h"
-#include "malloc_.h"
 #include "gx.h"
 #include "gserrors.h"
 #include "gp.h"
@@ -24,7 +23,7 @@ typedef struct _ramfs {
     gs_memory_t *memory;
     int blocksfree;
     int last_error;
-};
+}__ramfs;
 
 gs_private_st_ptrs3(st_ramfs, struct _ramfs, "gsram_ramfs",
     _ramfs_enum_ptrs, _ramfs_reloc_ptrs, files, active_enums, memory);
@@ -106,11 +105,11 @@ void ramfs_destroy(gs_memory_t *mem, ramfs * fs)
     ent = fs->files;
     while(ent) {
         ramdirent* prev;
-        free(ent->filename);
+        gs_free_object(mem, ent->filename, "ramfs_destroy, filename");
         unlink_node(ent->inode);
         prev = ent;
         ent = ent->next;
-        free(prev);
+        gs_free_object(mem, prev, "ramfs_destroy, entry");
     }
     gs_free_object(mem, fs, "ramfs_destroy");
 }
@@ -382,7 +381,7 @@ int ramfile_write(ramhandle * handle,const void * buf,int len)
     int left;
     char *t = (char *)buf;
 
-    if(!handle->mode & RAMFS_WRITE) {
+    if((!handle->mode) & RAMFS_WRITE) {
         handle->last_error = RAMFS_NOACCESS;
         return -1;
     }

commit d97571b56d7c740e7d6aeee064313b96048a5667

Fixed typo in lib.mak introduced in b5472ea6bf

gs/base/lib.mak
@@ -3174,7 +3174,7 @@ $(GLD)ramfs.dev : $(LIB_MAK) $(ECHOGS_XE) $(ramfs_)
 	$(ADDMOD) $(GLD)ramfs -obj $(GLOBJ)ramfs.$(OBJ)
 
 $(GLOBJ)ramfs.$(OBJ) : $(GLSRC)ramfs.c $(gp_h) $(gscdefs_h) $(gserrors_h)\
-  $(gsparam_h) $(gsstruct_h) $(gx_h) $(ramfs_h)$(string__h) $(unistd__h)
+  $(gsparam_h) $(gsstruct_h) $(gx_h) $(ramfs_h) $(string__h) $(unistd__h)
 	$(GLCC) $(GLO_)ramfs.$(OBJ) $(C_) $(GLSRC)ramfs.c
 
 $(GLOBJ)gsioram.$(OBJ) : $(GLSRC)gsioram.c $(gp_h) $(gscdefs_h) $(gserrors_h)\