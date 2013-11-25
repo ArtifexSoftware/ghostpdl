b81962611a292e1b2c5306f3d5cdfea832715169
Robin Watts <robin.watts@artifex.com>
2013-11-25 13:24:03 +0000

    Bug 694601: Fix big endian crash in mem_mono_copy_mono.
    While the crash is reported on a big endian device, the problem actually
    potentially occurs on little endian systems too. As it happens,
    x86, amd64, and many ARM configurations will read happily from unaligned
    addresses. Some ARMs and many other processors will not, however, so we
    apply a fix that will work properly everywhere.
    
    When calling device level functions, we should ensure that the 'raster'
    given is always a multiple of align_bitmap_mod bits.
    
    Thanks to John Wehle for the bug report and suggested fix.

--- gs/base/gsiparam.h
+++ gs/base/gsiparam.h
@@ -299,8 +299,21 @@ void gs_image_t_init_mask_adjust(gs_image_t * pim, bool write_1s,
  * 16 though, due to increased time in image_render_mono_ht in the loop
  * that copies data from scanlines to scancolumns. It seems that writing to
  * the buffer in positions [0] [16] [32] [48] etc is faster than writing in
- * positions [0] [32] [64] [96] etc. For now we leave LAND_BITS set to 16. */
-#define LAND_BITS 16
+ * positions [0] [32] [64] [96] etc. We would therefore like to leave
+ * LAND_BITS set to 16.
+ *
+ * Unfortunately, we call through the device interface to copy_mono with the
+ * results of these buffers, and various copy_mono implementations assume
+ * that the raster given is a multiple of align_bitmap_mod bits. In order to
+ * ensure safely, we therefore set LAND_BITS to max(16, align_bitmap_mod). We
+ * are guaranteed that align_bitmap_mod is a multiple of 16.
+ */
+#define LAND_BITS_MIN 16
+#if LAND_BITS_MIN < align_bitmap_mod
+#define LAND_BITS align_bitmap_mod
+#else
+#define LAND_BITS LAND_BITS_MIN
+#endif
 
 /* Used for bookkeeping ht buffer information in landscape mode */
 typedef struct ht_landscape_info_s {
