63a5201a2b5db40e11dd45cf991a7e5a84d7ce80
Ken Sharp <ken.sharp@artifex.com>
2014-05-02 09:51:40 +0100

    txtwrite - clear file handle when file is closed
    When we close the output file, set the handle to 0 so that we don't later
    attempt to close it again. Apparently closing a closed file causes a
    problem on Linux.
    
    No differences expected.

--- gs/devices/vector/gdevtxtw.c
+++ gs/devices/vector/gdevtxtw.c
@@ -284,8 +284,10 @@ txtwrite_close_device(gx_device * dev)
     int code = 0;
     gx_device_txtwrite_t *const tdev = (gx_device_txtwrite_t *) dev;
 
-    if (tdev->file)
+    if (tdev->file) {
         code = gx_device_close_output_file(dev, tdev->fname, tdev->file);
+        tdev->file = 0;
+    }
 
 #ifdef TRACE_TXTWRITE
     fclose(tdev->DebugFile);
