cc5257faa69ed02333ac63bd1f3c5e360931407a
Marcos H. Woehrmann <marcos.woehrmann@artifex.com>
2014-06-03 11:18:13 -0700

    Changed copyright date to 2014.

--- gs/base/gscdef.c
+++ gs/base/gscdef.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2013 Artifex Software, Inc.
+/* Copyright (C) 2001-2014 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
@@ -33,7 +33,7 @@ const long gs_buildtime = GS_BUILDTIME;
 
 #ifndef GS_COPYRIGHT
 #  define GS_COPYRIGHT\
-        "Copyright (C) 2013 Artifex Software, Inc.  All rights reserved."
+        "Copyright (C) 2014 Artifex Software, Inc.  All rights reserved."
 #endif
 const char *const gs_copyright = GS_COPYRIGHT;
 
