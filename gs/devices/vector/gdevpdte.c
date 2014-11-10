c98c38c8b9912fc3510ef2e1e53c3ff1837b0519
Ken Sharp <ken.sharp@artifex.com>
2014-11-10 14:31:44 +0000

    eps2write - don't clamp text BBox to the text enumerator origin
    Bug #695671 "eps2write gives incorrect bounding box with rotated text"
    
    The bug here is that, having gone to some effort to retrieve the bounding
    box of the text, the code then clamps the origin of the text box to the
    origin of the text enumerator.
    
    This is clearly incorrect for rotated text, and I'm doubtful it makes sense
    even for upright text. I'm somewhat puzzled because it looks quite deliberate
    but I don't see why, and my testing shows no differences if I don't do this.
    
    So I'm removing this for now, this may need some more attention in the future
    but then that is still generally true of the eps2write device.
    
    No differences expected, the eps2write device is not cluster tested.

--- gs/devices/vector/gdevpdte.c
+++ gs/devices/vector/gdevpdte.c
@@ -716,8 +716,16 @@ finish:
             if (glyphs_bbox.q.y < text_bbox.q.y)
                 text_bbox.q.y = glyphs_bbox.q.y;
         }
+        /* removed this section for bug #695671, where the rotated text
+         * doesn't contribute the 'height' of the text to the x dimension
+         * of the bounding box if this code is present. I can't see why
+         * this clamping was done, if it turns out to be required then
+         * we will need to revisit this and bug #695671.
+         */
+#if 0
         text_bbox.p.x = fixed2float(penum->origin.x);
         text_bbox.q.x = text_bbox.p.x + width_pt.x;
+#endif
         x0 = float2fixed(text_bbox.p.x);
         y0 = float2fixed(text_bbox.p.y);
         bx2 = float2fixed(text_bbox.q.x) - x0;
