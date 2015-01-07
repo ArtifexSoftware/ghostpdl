ArtifexSoftware/ghostpdl

commit 188c7b05a5c232ba43e6b3172023498f7aa72c70

Fix segfault with  ppmraw -r300 from fts_06_0626.pdf.xpse when allocation fails
The gxps had numerous places where allocation failures were ignored,
writing to RAM at offsets from 0 (NULL). This no longer fails, but the
fixes are not yet complete. Running a memento build with the parameters:
   MEMENTO_SQUEEZEAT=1 gxps -o /dev/null -sDEVICE=ppmraw in.xps

xps/xpszip.c
@@ -48,7 +48,11 @@ static inline int getlong(FILE *file)
 static void *
 xps_zip_alloc_items(xps_context_t *ctx, int items, int size)
 {
-    return xps_alloc(ctx, items * size);
+    void *item = xps_alloc(ctx, items * size);
+    if (!item) {
+        gs_throw(gs_error_VMerror, "out of memory: item.\n");
+        return NULL;
+    }
 }
 
 static void
@@ -126,6 +130,9 @@ xps_read_zip_entry(xps_context_t *ctx, xps_entry_t *ent, unsigned char *outbuf)
     else if (method == 8)
     {
         inbuf = xps_alloc(ctx, ent->csize);
+        if (!inbuf) {
+            return gs_throw(gs_error_VMerror, "out of memory.\n");
+        }
 
         fread(inbuf, 1, ent->csize, ctx->file);
 
@@ -196,7 +203,7 @@ xps_read_zip_dir(xps_context_t *ctx, int start_offset)
     ctx->zip_count = count;
     ctx->zip_table = xps_alloc(ctx, sizeof(xps_entry_t) * count);
     if (!ctx->zip_table)
-        return gs_throw(-1, "cannot allocate zip entry table");
+        return gs_throw(gs_error_VMerror, "cannot allocate zip entry table");
 
     memset(ctx->zip_table, 0, sizeof(xps_entry_t) * count);
 
@@ -227,7 +234,7 @@ xps_read_zip_dir(xps_context_t *ctx, int start_offset)
 
         ctx->zip_table[i].name = xps_alloc(ctx, namesize + 1);
         if (!ctx->zip_table[i].name)
-            return gs_throw(-1, "cannot allocate zip entry name");
+            return gs_throw(gs_error_VMerror, "cannot allocate zip entry name");
 
         fread(ctx->zip_table[i].name, 1, namesize, ctx->file);
         ctx->zip_table[i].name[namesize] = 0;

xps/xpsdoc.c
@@ -26,9 +26,18 @@ xps_new_part(xps_context_t *ctx, char *name, int size)
     xps_part_t *part;
 
     part = xps_alloc(ctx, sizeof(xps_part_t));
+    if (!part) {
+        gs_throw(gs_error_VMerror, "out of memory: xps_new_part\n");
+        return NULL;
+    }
     part->name = xps_strdup(ctx, name);
     part->size = size;
     part->data = xps_alloc(ctx, size);
+    if (!part->data) {
+        xps_free(ctx, part);
+        gs_throw(gs_error_VMerror, "out of memory: xps_new_part\n");
+        return NULL;
+    }
 
     return part;
 }
@@ -81,6 +90,10 @@ xps_add_fixed_document(xps_context_t *ctx, char *name)
     if_debug1m('|', ctx->memory, "doc: adding fixdoc %s\n", name);
 
     fixdoc = xps_alloc(ctx, sizeof(xps_document_t));
+    if (!fixdoc) {
+        gs_throw(gs_error_VMerror, "out of memory: xps_add_fixed_document\n");
+        return;
+    }
     fixdoc->name = xps_strdup(ctx, name);
     fixdoc->next = NULL;
 
@@ -124,6 +137,10 @@ xps_add_fixed_page(xps_context_t *ctx, char *name, int width, int height)
     if_debug1m('|', ctx->memory, "doc: adding page %s\n", name);
 
     page = xps_alloc(ctx, sizeof(xps_page_t));
+    if (!page) {
+        gs_throw(gs_error_VMerror, "out of memory: xps_add_fixed_page\n");
+        return;
+    }
     page->name = xps_strdup(ctx, name);
     page->width = width;
     page->height = height;

xps/xpsfont.c
@@ -53,7 +53,7 @@ xps_new_font(xps_context_t *ctx, byte *buf, int buflen, int index)
     font = xps_alloc(ctx, sizeof(xps_font_t));
     if (!font)
     {
-        gs_throw(-1, "out of memory");
+        gs_throw(gs_error_VMerror, "out of memory");
         return NULL;
     }
 

xps/xpsgradient.c
@@ -259,12 +259,20 @@ xps_create_gradient_stop_function(xps_context_t *ctx, struct stop *stops, int co
     k = count - 1; /* number of intervals / functions */
 
     domain = xps_alloc(ctx, 2 * sizeof(float));
+    if (!domain) {
+        gs_throw(gs_error_VMerror, "out of memory: domain\n");
+        return NULL;
+    }
     domain[0] = 0.0;
     domain[1] = 1.0;
     sparams.m = 1;
     sparams.Domain = domain;
 
     range = xps_alloc(ctx, 6 * sizeof(float));
+    if (!range) {
+        gs_throw(gs_error_VMerror, "out of memory: range\n");
+        return NULL;
+    }
     range[0] = 0.0;
     range[1] = 1.0;
     range[2] = 0.0;
@@ -274,8 +282,20 @@ xps_create_gradient_stop_function(xps_context_t *ctx, struct stop *stops, int co
     sparams.Range = range;
 
     functions = xps_alloc(ctx, k * sizeof(void*));
+    if (!functions) {
+        gs_throw(gs_error_VMerror, "out of memory: functions.\n");
+        return NULL;
+    }
     bounds = xps_alloc(ctx, (k - 1) * sizeof(float));
+    if (!bounds) {
+        gs_throw(gs_error_VMerror, "out of memory: bounds.\n");
+        return NULL;
+    }
     encode = xps_alloc(ctx, (k * 2) * sizeof(float));
+    if (!encode) {
+        gs_throw(gs_error_VMerror, "out of memory: encode.\n");
+        return NULL;
+    }
 
     sparams.k = k;
     sparams.Functions = functions;
@@ -296,12 +316,20 @@ xps_create_gradient_stop_function(xps_context_t *ctx, struct stop *stops, int co
     for (i = 0; i < k; i++)
     {
         domain = xps_alloc(ctx, 2 * sizeof(float));
+        if (!domain) {
+            gs_throw(gs_error_VMerror, "out of memory: domain.\n");
+            return NULL;
+    }
         domain[0] = 0.0;
         domain[1] = 1.0;
         lparams.m = 1;
         lparams.Domain = domain;
 
         range = xps_alloc(ctx, 6 * sizeof(float));
+        if (!range) {
+            gs_throw(gs_error_VMerror, "out of memory: range.\n");
+            return NULL;
+        }
         range[0] = 0.0;
         range[1] = 1.0;
         range[2] = 0.0;
@@ -311,9 +339,17 @@ xps_create_gradient_stop_function(xps_context_t *ctx, struct stop *stops, int co
         lparams.Range = range;
 
         c0 = xps_alloc(ctx, 3 * sizeof(float));
+        if (!c0) {
+            gs_throw(gs_error_VMerror, "out of memory: c0.\n");
+            return NULL;
+        }
         lparams.C0 = c0;
 
         c1 = xps_alloc(ctx, 3 * sizeof(float));
+        if (!c0) {
+            gs_throw(gs_error_VMerror, "out of memory: c1.\n");
+            return NULL;
+        }
         lparams.C1 = c1;
 
         if (opacity_only)

xps/xpshash.c
@@ -70,7 +70,7 @@ xps_hash_new(xps_context_t *ctx)
     table = xps_alloc(ctx, sizeof(xps_hash_table_t));
     if (!table)
     {
-        gs_throw(-1, "out of memory: hash table struct");
+        gs_throw(gs_error_VMerror, "out of memory: hash table struct");
         return NULL;
     }
 
@@ -81,7 +81,7 @@ xps_hash_new(xps_context_t *ctx)
     if (!table->entries)
     {
         xps_free(ctx, table);
-        gs_throw(-1, "out of memory: hash table entries array");
+        gs_throw(gs_error_VMerror, "out of memory: hash table entries array");
         return NULL;
     }
 
@@ -111,7 +111,7 @@ xps_hash_double(xps_context_t *ctx, xps_hash_table_t *table)
     old_entries = table->entries;
     new_entries = xps_alloc(ctx, sizeof(xps_hash_entry_t) * new_size);
     if (!new_entries)
-        return gs_throw(-1, "out of memory: hash table entries array");
+        return gs_throw(gs_error_VMerror, "out of memory: hash table entries array");
 
     table->size = new_size;
     table->entries = new_entries;

xps/xpsimage.c
@@ -35,6 +35,10 @@ xps_isolate_alpha_channel_8(xps_context_t *ctx, xps_image_t *image)
     byte *sp, *dp, *ap;
 
     image->alpha = xps_alloc(ctx, image->width * image->height);
+    if (!image->alpha) {
+        gs_throw(gs_error_VMerror, "out of memory: image->alpha.\n");
+        return;
+    }
 
     for (y = 0; y < image->height; y++)
     {
@@ -62,6 +66,10 @@ xps_isolate_alpha_channel_16(xps_context_t *ctx, xps_image_t *image)
     unsigned short *sp, *dp, *ap;
 
     image->alpha = xps_alloc(ctx, image->width * image->height * 2);
+    if (!image->alpha) {
+        gs_throw(gs_error_VMerror, "out of memory: image->alpha.\n");
+        return;
+    }
 
     for (y = 0; y < image->height; y++)
     {
@@ -250,7 +258,7 @@ xps_paint_image_brush_imp(xps_context_t *ctx, xps_image_t *image, int alpha)
 
     penum = gs_image_enum_alloc(ctx->memory, "xps_parse_image_brush (gs_image_enum_alloc)");
     if (!penum)
-        return gs_throw(-1, "gs_enum_allocate failed");
+        return gs_throw(gs_error_VMerror, "gs_enum_allocate failed");
 
     if ((code = gs_image_init(penum, &gsimage, false, ctx->pgs)) < 0)
         return gs_throw(code, "gs_image_init failed");

xps/xpsjpeg.c
@@ -129,7 +129,7 @@ xps_decode_jpeg(xps_context_t *ctx, byte *rbuf, int rlen, xps_image_t *image)
     wlen = image->stride * image->height;
     wbuf = xps_alloc(ctx, wlen);
     if (!wbuf)
-        return gs_throw1(-1, "out of memory allocating samples: %d", wlen);
+        return gs_throw1(gs_error_VMerror, "out of memory allocating samples: %d", wlen);
 
     image->samples = wbuf;
 

xps/xpsjxr.c
@@ -98,6 +98,10 @@ xps_decode_jpegxr_block(jxr_image_t image, int mx, int my, int *data)
         output->bits = 8;
         output->stride = output->width * output->comps;
         output->samples = xps_alloc(ctx, output->stride * output->height);
+        if (!output->samples) {
+            gs_throw(gs_error_VMerror, "out of memory: output->samples.\n");
+            return;
+        }
 
         switch (output->comps)
         {
@@ -142,6 +146,10 @@ xps_decode_jpegxr_alpha_block(jxr_image_t image, int mx, int my, int *data)
     if (!output->alpha)
     {
         output->alpha = xps_alloc(ctx, output->width * output->height);
+        if (!output->alpha) {
+            gs_throw(gs_error_VMerror, "out of memory: output->alpha.\n");
+            return;
+        }
     }
 
     depth = jxr_get_OUTPUT_BITDEPTH(image);

commit d78594c1982682a06c9fb8fc7f2ea746938a93cc

Fix problem in xps_zip_alloc_items caused by previous commit: 188c7b0
When the allocation succeeded, nothing was returned.

xps/xpszip.c
@@ -53,6 +53,7 @@ xps_zip_alloc_items(xps_context_t *ctx, int items, int size)
         gs_throw(gs_error_VMerror, "out of memory: item.\n");
         return NULL;
     }
+    return item;
 }
 
 static void