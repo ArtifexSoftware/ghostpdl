1552cdbaabfdda8753fa78cbb660b21d29e436d4
Ken Sharp <ken.sharp@artifex.com>
2015-01-23 13:30:58 +0000

    check for subclassed device after gdev_prn_open for spotcmyk device

--- gs/base/gdevdevn.c
+++ gs/base/gdevdevn.c
@@ -1856,6 +1856,9 @@ spotcmyk_prn_open(gx_device * pdev)
 {
     int code = gdev_prn_open(pdev);
 
+    if (pdev->child)
+        pdev = pdev->child;
+
     set_linear_color_bits_mask_shift(pdev);
     pdev->color_info.separable_and_linear = GX_CINFO_SEP_LIN;
     return code;
