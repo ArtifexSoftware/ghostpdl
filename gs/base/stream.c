edf3461901805376bdb1b9aa995df359531c7539
Chris Liddell <chris.liddell@artifex.com>
2015-03-16 22:18:41 +0000

    Fix a write after free memory issue.
    No cluster differences

--- gs/base/stream.c
+++ gs/base/stream.c
@@ -433,8 +433,9 @@ sclose(register stream * s)
         if (release != 0)
             (*release) (st);
         if (st != (stream_state *) s && st->memory != 0) {
-            gs_free_object(st->memory, st, "s_std_close");
-            st->memory = 0;
+            gs_memory_t *mem = st->memory;
+            st->memory = NULL;
+            gs_free_object(mem, st, "s_std_close");
         }
         s->state = (stream_state *) s;
     }
