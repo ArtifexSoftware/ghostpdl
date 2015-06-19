75c8ee6f7e8e3d87033a5d1055d66237ed28b387
Chris Liddell <chris.liddell@artifex.com>
2015-06-19 11:25:59 +0100

    Coverity: don't ignore error return.
    No cluster differences.

--- gs/base/fapi_ft.c
+++ gs/base/fapi_ft.c
@@ -1220,7 +1220,12 @@ gs_fapi_ft_get_scaled_font(gs_fapi_server * a_server, gs_fapi_font * a_font,
             ft_error =
                 FT_Open_Face(s->freetype_library, &open_args, a_font->subfont,
                              &ft_face);
-        }
+            if (ft_error) {
+                delete_inc_int (a_server, ft_inc_int);
+                FF_free(s->ftmemory, own_font_data);
+                return ft_to_gs_error(ft_error);
+            }
+       }
 
         if (ft_face) {
             face =
