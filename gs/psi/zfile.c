d64555d5f1180ee84875c7031d5acc3596397c24
Chris Liddell <chris.liddell@artifex.com>
2015-06-19 10:18:17 +0100

    Bug 696037: resolve issue with named pipes on Windows
    The stat() call does not work for named pipes on Windows.
    
    Move around the order of tests in lib_file_open_search_with_no_combine() so
    it works with names pipes.
    
    The way lib_file_open_search_with_no_combine() works is that we have a list of
    paths to search, and list of paths from which we are allowed to open files.
    
    If a file does not exist in one path from our search list, we continue
    searching.
    
    If, on the other hand, the file does exist, but is not in a path we are
    permitted to use files from, we have to throw an invalidaccess error.
    
    Previously, we'd stat() the file, then consult our allowed paths, then open the
    file - taking appropriate alternative action if any of those stages failed.
    
    Now, we consult the permitted paths first, and if that fails, only then do we
    call stat() to decide whether to throw an error, or continue searching.
    
    This should work correctly with Windows named pipes, as the names are always of
    the form "\\\\.\\pipe\\..." (local) or "\\ServerName\pipe\..." (remote) so in
    both cases the leading character being "\" will identify it as an absolute path
    and those are always allowed.
    
    No cluster differences.

--- gs/psi/zfile.c
+++ gs/psi/zfile.c
@@ -935,18 +935,25 @@ lib_file_open_search_with_no_combine(gs_file_path_ptr  lib_path, const gs_memory
     if (gp_file_name_reduce(fname, flen, buffer, &blen1) != gp_combine_success)
       goto skip;
 
-    if ((*iodev->procs.file_status)(iodev,  buffer, &fstat) >= 0) {
-        if (starting_arg_file || check_file_permissions_aux(i_ctx_p, buffer, blen1) >= 0) {
-            if (iodev_os_open_file(iodev, (const char *)buffer, blen1,
-                           (const char *)fmode, &s, (gs_memory_t *)mem) == 0) {
-                *pclen = blen1;
-                make_stream_file(pfile, s, "r");
-                return 0;
-            }
+    if (starting_arg_file || check_file_permissions_aux(i_ctx_p, buffer, blen1) >= 0) {
+        if (iodev_os_open_file(iodev, (const char *)buffer, blen1,
+                       (const char *)fmode, &s, (gs_memory_t *)mem) == 0) {
+            *pclen = blen1;
+            make_stream_file(pfile, s, "r");
+            return 0;
         }
-        return_error(gs_error_invalidfileaccess);
     }
- skip:;
+    else {
+        /* If we are not allowed to open the file by check_file_permissions_aux()
+         * and if the file exists, throw an error.......
+         * Otherwise, keep searching.
+         */
+        if ((*iodev->procs.file_status)(iodev,  buffer, &fstat) >= 0) {
+            return_error(gs_error_invalidfileaccess);
+        }
+    }
+
+ skip:
     return 1;
 }
 
