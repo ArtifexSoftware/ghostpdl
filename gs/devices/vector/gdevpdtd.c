ArtifexSoftware/ghostpdl

commit f544452718283d4d7122b3c22c1f3991313099ea

Coverity ID #94859 rearrange code to avoid potential NULL pointer dereference

gs/devices/vector/gdevpdtd.c
@@ -267,9 +267,7 @@ int pdf_font_descriptor_free(gx_device_pdf *pdev, pdf_resource_t *pres)
     gs_free_copied_font(copied);
     if (complete && copied != complete) {
         gs_free_copied_font(complete);
-        pbfont->complete = 0;
     }
-    pbfont->copied = 0;
     if (pbfont && pbfont->font_name.size) {
         gs_free_string(pdev->pdf_memory, pbfont->font_name.data, pbfont->font_name.size, "Free BaseFont FontName string");
         pbfont->font_name.data = (byte *)0L;
@@ -278,6 +276,9 @@ int pdf_font_descriptor_free(gx_device_pdf *pdev, pdf_resource_t *pres)
     if (pbfont) {
         gs_free_object(cos_object_memory(pres->object), pbfont, "Free base font from FontDescriptor)");
         pfd->base_font = 0;
+        pbfont->copied = 0;
+        if (complete && copied != complete)
+            pbfont->complete = 0;
     }
     if (pres->object) {
         gs_free_object(cos_object_memory(pres->object), pres->object, "free FontDescriptor object");

commit 7e8cd8ff35d36d66a7f5de50c16bcc0a4352777d

Commit f5444527 inadvertently introduced seg faults, fixed here.

gs/devices/vector/gdevpdtd.c
@@ -264,21 +264,20 @@ int pdf_font_descriptor_free(gx_device_pdf *pdev, pdf_resource_t *pres)
     pdf_base_font_t *pbfont = pfd->base_font;
     gs_font *copied = (gs_font *)pbfont->copied, *complete = (gs_font *)pbfont->complete;
 
-    gs_free_copied_font(copied);
-    if (complete && copied != complete) {
+    if (complete && copied != complete)
         gs_free_copied_font(complete);
-    }
-    if (pbfont && pbfont->font_name.size) {
-        gs_free_string(pdev->pdf_memory, pbfont->font_name.data, pbfont->font_name.size, "Free BaseFont FontName string");
-        pbfont->font_name.data = (byte *)0L;
-        pbfont->font_name.size = 0;
-    }
+
+    if (copied)
+        gs_free_copied_font(copied);
+
     if (pbfont) {
+        if (pbfont->font_name.size) {
+            gs_free_string(pdev->pdf_memory, pbfont->font_name.data, pbfont->font_name.size, "Free BaseFont FontName string");
+            pbfont->font_name.data = (byte *)0L;
+            pbfont->font_name.size = 0;
+        }
         gs_free_object(cos_object_memory(pres->object), pbfont, "Free base font from FontDescriptor)");
         pfd->base_font = 0;
-        pbfont->copied = 0;
-        if (complete && copied != complete)
-            pbfont->complete = 0;
     }
     if (pres->object) {
         gs_free_object(cos_object_memory(pres->object), pres->object, "free FontDescriptor object");