dc5cae259b8f5b7dc4b6105f60b56af72fed235a
Ken Sharp <ken.sharp@artifex.com>
2015-10-26 08:32:32 +0000

    check gp_fseek_64 return value and action it, silences Coverity CID 118348

--- devices/vector/gdevpdfp.c
+++ devices/vector/gdevpdfp.c
@@ -709,7 +709,10 @@ gdev_pdf_put_params_impl(gx_device * dev, const gx_device_pdf * save_dev, gs_par
 #undef MAX_EXTENT
     if (pdev->FirstObjectNumber != save_dev->FirstObjectNumber) {
         if (pdev->xref.file != 0) {
-            gp_fseek_64(pdev->xref.file, 0L, SEEK_SET);
+            if (gp_fseek_64(pdev->xref.file, 0L, SEEK_SET) != 0) {
+                ecode = gs_error_ioerror;
+                goto fail;
+            }
             pdf_initialize_ids(pdev);
         }
     }
