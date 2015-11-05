8e3cc2d061fe56c6978def59e1a3056013045266
Ken Sharp <ken.sharp@artifex.com>
2015-11-05 09:35:24 +0000

    Add array processing
    Tested with NeverEmbed, seems to work. This code does not nested arrays
    as I don't think we need those for distillerparams. In any event the
    easiest way to deal with these is, I thnk simply to expland them
    int o one single larger array.

--- pcl/pl/plparams.c
+++ pcl/pl/plparams.c
@@ -259,13 +259,16 @@ int pjl_dist_process_dict_or_hexstring(gs_memory_t *mem, gs_c_param_list *plist,
 
 int pjl_dist_process_name(gs_memory_t *mem, gs_c_param_list *plist, gs_param_name *key, char **p)
 {
-    char *start = *p;
+    char *start = *p + 1;
     gs_param_string ps;
 
     while (**p != ' ' && **p != 0x00)
-        *p++;
-    if (**p != 0x00)
+        (*p)++;
+
+    if (**p != 0x00) {
         **p = 0x00;
+        (*p)++;
+    }
 
     if (*key == NULL)
         *key = (gs_param_name)start;
@@ -274,6 +277,7 @@ int pjl_dist_process_name(gs_memory_t *mem, gs_c_param_list *plist, gs_param_nam
         ps.size = strlen(start);
         ps.persistent = false;
         param_write_name((gs_param_list *)plist, *key, &ps);
+        *key = NULL;
     }
     return 0;
 }
@@ -284,27 +288,263 @@ int pjl_dist_process_string(gs_memory_t *mem, gs_c_param_list *plist, gs_param_n
     gs_param_string ps;
 
     while (**p != ')' && **p != 0x00)
-        *p++;
+        (*p)++;
 
     if (**p != ')')
         return -1;
 
     **p= 0x00;
+    (*p)++;
+
     ps.data = (const byte *)start;
     ps.size = strlen(start);
     ps.persistent = false;
-    param_write_string((gs_param_list *)plist, *key, &ps);
+    param_write_string((gs_param_list *)plist, key, &ps);
 
     return 0;
+}
+
+int count_tokens(char *p)
+{
+    int tokens = 0;
+
+    while (*p != 0x00){
+        switch (*p) {
+            case ' ':
+                p++;
+                break;
+            case 'f':
+                if (strncmp(p, "false", 5) == 0) {
+                    p += 5;
+                    tokens++;
+                } else {
+                    return -1;
+                }
+                break;
+            case 't':
+                if (strncmp(p, "true", 4) == 0) {
+                    p += 4;
+                    tokens++;
+                } else {
+                    return -1;
+                }
+                break;
+            case '<':
+                if (p[1] == '>') {
+                    int nesting = 0;
+
+                    p+=2;
+                    while (*p != 0x00) {
+                        if (*p == '>' && p[1] == '>') {
+                            if (nesting == 0) {
+                                p++;
+                                break;
+                            }
+                            p++;
+                        } else
+                            if (*p == '<' && p[1] == '<') {
+                                p += 2;
+                                nesting++;
+                            } else
+                                p++;
+                    }
+                    if (*p == 0x00)
+                        return -1;
+                    tokens++;
+                    p++;
+                } else {
+                    while (*p != 0x00 && *p != '>') {
+                        p++;
+                    }
+                    if (*p == 0x00)
+                        return -1;
+                    tokens++;
+                    p++;
+                }
+                break;
+            case '/':
+                while (*p != 0x00 && *p != ' ') {
+                    p++;
+                }
+                tokens++;
+                break;
+            case '(':
+                while (*p != 0x00 && *p != ')') {
+                    p++;
+                }
+                if (*p == 0x00)
+                    return -1;
+                p++;
+                tokens++;
+                break;
+            case '[':
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '.':
+                while (*p != 0x00 && *p != ' ') {
+                    if ((*p < 0x30 || *p > 0x39) && *p != '.')
+                        return -1;
+                    p++;
+                }
+                tokens++;
+                break;
+            default:
+                return -1;
+                break;
+        }
+    }
+    return tokens;
 }
 
 int pjl_dist_process_array(gs_memory_t *mem, gs_c_param_list *plist, gs_param_name key, char **p)
 {
-    return 0;
+    int tokens = 0, nested = 0, index = 0, code;
+    char *start = *p + 1, *p1 = start;
+    gs_param_string *parray;
+    gs_param_string_array *array_list;
+
+    /* for now we won't handle nested arrays */
+    do {
+        while (*p1 != ']' && *p1 != 0x00) {
+            if (*p1 == '[')
+                nested++;
+            *p1++;
+        }
+    }while (nested--);
+
+    if (*p1 == 0x00)
+        return -1;
+
+    *p1 = 0x00;
+    p1 = start;
+
+    tokens = count_tokens(start);
+
+    array_list = (gs_param_string_array *)gs_alloc_bytes(mem, sizeof(gs_param_string_array), "array in distillerparams");
+    if (array_list == NULL)
+        return -1;
+
+    parray = (gs_param_string *)gs_alloc_bytes(mem, sizeof(gs_param_string) * tokens, "param string array in distillerparams");
+    if (parray == NULL){
+        gs_free_object(mem, array_list, "working buffer for distillerparams processing");
+        return -1;
+    }
+
+    while (*p1 != 0x00){
+        switch (*p1) {
+            case ' ':
+                p1++;
+                break;
+            case 'f':
+                if (strncmp(p1, "false", 5) == 0) {
+                    parray[index].data = (const byte *)p1;
+                    p1 += 5;
+                    *p1++ = 0x00;
+                    parray[index].size = 5;
+                    parray[index++].persistent = false;
+                } else {
+                    return -1;
+                }
+                break;
+            case 't':
+                if (strncmp(p1, "true", 4) == 0) {
+                    parray[index].data = (const byte *)p1;
+                    p1 += 4;
+                    *p1++ = 0x00;
+                    parray[index].size = 4;
+                    parray[index++].persistent = false;
+                } else {
+                    return -1;
+                }
+                break;
+            case '<':
+                break;
+            case '/':
+                parray[index].data = (const byte *)p1;
+                while (*p1 != ' ' && *p1 != 0x00)
+                    p1++;
+                if (*p1 == 0x00)
+                    return -1;
+                *p1++ = 0x00;
+                parray[index].size = strlen(parray[index].data);
+                parray[index++].persistent = false;
+                break;
+            case '(':
+                parray[index].data = (const byte *)p1;
+                while (*p1 != ')' && *p1 != 0x00)
+                    p1++;
+                if (*p1 == 0x00)
+                    return -1;
+                *p1++ = 0x00;
+                parray[index].size = strlen(parray[index].data);
+                parray[index++].persistent = false;
+                break;
+            case '[':
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '.':
+                break;
+            default:
+                return -1;
+                break;
+        }
+    }
+    array_list->data = parray;
+    array_list->persistent = 0;
+    array_list->size = tokens;
+
+    code = param_write_string_array((gs_param_list *)plist, key, array_list);
+    gs_free_object(mem, parray, "param string array in distillerparams");
+    gs_free_object(mem, array_list, "working buffer for distillerparams processing");
+
+    *p = p1++;
+
+    return code;
 }
 
 int pjl_dist_process_number(gs_memory_t *mem, gs_c_param_list *plist, gs_param_name key, char **p)
 {
+    char *start = *p;
+    bool integer = true;
+
+    while (**p != ' ' && **p != 0x00) {
+        if (**p == '.')
+            integer = false;
+
+        (*p)++;
+    }
+
+    if (**p != 0x00) {
+        **p = 0x00;
+        (*p)++;
+    }
+
+    if (integer) {
+        double f = atof(start);
+        param_write_float((gs_param_list *)plist, key, (float *)&f);
+    } else {
+        long i = atol(start);
+        param_write_long((gs_param_list *)plist, key, &i);
+    }
+
     return 0;
 }
 
@@ -317,6 +557,8 @@ int pcl_pjl_setdistillerparams(gs_memory_t *mem, gx_device *device, char *distil
 
     plist = gs_c_param_list_alloc(mem, "temp C param list for PJL distillerparams");
     gs_c_param_list_write(plist, mem);
+    gs_param_list_set_persistent_keys((gs_param_list *) plist, false);
+    gs_c_param_list_write_more(plist);
 
     /* Our parsing will alter the string contents, so copy it and perform the parsing on the copy */
     copy = (char *)gs_alloc_bytes(mem, strlen(distillerparams) + 1, "working buffer for distillerparams processing");
@@ -349,12 +591,38 @@ int pcl_pjl_setdistillerparams(gs_memory_t *mem, gx_device *device, char *distil
 
     while (*p != 0x00){
         switch (*p) {
+            case ' ':
+                p++;
+                break;
+            case 'f':
+                if (strncmp(p, "false", 5) == 0) {
+                    bool t = false;
+                    param_write_bool((gs_param_list *)plist, key, &t);
+                    p += 5;
+                    key = NULL;
+                } else {
+                    gs_free_object(mem, copy, "working buffer for distillerparams processing");
+                    return -1;
+                }
+                break;
+            case 't':
+                if (strncmp(p, "true", 4) == 0) {
+                    bool t = true;
+                    param_write_bool((gs_param_list *)plist, key, &t);
+                    p += 4;
+                    key = NULL;
+                } else {
+                    gs_free_object(mem, copy, "working buffer for distillerparams processing");
+                    return -1;
+                }
+                break;
             case '<':
                 if (key == NULL) {
                     gs_free_object(mem, copy, "working buffer for distillerparams processing");
                     return -1;
                 }
                 code = pjl_dist_process_dict_or_hexstring(mem, plist, key, &p);
+                key = NULL;
                 break;
             case '/':
                 code = pjl_dist_process_name(mem, plist, &key, &p);
@@ -365,6 +633,7 @@ int pcl_pjl_setdistillerparams(gs_memory_t *mem, gx_device *device, char *distil
                     return -1;
                 }
                 code = pjl_dist_process_string(mem, plist, key, &p);
+                key = NULL;
                 break;
             case '[':
                 if (key == NULL) {
@@ -372,6 +641,7 @@ int pcl_pjl_setdistillerparams(gs_memory_t *mem, gx_device *device, char *distil
                     return -1;
                 }
                 code = pjl_dist_process_array(mem, plist, key, &p);
+                key = NULL;
                 break;
             case '0':
             case '1':
@@ -389,6 +659,7 @@ int pcl_pjl_setdistillerparams(gs_memory_t *mem, gx_device *device, char *distil
                     return -1;
                 }
                 code = pjl_dist_process_number(mem, plist, key, &p);
+                key = NULL;
                 break;
             default:
                 gs_free_object(mem, copy, "working buffer for distillerparams processing");
@@ -402,6 +673,10 @@ int pcl_pjl_setdistillerparams(gs_memory_t *mem, gx_device *device, char *distil
     }
 
     gs_free_object(mem, copy, "working buffer for distillerparams processing");
+
+    gs_c_param_list_read(plist);
+    code = gs_putdeviceparams(device, (gs_param_list *)plist);
+
     gs_c_param_list_release(plist);
     return 0;
 }
