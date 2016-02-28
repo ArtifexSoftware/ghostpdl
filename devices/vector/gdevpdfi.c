5d15322e32f85f9b4d9fbebd19148d7a95428adf
Ken Sharp <ken.sharp@artifex.com>
2016-02-28 09:02:43 +0000

    pdfwrite - silence Coverity warning
    Change an indent to silence Coverity.....

--- devices/vector/gdevpdfi.c
+++ devices/vector/gdevpdfi.c
@@ -1248,7 +1248,7 @@ static int setup_image_colorspace(gx_device_pdf *pdev, image_union_t *image, con
                 }
                 else
                     *pcs_orig = (gs_color_space *)pcs;
-                    return 1;
+                return 1;
                 break;
 
             case gs_color_space_index_ICC:
