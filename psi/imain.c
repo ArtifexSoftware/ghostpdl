efe7fd03e0ca45aac5ee4610ed9a23b7ea712e0d
Chris Liddell <chris.liddell@artifex.com>
2016-04-25 13:20:35 +0100

    Bug 696730: print meaningful error when device shutdown fails
    When we explicitly close the default device, previously we just printed the
    error code, and a reference to gserrors.h - this finds the error code in the
    Postscript world, and prints the Postcript error string instead.

--- psi/imain.c
+++ psi/imain.c
@@ -904,12 +904,19 @@ gs_main_finit(gs_main_instance * minst, int exit_status, int code)
             code = interp_reclaim(&minst->i_ctx_p, avm_global);
 
             if (code < 0) {
+                ref error_name;
                 if (tempnames)
                     free(tempnames);
 
-                emprintf1(minst->heap,
-                          "ERROR %d reclaiming the memory while the interpreter finalization.\n",
-                          code);
+                if (gs_errorname(i_ctx_p, code, &error_name) >= 0) {
+                    char err_str[32] = {0};
+                    name_string_ref(imemory, &error_name, &error_name);
+                    memcpy(err_str, error_name.value.const_bytes, r_size(&error_name));
+                    emprintf2(imemory, "ERROR: %s (%d) reclaiming the memory while the interpreter finalization.\n", err_str, code);
+                }
+                else {
+                    emprintf1(imemory, "UNKNOWN ERROR %d reclaiming the memory while the interpreter finalization.\n", code);
+                }
                 return gs_error_Fatal;
             }
             i_ctx_p = minst->i_ctx_p; /* interp_reclaim could change it. */
@@ -928,11 +935,18 @@ gs_main_finit(gs_main_instance * minst, int exit_status, int code)
                 /.jobsavelevel get 0 eq {/quit} {/stop} ifelse .systemvar exec",
                 0 , &exit_code, &error_object);
             code = gs_closedevice(pdev);
-            if (code < 0)
-                emprintf2(pdev->memory,
-                          "ERROR %d closing %s device. See gs/psi/ierrors.h for code explanation.\n",
-                          code,
-                          dname);
+            if (code < 0) {
+                ref error_name;
+                if (gs_errorname(i_ctx_p, code, &error_name) >= 0) {
+                    char err_str[32] = {0};
+                    name_string_ref(imemory, &error_name, &error_name);
+                    memcpy(err_str, error_name.value.const_bytes, r_size(&error_name));
+                    emprintf3(imemory, "ERROR: %s (%d) on closing %s device.\n", err_str, code, dname);
+                }
+                else {
+                    emprintf2(imemory, "UNKNOWN ERROR %d closing %s device.\n", code, dname);
+               }
+            }
             rc_decrement(pdev, "gs_main_finit");                /* device might be freed */
             if (exit_status == 0 || exit_status == gs_error_Quit)
                 exit_status = code;
