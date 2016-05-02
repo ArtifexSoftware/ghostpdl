7ba62042a98039203b45b019ffc523778de38513
Ken Sharp <ken.sharp@artifex.com>
2016-05-02 08:26:14 +0100

    Coverity ID 94706 - fix a copy/paste error
    Commit 	1ba0d89544ba6d057867648027c5a6919888c619 was meant to test for
    'pgd' being non-NULL but a silly copy/paste error tested 'pgd->bits.data'
    by mistake.

--- base/gstype1.c
+++ base/gstype1.c
@@ -295,7 +295,7 @@ gs_type1_interpret(gs_type1_state * pcis, const gs_glyph_data_t *pgd,
                            Rewind the data pointer to the beginning of the glyph, re-initialise
                            the hinter, execute a '0' sbw op, and then carry on as if we had
                            actually received one. */
-                        if (pgd->bits.data)
+                        if (pgd)
                             cip = pgd->bits.data;
                         t1_hinter__init(h, pcis->path);
                         code = t1_hinter__sbw(h, fixed_0, fixed_0, fixed_0, fixed_0);
