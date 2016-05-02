08d98e00bc18b47bdf357f2c3f3ba5f47995ce68
Ken Sharp <ken.sharp@artifex.com>
2016-05-02 11:57:22 +0100

    Coverity ID 94487 - use pre-calculated value to avoid shift

--- base/gsfunc0.c
+++ base/gsfunc0.c
@@ -339,7 +339,7 @@ fn_Sd_encode(const gs_function_Sd_t *pfn, int i, double sample)
     if (pfn->params.Range)
         r0 = pfn->params.Range[2 * i], r1 = pfn->params.Range[2 * i + 1];
     else
-        r0 = 0, r1 = (float)((1 << bps) - 1);
+        r0 = 0, r1 = max_samp;
     if (pfn->params.Decode)
         d0 = pfn->params.Decode[2 * i], d1 = pfn->params.Decode[2 * i + 1];
     else
