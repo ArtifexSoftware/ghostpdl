ArtifexSoftware/ghostpdl

commit 9dba57f0f9a53c130ec2771c0ed1d7bd6bbef6ab

pdfwrite - ToUnicode revamp
Bug 695461 " Why does the search results changes after pdf optimzation in ghostscript?"

The existing ToUnicode functions were written against the original
ToUnicode specification, and assume that there will be no more than
2 unsigned shorts returned as the Unicode code point for any given
glyph. Sadly Adobe have revised the ToUnicode CMap making it the same
as a regular CMap, and then extending it still further.

It is now possible for a single glyph to map to a string of up to
512 bytes.

This commit revises the existing C 'decode_glyph's so that instead of
returning a gs_char for the Unicode code point, we return a string of
bytes. If the caller initially says that the string it is passing is
zero bytes, then we do not copy the bytes, we just return the required
size of the string (in bytes).

A return value of 0 from a decode_glyph function means that the glyph
was not in the map and so could not be 'decoded'.

As a consequence of this change, and to further permit more than
2 unsigned shorts for ToUnicode CMaps, the CMap lookup enumerator
now needs to be able to allocate memory, so the 'next_lookup'
methods all now take a gs_memory_t pointer to make ths possible.

The ToUnicode cmap table also has to change. Formerly it was a simple
4 bytes per code, either 255 or 65535 codes array. For simplicity
I've chosen to keep it as a large continuous array, but each entry
is now the number of bytes required to store the longest defined
Unicode value for that font, plus 2 bytes. The 2 bytes give the length
of the reserved space actually used by each Unicode code point. The
bytes are stored immediately following the length (so a 2 byte length
Pascal string if you like). This may possibly cause ToUnicode maps
to use a lot of memory, in the short term we'll live with it because
these only exist with pdfwrite, and that only really is expected
to run on decent sized platforms. May need to do something better in
future.

devices/vector/gdevtxtw.c
@@ -1669,22 +1669,20 @@ txt_shift_text_currentpoint(textw_text_enum_t *penum, gs_point *wpt)
 /* Try to convert glyph names/character codes to Unicode. We first try to see
  * if we have any Unicode information either from a ToUnicode CMap or GlyphNames2Unicode
  * table. If that fails we look at the glyph name to see if it starts 'uni'
- * in which case we assume hte remainder of the name is the Unicode value. If
- * that fails we currently just return the character code. We could go further
- * and see if the glyph name is one we recognise (eg /A etc) and convert that.
- * For CIDFonts we might be able to look at the Regiostry and Ordering and
- * perhaps convert the CID into Unicode frmo that information. These are
- * future enhancements for now.
+ * in which case we assume the remainder of the name is the Unicode value. If
+ * its not a glyph of that form then we search a bunch of tables whcih map standard
+ * glyph names to Unicode code points. If that fails we finally just return the character code.
  */
-static int get_unicode(gs_font *font, gs_glyph glyph, gs_char ch, unsigned short *Buffer)
+static int get_unicode(textw_text_enum_t *penum, gs_font *font, gs_glyph glyph, gs_char ch, unsigned short *Buffer)
 {
-    gs_char unicode;
     int code;
     gs_const_string gnstr;
     unsigned short fallback = ch;
+    ushort *unicode = NULL;
+    int length;
 
-    unicode = font->procs.decode_glyph((gs_font *)font, glyph, ch);
-    if (unicode == GS_NO_CHAR) {
+    length = font->procs.decode_glyph((gs_font *)font, glyph, ch, NULL, 0);
+    if (length == 0) {
         code = font->procs.glyph_name(font, glyph, &gnstr);
         if (code >= 0 && gnstr.size == 7) {
             if (!memcmp(gnstr.data, "uni", 3)) {
@@ -1694,12 +1692,14 @@ static int get_unicode(gs_font *font, gs_glyph glyph, gs_char ch, unsigned short
                 char *d2 = strchr(hexdigits, gnstr.data[5]);
                 char *d3 = strchr(hexdigits, gnstr.data[6]);
 
-                if (d0 != NULL && d1 != NULL && d2 != NULL && d3 != NULL)
-                    unicode = ((d0 - hexdigits) << 12) + ((d1 - hexdigits) << 8) +
-                      ((d2 - hexdigits) << 4 ) +  (d3 - hexdigits);
+                if (d0 != NULL && d1 != NULL && d2 != NULL && d3 != NULL) {
+                    *Buffer++ = ((d0 - hexdigits) << 8) + ((d1 - hexdigits));
+                    *Buffer++ = ((d2 - hexdigits) << 8) + ((d3 - hexdigits));
+                    return 2;
+                }
             }
         }
-        if (unicode == GS_NO_CHAR) {
+        if (length == 0) {
             single_glyph_list_t *sentry = (single_glyph_list_t *)&SingleGlyphList;
             double_glyph_list_t *dentry = (double_glyph_list_t *)&DoubleGlyphList;
             treble_glyph_list_t *tentry = (treble_glyph_list_t *)&TrebleGlyphList;
@@ -1777,13 +1777,15 @@ static int get_unicode(gs_font *font, gs_glyph glyph, gs_char ch, unsigned short
                 qentry++;
             }
         }
-        if (unicode == GS_NO_CHAR) {
-            *Buffer = fallback;
-            return 1;
-        }
+        *Buffer = fallback;
+        return 1;
+    } else {
+        unicode = (ushort *)gs_alloc_bytes(penum->dev->memory, length * sizeof(ushort), "temporary Unicode array");
+        length = font->procs.decode_glyph((gs_font *)font, glyph, ch, unicode, length);
+        memcpy(Buffer, unicode, length * sizeof(short));
+        gs_free(penum->dev->memory, unicode, length, sizeof(ushort), "free temporary unicode buffer");
+        return length * sizeof(short);
     }
-    *Buffer = (unsigned short)unicode;
-    return 1;
 }
 
 /* Routines to enumerate each glyph/character code in turn, find its width
@@ -1862,7 +1864,7 @@ txtwrite_process_cmap_text(gs_text_enum_t *pte)
                 pte->returned.total_width.x += dpt.x;
                 pte->returned.total_width.y += dpt.y;
 
-                penum->TextBufferIndex += get_unicode((gs_font *)pte->orig_font, glyph, chr, &penum->TextBuffer[penum->TextBufferIndex]);
+                penum->TextBufferIndex += get_unicode(penum, (gs_font *)pte->orig_font, glyph, chr, &penum->TextBuffer[penum->TextBufferIndex]);
                 penum->Widths[pte->index - 1] += dpt.x;
                 break;
             case 2:		/* end of string */
@@ -1942,7 +1944,7 @@ txtwrite_process_plain_text(gs_text_enum_t *pte)
         pte->returned.total_width.x += dpt.x;
         pte->returned.total_width.y += dpt.y;
 
-        penum->TextBufferIndex += get_unicode((gs_font *)pte->orig_font, glyph, ch, &penum->TextBuffer[penum->TextBufferIndex]);
+        penum->TextBufferIndex += get_unicode(penum, (gs_font *)pte->orig_font, glyph, ch, &penum->TextBuffer[penum->TextBufferIndex]);
         penum->Widths[pte->index - 1] += dpt.x;
     }
     return 0;

base/gsfcid2.c
@@ -164,8 +164,9 @@ tt_16bit_format4_enum_ranges(const gs_cmap_t *pcmap,
     gs_cmap_ranges_enum_setup(pre, pcmap, &tt_16bit_format4_range_procs);
 }
 static int
-tt_16bit_format4_next_lookup(gs_cmap_lookups_enum_t *penum)
+tt_16bit_format4_next_lookup(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum)
 {
+    penum->entry.value.data = 0L;
     if (penum->index[0] == 0) {
         penum->entry.key_size = 2;
         penum->entry.key_is_range = true;

base/gsfcmap.c
@@ -98,8 +98,9 @@ identity_enum_ranges(const gs_cmap_t *pcmap, gs_cmap_ranges_enum_t *pre)
     gs_cmap_ranges_enum_setup(pre, pcmap, &identity_range_procs);
 }
 static int
-identity_next_lookup(gs_cmap_lookups_enum_t *penum)
+identity_next_lookup(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum)
 {
+    penum->entry.value.data = 0L;
     if (penum->index[0] == 0) {
         const gs_cmap_identity_t *const pcimap =
             (const gs_cmap_identity_t *)penum->cmap;
@@ -120,8 +121,9 @@ identity_next_lookup(gs_cmap_lookups_enum_t *penum)
     return 1;
 }
 static int
-no_next_lookup(gs_cmap_lookups_enum_t *penum)
+no_next_lookup(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum)
 {
+    penum->entry.value.data = 0L;
     return 1;
 }
 static int
@@ -267,9 +269,9 @@ gs_cmap_lookups_enum_init(const gs_cmap_t *pcmap, int which,
     pcmap->procs->enum_lookups(pcmap, which, penum);
 }
 int
-gs_cmap_enum_next_lookup(gs_cmap_lookups_enum_t *penum)
+gs_cmap_enum_next_lookup(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum)
 {
-    return penum->procs->next_lookup(penum);
+    return penum->procs->next_lookup(mem, penum);
 }
 int
 gs_cmap_enum_next_entry(gs_cmap_lookups_enum_t *penum)
@@ -385,7 +387,7 @@ gs_cmap_compute_identity(const gs_cmap_t *pcmap, int font_index_only)
                     (const byte *)"Artifex", 7))
         return false;
     for (gs_cmap_lookups_enum_init(pcmap, which, &lenum);
-         (code = gs_cmap_enum_next_lookup(&lenum)) == 0; ) {
+         (code = gs_cmap_enum_next_lookup(NULL, &lenum)) == 0; ) {
         if (font_index_only >= 0 && lenum.entry.font_index != font_index_only)
             continue;
         if (font_index_only < 0 && lenum.entry.font_index > 0)
@@ -423,13 +425,6 @@ gs_cmap_compute_identity(const gs_cmap_t *pcmap, int font_index_only)
 
 static const int gs_cmap_ToUnicode_code_bytes = 2;
 
-typedef struct gs_cmap_ToUnicode_s {
-    GS_CMAP_COMMON;
-    int num_codes;
-    int key_size;
-    bool is_identity;
-} gs_cmap_ToUnicode_t;
-
 gs_private_st_suffix_add0(st_cmap_ToUnicode, gs_cmap_ToUnicode_t,
     "gs_cmap_ToUnicode_t", cmap_ToUnicode_enum_ptrs, cmap_ToUnicode_reloc_ptrs,
     st_cmap);
@@ -466,19 +461,19 @@ gs_cmap_ToUnicode_enum_ranges(const gs_cmap_t *pcmap, gs_cmap_ranges_enum_t *pre
 }
 
 static int
-gs_cmap_ToUnicode_next_lookup(gs_cmap_lookups_enum_t *penum)
+gs_cmap_ToUnicode_next_lookup(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum)
 {   const gs_cmap_ToUnicode_t *cmap = (gs_cmap_ToUnicode_t *)penum->cmap;
 
     if (penum->index[0]++ > 0)
         return 1;
-    penum->entry.value.data = penum->temp_value;
-    penum->entry.value.size = gs_cmap_ToUnicode_code_bytes;
     penum->index[1] = 0;
     penum->entry.key_is_range = true;
     penum->entry.value_type = CODE_VALUE_CHARS;
     penum->entry.key_size = cmap->key_size;
     penum->entry.value.size = gs_cmap_ToUnicode_code_bytes;
     penum->entry.font_index = 0;
+    penum->entry.value.data = gs_alloc_bytes(mem, cmap->value_size, "working ToUnicode buffer");
+    penum->entry.value.size = cmap->value_size;
     return 0;
 }
 
@@ -491,12 +486,12 @@ gs_cmap_ToUnicode_next_entry(gs_cmap_lookups_enum_t *penum)
     uchar c0, c1, c2;
 
     for (i = index; i < num_codes; i++)
-        if (map[i + i + 0] != 0 || map[i + i + 1] != 0)
+        if (map[i * (cmap->value_size + 2)] != 0 || map[i * (cmap->value_size + 2) + 1] != 0)
             break;
     if (i >= num_codes)
         return 1;
-    c0 = map[i + i + 0];
-    c1 = map[i + i + 1];
+    c0 = map[i * (cmap->value_size + 2) + 2];
+    c1 = map[i * (cmap->value_size + 2) + 3];
     for (j = i + 1, c2 = c1 + 1; j < num_codes; j++, c2++) {
         /* Due to PDF spec, *bfrange boundaries may differ
            in the last byte only. */
@@ -517,8 +512,11 @@ gs_cmap_ToUnicode_next_entry(gs_cmap_lookups_enum_t *penum)
         penum->entry.key[0][0] = (uchar)(i);
         penum->entry.key[1][0] = (uchar)(j - 1);
     }
-    memcpy(penum->temp_value, map + i * gs_cmap_ToUnicode_code_bytes,
-        gs_cmap_ToUnicode_code_bytes);
+    c0 = map[i * (cmap->value_size + 2)];
+    c1 = map[i * (cmap->value_size + 2) + 1];
+    penum->entry.value.size = (c0 << 8) + c1;
+    memcpy((void *)penum->entry.value.data, map + (i * (cmap->value_size + 2)) + 2,
+        penum->entry.value.size);
     return 0;
 }
 
@@ -552,7 +550,7 @@ static const gs_cmap_procs_t gs_cmap_ToUnicode_procs = {
  * Allocate and initialize a ToUnicode CMap.
  */
 int
-gs_cmap_ToUnicode_alloc(gs_memory_t *mem, int id, int num_codes, int key_size, gs_cmap_t **ppcmap)
+gs_cmap_ToUnicode_alloc(gs_memory_t *mem, int id, int num_codes, int key_size, int value_size, gs_cmap_t **ppcmap)
 {   int code;
     uchar *map, *cmap_name = NULL;
     gs_cmap_ToUnicode_t *cmap;
@@ -580,18 +578,19 @@ gs_cmap_ToUnicode_alloc(gs_memory_t *mem, int id, int num_codes, int key_size, g
               0, cmap_name, name_len, NULL, 0, &gs_cmap_ToUnicode_procs, mem);
     if (code < 0)
         return code;
-    map = (uchar *)gs_alloc_bytes(mem, num_codes * gs_cmap_ToUnicode_code_bytes,
+    map = (uchar *)gs_alloc_bytes(mem, num_codes * (value_size + 2),
                                   "gs_cmap_ToUnicode_alloc");
     if (map == NULL) {
         gs_cmap_free(*ppcmap, mem);
         return_error(gs_error_VMerror);
     }
-    memset(map, 0, num_codes * gs_cmap_ToUnicode_code_bytes);
+    memset(map, 0, num_codes * (value_size + 2));
     cmap = (gs_cmap_ToUnicode_t *)*ppcmap;
     cmap->glyph_name_data = map;
     cmap->CMapType = 2;
     cmap->num_fonts = 1;
     cmap->key_size = key_size;
+    cmap->value_size = value_size;
     cmap->num_codes = num_codes;
     cmap->ToUnicode = true;
     cmap->is_identity = true;
@@ -605,18 +604,60 @@ int gs_cmap_ToUnicode_free(gs_memory_t *mem, gs_cmap_t *pcmap)
     return 0;
 }
 
+/* Ths function is called when we discover that the value length we are using to
+ * store Unicode code points is too small for a new value. It increases
+ * the size of the map, and of each entry in the map, which is why we have to
+ * use a for loop rather than a memcpy. Note that when we increase the number
+ * of bytes used for a map entry, unused bytes are stored at the end, the initial
+ * 2 bytes are the length (in bytes) actually used by ths entry.
+ */
+int
+gs_cmap_ToUnicode_realloc(gs_memory_t *mem, int new_value_size, gs_cmap_t **ppcmap)
+{
+    gs_cmap_ToUnicode_t *cmap = (gs_cmap_ToUnicode_t *)*ppcmap;
+    uchar *new_ptr, *new_map, *old_map = cmap->glyph_name_data;
+    int i;
+
+    new_map = (uchar *)gs_alloc_bytes(mem, cmap->num_codes * (new_value_size + 2),
+                                  "gs_cmap_ToUnicode_alloc");
+    if (new_map == NULL) {
+        return_error(gs_error_VMerror);
+    }
+    new_ptr = new_map;
+    memset(new_map, 0, cmap->num_codes * (new_value_size + 2));
+
+    for (i=0;i<cmap->num_codes;i++) {
+        memcpy(new_ptr, old_map, cmap->value_size + 2);
+        old_map += cmap->value_size + 2;
+        new_ptr += new_value_size + 2;
+    }
+    gs_free_object(mem, cmap->glyph_name_data, "Free (realloc) ToUnicode glyph data");
+    cmap->glyph_name_data = new_map;
+    cmap->value_size = new_value_size;
+    return 0;
+}
+
 /*
  * Write a code pair to ToUnicode CMap.
  */
 void
-gs_cmap_ToUnicode_add_pair(gs_cmap_t *pcmap, int code0, int code1)
-{   gs_cmap_ToUnicode_t *cmap = (gs_cmap_ToUnicode_t *)pcmap;
-    uchar *map = pcmap->glyph_name_data;
+gs_cmap_ToUnicode_add_pair(gs_cmap_t *pcmap, int code0, ushort *u, unsigned int length)
+{
+    gs_cmap_ToUnicode_t *cmap = (gs_cmap_ToUnicode_t *)pcmap;
+    uchar *map = pcmap->glyph_name_data, *unicode = (uchar *)u;
     const int num_codes = ((gs_cmap_ToUnicode_t *)pcmap)->num_codes;
+    int i, code1 = 0;
 
     if (code0 >= num_codes)
         return; /* must not happen. */
-    map[code0 * gs_cmap_ToUnicode_code_bytes + 0] = (uchar)(code1 >> 8);
-    map[code0 * gs_cmap_ToUnicode_code_bytes + 1] = (uchar)(code1 & 0xFF);
-    cmap->is_identity &= (code0 == code1);
+    map[code0 * (cmap->value_size + 2)] = (uchar)(length >> 8);
+    map[code0 * (cmap->value_size + 2) + 1] = (uchar)(length & 0xFF);
+
+    memcpy(&map[(code0 * (cmap->value_size + 2)) + 2], unicode, length);
+    if (length <= 4) {
+        for (i=0;i<length;i++) {
+            code1 = (code1 << 8) + unicode[i];
+        }
+        cmap->is_identity &= (code0 == code1);
+    }
 }

base/gsfcmap.h
@@ -57,13 +57,16 @@ int gs_cmap_decode_next(const gs_cmap_t *pcmap, const gs_const_string *str,
  * Allocate and initialize a ToUnicode CMap.
  */
 int gs_cmap_ToUnicode_alloc(gs_memory_t *mem, int id, int num_codes, int key_size,
-                            gs_cmap_t **ppcmap);
+                            int value_size, gs_cmap_t **ppcmap);
 
 int gs_cmap_ToUnicode_free(gs_memory_t *mem, gs_cmap_t *pcmap);
+
+int gs_cmap_ToUnicode_realloc(gs_memory_t *mem, int new_value_size, gs_cmap_t **ppcmap);
+
 
 /*
  * Write a code pair to ToUnicode CMap.
  */
-void gs_cmap_ToUnicode_add_pair(gs_cmap_t *pcmap, int code0, int code2);
+void gs_cmap_ToUnicode_add_pair(gs_cmap_t *pcmap, int code0, ushort *unicode, unsigned int length);
 
 #endif /* gsfcmap_INCLUDED */

base/gsfcmap1.c
@@ -496,6 +496,7 @@ adobe1_next_lookup(gs_cmap_lookups_enum_t *penum, const gx_code_map_t *pcm)
 {
     const gx_cmap_lookup_range_t *lookup = &pcm->lookup[penum->index[0]];
 
+    penum->entry.value.data = 0L;
     if (penum->index[0] >= pcm->num_lookup)
         return 1;
     penum->entry.key_size = lookup->key_prefix_size + lookup->key_size;
@@ -508,13 +509,13 @@ adobe1_next_lookup(gs_cmap_lookups_enum_t *penum, const gx_code_map_t *pcm)
     return 0;
 }
 static int
-adobe1_next_lookup_def(gs_cmap_lookups_enum_t *penum)
+adobe1_next_lookup_def(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum)
 {
     return adobe1_next_lookup(penum,
                         &((const gs_cmap_adobe1_t *)penum->cmap)->def);
 }
 static int
-adobe1_next_lookup_notdef(gs_cmap_lookups_enum_t *penum)
+adobe1_next_lookup_notdef(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum)
 {
     return adobe1_next_lookup(penum,
                         &((const gs_cmap_adobe1_t *)penum->cmap)->notdef);

base/gsfont.c
@@ -981,8 +981,8 @@ gs_no_encode_char(gs_font *pfont, gs_char chr, gs_glyph_space_t glyph_space)
 }
 
 /* Dummy glyph decoding procedure */
-gs_char
-gs_no_decode_glyph(gs_font *pfont, gs_glyph glyph, int ch)
+int
+gs_no_decode_glyph(gs_font *pfont, gs_glyph glyph, int ch, ushort *unicode_return, unsigned int length)
 {
     return GS_NO_CHAR;
 }

base/gxfcmap.h
@@ -197,6 +197,14 @@ struct gs_cmap_s {
     GS_CMAP_COMMON;
 };
 
+typedef struct gs_cmap_ToUnicode_s {
+    GS_CMAP_COMMON;
+    int num_codes;
+    int key_size;
+    int value_size;
+    bool is_identity;
+} gs_cmap_ToUnicode_t;
+
 /* ---------------- Enumerators ---------------- */
 
 /*
@@ -222,7 +230,7 @@ struct gs_cmap_ranges_enum_s {
 };
 
 typedef struct gs_cmap_lookups_enum_procs_s {
-    int (*next_lookup)(gs_cmap_lookups_enum_t *penum);
+    int (*next_lookup)(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum);
     int (*next_entry)(gs_cmap_lookups_enum_t *penum);
 } gs_cmap_lookups_enum_procs_t;
 struct gs_cmap_lookups_enum_s {
@@ -286,7 +294,7 @@ int gs_cmap_enum_next_range(gs_cmap_ranges_enum_t *penum);
  */
 void gs_cmap_lookups_enum_init(const gs_cmap_t *pcmap, int which,
                                gs_cmap_lookups_enum_t *penum);
-int gs_cmap_enum_next_lookup(gs_cmap_lookups_enum_t *penum);
+int gs_cmap_enum_next_lookup(gs_memory_t *mem, gs_cmap_lookups_enum_t *penum);
 int gs_cmap_enum_next_entry(gs_cmap_lookups_enum_t *penum);
 
 /* ---------------- Implementation procedures ---------------- */

base/gxfont.h
@@ -229,10 +229,17 @@ typedef struct gs_font_procs_s {
     font_proc_encode_char((*encode_char));
 
     /* Map a glyph name to Unicode UTF-16.
+     * decode_glyph procedures return '0' if the code is not in the map
+     * and could not be decoded. Otherwise they return the size of the
+     * string (in bytes) needed to contain the return values. Passing
+     * any value for the length less than the required number of bytes to the
+     * functions will cause them not to copy the data, they will still
+     * return the required size however, to allow for dynamic allocation
+     * of sufficiently large buffers.
      */
 
 #define font_proc_decode_glyph(proc)\
-  gs_char proc(gs_font *, gs_glyph, int)
+  int proc(gs_font *, gs_glyph, int, ushort *, unsigned int)
     font_proc_decode_glyph((*decode_glyph));
 
     /*

commit d5f17bbf37e4090952080423e3aa35c29c2751f5

txtwrite - fix the ToUnicode processing
commit 9dba57f0f9a53c130ec2771c0ed1d7bd6bbef6ab inadvertently broke
txtweite, I changed the meaning of the return value from the font
decode_glyph method. Initially I had intended it to be a count of
unsigned shorts, but altered it to bytes. Unfortunately I had
changed txtwrite for shorts, but forgot to change it for bytes.

This led to a buffer overrun and heap corruption.

devices/vector/gdevtxtw.c
@@ -1780,10 +1780,10 @@ static int get_unicode(textw_text_enum_t *penum, gs_font *font, gs_glyph glyph, 
         *Buffer = fallback;
         return 1;
     } else {
-        unicode = (ushort *)gs_alloc_bytes(penum->dev->memory, length * sizeof(ushort), "temporary Unicode array");
+        unicode = (ushort *)gs_alloc_bytes(penum->dev->memory, length, "temporary Unicode array");
         length = font->procs.decode_glyph((gs_font *)font, glyph, ch, unicode, length);
-        memcpy(Buffer, unicode, length * sizeof(short));
-        gs_free(penum->dev->memory, unicode, length, sizeof(ushort), "free temporary unicode buffer");
+        memcpy(Buffer, unicode, length);
+        gs_free_object(penum->dev->memory, unicode, "free temporary unicode buffer");
         return length * sizeof(short);
     }
 }