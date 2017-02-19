e548d290d7d052a32e65493a703e1276230554c3
Henry Stiles <henry.stiles@artifex.com>
2017-02-18 17:57:18 -0700

    Fix 697576: false colors with pxlcolor.
    PXL high level image processing does not support DeviceN, fall back to
    rendering rectangles.

--- devices/vector/gdevpx.c
+++ devices/vector/gdevpx.c
@@ -359,6 +359,7 @@ pclxl_can_handle_color_space(const gs_color_space * pcs)
 
     return !(index == gs_color_space_index_Separation ||
              index == gs_color_space_index_Pattern ||
+             index == gs_color_space_index_DeviceN ||
              index == gs_color_space_index_ICC);
 }
 
