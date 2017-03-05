1df92f6952038fa70aa9cad8c398f360b0323539
Shailesh Mistry <shailesh.mistry@hotmail.co.uk>
2017-03-05 20:16:39 +0000

    Bug 697545: Memory Squeezing fix.
    Fix memory leak for allocation event number 170
    when memory squeezing:
    
    gs -sDEVICE=bit -o /dev/null: examples/tiger.eps

--- psi/interp.c
+++ psi/interp.c
@@ -296,12 +296,18 @@ gs_interp_init(i_ctx_t **pi_ctx_p, const ref *psystem_dict,
     /* Create and initialize a context state. */
     gs_context_state_t *pcst = 0;
     int code = context_state_alloc(&pcst, psystem_dict, dmem);
-
-    if (code >= 0)
+    if (code >= 0) {
         code = context_state_load(pcst);
+        if (code < 0) {
+            context_state_free(pcst);
+            pcst = NULL;
+        }
+    }
+
     if (code < 0)
         lprintf1("Fatal error %d in gs_interp_init!\n", code);
     *pi_ctx_p = pcst;
+
     return code;
 }
 /*
