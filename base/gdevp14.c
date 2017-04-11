e90c8f5a93e274b02b7e25762ef83995455f8464
Ray Johnston <ray.johnston@artifex.com>
2017-04-11 14:55:26 -0700

    Yet another fix for tag handling that affects shading fills and pdf14
    The comp_shift that is relied upon for color generation in fill_linear
    color_scanline did not work because the comp_shift[num_components] was
    not set for the pdf14 device when the device "has_tags".
    
    Also fix a bunch of whitespace variances.

--- base/gdevp14.c
+++ base/gdevp14.c
@@ -3670,6 +3670,7 @@ pdf14_recreate_device(gs_memory_t *mem,	gs_gstate	* pgs,
     pdev->procs = dev_proto->procs;
     if (has_tags) {
         pdev->procs.encode_color = pdf14_encode_color_tag;
+        pdev->color_info.comp_shift[pdev->color_info.num_components] = pdev->color_info.depth;
         pdev->color_info.depth += 8;
     }
     dev->static_procs = dev_proto->static_procs;
@@ -4489,13 +4490,14 @@ pdf14_update_device_color_procs(gx_device *dev,
     pdev->ctx->additive = new_additive;
     pdev->pdf14_procs = new_14procs;
     pdev->color_info.depth = new_num_comps * 8;
-    if (has_tags) {
-        pdev->color_info.depth += 8;
-    }
     memset(&(pdev->color_info.comp_bits), 0, GX_DEVICE_COLOR_MAX_COMPONENTS);
     memset(&(pdev->color_info.comp_shift), 0, GX_DEVICE_COLOR_MAX_COMPONENTS);
     memcpy(&(pdev->color_info.comp_bits), comp_bits, new_num_comps);
     memcpy(&(pdev->color_info.comp_shift), comp_shift, new_num_comps);
+    if (has_tags) {
+        pdev->color_info.comp_shift[pdev->color_info.num_components] = pdev->color_info.depth;
+        pdev->color_info.depth += 8;
+    }
     pdev->color_info.max_color = 255;
     pdev->color_info.max_gray = 255;
     /* If the CS was ICC based, we need to update the device ICC profile
@@ -4550,184 +4552,184 @@ pdf14_update_device_color_procs_push_c(gx_device *dev,
       isolated transparency group, we will do the blending in the proper color
       space and then transform the data when we pop the group.  Remember that only
       isolated groups can have color spaces that are different than their parent. */
-        update_color_info = false;
-        switch (group_color) {
-            case GRAY_SCALE:
-                  if (pdev->color_info.num_components != 1){
-                    update_color_info = true;
+    update_color_info = false;
+    switch (group_color) {
+        case GRAY_SCALE:
+            if (pdev->color_info.num_components != 1){
+                update_color_info = true;
+                new_polarity = GX_CINFO_POLARITY_ADDITIVE;
+                new_num_comps = 1;
+                pdevproto = (pdf14_device *)&gs_pdf14_Gray_device;
+                new_additive = true;
+                new_14procs = &gray_pdf14_procs;
+                new_depth = 8;
+                comp_bits[0] = 8;
+                comp_shift[0] = 0;
+            }
+            break;
+        case DEVICE_RGB:
+        case CIE_XYZ:
+            if (pdev->color_info.num_components != 3){
+                update_color_info = true;
+                new_polarity = GX_CINFO_POLARITY_ADDITIVE;
+                new_num_comps = 3;
+                pdevproto = (pdf14_device *)&gs_pdf14_RGB_device;
+                new_additive = true;
+                new_14procs = &rgb_pdf14_procs;
+                new_depth = 24;
+                for (k = 0; k < 3; k++) {
+                    comp_bits[k] = 8;
+                    comp_shift[k] = (2-k)*8;
+                }
+            }
+            break;
+        case DEVICE_CMYK:
+            if (pdev->color_info.num_components != 4){
+                update_color_info = true;
+                new_polarity = GX_CINFO_POLARITY_SUBTRACTIVE;
+                new_num_comps = 4;
+                pdevproto = (pdf14_device *)&gs_pdf14_CMYK_device;
+                new_additive = false;
+                /* This is needed due to the mismatched compressed encode decode
+                   between the device procs and the pdf14 procs */
+                if (dev->color_info.num_components > 4){
+                    new_14procs = &cmykspot_pdf14_procs;
+                } else {
+                    new_14procs = &cmyk_pdf14_procs;
+                }
+                new_depth = 32;
+                for (k = 0; k < 4; k++) {
+                    comp_bits[k] = 8;
+                    comp_shift[k] = (3-k)*8;
+                }
+            }
+            break;
+        case ICC:
+            /* Check if the profile is different. */
+            dev_proc(dev, get_profile)(dev,  &dev_profile);
+            gsicc_extract_profile(GS_UNKNOWN_TAG, dev_profile,
+                                  &icc_profile_dev, &render_cond);
+            if (icc_profile_dev->hashcode != icc_profile->hashcode) {
+                update_color_info = true;
+                new_num_comps = icc_profile->num_comps;
+                new_depth = icc_profile->num_comps * 8;
+                switch (new_num_comps) {
+                case 1:
+                    if (pdev->sep_device && !is_mask) {
+                        pdevproto = (pdf14_device *)&gs_pdf14_Grayspot_device;
+                        new_14procs = &grayspot_pdf14_procs;
+                    } else {
+                        pdevproto = (pdf14_device *)&gs_pdf14_Gray_device;
+                        new_14procs = &gray_pdf14_procs;
+                    }
                     new_polarity = GX_CINFO_POLARITY_ADDITIVE;
-                    new_num_comps = 1;
-                    pdevproto = (pdf14_device *)&gs_pdf14_Gray_device;
                     new_additive = true;
-                    new_14procs = &gray_pdf14_procs;
-                    new_depth = 8;
                     comp_bits[0] = 8;
                     comp_shift[0] = 0;
-                }
-                break;
-            case DEVICE_RGB:
-            case CIE_XYZ:
-                if (pdev->color_info.num_components != 3){
-                    update_color_info = true;
+                    break;
+                case 3:
+                    if (pdev->sep_device) {
+                        pdevproto = (pdf14_device *)&gs_pdf14_RGBspot_device;
+                        new_14procs = &rgbspot_pdf14_procs;
+                    } else {
+                        pdevproto = (pdf14_device *)&gs_pdf14_RGB_device;
+                        new_14procs = &rgb_pdf14_procs;
+                    }
                     new_polarity = GX_CINFO_POLARITY_ADDITIVE;
-                    new_num_comps = 3;
-                    pdevproto = (pdf14_device *)&gs_pdf14_RGB_device;
                     new_additive = true;
-                    new_14procs = &rgb_pdf14_procs;
-                    new_depth = 24;
                     for (k = 0; k < 3; k++) {
                         comp_bits[k] = 8;
                         comp_shift[k] = (2-k)*8;
                     }
-                }
-                break;
-            case DEVICE_CMYK:
-                if (pdev->color_info.num_components != 4){
-                    update_color_info = true;
-                    new_polarity = GX_CINFO_POLARITY_SUBTRACTIVE;
-                    new_num_comps = 4;
-                    pdevproto = (pdf14_device *)&gs_pdf14_CMYK_device;
-                    new_additive = false;
-                /* This is needed due to the mismatched compressed encode decode
-                   between the device procs and the pdf14 procs */
-                    if (dev->color_info.num_components > 4){
+                    break;
+                case 4:
+                    if (pdev->sep_device) {
+                        pdevproto = (pdf14_device *)&gs_pdf14_CMYKspot_device;
                         new_14procs = &cmykspot_pdf14_procs;
                     } else {
+                        pdevproto = (pdf14_device *)&gs_pdf14_CMYK_device;
                         new_14procs = &cmyk_pdf14_procs;
                     }
-                    new_depth = 32;
+                    new_polarity = GX_CINFO_POLARITY_SUBTRACTIVE;
+                    new_additive = false;
                     for (k = 0; k < 4; k++) {
                         comp_bits[k] = 8;
                         comp_shift[k] = (3-k)*8;
                     }
+                    break;
+                default:
+                    return gs_throw(gs_error_undefinedresult,
+                                    "ICC Number of colorants illegal");
                 }
-                break;
-            case ICC:
-                /* Check if the profile is different. */
-                dev_proc(dev, get_profile)(dev,  &dev_profile);
-                gsicc_extract_profile(GS_UNKNOWN_TAG, dev_profile,
-                                      &icc_profile_dev, &render_cond);
-                if (icc_profile_dev->hashcode != icc_profile->hashcode) {
-                    update_color_info = true;
-                    new_num_comps = icc_profile->num_comps;
-                    new_depth = icc_profile->num_comps * 8;
-                    switch (new_num_comps) {
-                    case 1:
-                        if (pdev->sep_device && !is_mask) {
-                            pdevproto = (pdf14_device *)&gs_pdf14_Grayspot_device;
-                            new_14procs = &grayspot_pdf14_procs;
-                        } else {
-                            pdevproto = (pdf14_device *)&gs_pdf14_Gray_device;
-                            new_14procs = &gray_pdf14_procs;
-                        }
-                        new_polarity = GX_CINFO_POLARITY_ADDITIVE;
-                        new_additive = true;
-                        comp_bits[0] = 8;
-                        comp_shift[0] = 0;
-                        break;
-                    case 3:
-                        if (pdev->sep_device) {
-                            pdevproto = (pdf14_device *)&gs_pdf14_RGBspot_device;
-                            new_14procs = &rgbspot_pdf14_procs;
-                        } else {
-                            pdevproto = (pdf14_device *)&gs_pdf14_RGB_device;
-                            new_14procs = &rgb_pdf14_procs;
-                        }
-                        new_polarity = GX_CINFO_POLARITY_ADDITIVE;
-                        new_additive = true;
-                        for (k = 0; k < 3; k++) {
-                            comp_bits[k] = 8;
-                            comp_shift[k] = (2-k)*8;
-                        }
-                        break;
-                    case 4:
-                        if (pdev->sep_device) {
-                            pdevproto = (pdf14_device *)&gs_pdf14_CMYKspot_device;
-                            new_14procs = &cmykspot_pdf14_procs;
-                        } else {
-                            pdevproto = (pdf14_device *)&gs_pdf14_CMYK_device;
-                            new_14procs = &cmyk_pdf14_procs;
-                        }
-                        new_polarity = GX_CINFO_POLARITY_SUBTRACTIVE;
-                        new_additive = false;
-                        for (k = 0; k < 4; k++) {
-                            comp_bits[k] = 8;
-                            comp_shift[k] = (3-k)*8;
-                        }
-                        break;
-                    default:
-                        return gs_throw(gs_error_undefinedresult,
-                                        "ICC Number of colorants illegal");
-                    }
+            }
+            break;
+        case UNKNOWN:
+            return 0;
+            break;
+        default:
+            return_error(gs_error_rangecheck);
+            break;
+     }
+    if (update_color_info){
+        if (pdev->sep_device && !is_mask) {
+            int num_spots = pdev->color_info.num_components - icc_profile_dev->num_comps;
+
+            if (num_spots > 0) {
+                new_num_comps += num_spots;
+                for (k = 0; k < new_num_comps; k++) {
+                    comp_bits[k] = 8;
+                    comp_shift[k] = (new_num_comps - k - 1) * 8;
                 }
-                break;
-            case UNKNOWN:
-                return 0;
-                break;
-            default:
-                return_error(gs_error_rangecheck);
-                break;
-         }
-         if (update_color_info){
-             if (pdev->sep_device && !is_mask) {
-                 int num_spots = pdev->color_info.num_components -
-                     icc_profile_dev->num_comps;
-                 if (num_spots > 0) {
-                     new_num_comps += num_spots;
-                     for (k = 0; k < new_num_comps; k++) {
-                         comp_bits[k] = 8;
-                         comp_shift[k] = (new_num_comps - k - 1) * 8;
-                     }
-                     new_depth = 8 * new_num_comps;
-                 }
-             }
-             if (has_tags) {
-                 new_depth += 8;
-             }
-            if_debug2m('v', pdev->memory,
-                       "[v]pdf14_update_device_color_procs_push_c,num_components_old = %d num_components_new = %d\n",
-                       pdev->color_info.num_components,new_num_comps);
-            /* Set new information in the device */
-            pdev->procs.get_color_mapping_procs =
-                pdevproto->static_procs->get_color_mapping_procs;
-            pdev->procs.get_color_comp_index =
-                pdevproto->static_procs->get_color_comp_index;
-            pdev->blend_procs = pdevproto->blend_procs;
-            pdev->color_info.polarity = new_polarity;
-            pdev->color_info.num_components = new_num_comps;
-            pdev->color_info.max_color = 255;
-            pdev->color_info.max_gray = 255;
-            pdev->pdf14_procs = new_14procs;
-            pdev->color_info.depth = new_depth;
-            memset(&(pdev->color_info.comp_bits),0,GX_DEVICE_COLOR_MAX_COMPONENTS);
-            memset(&(pdev->color_info.comp_shift),0,GX_DEVICE_COLOR_MAX_COMPONENTS);
-            memcpy(&(pdev->color_info.comp_bits), comp_bits, new_num_comps);
-            memcpy(&(pdev->color_info.comp_shift), comp_shift, new_num_comps);
-            /* If we have a compressed color codec, and we are doing a soft mask
-               push operation then go ahead and update the color encode and
-               decode for the pdf14 device to not used compressed color
-               encoding while in the soft mask.  We will just check for gray
-               and compressed.  Note that we probably don't have_tags if we
-               are dealing with compressed color.  But is is possible so
-               we add it in to catch for future use. */
-            cldev->clist_color_info.depth = pdev->color_info.depth;
-            cldev->clist_color_info.polarity = pdev->color_info.polarity;
-            cldev->clist_color_info.num_components = pdev->color_info.num_components;
-            cldev->clist_color_info.max_color = pdev->color_info.max_color;
-            cldev->clist_color_info.max_gray = pdev->color_info.max_gray;
-            /* For the ICC profiles, we want to update the ICC profile for the
-               device in the ICC manager.  We already stored in in pdf14_parent_color_t.
-               That will be stored in the clist and restored during the reading phase. */
-           if (group_color == ICC) {
-                dev->icc_struct->device_profile[0] = icc_profile;
+                new_depth = 8 * new_num_comps;
             }
-            if (pdev->ctx) {
-               pdev->ctx->additive = new_additive;
-            }
-            return(1);  /* Lets us detect that we did do an update */
-         }
-         if_debug0m('v', pdev->memory, "[v]procs not updated\n");
-         return 0;
+        }
+        if (has_tags) {
+            new_depth += 8;
+        }
+        if_debug2m('v', pdev->memory,
+                   "[v]pdf14_update_device_color_procs_push_c,num_components_old = %d num_components_new = %d\n",
+                   pdev->color_info.num_components,new_num_comps);
+        /* Set new information in the device */
+        pdev->procs.get_color_mapping_procs = pdevproto->static_procs->get_color_mapping_procs;
+        pdev->procs.get_color_comp_index = pdevproto->static_procs->get_color_comp_index;
+        pdev->blend_procs = pdevproto->blend_procs;
+        pdev->color_info.polarity = new_polarity;
+        pdev->color_info.num_components = new_num_comps;
+        pdev->color_info.max_color = 255;
+        pdev->color_info.max_gray = 255;
+        pdev->pdf14_procs = new_14procs;
+        pdev->color_info.depth = new_depth;
+        memset(&(pdev->color_info.comp_bits),0,GX_DEVICE_COLOR_MAX_COMPONENTS);
+        memset(&(pdev->color_info.comp_shift),0,GX_DEVICE_COLOR_MAX_COMPONENTS);
+        memcpy(&(pdev->color_info.comp_bits), comp_bits, new_num_comps);
+        memcpy(&(pdev->color_info.comp_shift), comp_shift, new_num_comps);
+        pdev->color_info.comp_shift[new_num_comps] = 1 << (new_depth - 8);	/* in case we has_tags is set */
+
+        /* If we have a compressed color codec, and we are doing a soft mask
+           push operation then go ahead and update the color encode and
+           decode for the pdf14 device to not used compressed color
+           encoding while in the soft mask.  We will just check for gray
+           and compressed.  Note that we probably don't have_tags if we
+           are dealing with compressed color.  But is is possible so
+           we add it in to catch for future use. */
+        cldev->clist_color_info.depth = pdev->color_info.depth;
+        cldev->clist_color_info.polarity = pdev->color_info.polarity;
+        cldev->clist_color_info.num_components = pdev->color_info.num_components;
+        cldev->clist_color_info.max_color = pdev->color_info.max_color;
+        cldev->clist_color_info.max_gray = pdev->color_info.max_gray;
+        /* For the ICC profiles, we want to update the ICC profile for the
+           device in the ICC manager.  We already stored in in pdf14_parent_color_t.
+           That will be stored in the clist and restored during the reading phase. */
+        if (group_color == ICC) {
+            dev->icc_struct->device_profile[0] = icc_profile;
+        }
+        if (pdev->ctx) {
+           pdev->ctx->additive = new_additive;
+        }
+        return(1);  /* Lets us detect that we did do an update */
+    }
+    if_debug0m('v', pdev->memory, "[v]procs not updated\n");
+    return 0;
 }
 
 static int
@@ -4781,10 +4783,10 @@ pdf14_update_device_color_procs_pop_c(gx_device *dev,gs_gstate *pgs)
             pdev->ctx->additive = parent_color->isadditive;
         }
        /* The device profile must be restored. */
-         dev->icc_struct->device_profile[0] = parent_color->icc_profile;
-         rc_decrement(parent_color->icc_profile, "pdf14_update_device_color_procs_pop_c");
-         parent_color->icc_profile = NULL;
-         if_debug0m('v', dev->memory, "[v]procs updated\n");
+        dev->icc_struct->device_profile[0] = parent_color->icc_profile;
+        rc_decrement(parent_color->icc_profile, "pdf14_update_device_color_procs_pop_c");
+        parent_color->icc_profile = NULL;
+        if_debug0m('v', dev->memory, "[v]procs updated\n");
     } else {
         if_debug0m('v', dev->memory, "[v]pdf14_update_device_color_procs_pop_c ERROR \n");
     }
@@ -5945,6 +5947,7 @@ gs_pdf14_device_push(gs_memory_t *mem, gs_gstate * pgs,
        tags that will be a special project at that time */
     if (has_tags) {
         p14dev->procs.encode_color = pdf14_encode_color_tag;
+        p14dev->color_info.comp_shift[p14dev->color_info.num_components] = p14dev->color_info.depth;
         p14dev->color_info.depth += 8;
     }
     check_device_separable((gx_device *)p14dev);
@@ -7182,6 +7185,7 @@ pdf14_create_clist_device(gs_memory_t *mem, gs_gstate * pgs,
        do compressed color with tags that will be a special project at that time */
     if (has_tags) {
         pdev->procs.encode_color = pdf14_encode_color_tag;
+        pdev->color_info.comp_shift[pdev->color_info.num_components] = pdev->color_info.depth;
         pdev->color_info.depth += 8;
     }
     check_device_separable((gx_device *)pdev);
