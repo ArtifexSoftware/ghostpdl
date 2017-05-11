31f9fddb4a330c21ea8268fc9e8ae0ead23296bf
Chris Liddell <chris.liddell@artifex.com>
2017-05-11 09:57:48 +0100

    Bug 696417: fully initialise TTF bytecode context.
    Although the font and CVT programs aren't supposed to use certain opcodes,
    specifically, in this case, the opcodes that reference the contents of the
    zone pointers. But this font contains programs that do. Since other interpreters
    handle this, just ensure we initialize the context fully (so the zone pointers
    are valid) before executing the context.

--- base/ttobjs.c
+++ base/ttobjs.c
@@ -666,6 +666,28 @@ static int free_aux(ttfMemory *mem, void *ptr)
       if ( error )
         goto Fin;
 
+      exec->zp0 = exec->pts;
+      exec->zp1 = exec->pts;
+      exec->zp2 = exec->pts;
+
+      exec->GS.gep0 = 1;
+      exec->GS.gep1 = 1;
+      exec->GS.gep2 = 1;
+
+      exec->GS.projVector.x = 0x4000;
+      exec->GS.projVector.y = 0x0000;
+
+      exec->GS.freeVector = exec->GS.projVector;
+      exec->GS.dualVector = exec->GS.projVector;
+
+      exec->GS.round_state = 1;
+      exec->GS.loop        = 1;
+
+      /* some glyphs leave something on the stack. so we clean it */
+      /* before a new execution.                                  */
+      exec->top     = 0;
+      exec->callTop = 0;
+
       error = RunIns( exec );
       Unset_CodeRange(exec);
     }
@@ -780,6 +802,28 @@ static int free_aux(ttfMemory *mem, void *ptr)
       error = Goto_CodeRange( exec, TT_CodeRange_Cvt, 0 );
       if (error)
         goto Fin;
+
+      exec->zp0 = exec->pts;
+      exec->zp1 = exec->pts;
+      exec->zp2 = exec->pts;
+
+      exec->GS.gep0 = 1;
+      exec->GS.gep1 = 1;
+      exec->GS.gep2 = 1;
+
+      exec->GS.projVector.x = 0x4000;
+      exec->GS.projVector.y = 0x0000;
+
+      exec->GS.freeVector = exec->GS.projVector;
+      exec->GS.dualVector = exec->GS.projVector;
+
+      exec->GS.round_state = 1;
+      exec->GS.loop        = 1;
+
+      /* some glyphs leave something on the stack. so we clean it */
+      /* before a new execution.                                  */
+      exec->top     = 0;
+      exec->callTop = 0;
 
       error = RunIns( exec );
       Unset_CodeRange(exec);
