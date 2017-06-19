2fa6beaa40144c592661a611bf35ff6f06d3354f
Ken Sharp <ken.sharp@artifex.com>
2017-06-19 09:46:21 +0100

    Tools - remove ps2ascii.ps, recode scripts to use txtwrite
    ps2ascii.ps uses DELAYBIND, which we would like to get rid of, in
    addition it doesn't do a terribly good job of actually extracting text.
    The txtwrite device is more sophisticated, although it does not have the
    added 'COMPLEX' feature of ps2ascii.ps which emits strokes and fills
    as rectangles.
    
    If the COMPLEX feature set should prove useful enough it would be easy
    to add this functionality to a new device, but in reality I doubt
    anyone is using this. The scripts specifically set -dSIMPLE.
    
    This allows us to deprecate DELAYBIND and in future remove it.

--- lib/ps2ascii.bat
+++ lib/ps2ascii.bat
@@ -3,12 +3,12 @@
 call "%~dp0gssetgs.bat"
 if '%1'=='' goto a0
 if '%2'=='' goto a1
-%GSC% -q -dNODISPLAY -P- -dSAFER -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps %1 -c quit >%2
+%GSC% -dSAFER -sDEVICE=txtwrite -o %2 %1
 goto x
 :a0
-%GSC% -q -dNODISPLAY -P- -dSAFER -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps - -c quit
+%GSC% -dSAFER -sDEVICE=txtwrite -o - -
 goto x
 :a1
-%GSC% -q -dNODISPLAY -P- -dSAFER -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps %1 -c quit
+%GSC% -dSAFER -sDEVICE=txtwrite -o - %1
 goto x
 :x
--- lib/ps2ascii.cmd
+++ lib/ps2ascii.cmd
@@ -6,12 +6,12 @@
 @echo off
 if '%1'=='' goto a0
 if '%2'=='' goto a1
-gsos2 -q -dNODISPLAY -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps %1 -c quit >%2
+gsos2 -q -dSAFER -sDEVICE=txtwrite -o %2 %1
 goto x
 :a0
-gsos2 -q -dNODISPLAY -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps - -c quit
+gsos2 -q -dSAFER -sDEVICE=txtwrite -o - -
 goto x
 :a1
-gsos2 -q -dNODISPLAY -dDELAYBIND -dWRITESYSTEMDICT -dSIMPLE ps2ascii.ps %1 -c quit
+gsos2 -q -dSAFER -sDEVICE=txtwrite -o - %1
 goto x
 :x
