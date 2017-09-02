a48211dc97ef345261e7e9a08818dd274b07040e
Ken Sharp <ken.sharp@artifex.com>
2017-09-02 10:54:06 +0100

    Coverity ID 94736
    Guard against a NULL pointer dereference. If we didn't recognise the
    pritner type, then it was possible to attempt to use a NULL pointer.

--- devices/gdevcdj.c
+++ devices/gdevcdj.c
@@ -863,7 +863,8 @@ hp_colour_open(gx_device *pdev, int ptype)
 
     break;
   }
-  gx_device_set_margins(pdev, m, true);
+  if (m != 0)
+    gx_device_set_margins(pdev, m, true);
   return gdev_prn_open(pdev);
 }
 
