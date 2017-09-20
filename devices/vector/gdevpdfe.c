4bb7249ce9320c566da5dac3901c41c7965d37d8
Ken Sharp <ken.sharp@artifex.com>
2017-09-20 15:37:19 +0100

    pdfwrite - fix UTF16 to UTF8 conversion
    There was a signed/unsigned error in the arguments to this routine,
    which meant that bytes with the top bit set caused the whole UTF16
    short to become negative as well.
    
    Fixed here.

--- devices/vector/gdevpdfe.c
+++ devices/vector/gdevpdfe.c
@@ -351,7 +351,7 @@ decode_escape(const byte *data, int data_length, int *index)
  */
 static const char firstByteMark[7] = { 0x00, 0x00, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC };
 
-static int gs_ConvertUTF16(char *UTF16, int UTF16Len, unsigned char **UTF8Start, int UTF8Len)
+static int gs_ConvertUTF16(unsigned char *UTF16, int UTF16Len, unsigned char **UTF8Start, int UTF8Len)
 {
     int i, bytes = 0;
     unsigned short U16;
