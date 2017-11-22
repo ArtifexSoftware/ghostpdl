1cfc56251773304fb102237ff8a63c5576f08fe7
Robin Watts <robin.watts@artifex.com>
2017-11-22 21:29:04 +0000

    Rewritten version of squeeze2html.pl
    This works with the latest memento, and gives nicer results in
    the browser.
    
    Invoke using something like:
    
      MEMENTO_SQUEEZEAT=1 membin/gs -sDEVICE=ppmraw -o /dev/null
         in.pdf |& perl toolbin/squeeze2html.pl -q | gzip -9c > out.html.gz

--- toolbin/squeeze2html.pl
+++ toolbin/squeeze2html.pl
@@ -25,154 +25,106 @@ if ($opts{q} == 1) {
     $quiet = 1;
 }
 
-# First we output the headers, our javascript code, and an empty table.
-
-print "<html>\n<head>\n<title>Memory squeeze results</title>\n";
-print "<script>\n";
-print "var count=0;\n";
-print "var maxpoint=0;\n";
-print "function hide(n) {\n";
-print "  tag = document.getElementById('point'+n);\n";
-print "  if (tag) {\n";
-print "    tag.setAttribute('style', 'display : none');\n";
-print "  }\n";
-print "}\n";
-print "function show(n) {\n";
-print "  for (i = 0; i <= n; i++) {\n";
-print "    tag = document.getElementById('point'+(2*i-1));\n";
-print "    if (tag) {\n";
-print "      tag.setAttribute('style', 'display : block');\n";
-print "    }\n";
-print "    tag = document.getElementById('point'+(2*i));\n";
-print "    if (tag) {\n";
-print "      tag.setAttribute('style', 'display : none');\n";
-print "    }\n";
-print "  }\n";
-print "  tag = document.getElementById('point'+(2*n));\n";
-print "  if (tag) {\n";
-print "    tag.setAttribute('style', 'display : block');\n";
-print "  }\n";
-print "  for (i = 2*n+1; i <= 2*maxpoint; i++) {\n";
-print "    tag = document.getElementById('point'+i);\n";
-print "    if (tag) {\n";
-print "      tag.setAttribute('style', 'display : none');\n";
-print "    }\n";
-print "  }\n";
-print "}\n";
-print "function addCell(n, i) {\n";
-print "  var row;\n";
-print "  rownum = count & ~31;\n";
-print "  cell = document.createElement('td');\n";
-print "  cell.setAttribute('style', 'background-color: '+(i == 0 ? '#0f0' : (i == 1 ? '#ff0' : '#f00')));\n";
-print "  a = document.createElement('a');\n";
-print "  a.appendChild(document.createTextNode(n));\n";
-print "  a.href='javascript:show('+n+')';\n";
-print "  cell.appendChild(a);\n";
-print "  if ((count & 31) == 0) {\n";
-print "    row = document.createElement('tr');\n";
-print "    row.setAttribute('id', 'row'+rownum);\n";
-print "  } else {\n";
-print "    row = document.getElementById('row'+rownum);\n";
-print "  }\n";
-print "  row.appendChild(cell);\n";
-print "  if ((count & 31) == 0) {\n";
-print "    table = document.getElementById('points');\n";
-print "    table.appendChild(row);\n";
-print "  }\n";
-print "  count++;\n";
-print "  if (n > maxpoint) {\n";
-print "    maxpoint = n;\n";
-print "  }\n";
-print "  hide(2*n);\n";
-print "  hide(2*n+1);\n";
-print "}\n";
-print "function r(n) { addCell(n>>1, 2); }\n";
-print "function y(n) { addCell(n>>1, 1); }\n";
-print "function g(n) { addCell(n>>1, 0); }\n";
-print "</script>\n";
-print "</head>\n";
-print "<body><table id=\"points\">";
-print "</table>";
-
-# Now we process the input.
-
-my $at = 1;
-my $i;
-my $text = "";
-my $status;
-my $started;
-
-LINE: while (my $line = <>)
+sub html_escape($)
 {
-    chomp $line;
-    if (($i) = $line =~ m/SEGV after Memory squeezing @ (\d+)/)
-    {
-	$status |= 8;
-    }
-    elsif (($i) = $line =~ m/Memory squeezing @ (\d+) complete/)
-    {
-	$status |= 2;
-	if ($started) {
-            if (($quiet == 0) || (($at & 1) == 1) || (($status & 12) != 0)) {
-                print "$text</pre></div>";
-	    }
-  	    if ($at & 1) {
-	    } elsif ($status & 8) {
-		print "<script>r($at)</script>";
-	    } elsif ($status & 4) {
-		print "<script>y($at)</script>";
-	    } elsif ($quiet == 0) {
-		print "<script>g($at)</script>";
-	    }
-	}
-	$started = 0;
-	$text = "";
-        $at |= 1;
-	next LINE;
-    }
-    elsif (($i) = $line =~ m/Memory squeezing @ (\d+)/)
-    {
-	if ($started) {
-            if (($quiet == 0) || (($at & 1) == 1) || (($status & 12) != 0)) {
-                print "$text</pre></div>";
-            }
-	    if ($at & 1) {
-	    } elsif ($status & 8) {
-		print "<script>r($at)</script>";
-	    } elsif ($status & 4) {
-		print "<script>y($at)</script>";
-	    } elsif ($quiet == 0) {
-		print "<script>g($at)</script>";
-	    }
-	}
-	$at = $i * 2;
-	$started = 0;
-	$status = 1;
-	next LINE;
-    }
-    elsif ($line =~ m/Allocated blocks:/)
-    {
-	$status |= 4;
-    }
-    if ($started == 0) {
-        $text = "<div id='point$at'><pre>$line";
-	$started = 1;
-    } else {
-        $text = $text."\n".$line;
-    }
-}
-if ($started) {
-  if (($quiet == 0) || (($at & 1) == 1) || (($status & 12) != 0)) {
-    print "$text</pre></div>";
-  }
-  if ($at & 1) {
-  } elsif ($status & 8) {
-    print "<script>r($at)</script>";
-  } elsif ($status & 4) {
-    print "<script>y($at)</script>";
-  } elsif ($quiet == 0) {
-    print "<script>g($at)</script>";
-  }
+    my $line = shift;
+
+    $line =~ s/&/&amp;/g;
+    $line =~ s/\</&lt;/g;
+    $line =~ s/\>/&gt;/g;
+
+    return $line;
 }
 
-print "</body></html>";
+# First we output the headers, our javascript code, and an empty table.
+print "<html><head><title>Memory squeezer results</title>";
+print "<script>\n";
+print "function show(id) {\n";
+print "  tag = document.getElementById('txt'+id);\n";
+print "  if (tag == null) {return;}\n";
+print "  if (tag.hiddenFlag == 0) {\n";
+print "    tag.setAttribute('style', 'display:none')\n";
+print "    tag.hiddenFlag = 1;\n";
+print "  } else {\n";
+print "    tag.setAttribute('style', 'display:block')\n";
+print "    tag.hiddenFlag = 0;\n";
+print "  }\n";
+print "}\n";
+print "function segv(id) {\n";
+print "  tag = document.getElementById('but'+id);\n";
+print "  if (tag == null) {return;}\n";
+print "  tag.setAttribute('style','background-color:#f88');";
+print "}\n";
+print "function leak(id) {\n";
+print "  tag = document.getElementById('but'+id);\n";
+print "  if (tag == null) {return;}\n";
+print "  tag.setAttribute('style','background-color:#fc8');";
+print "}\n";
+print "function ok(id) {\n";
+print "  tag = document.getElementById('but'+id);\n";
+print "  if (tag == null) {return;}\n";
+print "  tag.setAttribute('style','background-color:#8f8');";
+print "}\n";
+print "</script>";
+print "</head><body>";
+
+my $segv = 0;
+my $leak = 0;
+my $in = 0;
+my $buffer = "";
+my $inbuffer = "";
+
+sub start_run($) {
+    my $in = shift;
+    $segv = 0;
+    $leak = 0;
+}
+
+sub end_run($) {
+    my $in = shift;
+
+    if ($quiet == 0 || $segv != 0 || $leak != 0) {
+        print "<span id=\"but$in\"><a href=\"javascript:show($in);\">$in</a></span>&nbsp;<div id=\"txt$in\" style=\"display: none\"><pre>";
+        print html_escape($inbuffer);
+        print "</pre></div>\n";
+        if ($segv == 1) {
+            print "<script>segv($in);</script>";
+        } elsif ($leak == 1) {
+            print "<script>leak($in);</script>";
+        } else {
+            print "<script>ok($in);</script>";
+        }
+    }
+    $inbuffer="";
+}
+
+while (<>)
+{
+    my $line = $_;
+    chomp $line;
+
+    if ($line =~ m/^Memory squeezing @ (.*) complete/) {
+        end_run($in);
+	$in = 0;
+    } elsif  ($line =~ m/^Memory squeezing @ (.*)$/) {
+        $in = $1;
+        if ($buffer ne "") {
+            print "<!-- ".html_escape($buffer)." -->\n";
+            $buffer="";
+        }
+        start_run($in);
+    } elsif ($in) {
+        if ($line =~ m/^SEGV at:/) {
+            $segv = 1;
+        } elsif ($line =~ m/^Allocated blocks/) {
+            $leak = 1;
+        }
+	$inbuffer = $inbuffer.$line."\n";
+    } else {
+        $buffer = $buffer."\n".$line;
+    }
+}
+
+start_run("final");
+print html_escape("$buffer\n");
+end_run("final");
