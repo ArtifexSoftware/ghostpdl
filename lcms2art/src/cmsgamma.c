f5e42eb1dc1cc96bab67990e886756a089a3fa5a
Shailesh Mistry <shailesh.mistry@hotmail.co.uk>
2018-01-23 18:15:18 +0000

    Bug 697545 : Fix memory leak in AllocateToneCurveStruct.
    Fix memory leak for allocation event number 741.
    
    Error created using :-
    MEMENTO_FAILAT=742 ./membin/gpcl6 -sDEVICE=ppmraw -o /dev/null ./pcl/examples/owl.pcl

--- lcms2art/src/cmsgamma.c
+++ lcms2art/src/cmsgamma.c
@@ -300,6 +300,7 @@ cmsToneCurve* AllocateToneCurveStruct(cmsContext ContextID, cmsUInt32Number nEnt
         return p;
 
 Error:
+    if (p->SegInterp) _cmsFree(ContextID, p->SegInterp);
     if (p -> Segments) _cmsFree(ContextID, p ->Segments);
     if (p -> Evals) _cmsFree(ContextID, p -> Evals);
     if (p ->Table16) _cmsFree(ContextID, p ->Table16);
