57d33404ce0fab68a909fff501c0b8e2912b9956
Chris Liddell <chris.liddell@artifex.com>
2018-03-22 11:30:03 +0000

    Check __STDC_VERSION__ is defined before using it
    Not all compilers (and versions) have __STDC_VERSION__

--- base/stdpre.h
+++ base/stdpre.h
@@ -94,7 +94,7 @@
 #endif
 
 /* restrict is standard in C99, but not in all C++ compilers. */
-#if __STDC_VERSION__ == 199901L /* C99 */
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ == 199901L /* C99 */
 #elif defined(_MSC_VER) && _MSC_VER >= 1500 /* MSVC 9 or newer */
 #define gs_restrict __restrict
 #elif __GNUC__ >= 3 /* GCC 3 or newer */
