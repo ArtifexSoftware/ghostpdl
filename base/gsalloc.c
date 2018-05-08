f302fbc59b974a167a7a0023ac6f9e89ed27d243
Chris Liddell <chris.liddell@artifex.com>
2018-05-08 12:37:07 +0100

    Honor the single object clump flag when allocating strings
    When checking whether a given clump was a candidate to allocate a string,
    the code was not checking the flag that marks the clump as being for a
    single object. That could lead to the clump being freed (with it's original,
    single object content, along with the string) when the string(s) were still
    in use.
    
    This is better than resetting the flag since single object clumps are such
    for a reason (they implement immovable memory), and we *never* want
    immovable memory to become movable

--- base/gsalloc.c
+++ base/gsalloc.c
@@ -1597,7 +1597,7 @@ i_alloc_string(gs_memory_t * mem, uint nbytes, client_name_t cname)
         alloc_open_clump(imem);
     }
 top:
-    if (imem->cc && imem->cc->ctop - imem->cc->cbot > nbytes) {
+    if (imem->cc && !imem->cc->c_alone && imem->cc->ctop - imem->cc->cbot > nbytes) {
         if_debug4m('A', mem, "[a%d:+> ]%s(%u) = 0x%lx\n",
                    alloc_trace_space(imem), client_name_string(cname), nbytes,
                    (ulong) (imem->cc->ctop - nbytes));
