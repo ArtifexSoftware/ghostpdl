ArtifexSoftware/ghostpdl

commit f2cf68297e3d63cb927db3c98d317f7ee68e7898

Bug 699381 Add error checking for device icc profiles
Make sure that the various types of profiles that can be set work with
the device color model and with each other.   Only allow the use of
the post render ICC profile when the device supports it.

base/gsicc_manage.c
@@ -39,6 +39,7 @@
 #include "gsicc_create.h"
 #include "gpmisc.h"
 #include "gxdevice.h"
+#include "gxdevsop.h"
 
 #define ICC_HEADER_SIZE 128
 #define CREATE_V2_DATA 0
@@ -1756,6 +1757,88 @@ char* gsicc_get_dev_icccolorants(cmm_dev_profile_t *dev_profile)
         return dev_profile->spotnames->name_str;
 }
 
+/* Check that the current state of the profiles is fine. Proof profile is of no
+   concern since it is doing a CIELAB to CIELAB mapping in the mashed up
+   transform. */
+static int
+gsicc_verify_device_profiles(gx_device * pdev)
+{
+    int k;
+    bool objects = false;
+    cmm_dev_profile_t *dev_icc = pdev->icc_struct;
+
+    if (dev_icc->device_profile[0] == NULL)
+        return 0;
+
+    if (dev_icc->postren_profile != NULL && dev_icc->link_profile != NULL) {
+        return gs_rethrow(-1, "Post render profile not allowed with device link profile");
+    }
+
+    if (dev_icc->blend_profile != NULL) {
+        if (!(dev_icc->blend_profile->data_cs == gsGRAY ||
+            dev_icc->blend_profile->data_cs == gsRGB ||
+            dev_icc->blend_profile->data_cs == gsCMYK))
+            return gs_rethrow(-1, "Blending color space must be Gray, RGB or CMYK");
+    }
+
+    if (dev_icc->postren_profile != NULL) {
+        if (pdev->procs.dev_spec_op == NULL ||
+            !dev_proc(pdev, dev_spec_op)(pdev, gxdso_supports_iccpostrender, NULL, 0)) {
+            return gs_rethrow(-1, "Post render profile not supported by device");
+        }
+        if (!dev_proc(pdev, dev_spec_op)(pdev, gxdso_supports_devn, NULL, 0)) {
+            if (dev_icc->postren_profile->num_comps !=
+                pdev->color_info.num_components) {
+                return gs_rethrow(-1, "Post render profile does not match the device color model");
+            }
+            return 0;
+        }
+        return 0; /* An interesting case with sep device.  Need to do a bit of testing here */
+    }
+
+    for (k = 1; k < NUM_DEVICE_PROFILES; k++) {
+        if (dev_icc->device_profile[k] != NULL) {
+            objects = true;
+            break;
+        }
+    }
+
+    if (dev_icc->link_profile == NULL) {
+        if (!objects) {
+            if (dev_icc->device_profile[0]->num_comps !=
+                pdev->color_info.num_components)
+                return gs_rethrow(-1, "Mismatch of ICC profiles and device color model");
+            else
+                return 0;
+        } else {
+            for (k = 1; k < NUM_DEVICE_PROFILES; k++)
+                if (dev_icc->device_profile[k] != NULL) {
+                    if (dev_icc->device_profile[k]->num_comps !=
+                        pdev->color_info.num_components)
+                        return gs_rethrow(-1, "Mismatch of object dependent ICC profiles and device color model");
+                }
+            return 0;
+        }
+    } else {
+        /* The input of the device link must match the output of the device
+           profile and the device link output must match the device color
+           model */
+        if (dev_icc->link_profile->num_comps_out !=
+            pdev->color_info.num_components) {
+            return gs_rethrow(-1, "Mismatch of device link profile and device color model");
+        }
+        for (k = 0; k < NUM_DEVICE_PROFILES; k++) {
+            if (dev_icc->device_profile[k] != NULL) {
+                if (dev_icc->device_profile[k]->num_comps !=
+                    dev_icc->link_profile->num_comps) {
+                    return gs_rethrow(-1, "Mismatch of device link profile and device ICC profile");
+                }
+            }
+        }
+        return 0;
+    }
+}
+
 /*  This computes the hash code for the device profile and assigns the profile
     in the icc_struct member variable of the device.  This should
     really occur only one time, but may occur twice if a color model is
@@ -1819,6 +1902,7 @@ gsicc_set_device_profile(gx_device * pdev, gs_memory_t * mem,
                     pdev->icc_struct->blend_profile = icc_profile;
                 }
             }
+
             /* Get the profile handle */
             icc_profile->profile_handle =
                 gsicc_get_profile_handle_buffer(icc_profile->buffer,
@@ -1826,12 +1910,14 @@ gsicc_set_device_profile(gx_device * pdev, gs_memory_t * mem,
                                                 mem);
             if (icc_profile->profile_handle == NULL)
                 return_error(gs_error_unknownerror);
+
             /* Compute the hash code of the profile. Everything in the
                ICC manager will have it's hash code precomputed */
             gsicc_get_icc_buff_hash(icc_profile->buffer,
                                     &(icc_profile->hashcode),
                                     icc_profile->buffer_size);
             icc_profile->hash_is_valid = true;
+
             /* Get the number of channels in the output profile */
             icc_profile->num_comps =
                 gscms_get_input_channel_count(icc_profile->profile_handle,
@@ -1844,6 +1930,12 @@ gsicc_set_device_profile(gx_device * pdev, gs_memory_t * mem,
             icc_profile->data_cs =
                 gscms_get_profile_data_space(icc_profile->profile_handle,
                     icc_profile->memory);
+
+            /* Check that everything is OK with regard to the number of
+               components. */
+            if (gsicc_verify_device_profiles(pdev) < 0)
+                return gs_rethrow(-1, "Error in device profiles");
+
             /* We need to know if this is one of the "default" profiles or
                if someone has externally set it.  The reason is that if there
                is an output intent in the file, and someone wants to use the

devices/devs.mak
@@ -1669,7 +1669,7 @@ $(DD)tiffgray.dev : $(libtiff_dev) $(tiffgray_) $(DD)tiffs.dev\
 $(DEVOBJ)gdevtsep.$(OBJ) : $(DEVSRC)gdevtsep.c $(PDEVH) $(stdint__h)\
  $(gdevtifs_h) $(gdevdevn_h) $(gxdevsop_h) $(gsequivc_h) $(stdio__h) $(ctype__h)\
  $(gxgetbit_h) $(gdevppla_h) $(gp_h) $(gstiffio_h)\
- $(gscms_h) $(gsicc_cache_h) $(GDEV) $(DEVS_MAK) $(MAKEDIRS)
+ $(gscms_h) $(gsicc_cache_h) $(gxdevsop_h) $(GDEV) $(DEVS_MAK) $(MAKEDIRS)
 	$(DEVCC) $(I_)$(TI_)$(_I) $(DEVO_)gdevtsep.$(OBJ) $(C_) $(DEVSRC)gdevtsep.c
 
 # TIFF Scaled (downscaled gray -> mono), configurable compression

base/gdevdflt.c
@@ -1021,6 +1021,7 @@ gx_default_dev_spec_op(gx_device *pdev, int dev_spec_op, void *data, int size)
         case gxdso_supports_hlcolor:
         case gxdso_supports_saved_pages:
         case gxdso_needs_invariant_palette:
+        case gxdso_supports_iccpostrender:
             return 0;
         case gxdso_pattern_shfill_doesnt_need_path:
             return (dev_proc(pdev, fill_path) == gx_default_fill_path);

base/gsdparam.c
@@ -1620,6 +1620,18 @@ nce:
             param_signal_error(plist, "ICCOutputColors", ecode);
         }
     }
+    if ((code = param_read_string(plist, "DeviceLinkProfile", &icc_pro)) != 1) {
+        if ((code = gx_default_put_icc(&icc_pro, dev, gsLINKPROFILE)) < 0) {
+            ecode = code;
+            param_signal_error(plist, "DeviceLinkProfile", ecode);
+        }
+    }
+    if ((code = param_read_string(plist, "PostRenderProfile", &icc_pro)) != 1) {
+        if ((code = gx_default_put_icc(&icc_pro, dev, gsPRPROFILE)) < 0) {
+            ecode = code;
+            param_signal_error(plist, "PostRenderProfile", ecode);
+        }
+    }
     if ((code = param_read_string(plist, "OutputICCProfile", &icc_pro)) != 1) {
         if ((code = gx_default_put_icc(&icc_pro, dev, gsDEFAULTPROFILE)) < 0) {
             ecode = code;
@@ -1650,18 +1662,6 @@ nce:
         if ((code = gx_default_put_icc(&icc_pro, dev, gsPROOFPROFILE)) < 0) {
             ecode = code;
             param_signal_error(plist, "ProofProfile", ecode);
-        }
-    }
-    if ((code = param_read_string(plist, "DeviceLinkProfile", &icc_pro)) != 1) {
-        if ((code = gx_default_put_icc(&icc_pro, dev, gsLINKPROFILE)) < 0) {
-            ecode = code;
-            param_signal_error(plist, "DeviceLinkProfile", ecode);
-        }
-    }
-    if ((code = param_read_string(plist, "PostRenderProfile", &icc_pro)) != 1) {
-        if ((code = gx_default_put_icc(&icc_pro, dev, gsPRPROFILE)) < 0) {
-            ecode = code;
-            param_signal_error(plist, "PostRenderProfile", ecode);
         }
     }
     if ((code = param_read_string(plist, "BlendColorProfile", &icc_pro)) != 1) {

base/gxdevsop.h
@@ -325,6 +325,7 @@ enum {
     gxdso_JPEG_passthrough_begin,
     gxdso_JPEG_passthrough_data,
     gxdso_JPEG_passthrough_end,
+    gxdso_supports_iccpostrender,
     /* Add new gxdso_ keys above this. */
     gxdso_pattern__LAST
 };

base/lib.mak
@@ -2882,7 +2882,7 @@ $(GLOBJ)gsicc_manage.$(OBJ) : $(GLSRC)gsicc_manage.c $(AK) $(gx_h)\
  $(gxgstate_h) $(gxcspace_h) $(gscms_h) $(gsicc_manage_h) $(gsicc_cache_h)\
  $(gsicc_profilecache_h) $(gserrors_h) $(string__h) $(gxclist_h) $(gxcldev_h)\
  $(gzstate_h) $(gsicc_create_h) $(gpmisc_h) $(gxdevice_h) $(std_h)\
- $(gsicc_cms_h) $(gp_h) $(LIB_MAK) $(MAKEDIRS)
+ $(gsicc_cms_h) $(gp_h) $(gxdevsop_h) $(LIB_MAK) $(MAKEDIRS)
 	$(GLCC) $(GLO_)gsicc_manage.$(OBJ) $(C_) $(GLSRC)gsicc_manage.c
 
 $(GLOBJ)gsicc_cache.$(OBJ) : $(GLSRC)gsicc_cache.c $(AK) $(gx_h)\

commit 250b5e83e923bed9b6d48bf3965f3b38af52286d

Fix errors introduced in previous commit
The commit f2cf68297e3d63cb927db3c98d317f7ee68e7898
resulted in errors with the separation type devices.
With these devices, we can simply check if the color
model matches the ICC profile since these devices
change their number of components.  Will likely need
to do some testing with these device and different
profiles to see what breaks when and make sure we
exit gracefully.

base/gsicc_manage.c
@@ -1764,8 +1764,15 @@ static int
 gsicc_verify_device_profiles(gx_device * pdev)
 {
     int k;
-    bool objects = false;
     cmm_dev_profile_t *dev_icc = pdev->icc_struct;
+    bool is_sep = false;
+    bool can_postrender = false;
+    bool objects = false;
+
+    if (pdev->procs.dev_spec_op != NULL) {
+        is_sep = dev_proc(pdev, dev_spec_op)(pdev, gxdso_supports_devn, NULL, 0);
+        can_postrender = dev_proc(pdev, dev_spec_op)(pdev, gxdso_supports_iccpostrender, NULL, 0);
+    }
 
     if (dev_icc->device_profile[0] == NULL)
         return 0;
@@ -1782,11 +1789,10 @@ gsicc_verify_device_profiles(gx_device * pdev)
     }
 
     if (dev_icc->postren_profile != NULL) {
-        if (pdev->procs.dev_spec_op == NULL ||
-            !dev_proc(pdev, dev_spec_op)(pdev, gxdso_supports_iccpostrender, NULL, 0)) {
+        if (!can_postrender) {
             return gs_rethrow(-1, "Post render profile not supported by device");
         }
-        if (!dev_proc(pdev, dev_spec_op)(pdev, gxdso_supports_devn, NULL, 0)) {
+        if (!is_sep) {
             if (dev_icc->postren_profile->num_comps !=
                 pdev->color_info.num_components) {
                 return gs_rethrow(-1, "Post render profile does not match the device color model");
@@ -1805,14 +1811,14 @@ gsicc_verify_device_profiles(gx_device * pdev)
 
     if (dev_icc->link_profile == NULL) {
         if (!objects) {
-            if (dev_icc->device_profile[0]->num_comps !=
+            if (!is_sep && dev_icc->device_profile[0]->num_comps !=
                 pdev->color_info.num_components)
                 return gs_rethrow(-1, "Mismatch of ICC profiles and device color model");
             else
-                return 0;
+                return 0;  /* Currently sep devices have some leeway here */
         } else {
             for (k = 1; k < NUM_DEVICE_PROFILES; k++)
-                if (dev_icc->device_profile[k] != NULL) {
+                if (!is_sep && dev_icc->device_profile[k] != NULL) {
                     if (dev_icc->device_profile[k]->num_comps !=
                         pdev->color_info.num_components)
                         return gs_rethrow(-1, "Mismatch of object dependent ICC profiles and device color model");
@@ -1823,12 +1829,12 @@ gsicc_verify_device_profiles(gx_device * pdev)
         /* The input of the device link must match the output of the device
            profile and the device link output must match the device color
            model */
-        if (dev_icc->link_profile->num_comps_out !=
+        if (!is_sep && dev_icc->link_profile->num_comps_out !=
             pdev->color_info.num_components) {
             return gs_rethrow(-1, "Mismatch of device link profile and device color model");
         }
         for (k = 0; k < NUM_DEVICE_PROFILES; k++) {
-            if (dev_icc->device_profile[k] != NULL) {
+            if (!is_sep && dev_icc->device_profile[k] != NULL) {
                 if (dev_icc->device_profile[k]->num_comps !=
                     dev_icc->link_profile->num_comps) {
                     return gs_rethrow(-1, "Mismatch of device link profile and device ICC profile");

devices/devs.mak
@@ -1205,7 +1205,7 @@ $(DEVOBJ)gdevpsd.$(OBJ) : $(DEVSRC)gdevpsd.c $(PDEVH) $(math__h)\
  $(gdevdcrd_h) $(gscrd_h) $(gscrdp_h) $(gsparam_h) $(gxlum_h)\
  $(gstypes_h) $(gxdcconv_h) $(gdevdevn_h) $(gxdevsop_h) $(gsequivc_h)\
  $(gscms_h) $(gsicc_cache_h) $(gsicc_manage_h) $(gxgetbit_h)\
- $(gdevppla_h) $(gxiodev_h) $(gdevpsd_h) $(DEVS_MAK) $(MAKEDIRS)
+ $(gdevppla_h) $(gxiodev_h) $(gdevpsd_h) $(gxdevsop_h) $(DEVS_MAK) $(MAKEDIRS)
 	$(DEVCC) $(DEVO_)gdevpsd.$(OBJ) $(C_) $(DEVSRC)gdevpsd.c
 
 ### --------------------------- The GPRF device ------------------------- ###
@@ -1219,14 +1219,14 @@ $(DEVOBJ)gdevgprf_1.$(OBJ) : $(DEVSRC)gdevgprf.c $(PDEVH) $(math__h)\
  $(gdevdcrd_h) $(gscrd_h) $(gscrdp_h) $(gsparam_h) $(gxlum_h)\
  $(gstypes_h) $(gxdcconv_h) $(gdevdevn_h) $(gsequivc_h) \
  $(gscms_h) $(gsicc_cache_h) $(gsicc_manage_h) $(gxgetbit_h)\
- $(gdevppla_h) $(DEVS_MAK) $(MAKEDIRS)
+ $(gdevppla_h) $(gxdevsop_h) $(DEVS_MAK) $(MAKEDIRS)
 	$(DEVCC) $(DEVO_)gdevgprf_1.$(OBJ) $(II)$(ZI_)$(_I) $(C_) $(DEVSRC)gdevgprf.c
 
 $(DEVOBJ)gdevgprf_0.$(OBJ) : $(DEVSRC)gdevgprf.c $(PDEVH) $(math__h)\
  $(gdevdcrd_h) $(gscrd_h) $(gscrdp_h) $(gsparam_h) $(gxlum_h)\
  $(gstypes_h) $(gxdcconv_h) $(gdevdevn_h) $(gsequivc_h) $(zlib_h)\
  $(gscms_h) $(gsicc_cache_h) $(gsicc_manage_h) $(gxgetbit_h)\
- $(gdevppla_h) $(DEVS_MAK) $(MAKEDIRS)
+ $(gdevppla_h) $(gxdevsop_h) $(DEVS_MAK) $(MAKEDIRS)
 	$(DEVCC) $(DEVO_)gdevgprf_0.$(OBJ) $(II)$(ZI_)$(_I) $(C_) $(DEVSRC)gdevgprf.c
 
 $(DEVOBJ)gdevgprf.$(OBJ) : $(DEVOBJ)gdevgprf_$(SHARE_ZLIB).$(OBJ) $(DEVS_MAK) $(MAKEDIRS)

devices/gdevcmykog.c
@@ -213,6 +213,9 @@ cmykog_dev_spec_op(gx_device *dev_, int op, void *data, int datasize)
       if (code != gs_error_undefined)
           return code;
   }
+  if (op == gxdso_supports_devn) {
+    return true;
+  }
   return gx_default_dev_spec_op(dev_, op, data, datasize);
 }
 

devices/gdevgprf.c
@@ -35,6 +35,7 @@
 #include "gdevppla.h"
 #include "gxdownscale.h"
 #include "gdevdevnprn.h"
+#include "gxdevsop.h"
 
 #ifndef MAX_CHAN
 #   define MAX_CHAN 15
@@ -88,6 +89,15 @@ static RELOC_PTRS_WITH(gprf_device_reloc_ptrs, gprf_device *pdev)
     (void)pdev; /* Stop unused var warning */
 }
 RELOC_PTRS_END
+
+static int
+gprf_spec_op(gx_device *dev_, int op, void *data, int datasize)
+{
+    if (op == gxdso_supports_devn) {
+        return true;
+    }
+    return gx_default_dev_spec_op(dev_, op, data, datasize);
+}
 
 /* Even though gprf_device_finalize is the same as gx_devn_prn_device_finalize,
  * we need to implement it separately because st_composite_final
@@ -167,7 +177,12 @@ gs_private_st_composite_final(st_gprf_device, gprf_device,
         NULL,				/* fill_linear_color_trapezoid */\
         NULL,				/* fill_linear_color_triangle */\
         gx_devn_prn_update_spot_equivalent_colors, /* update_spot_equivalent_colors */\
-        gx_devn_prn_ret_devn_params	/* ret_devn_params */\
+        gx_devn_prn_ret_devn_params, /* ret_devn_params */\
+        NULL,                        /* fillpage */\
+        NULL,                        /* push_transparency_state */\
+        NULL,                        /* pop_transparency_state */\
+        NULL,                        /* put_image */\
+        gprf_spec_op                 /* dev_spec_op */\
 }
 
 #define gprf_device_body(procs, dname, ncomp, pol, depth, mg, mc, sl, cn)\
@@ -236,16 +251,16 @@ gprf_prn_open(gx_device * pdev)
     pdev->log2_align_mod = 6;
 #endif
 
-    /* There are 2 approaches to the use of a DeviceN ICC output profile.  
+    /* There are 2 approaches to the use of a DeviceN ICC output profile.
        One is to simply limit our device to only output the colorants
-       defined in the output ICC profile.   The other is to use the 
+       defined in the output ICC profile.   The other is to use the
        DeviceN ICC profile to color manage those N colorants and
-       to let any other separations pass through unmolested.   The define 
+       to let any other separations pass through unmolested.   The define
        LIMIT_TO_ICC sets the option to limit our device to only the ICC
        colorants defined by -sICCOutputColors (or to the ones that are used
-       as default names if ICCOutputColors is not used).  The pass through option 
-       (LIMIT_TO_ICC set to 0) makes life a bit more difficult since we don't 
-       know if the page_spot_colors overlap with any spot colorants that exist 
+       as default names if ICCOutputColors is not used).  The pass through option
+       (LIMIT_TO_ICC set to 0) makes life a bit more difficult since we don't
+       know if the page_spot_colors overlap with any spot colorants that exist
        in the DeviceN ICC output profile. Hence we don't know how many planes
        to use for our device.  This is similar to the issue when processing
        a PostScript file.  So that I remember, the cases are
@@ -274,7 +289,7 @@ gprf_prn_open(gx_device * pdev)
     pdev_gprf->warning_given = false;
     /* With planar the depth can be more than 64.  Update the color
        info to reflect the proper depth and number of planes.  Also note
-       that the number of spot colors can change from page to page.  
+       that the number of spot colors can change from page to page.
        Update things so that we only output separations for the
        inks on that page. */
 
@@ -321,14 +336,14 @@ gprf_prn_open(gx_device * pdev)
         for (k = 0; k < GS_CLIENT_COLOR_MAX_COMPONENTS; k++) {
             pdev_gprf->devn_params.separation_order_map[k] = k;
         }
-    pdev->color_info.depth = pdev->color_info.num_components * 
+    pdev->color_info.depth = pdev->color_info.num_components *
                              pdev_gprf->devn_params.bitspercomponent;
     pdev->color_info.separable_and_linear = GX_CINFO_SEP_LIN;
     profile_struct->supports_devn = true;
     code = gdev_prn_open_planar(pdev, true);
 
     /* Take care of the ICC transform now.  There are several possible ways
-     * that this could go.  One is that the CMYK colors are color managed 
+     * that this could go.  One is that the CMYK colors are color managed
      * but the non-standard colorants are not.  That is, we have specified
      * a CMYK profile for our device but we have other spot colorants in
      * the source file.  In this case, we will do managed proofing for the
@@ -336,10 +351,10 @@ gprf_prn_open(gx_device * pdev)
      * DeviceN colorants will be problematic in this case and we will do the
      * the best we can with the tools given to us.   The other possibility is
      * that someone has given an NColor ICC profile for the device and we can
-     * actually deal with the mapping directly. This is a much cleaner case 
-     * but as we know NColor profiles are not too common.  Also we need to 
-     * deal with transparent colorants (e.g. varnish) in an intelligent manner. 
-     * Note that we require the post rendering profile to be RGB based for 
+     * actually deal with the mapping directly. This is a much cleaner case
+     * but as we know NColor profiles are not too common.  Also we need to
+     * deal with transparent colorants (e.g. varnish) in an intelligent manner.
+     * Note that we require the post rendering profile to be RGB based for
      * this particular device. */
 
     if (pdev_gprf->icclink == NULL && profile_struct->device_profile[0] != NULL
@@ -364,7 +379,7 @@ gprf_prn_open(gx_device * pdev)
 * The following procedures are used to map the standard color spaces into
 * the color components. This is not where the color management occurs.  These
 * may do a reordering of the colors following some ICC base color management
-* or they may be used in the case of -dUseFastColor as that relies upon the 
+* or they may be used in the case of -dUseFastColor as that relies upon the
 * device color mapping procedures
 */
 static void
@@ -524,14 +539,14 @@ gprf_get_color_comp_index(gx_device * dev, const char * pname,
     if (strncmp(pname, "None", name_size) == 0) return -1;
     index = gx_devn_prn_get_color_comp_index(dev, pname, name_size,
                                              component_type);
-    /* This is a one shot deal.  That is it will simply post a notice once that 
+    /* This is a one shot deal.  That is it will simply post a notice once that
        some colorants will be converted due to a limit being reached.  It will
-       not list names of colorants since then I would need to keep track of 
+       not list names of colorants since then I would need to keep track of
        which ones I have already mentioned.  Also, if someone is fooling with
        num_order, then this warning is not given since they should know what
        is going on already */
-    if (index < 0 && component_type == SEPARATION_NAME && 
-        pdev->warning_given == false && 
+    if (index < 0 && component_type == SEPARATION_NAME &&
+        pdev->warning_given == false &&
         pdev->devn_params.num_separation_order_names == 0) {
         dmlprintf(dev->memory, "**** Max spot colorants reached.\n");
         dmlprintf(dev->memory, "**** Some colorants will be converted to equivalent CMYK values.\n");
@@ -603,7 +618,7 @@ gprf_setup(gprf_write_ctx *xc, gx_device_printer *pdev, FILE *file, int w, int h
      * which planes are actually imaged.  For the process color model channels
      * we image the channels which are requested.  Non requested process color
      * model channels are simply filled with white.  For spot colors we only
-     * image the requested channels. 
+     * image the requested channels.
      */
     for (i = 0; i < xc->num_channels + xc->n_extra_channels; i++) {
         xc->chnl_to_position[i] = i;
@@ -779,7 +794,7 @@ gprf_write_header(gprf_write_ctx *xc)
             cmyk[3] = (k < 0 ? 0 : (k > 255 ? 255 : k));
         }
 
-        /* Convert color to RGBA. To get the A value, we are going to need to 
+        /* Convert color to RGBA. To get the A value, we are going to need to
            deal with the mixing hints information in the PDF content.  A ToDo
            project. At this point, everything has an alpha of 1.0 */
         rgba[3] = 255;
@@ -975,14 +990,14 @@ compressAndWrite(gprf_write_ctx *xc, byte *data, int tile_w, int tile_h, int ras
     }
     deflate(&zstm, Z_FINISH);
     deflateEnd(&zstm);
-   
+
     code = gprf_write(xc, xc->deflate_block, xc->deflate_bound - zstm.avail_out);
     if (code < 0)
         return_error(gs_error_ioerror);
     return 0;
 }
 
-/* If the profile is NULL or if the profile does not support the spot 
+/* If the profile is NULL or if the profile does not support the spot
    colors then we have to calculate the equivalent CMYK values and then color
    manage. */
 static void
@@ -1036,7 +1051,7 @@ build_cmyk_planar_raster(gprf_write_ctx *xc, byte *planes[], byte *cmyk_in,
 
 /* Create RGB from CMYK the horribly slow way */
 static void
-get_rgb_planar_line(gprf_write_ctx *xc, byte *c, byte *m, byte *y, byte *k, 
+get_rgb_planar_line(gprf_write_ctx *xc, byte *c, byte *m, byte *y, byte *k,
     byte *red_in, byte *green_in, byte *blue_in, int width)
 {
     int x_pos;
@@ -1280,7 +1295,7 @@ gprf_write_image_data(gprf_write_ctx *xc)
 
 cleanup:
     gx_downscaler_fin(&ds);
-    gs_free_object(pdev->memory, planes[0], 
+    gs_free_object(pdev->memory, planes[0],
                     "gprf_write_image_data");
     gs_free_object(pdev->memory, rgb[0],
                    "gprf_write_image_data");

devices/gdevpsd.c
@@ -36,6 +36,7 @@
 #include "gxdownscale.h"
 #include "gdevdevnprn.h"
 #include "gdevpsd.h"
+#include "gxdevsop.h"
 
 #ifndef cmm_gcmmhlink_DEFINED
     #define cmm_gcmmhlink_DEFINED
@@ -135,6 +136,15 @@ static RELOC_PTRS_WITH(psd_device_reloc_ptrs, psd_device *pdev)
 }
 RELOC_PTRS_END
 
+static int
+psd_spec_op(gx_device *dev_, int op, void *data, int datasize)
+{
+    if (op == gxdso_supports_devn) {
+        return true;
+    }
+    return gx_default_dev_spec_op(dev_, op, data, datasize);
+}
+
 /* Even though psd_device_finalize is the same as gx_devn_prn_device_finalize,
  * we need to implement it separately because st_composite_final
  * declares all 3 procedures as private. */
@@ -213,7 +223,12 @@ gs_private_st_composite_final(st_psd_device, psd_device,
         NULL,				/* fill_linear_color_trapezoid */\
         NULL,				/* fill_linear_color_triangle */\
         gx_devn_prn_update_spot_equivalent_colors, /* update_spot_equivalent_colors */\
-        gx_devn_prn_ret_devn_params	/* ret_devn_params */\
+        gx_devn_prn_ret_devn_params, /* ret_devn_params */\
+        NULL,                       /* fillpage */\
+        NULL,                       /* push_transparency_state */\
+        NULL,                       /* pop_transparency_state */\
+        NULL,                       /* put_image */\
+        psd_spec_op                 /* dev_spec_op */\
 }
 
 static fixed_colorant_name DeviceGrayComponents[] = {