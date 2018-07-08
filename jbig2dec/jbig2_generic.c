7f4cd0ad4c0d8636141de50bb6eb76b17e574eb5
Sebastian Rasmussen <sebras@gmail.com>
2018-07-08 22:05:57 +0800

    jbig2dec: Fix accidental typo in context size function.

--- jbig2dec/jbig2_generic.c
+++ jbig2dec/jbig2_generic.c
@@ -50,7 +50,7 @@
 int
 jbig2_generic_stats_size(Jbig2Ctx *ctx, int template)
 {
-    int stats_size = template == 0 ? 1 << 16 : template == 1 ? 1 << 1 << 13 : 1 << 10;
+    int stats_size = template == 0 ? 1 << 16 : template == 1 ? 1 << 13 : 1 << 10;
 
     return stats_size;
 }
