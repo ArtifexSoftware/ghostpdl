78911a01b67d590b4a91afac2e8417360b934156
Chris Liddell <chris.liddell@artifex.com>
2018-08-23 09:54:59 +0100

    Bug 699654: Check the restore operand type
    The primary function that implements restore correctly checked its parameter,
    but a function that does some preliminary work for the restore (gstate and
    device handling) did not check.
    
    So, even though the restore correctly errored out, it left things partially done
    and, in particular, the device in partially restored state. Meaning the
    LockSafetyParams was not correctly set.

--- psi/zdevice2.c
+++ psi/zdevice2.c
@@ -312,6 +312,9 @@ z2grestoreall(i_ctx_t *i_ctx_p)
 static int
 z2restore(i_ctx_t *i_ctx_p)
 {
+    os_ptr op = osp;
+    check_type(*op, t_save);
+
     while (gs_gstate_saved(gs_gstate_saved(igs))) {
         if (restore_page_device(igs, gs_gstate_saved(igs)))
             return push_callout(i_ctx_p, "%restore1pagedevice");
