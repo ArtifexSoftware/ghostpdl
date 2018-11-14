bfddf70400bb1fd4b3f7bfd72f333ad09a0b6cba
Chris Liddell <chris.liddell@artifex.com>
2018-11-14 22:29:24 +0000

    Add a setweightvector analogue in Freetype
    Avoids the need to recreate the FT font everytime the weightvector changes.
    
    These Freetype changes have been accepted upstream:
    http://git.savannah.gnu.org/cgit/freetype/freetype2.git/commit/?id=78a1e69517ca067224d6e33beffd25e2ec8361d2

--- freetype/include/freetype/ftmm.h
+++ freetype/include/freetype/ftmm.h
@@ -527,6 +527,87 @@ FT_BEGIN_HEADER
   /*************************************************************************/
   /*                                                                       */
   /* <Function>                                                            */
+  /*    FT_Set_MM_WeightVector                                             */
+  /*                                                                       */
+  /* <Description>                                                         */
+  /*    For Adobe MM fonts, choose an interpolated font design by          */
+  /*    directly setting the weight vector.                                */
+  /*                                                                       */
+  /*    This function can't be used with TrueType GX or OpenType variation */
+  /*    fonts.                                                             */
+  /*                                                                       */
+  /* <InOut>                                                               */
+  /*    face         :: A handle to the source face.                       */
+  /*                                                                       */
+  /* <Input>                                                               */
+  /*    len          :: The length of the weight vector array. If it is    */
+  /*                    larger than the number of designs, the extra       */
+  /*                    values are ignored. If it is less than the number  */
+  /*                    of designs, the remaining values are set to zero.  */
+  /*                                                                       */
+  /*    weightvector :: An array representing the weight vector.           */
+  /*                                                                       */
+  /* <Return>                                                              */
+  /*    FreeType error code.  0~means success.                             */
+  /*                                                                       */
+  /* <Note>                                                                */
+  /*       Adobe Multiple Master fonts limit the number of designs, and    */
+  /*       thus the length of the WeightVector to 16.                      */
+  /*                                                                       */
+  /*       If len is zero and weightvector is NULL, the WeightVector is    */
+  /*       reset to the default values.                                    */
+  /*       The Adobe documentation also states that the values in the      */
+  /*       WeightVector must total 1.0 +/- 0.001. In practice this does    */
+  /*       not seem to be enforced, so is not enforced, here either.       */
+  /*                                                                       */
+  FT_EXPORT( FT_Error )
+  FT_Set_MM_WeightVector( FT_Face    face,
+                          FT_UInt    len,
+                          FT_Fixed*  weightvector );
+
+  /*************************************************************************/
+  /*                                                                       */
+  /* <Function>                                                            */
+  /*    FT_Get_MM_WeightVector                                             */
+  /*                                                                       */
+  /* <Description>                                                         */
+  /*    For Adobe MM fonts, retrieve the current weight vector of the font */
+  /*                                                                       */
+  /*    This function can't be used with TrueType GX or OpenType variation */
+  /*    fonts.                                                             */
+  /*                                                                       */
+  /* <InOut>                                                               */
+  /*    face         :: A handle to the source face.                       */
+  /*                                                                       */
+  /* <InOut>                                                               */
+  /*    len          :: Pointer to the size of the array to be filled. If  */
+  /*                    size of the array less than the number of designs, */
+  /*                    an Invalid_Argument is returned, and len is set to */
+  /*                    the required size (the number of designs). If the  */
+  /*                    size of the array is greater than the number of    */
+  /*                    designs, the remaining entries are set to 0. On    */
+  /*                    successful completion, len is set to the number of */
+  /*                    designs (i.e. the number of values written to the  */
+  /*                    array.                                             */
+  /*                                                                       */
+  /* <Output>                                                              */
+  /*    weightvector :: An array to be filled.                             */
+  /*                                                                       */
+  /* <Return>                                                              */
+  /*    FreeType error code.  0~means success.                             */
+  /*                                                                       */
+  /* <Note>                                                                */
+  /*       Adobe Multiple Master fonts limit the number of designs, and    */
+  /*       thus the length of the WeightVector to 16.                      */
+  /*                                                                       */
+  FT_EXPORT( FT_Error )
+  FT_Get_MM_WeightVector( FT_Face   face,
+                          FT_UInt*  len,
+                          FT_Fixed* weightvector );
+
+  /*************************************************************************/
+  /*                                                                       */
+  /* <Function>                                                            */
   /*    FT_Get_Var_Blend_Coordinates                                       */
   /*                                                                       */
   /* <Description>                                                         */
--- freetype/include/freetype/internal/services/svmm.h
+++ freetype/include/freetype/internal/services/svmm.h
@@ -86,17 +86,31 @@ FT_BEGIN_HEADER
   typedef void
   (*FT_Done_Blend_Func)( FT_Face );
 
+  /* use return value -1 to indicate that the new coordinates  */
+  /* are equal to the current ones; no changes are thus needed */
+  typedef FT_Error
+  (*FT_Set_MM_WeightVector_Func)( FT_Face    face,
+                                  FT_UInt    len,
+                                  FT_Fixed*  weight_vector );
+
+  typedef FT_Error
+  (*FT_Get_MM_WeightVector_Func)( FT_Face   face,
+                                  FT_UInt*  len,
+                                  FT_Fixed* weight_vector );
+
 
   FT_DEFINE_SERVICE( MultiMasters )
   {
-    FT_Get_MM_Func          get_mm;
-    FT_Set_MM_Design_Func   set_mm_design;
-    FT_Set_MM_Blend_Func    set_mm_blend;
-    FT_Get_MM_Blend_Func    get_mm_blend;
-    FT_Get_MM_Var_Func      get_mm_var;
-    FT_Set_Var_Design_Func  set_var_design;
-    FT_Get_Var_Design_Func  get_var_design;
-    FT_Set_Instance_Func    set_instance;
+    FT_Get_MM_Func                 get_mm;
+    FT_Set_MM_Design_Func          set_mm_design;
+    FT_Set_MM_Blend_Func           set_mm_blend;
+    FT_Get_MM_Blend_Func           get_mm_blend;
+    FT_Get_MM_Var_Func             get_mm_var;
+    FT_Set_Var_Design_Func         set_var_design;
+    FT_Get_Var_Design_Func         get_var_design;
+    FT_Set_Instance_Func           set_instance;
+    FT_Set_MM_WeightVector_Func    set_mm_weightvector;
+    FT_Get_MM_WeightVector_Func    get_mm_weightvector;
 
     /* for internal use; only needed for code sharing between modules */
     FT_Get_Var_Blend_Func   get_var_blend;
@@ -106,29 +120,33 @@ FT_BEGIN_HEADER
 
 #ifndef FT_CONFIG_OPTION_PIC
 
-#define FT_DEFINE_SERVICE_MULTIMASTERSREC( class_,          \
-                                           get_mm_,         \
-                                           set_mm_design_,  \
-                                           set_mm_blend_,   \
-                                           get_mm_blend_,   \
-                                           get_mm_var_,     \
-                                           set_var_design_, \
-                                           get_var_design_, \
-                                           set_instance_,   \
-                                           get_var_blend_,  \
-                                           done_blend_ )    \
-  static const FT_Service_MultiMastersRec  class_ =         \
-  {                                                         \
-    get_mm_,                                                \
-    set_mm_design_,                                         \
-    set_mm_blend_,                                          \
-    get_mm_blend_,                                          \
-    get_mm_var_,                                            \
-    set_var_design_,                                        \
-    get_var_design_,                                        \
-    set_instance_,                                          \
-    get_var_blend_,                                         \
-    done_blend_                                             \
+#define FT_DEFINE_SERVICE_MULTIMASTERSREC( class_,           \
+                                           get_mm_,          \
+                                           set_mm_design_,   \
+                                           set_mm_blend_,    \
+                                           get_mm_blend_,    \
+                                           get_mm_var_,      \
+                                           set_var_design_,  \
+                                           get_var_design_,  \
+                                           set_instance_,    \
+                                           set_weightvector_,\
+                                           get_weightvector_,\
+                                           get_var_blend_,   \
+                                           done_blend_ )     \
+  static const FT_Service_MultiMastersRec  class_ =          \
+  {                                                          \
+    get_mm_,                                                 \
+    set_mm_design_,                                          \
+    set_mm_blend_,                                           \
+    get_mm_blend_,                                           \
+    get_mm_var_,                                             \
+    set_var_design_,                                         \
+    get_var_design_,                                         \
+    set_instance_,                                           \
+    set_weightvector_,                                       \
+    get_weightvector_,                                       \
+    get_var_blend_,                                          \
+    done_blend_                                              \
   };
 
 #else /* FT_CONFIG_OPTION_PIC */
@@ -142,21 +160,25 @@ FT_BEGIN_HEADER
                                            set_var_design_,      \
                                            get_var_design_,      \
                                            set_instance_,        \
+                                           set_weightvector,     \
+                                           get_weightvector,     \
                                            get_var_blend_,       \
                                            done_blend_ )         \
   void                                                           \
   FT_Init_Class_ ## class_( FT_Service_MultiMastersRec*  clazz ) \
   {                                                              \
-    clazz->get_mm         = get_mm_;                             \
-    clazz->set_mm_design  = set_mm_design_;                      \
-    clazz->set_mm_blend   = set_mm_blend_;                       \
-    clazz->get_mm_blend   = get_mm_blend_;                       \
-    clazz->get_mm_var     = get_mm_var_;                         \
-    clazz->set_var_design = set_var_design_;                     \
-    clazz->get_var_design = get_var_design_;                     \
-    clazz->set_instance   = set_instance_;                       \
-    clazz->get_var_blend  = get_var_blend_;                      \
-    clazz->done_blend     = done_blend_;                         \
+    clazz->get_mm           = get_mm_;                             \
+    clazz->set_mm_design    = set_mm_design_;                      \
+    clazz->set_mm_blend     = set_mm_blend_;                       \
+    clazz->get_mm_blend     = get_mm_blend_;                       \
+    clazz->get_mm_var       = get_mm_var_;                         \
+    clazz->set_var_design   = set_var_design_;                     \
+    clazz->get_var_design   = get_var_design_;                     \
+    clazz->set_instance     = set_instance_;                       \
+    clazz->set_weightvector = set_weightvector;                    \
+    clazz->get_weightvector = get_weightvector;                    \
+    clazz->get_var_blend    = get_var_blend_;                      \
+    clazz->done_blend       = done_blend_;                         \
   }
 
 #endif /* FT_CONFIG_OPTION_PIC */
