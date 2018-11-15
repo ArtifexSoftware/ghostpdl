f0ca2c929171a025e846a2eddd51f767e04c8e23
Ray Johnston <ray.johnston@artifex.com>
2018-11-14 16:56:43 -0800

    Bug 695813. Reduce memory usage for ICC profiles. Example AIX361DC_Save.pdf
    The same ICC profiles and colorspace were used many times (in the example
    the cited PDF_1.7_ATS file) which could result in memory usage growth and
    performance impact associated with loading the same profile many times.
    There was a icc_profile_cache in the gstate, but it was not being used for
    ICCBased colorspaces, only for CIEBased* spaces.
    
    Adding logic to cache ICCBased colorspaces and change ICCBased space to
    be in stable memory so it can exist in the cache. This results in a
    reduction of the RAM needed for the cited file from 1.0Gb down to 44Mb
    and reduced the parse time from 29.4s down to 24.4s (overall time from
    94.5s to 91.3s) for ppmraw 600 dpi.
    
    Note that this optimization results in many fewer CS/cs operations being
    emitted by pdfwrite, but this also can result in text being emitted as
    TJ operations (glyphs with positioning) which can result in single pixel
    shifts in glyphs. Sometimes this looks better, sometimes not, but the
    reduction in size/complexity of the resulting PDF is worth it.

--- base/gsicc_profilecache.c
+++ base/gsicc_profilecache.c
@@ -113,6 +113,9 @@ gsicc_add_cs(gs_gstate * pgs, gs_color_space * colorspace, uint64_t dictkey)
     result->color_space = colorspace;
     rc_increment(colorspace);
     result->key = dictkey;
+    if_debug2m(gs_debug_flag_icc, memory,
+               "[icc] Add cs to cache = 0x%p, hash = %lld \n",
+               result->color_space, (uint64_t)result->key);
     profile_cache->num_entries++;
 }
 
@@ -125,6 +128,9 @@ gsicc_find_cs(uint64_t key_test, gs_gstate * pgs)
     /* Look through the cache for the key. If found, move to MRU */
     while (curr != NULL ){
         if (curr->key == key_test){
+            if_debug2m(gs_debug_flag_icc, pgs->memory,
+                       "[icc] Found cs = 0x%p, hash = %lld \n",
+                       curr->color_space, (uint64_t)curr->key);
             /* If not already at head of list, move this one there */
             if (curr != profile_cache->head) {
                 /* We need to move found one to the top of the list. */
@@ -175,6 +181,9 @@ gsicc_remove_cs_entry(gsicc_profile_cache_t *profile_cache)
     /* Decremented, but someone could still be referencing this */
     /* If found again in the source document, it will be regenerated
        and added back into the cache. */
+    if_debug2m(gs_debug_flag_icc, memory,
+               "[icc] Remove cs from cache = 0x%p, hash = %lld \n",
+               curr->color_space, (uint64_t)curr->key);
     rc_decrement(curr->color_space, "gsicc_remove_cs_entry");
     gs_free_object(memory->stable_memory, curr, "gsicc_remove_cs_entry");
 }
