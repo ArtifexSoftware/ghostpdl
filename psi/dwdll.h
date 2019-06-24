93fc44b3426cd771ee0a9358da805cd2ac93a649
Robin Watts <Robin.Watts@artifex.com>
2019-06-24 13:23:34 +0100

    Fix gsapi exposure through the DLL.
    Lots of functions that should have been exposed, weren't.

--- psi/dwdll.h
+++ psi/dwdll.h
@@ -41,6 +41,19 @@ typedef struct GSDLL_S {
         PFN_gsapi_set_arg_encoding set_arg_encoding;
         PFN_gsapi_set_default_device_list set_default_device_list;
         PFN_gsapi_get_default_device_list get_default_device_list;
+        PFN_gsapi_run_file run_file;
+        PFN_gsapi_run_string_begin run_string_begin;
+        PFN_gsapi_run_string_continue run_string_continue;
+        PFN_gsapi_run_string_end run_string_end;
+        PFN_gsapi_run_string_with_length run_string_with_length;
+        PFN_gsapi_set_param set_param;
+        PFN_gsapi_add_control_path add_control_path;
+        PFN_gsapi_remove_control_path remove_control_path;
+        PFN_gsapi_purge_control_paths purge_control_paths;
+        PFN_gsapi_activate_path_control activate_path_control;
+        PFN_gsapi_is_path_control_active is_path_control_active;
+        PFN_gsapi_add_fs add_fs;
+        PFN_gsapi_remove_fs remove_fs;
 } GSDLL;
 
 /* Load the Ghostscript DLL.
