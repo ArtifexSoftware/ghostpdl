ArtifexSoftware/ghostpdl

commit 7ecbfda92b4c8dbf6f6c2bf8fc82020a29219eff

Add use of new file access permissions to PS interp
(this is a squash of several commits from filesec branch)

Use errno to indicate invalidfileaccess

Integrate fontconfig with file access controls

fontconfig API has a call to retrieve all the directories about which fontconfig
knows, so we can get those, and add them to the paths from which we're permitted
to read.

Add (most of) the Postscript "managed" paths....

...to the access controls.

GenericResourceDir, ICCProfilesDir (reading) and temporary file directories for
read/write/control.

Add paths/files from C to the file access controls

The -I paths, environment variables and build time paths.

The command line specified output file, permit writing.

Lastly, the command line specified input file is added to the readable list,
interpreted, and then removed from the list.

Add the FONTPATH path list to permit read access control list.

I opted to do this in Postscript because the list is (normally) a colon
separated list of paths, which we split into an array of paths (in Postscript) -
it seems sensible to do this only once, both for storage and permissions.

Add a non-standard string 'reverse search" operator

The search operator searches for the first occurrence of a character code in a
string (i.e. searches forwards from the start of the string), 'rsearch' finds
the last instance of a character code (i.e. search backwards from the end of
the string).

Fix gp_open_scratch_file_rm for access permissions

i.e. the same as gp_open_scratch_file

Handle clist file 'rewind' failures

'Rewinding' clist files may involved closing and recreating the temporary files,
which conceivably can fail, but the clist procs rewind method was a void
function, so couldn't return an error.

It now can, and does return an error, and we handle the error.

Add file permissions support for piped output.

Paths from Fontmap to PermitReading list

cidfmap paths, add to PermitReading list

psi/imainarg.c
@@ -309,6 +309,65 @@ gs_main_run_start(gs_main_instance * minst)
     return run_string(minst, "systemdict /start get exec", runFlush, minst->user_errors, NULL, NULL);
 }
 
+/* For the OutputFile permission we have to deal with formattable strings
+   i.e. ones that have "%d" or similar in them. For these we want to replace
+   everything after the %d with a wildcard "*".
+   Since we do this in two places (the -s and -o cases) split it into a
+   function.
+ */
+#define IS_WHITESPACE(c) ((c == 0x20) || (c == 0x9) || (c == 0xD) || (c == 0xA))
+static int
+gs_main_add_outputfile_control_path(gs_memory_t *mem, const char *fname)
+{
+    char *fp, f[gp_file_name_sizeof] = {0};
+    const int percent = 37; /* ASCII code for '%' */
+    const int pipe = 124; /* ASCII code for '|' */
+    const int len = strlen(fname);
+
+    strncpy(f, fname, len);
+    fp = strchr(f, percent);
+    if (fp != NULL) {
+        fp[0] = '*';
+        fp[1] = '\0';
+        fp = f;
+    } else {
+        int i;
+        fp = f;
+        for (i = 0; i < len; i++) {
+            if (f[i] == pipe) {
+               fp = &f[i + 1];
+               /* Because we potentially have to check file permissions at two levels
+                  for the output file (gx_device_open_output_file and the low level
+                  fopen API, if we're using a pipe, we have to add both the full string,
+                  (including the '|', and just the command to which we pipe - since at
+                  the pipe_fopen(), the leading '|' has been stripped.
+                */
+               gs_add_control_path(mem, gs_permit_file_writing, f);
+               break;
+            }
+            if (!IS_WHITESPACE(f[i]))
+                break;
+        }
+    }
+    return gs_add_control_path(mem, gs_permit_file_writing, fp);
+}
+
+static int
+gs_main_add_explicit_control_path(gs_memory_t *mem, const char *arg, gs_path_control_t control)
+{
+    char *p2, *p1 = (char *)arg + 17;
+    const char *lim = arg + strlen(arg);
+    int code = 0;
+
+    while (code >= 0 && p1 < lim && (p2 = strchr(p1, (int)gp_file_name_list_separator)) != NULL) {
+        code = gs_add_control_path_len(mem, gs_permit_file_reading, p1, (int)(p2 - p1));
+        p1 = p2 + 1;
+    }
+    if (p1 < lim)
+        code = gs_add_control_path_len(mem, control, p1, (int)(lim - p1));
+    return code;
+}
+
 /* Process switches.  Return 0 if processed, 1 for unknown switch, */
 /* <0 if error. */
 static int
@@ -428,6 +487,27 @@ run_stdin:
             } else if (strncmp(arg, "saved-pages-test", 16) == 0) {
                 minst->saved_pages_test_mode = true;
                 break;
+            /* Now handle the explicitly added paths to the file control lists */
+            } else if (strncmp(arg, "permit-file-read", 16) == 0) {
+                code = gs_main_add_explicit_control_path(minst->heap, arg, gs_permit_file_reading);
+                if (code < 0) return code;
+                break;
+            } else if (strncmp(arg, "permit-file-write", 17) == 0) {
+                code = gs_main_add_explicit_control_path(minst->heap, arg, gs_permit_file_writing);
+                if (code < 0) return code;
+                break;
+            } else if (strncmp(arg, "permit-file-control", 19) == 0) {
+                code = gs_main_add_explicit_control_path(minst->heap, arg, gs_permit_file_control);
+                if (code < 0) return code;
+                break;
+            } else if (strncmp(arg, "permit-file-all", 15) == 0) {
+                code = gs_main_add_explicit_control_path(minst->heap, arg, gs_permit_file_reading);
+                if (code < 0) return code;
+                code = gs_main_add_explicit_control_path(minst->heap, arg, gs_permit_file_writing);
+                if (code < 0) return code;
+                code = gs_main_add_explicit_control_path(minst->heap, arg, gs_permit_file_control);
+                if (code < 0) return code;
+                break;
             }
             /* FALLTHROUGH */
         case '+':
@@ -514,7 +594,10 @@ run_stdin:
             }
         case 'f':               /* run file of arbitrary name */
             if (*arg != 0) {
-                code = argproc(minst, arg);
+                code = gs_add_control_path(minst->heap, gs_permit_file_reading, arg);
+                if (code > 0)
+                    code = argproc(minst, arg);
+                (void)gs_remove_control_path(minst->heap, gs_permit_file_reading, arg);
                 if (code < 0)
                     return code;
                 /* If in saved_pages_test_mode, print and flush previous job before the next file */
@@ -549,7 +632,12 @@ run_stdin:
                 uint bsize = minst->run_buffer_size;
 
                 minst->run_buffer_size = 1;
-                code = argproc(minst, arg);
+                code = gs_add_control_path(minst->heap, gs_permit_file_reading, arg);
+                if (code > 0)
+                    code = argproc(minst, arg);
+                (void)gs_remove_control_path(minst->heap, gs_permit_file_reading, arg);
+                if (code < 0)
+                    return code;
                 minst->run_buffer_size = bsize;
                 if (code < 0)
                     return code;
@@ -671,6 +759,12 @@ run_stdin:
                     adef = arg;
                 if ((code = gs_main_init1(minst)) < 0)
                     return code;
+
+                if (strlen(adef) > 0) {
+                    code = gs_main_add_outputfile_control_path(minst->heap, adef);
+                    if (code < 0) return code;
+                }
+
                 ialloc_set_space(idmemory, avm_system);
                 len = strlen(adef);
                 str = ialloc_string(len, "-o");
@@ -755,6 +849,12 @@ run_stdin:
                     uint space = icurrent_space;
 
                     *eqp++ = 0;
+
+                    if (strlen(adef) == 10 && strncmp(adef, "OutputFile", 10) == 0 && strlen(eqp) > 0) {
+                        code = gs_main_add_outputfile_control_path(minst->heap, eqp);
+                        if (code < 0) return code;
+                    }
+
                     ialloc_set_space(idmemory, avm_system);
                     if (isd) {
                         int num, i;
@@ -922,17 +1022,26 @@ esc_strcat(char *dest, const char *src)
 static int
 argproc(gs_main_instance * minst, const char *arg)
 {
-    int code = gs_main_init1(minst);            /* need i_ctx_p to proceed */
+    int code1, code = gs_main_init1(minst);            /* need i_ctx_p to proceed */
 
     if (code < 0)
         return code;
+
+    code = gs_add_control_path(minst->heap, gs_permit_file_reading, arg);
+    if (code < 0) return code;
+
     if (minst->run_buffer_size) {
         /* Run file with run_string. */
-        return run_buffered(minst, arg);
+        code = run_buffered(minst, arg);
     } else {
         /* Run file directly in the normal way. */
-        return runarg(minst, "", arg, ".runfile", runInit | runFlush, minst->user_errors, NULL, NULL);
+        code = runarg(minst, "", arg, ".runfile", runInit | runFlush, minst->user_errors, NULL, NULL);
     }
+
+    code1 = gs_remove_control_path(minst->heap, gs_permit_file_reading, arg);
+    if (code >= 0 && code1 < 0) code = code1;
+
+    return code;
 }
 static int
 run_buffered(gs_main_instance * minst, const char *arg)

Resource/Init/gs_cidfm.ps
@@ -133,9 +133,39 @@ currentdict end def
   } if
 >> def
 
+% <dir.../base.extn> .basename <dir>
+/.splitdirname {
+  (/) rsearch { //true } { (\\) rsearch } ifelse
+  {3 -2 roll pop pop //true}{//false} ifelse
+} bind def
+
+% <file> .addcidfmappath -
+/.addcidfmpermitpath
+{
+  .filename
+  {
+    //.splitdirname exec
+    {dup def} if
+  } if
+} bind def
+
 /VerifyMap  % <raw_map> VerifyMap -
 {
+  % This is where we insert the default fallback into the map
+  % checking first to see if there is a user specified alternative
+  dup /CIDFallBack known not{
+     //DefaultCIDFallBack dup /Path get status
+     {
+       pop pop pop pop
+       /CIDFallBack exch 2 index 3 1 roll put
+     }
+     {
+       pop
+     }ifelse
+  } if
+
   % Remove any mappings for which we cannot find the font file
+  dup length dict begin
   dup
   {
     % if we have a name1->name2 mapping, follow the "trail",
@@ -163,11 +193,12 @@ currentdict end def
           /Path .knownget
           {
             .libfile
-            {closefile pop}
+            {
+              dup //.addcidfmpermitpath exec closefile pop}
             {
               {(r) file} //.internalstopped exec
               {pop pop 2 index exch .undef}
-              {closefile pop} ifelse
+              {dup //.addcidfmpermitpath exec closefile pop} ifelse
             } ifelse
           }
           {2 index exch .undef}
@@ -177,19 +208,8 @@ currentdict end def
       } ifelse
     } loop
   } forall
-
-  % This is where we insert the default fallback into the map
-  % checking first to see if there is a user specified alternative
-  dup /CIDFallBack known not{
-     //DefaultCIDFallBack dup /Path get status
-     {
-       pop pop pop pop
-       /CIDFallBack exch 2 index 3 1 roll put
-     }
-     {
-       pop
-     }ifelse
-  } if
+  currentdict end
+  {exch pop /PermitFileReading exch .addcontrolpath} forall
 
   % Checks for vicious substitution cycles.
   dup length dict copy                  % <<map>>
@@ -232,6 +252,9 @@ currentdict end def
   } loop
   pop pop
 } bind def
+
+currentdict /.splitdirname undef
+currentdict /.addcidfmpermitpath undef
 
 /PreprocessRecord  % <map> <Name> <raw_record> PreprocessRecord <map> <Name> <record> <bool>
 {

Resource/Init/gs_fonts.ps
@@ -36,7 +36,6 @@
 end def
 .setlanguagelevel
 
-
 % ------ End of editable parameters ------ %
 
 % Define the UniqueIDs and organization XUID assigned to Artifex.
@@ -67,6 +66,36 @@ end def
    loop
  } bind def
 
+% <dir.../base.extn> .basename <dir>
+/.splitdirname {
+    (/) rsearch { //true } { (\\) rsearch } ifelse
+  {3 -2 roll pop pop //true}{//false} ifelse
+} bind def
+
+% <dict> .addfontmappermitpaths
+% As fontmap can contain a *lot* of individual entries
+% we split off the filenames, and add directories to the
+% allowed paths.
+% We accumulate the paths in a dictionary, then iterate
+% the dictionary adding the paths to the PermitReading
+% list. This leverages the efficiency of dictionaries
+% handling duplicate keys.
+/.addfontmappermitpaths
+{
+  dup length dict begin
+  {
+    exch pop dup type /stringtype eq
+    {//.splitdirname exec {dup def} {pop} ifelse}
+    {pop}
+    ifelse
+  } forall
+  currentdict end
+  {exch pop /PermitFileReading exch .addcontrolpath}forall
+} bind def
+
+defaultfontmap_content length 0 gt
+{defaultfontmap_content .addfontmappermitpaths} if
+
 % Load a font name -> font file name map.
 userdict /Fontmap .FontDirectory maxlength dict put
 userdict /.nativeFontmap .FontDirectory maxlength dict put
@@ -76,7 +105,8 @@ userdict /.nativeFontmap .FontDirectory maxlength dict put
                 % specification that makes later entries override earlier
                 % ones within the same file.
    50 dict exch .readFontmap
-    { .definefontmap } forall
+   dup //.addfontmappermitpaths exec
+   { .definefontmap } forall
 } bind def
 /.readFontmap {         % <dict> <file> .readFontmap <dict>
     { dup token not { closefile exit } if
@@ -231,6 +261,11 @@ ifelse
 
 FONTPATH length 0 eq { (%END FONTPATH) .skipeof } if
 /FONTPATH [ FONTPATH .pathlist ] def
+
+FONTPATH
+{
+    /PermitFileReading exch .addcontrolpath
+} forall
 
 % Scan directories looking for plausible fonts.  "Plausible" means that
 % the file begins with %!PS-AdobeFont or %!FontType1, or with \200\001
@@ -1292,5 +1327,5 @@ FAKEFONTS { exch } if pop def   % don't bind, .current/setglobal get redefined
  /.nametostring /.fontnamestring /.checkalias /.fontknownget /.stdsubstfont
  /.putgstringcopy
  /.definenativefontmap
- /.findfontname
+ /.findfontname /.addfontmappermitpaths /.splitdirname
 ] {systemdict exch .forceundef} forall

Resource/Init/gs_init.ps
@@ -187,6 +187,8 @@ currentdict /DELAYSAFER known { /DELAYSAFER //true def /NOSAFER //true def } if
 }
 ifelse def
 
+/OLDSAFER currentdict /OLDSAFER known def
+
 /SAFERERRORS
 currentdict /NOSAFERERRORS known
 {
@@ -2162,6 +2164,31 @@ readonly def
   } if
 ] def
 
+/.lockfileaccess {
+  [
+    //tempfilepaths (*) .generate_dir_list_templates
+    /FONTPATH .systemvar (*) .generate_dir_list_templates
+    .languagelevel 2 ge {
+        % Default resources :
+      [ currentsystemparams /GenericResourceDir get] (*) .generate_dir_list_templates
+    } if
+    currentuserparams /ICCProfilesDir known {
+      [currentuserparams /ICCProfilesDir get] (*)
+      .generate_dir_list_templates
+    } if
+  ] {/PermitFileReading exch .addcontrolpath} forall
+
+  [
+    //tempfilepaths (*) .generate_dir_list_templates
+  ] {/PermitFileWriting exch .addcontrolpath} forall
+
+  [
+    //tempfilepaths (*) .generate_dir_list_templates
+  ] {/PermitFileControl exch .addcontrolpath} forall
+
+  .activatepathcontrol
+} bind def
+
 /.locksafe_userparams {
   SAFETY /safe get not {
     <<
@@ -2205,7 +2232,13 @@ currentdict /tempfilepaths undef
 
 %% --- These are documented extensions ---
 /.locksafe {
-  //.locksafe_userparams exec
+  OLDSAFER
+  {
+    //.locksafe_userparams exec
+  }
+  {
+    //.lockfileaccess exec
+  } ifelse
   //systemdict /getenv {pop //false} .forceput
   % setpagedevice has the side effect of clearing the page, but
   % we will just document that. Using setpagedevice keeps the device
@@ -2238,7 +2271,19 @@ currentdict /tempfilepaths undef
 % in gs_setpd.ps)
 %
 /.locksafeglobal {
-  //.locksafe_userparams exec
+  OLDSAFER
+  {
+    //.locksafe_userparams exec
+  }
+  {
+    <<
+      /PermitFileReading [(*)]
+      /PermitFileWriting [(*)]
+      /PermitFileControl [(*)]
+    >> setuserparams
+    //.lockfileaccess exec
+  } ifelse
+
   systemdict /getenv {pop //false} put
   % setpagedevice has the side effect of clearing the page, but
   % we will just document that. Using setpagedevice keeps the device

base/gdevpipe.c
@@ -34,7 +34,9 @@ const gx_io_device gs_iodev_pipe = {
      iodev_no_delete_file, iodev_no_rename_file, iodev_no_file_status,
      iodev_no_enumerate_files, NULL, NULL,
      iodev_no_get_params, iodev_no_put_params
-    }
+    },
+    NULL,
+    NULL
 };
 
 /* The file device procedures */

base/gp.h
@@ -20,6 +20,7 @@
 #ifndef gp_INCLUDED
 #  define gp_INCLUDED
 
+#include "stat_.h"
 #include "gstypes.h"
 #include "gscdefs.h"		/* for gs_serialnumber */
 /*
@@ -431,6 +432,8 @@ gp_file *gp_open_scratch_file_rm(const gs_memory_t *mem,
                                        char         fname[gp_file_name_sizeof],
                                  const char        *mode);
 
+/* gp_stat is defined in stat_.h rather than here due to macro problems */
+
 typedef enum {
     gp_combine_small_buffer = -1,
     gp_combine_cant_handle = 0,
@@ -569,8 +572,7 @@ typedef struct file_enum_s file_enum;
  * string of ?s should be interpreted as *.  Note that \ can appear in
  * the pattern also, as a quoting character.
  */
-file_enum *gp_enumerate_files_init(const char *pat, uint patlen,
-                                   gs_memory_t * memory);
+file_enum *gp_enumerate_files_init(gs_memory_t *memory, const char *pat, uint patlen);
 
 /*
  * Return the next file name in the enumeration.  The client passes in
@@ -579,7 +581,7 @@ file_enum *gp_enumerate_files_init(const char *pat, uint patlen,
  * returns max length +1.  If there are no more files, the procedure
  * returns -1.
  */
-uint gp_enumerate_files_next(file_enum * pfen, char *ptr, uint maxlen);
+uint gp_enumerate_files_next(gs_memory_t *memory, file_enum * pfen, char *ptr, uint maxlen);
 
 /*
  * Clean up a file enumeration.  This is only called to abandon
@@ -587,7 +589,7 @@ uint gp_enumerate_files_next(file_enum * pfen, char *ptr, uint maxlen);
  * no more files to enumerate.  This should deallocate the file_enum
  * structure and any subsidiary structures, strings, buffers, etc.
  */
-void gp_enumerate_files_close(file_enum * pfen);
+void gp_enumerate_files_close(gs_memory_t *memory, file_enum * pfen);
 
 /* ------ Font enumeration ------ */
 
@@ -645,8 +647,6 @@ gp_open_printer_impl(gs_memory_t *mem,
                      int         *binary_mode,
                      int          (**close)(FILE *));
 
-/* gp_stat is defined in stat_.h rather than here due to macro problems */
-
 /* Create a scratch file (utf8) (self-deleting if remove) */
 FILE *gp_open_scratch_file_impl(const gs_memory_t *mem,
                                 const char        *prefix,
@@ -656,6 +656,14 @@ FILE *gp_open_scratch_file_impl(const gs_memory_t *mem,
 
 /* Test whether this platform supports the sharing of file descriptors */
 int gp_can_share_fdesc(void);
+
+int gp_stat_impl(const gs_memory_t *mem, const char *path, struct stat *buf);
+
+file_enum *gp_enumerate_files_init_impl(gs_memory_t *memory, const char *pat, uint patlen);
+
+uint gp_enumerate_files_next_impl(gs_memory_t *memory, file_enum * pfen, char *ptr, uint maxlen);
+
+void gp_enumerate_files_close_impl(gs_memory_t *memory, file_enum * pfen);
 
 /* We don't define gp_fread_64, gp_fwrite_64,
    because (1) known platforms allow regular fread, fwrite

base/gp_dosfe.c
@@ -42,7 +42,7 @@ gs_private_st_ptrs1(st_file_enum, struct file_enum_s, "file_enum",
 /* Initialize an enumeration.  Note that * and ? in a directory */
 /* don't work, and \ is taken literally unless a second \ follows. */
 file_enum *
-gp_enumerate_files_init(const char *pat, uint patlen, gs_memory_t * mem)
+gp_enumerate_files_init_impl(gs_memory_t * mem, const char *pat, uint patlen)
 {
     file_enum *pfen = gs_alloc_struct(mem, file_enum, &st_file_enum, "gp_enumerate_files");
     int pat_size = 2 * patlen + 1;
@@ -107,7 +107,7 @@ static const string_match_params smp_file =
 {'*', '?', -1, true, true};
 
 uint
-gp_enumerate_files_next(file_enum * pfen, char *ptr, uint maxlen)
+gp_enumerate_files_next_impl(gs_memory_t * mem, file_enum * pfen, char *ptr, uint maxlen)
 {
     int code;
     char *p, *q;
@@ -120,7 +120,7 @@ gp_enumerate_files_next(file_enum * pfen, char *ptr, uint maxlen)
     } else
         code = dos_findnext(&pfen->ffblk);
     if (code != 0) {		/* All done, clean up. */
-        gp_enumerate_files_close(pfen);
+        gp_enumerate_files_close(mem, pfen);
         return ~(uint) 0;
     }
     if (maxlen < 13 + pfen->head_size)
@@ -138,11 +138,12 @@ gp_enumerate_files_next(file_enum * pfen, char *ptr, uint maxlen)
 
 /* Clean up the file enumeration. */
 void
-gp_enumerate_files_close(file_enum * pfen)
+gp_enumerate_files_close_impl(gs_memory_t * mem, file_enum * pfen)
 {
-    gs_memory_t *mem = pfen->memory;
+    gs_memory_t *mem2 = pfen->memory;
+    (void)mem;
 
-    gs_free_object(mem, pfen->pattern,
+    gs_free_object(mem2, pfen->pattern,
                    "gp_enumerate_files_close(pattern)");
-    gs_free_object(mem, pfen, "gp_enumerate_files_close");
+    gs_free_object(mem2, pfen, "gp_enumerate_files_close");
 }

base/gp_mshdl.c
@@ -48,7 +48,9 @@ const gx_io_device gs_iodev_handle = {
      iodev_no_delete_file, iodev_no_rename_file, iodev_no_file_status,
      iodev_no_enumerate_files, NULL, NULL,
      iodev_no_get_params, iodev_no_put_params
-    }
+    },
+    NULL,
+    NULL
 };
 
 /* The file device procedures */

commit ddddd71785d058b036775566d36d6848c9817f80

Fix -F____ and -f____ to actually run the file ____.
Commit 7ecbfda9 broke -F completely since it requires the file to run to
immediately follow the switch, and -f____ use to run the file, but since
gs_add_control_path returns 0 as long as there was no error, the code
never called argproc. Also, argproc already does the add_control_path
and remove_control_path, so simplify -f and -F code.

Also gs_add_outputfile_control_path was ignoring the return code when
adding the full pipe string as a path, so fix it.

Note that plmain ignores the return code from gs_remove_control_path,
but that should be OK.

base/gslibctx.c
@@ -593,6 +593,8 @@ gs_add_outputfile_control_path(gs_memory_t *mem, const char *fname)
         fp = f;
         for (i = 0; i < len; i++) {
             if (f[i] == pipe) {
+               int code;
+
                fp = &f[i + 1];
                /* Because we potentially have to check file permissions at two levels
                   for the output file (gx_device_open_output_file and the low level
@@ -600,7 +602,9 @@ gs_add_outputfile_control_path(gs_memory_t *mem, const char *fname)
                   (including the '|', and just the command to which we pipe - since at
                   the pipe_fopen(), the leading '|' has been stripped.
                 */
-               gs_add_control_path(mem, gs_permit_file_writing, f);
+               code = gs_add_control_path(mem, gs_permit_file_writing, f);
+               if (code < 0)
+                   return code;
                break;
             }
             if (!IS_WHITESPACE(f[i]))

psi/imainarg.c
@@ -536,10 +536,7 @@ run_stdin:
             }
         case 'f':               /* run file of arbitrary name */
             if (*arg != 0) {
-                code = gs_add_control_path(minst->heap, gs_permit_file_reading, arg);
-                if (code > 0)
-                    code = argproc(minst, arg);
-                (void)gs_remove_control_path(minst->heap, gs_permit_file_reading, arg);
+                code = argproc(minst, arg);
                 if (code < 0)
                     return code;
                 /* If in saved_pages_test_mode, print and flush previous job before the next file */
@@ -574,10 +571,7 @@ run_stdin:
                 uint bsize = minst->run_buffer_size;
 
                 minst->run_buffer_size = 1;
-                code = gs_add_control_path(minst->heap, gs_permit_file_reading, arg);
-                if (code > 0)
-                    code = argproc(minst, arg);
-                (void)gs_remove_control_path(minst->heap, gs_permit_file_reading, arg);
+                code = argproc(minst, arg);
                 minst->run_buffer_size = bsize;
                 if (code < 0)
                     return code;