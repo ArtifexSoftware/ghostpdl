cea41505580dfe55234bd4972fbb09af5e2e6bb8
Robin Watts <Robin.Watts@artifex.com>
2019-09-12 19:41:47 +0100

    Fix various printer devices to properly call spec ops.
    Various printer devices (psd, psdcmykog, tiffsep, tiffscaled)
    were passing the spec ops on to gx_default_dev_spec_op rather
    than gdev_prn_dev_spec_op.
    
    The most obvious implication of this was that we'd sometimes
    get a warning about printer devices having a private spec op.

--- devices/gdevcmykog.c
+++ devices/gdevcmykog.c
@@ -228,7 +228,7 @@ cmykog_dev_spec_op(gx_device *dev_, int op, void *data, int datasize)
   if (op == gxdso_supports_devn) {
     return true;
   }
-  return gx_default_dev_spec_op(dev_, op, data, datasize);
+  return gdev_prn_dev_spec_op(dev_, op, data, datasize);
 }
 
 /*
