569a551f2a57f6e6c1e692f6b834c765dcbe7cfa
Chris Liddell <chris.liddell@artifex.com>
2019-09-18 09:24:25 +0100

    Dates, docs, etc for 9.28 rc3

--- base/gscdefs.h
+++ base/gscdefs.h
@@ -37,7 +37,7 @@
 
 #ifndef GS_PRODUCT
 #  define GS_PRODUCT\
-        GS_PRODUCTFAMILY " RELEASE CANDIDATE 2"
+        GS_PRODUCTFAMILY " RELEASE CANDIDATE 3"
 #endif
 
 /* Prototypes for configuration definitions in gconfig.c. */
