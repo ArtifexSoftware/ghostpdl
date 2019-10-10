ArtifexSoftware/ghostpdl

commit 96c381cce28c27eac182549441a6c5025b0dcdd6

ps2write - move the page level save/restore wrapper
Bug #700232 "A5 duplex to duplex-capable Postscript printer prints single-sided"

Commit 0f6067d2531298060392d0e25fa759d320e03021 for bug #697245
removed a duplicate definition of pagesave, because it transpires that
Adobe interpreters don't free save objects when they are no longer
referenced. Previously we had defined /pagesave in both the page
content stream, and the code for altering page size.

The problem is that we alter media size as part of processing the
'stream' keyword, which is after we have defined /pagesave. Previously
we would then have redefined it if we needed to alter the media size.

When we got to the end of the page (after the showpage) we would then
restore the save object. Because we had defined the save object after we
had altered the media size the media would remain unchanged. With the
commit above, however, the media size would be restored back to the
original.

When we process page 2, the media size would then need to he altered
again, requiring a setpagedevice to be executed. The action of
setpagedevice, when Duplex is true, is to eject any unpaired pages and
start again with the first side.

Thus Duplex was working entirely correctly, simply not as desired.

This commit moves the pagesave and restore into the 'stream' and
'endstream' processing, so that is taken care of after any media size
changes.

devices/vector/gdevpdf.c
@@ -2903,12 +2903,10 @@ pdf_close(gx_device * dev)
                     pdf_write_page(pdev, pagecount++);
 
                     stream_puts(pdev->strm, "%%EndPageSetup\n");
-                    stream_puts(pdev->strm, "/pagesave save def\n");
                     pprintld1(pdev->strm, "%ld 0 obj\n", pres->object->id);
                     code = cos_write(pres->object, pdev, pres->object->id);
                     stream_puts(pdev->strm, "endobj\n");
                     pres->object->written = true;
-                    stream_puts(pdev->strm, "pagesave restore\n%%PageTrailer\n");
                 }
         }
         code1 = pdf_free_resource_objects(pdev, resourcePage);

devices/vector/opdfread.ps
@@ -748,6 +748,7 @@ currentdict end readonly def
       } repeat
     } if
     EPS2Write not {showpage} if
+    pagesave restore
   } if
 } bind def
 
@@ -999,6 +1000,8 @@ currentdict end readonly def
   dup /ImmediateExec true put
   dup /IsPage true put
   SetPageSize {dup /Context get //SetupPageView exec} if
+  % This gets restored at the end of ExecuteStream if IsPage is true.
+  /pagesave save def
 } bind def
 
 /FontFileDaemon % <id> <obj> <font_descriptor> FontFileDaemon <id> <obj>

commit d45d594ea5660976a21106ea864eb879801827b1

ps2write - restore missing %%PageTrailer comment
Bug #701659 "Missing %%PageTrailer commen"

An oversight in commit 96c381cce28c27eac182549441a6c5025b0dcdd6
caused the comment to be omitted.

devices/vector/gdevpdf.c
@@ -2930,6 +2930,7 @@ pdf_close(gx_device * dev)
                     code = cos_write(pres->object, pdev, pres->object->id);
                     stream_puts(pdev->strm, "endobj\n");
                     pres->object->written = true;
+                    stream_puts(pdev->strm, "%%PageTrailer\n");
                 }
         }
         code1 = pdf_free_resource_objects(pdev, resourcePage);