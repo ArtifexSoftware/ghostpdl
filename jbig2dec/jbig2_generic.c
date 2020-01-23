94d8ea178fa7c36ad7b844d6d0912b6e090a35e2
Robin Watts <Robin.Watts@artifex.com>
2020-01-23 12:55:27 +0000

    Further optimisations to jbig2_decode_generic_template0_TPGDON.
    Add a function to get a 'run' of pixels, and call that.
    
    This takes the time spent in this function from 29% to 22% for
    Jbig2_042_08.pdf.

--- jbig2dec/jbig2_generic.c
+++ jbig2dec/jbig2_generic.c
@@ -863,6 +863,10 @@ jbig2_decode_generic_template0_TPGDON(Jbig2Ctx *ctx,
         left = -gmin;
     if (right < gmax)
         right = gmax;
+    /* We need to guarantee 9 pixels in the right margin to be able
+     * to use jbig2_image_get_pixels_fast. */
+    if (right < 9)
+        right = 9;
     right = GBW - right;
     /* So 0 <= x < left or right <= x < GBW needs bounds checking. */
 
@@ -892,21 +896,12 @@ jbig2_decode_generic_template0_TPGDON(Jbig2Ctx *ctx,
                 }
                 if (y >= top && x >= left && x < right)
                 {
-                    CONTEXT = jbig2_image_get_pixel_fast(image, x - 1, y);
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x - 2, y) << 1;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x - 3, y) << 2;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x - 4, y) << 3;
+                    CONTEXT = jbig2_image_get_pixels_fast(image, x - 4, y, 4);
                     CONTEXT |= jbig2_image_get_pixel_fast(image, x + params->gbat[0], y + params->gbat[1]) << 4;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x + 2, y - 1) << 5;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x + 1, y - 1) << 6;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x, y - 1) << 7;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x - 1, y - 1) << 8;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x - 2, y - 1) << 9;
+                    CONTEXT |= jbig2_image_get_pixels_fast(image, x - 2, y - 1, 5) << 5;
                     CONTEXT |= jbig2_image_get_pixel_fast(image, x + params->gbat[2], y + params->gbat[3]) << 10;
                     CONTEXT |= jbig2_image_get_pixel_fast(image, x + params->gbat[4], y + params->gbat[5]) << 11;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x + 1, y - 2) << 12;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x, y - 2) << 13;
-                    CONTEXT |= jbig2_image_get_pixel_fast(image, x - 1, y - 2) << 14;
+                    CONTEXT |= jbig2_image_get_pixels_fast(image, x - 1, y - 2, 3) << 12;
                     CONTEXT |= jbig2_image_get_pixel_fast(image, x + params->gbat[6], y + params->gbat[7]) << 15;
                 }
                 else
