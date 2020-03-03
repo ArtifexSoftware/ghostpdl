cbee08400ca895bb5f52de6cf8cb44ba59cc5fd8
Robin Watts <Robin.Watts@artifex.com>
2020-03-03 16:40:31 +0000

    Bug 702177: Avoid SEGV on low memory in cmd_put_halftone.
    Ensure that cmd_put_list_op cannot return NULL without
    setting an error code.

--- base/gxclutil.c
+++ base/gxclutil.c
@@ -311,6 +311,8 @@ cmd_put_list_op(gx_device_clist_writer * cldev, cmd_list * pcl, uint size)
         if ((cldev->error_code =
              cmd_write_buffer(cldev, cmd_opv_end_run)) != 0 ||
             (size + cmd_headroom > cldev->cend - cldev->cnext)) {
+            if (cldev->error_code == 0)
+                cldev->error_code = gs_error_VMerror;
             return 0;
         }
         else
