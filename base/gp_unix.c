09a575f0fdc6edde7e42f325167318a1b199839f
Chris Liddell <chris.liddell@artifex.com>
2020-03-11 13:18:00 +0000

    (fontconfig) Don't stop enumerating fonts on error.
    We only want to finish enumerating the fontconfig fonts list once the list
    is exhausted, not when an individual font gives an error.
    
    In the event a font gives an error, skip it and move on.

--- base/gp_unix.c
+++ base/gp_unix.c
@@ -411,42 +411,47 @@ int gp_enumerate_fonts_next(void *enum_state, char **fontname, char **path)
         return 0;   /* gp_enumerate_fonts_init failed for some reason */
     }
 
-    if (state->index == state->font_list->nfont) {
-        return 0; /* we've run out of fonts */
-    }
+    /* We use the loop so we can skip over fonts that return errors */
+    while(1) {
+        if (state->index == state->font_list->nfont) {
+            return 0; /* we've run out of fonts */
+        }
 
-    /* Bits of the following were borrowed from Red Hat's
-     * fontconfig patch for Ghostscript 7 */
-    font = state->font_list->fonts[state->index];
+        /* Bits of the following were borrowed from Red Hat's
+         * fontconfig patch for Ghostscript 7 */
+        font = state->font_list->fonts[state->index];
+        state->index++;
 
-    result = FcPatternGetString (font, FC_FAMILY, 0, &family_fc);
-    if (result != FcResultMatch || family_fc == NULL) {
-        dmlprintf(state->mem, "DEBUG: FC_FAMILY mismatch\n");
-        return 0;
-    }
+        result = FcPatternGetString (font, FC_FAMILY, 0, &family_fc);
+        if (result != FcResultMatch || family_fc == NULL) {
+            dmlprintf(state->mem, "DEBUG: FC_FAMILY mismatch\n");
+            continue;
+        }
 
-    result = FcPatternGetString (font, FC_FILE, 0, &file_fc);
-    if (result != FcResultMatch || file_fc == NULL) {
-        dmlprintf(state->mem, "DEBUG: FC_FILE mismatch\n");
-        return 0;
-    }
+        result = FcPatternGetString (font, FC_FILE, 0, &file_fc);
+        if (result != FcResultMatch || file_fc == NULL) {
+            dmlprintf(state->mem, "DEBUG: FC_FILE mismatch\n");
+            continue;
+        }
 
-    result = FcPatternGetBool (font, FC_OUTLINE, 0, &outline_fc);
-    if (result != FcResultMatch) {
-        dmlprintf1(state->mem, "DEBUG: FC_OUTLINE failed to match on %s\n", (char*)family_fc);
-        return 0;
-    }
+        result = FcPatternGetBool (font, FC_OUTLINE, 0, &outline_fc);
+        if (result != FcResultMatch) {
+            dmlprintf1(state->mem, "DEBUG: FC_OUTLINE failed to match on %s\n", (char*)family_fc);
+            continue;
+        }
 
-    result = FcPatternGetInteger (font, FC_SLANT, 0, &slant_fc);
-    if (result != FcResultMatch) {
-        dmlprintf(state->mem, "DEBUG: FC_SLANT didn't match\n");
-        return 0;
-    }
+        result = FcPatternGetInteger (font, FC_SLANT, 0, &slant_fc);
+        if (result != FcResultMatch) {
+            dmlprintf(state->mem, "DEBUG: FC_SLANT didn't match\n");
+            continue;
+        }
 
-    result = FcPatternGetInteger (font, FC_WEIGHT, 0, &weight_fc);
-    if (result != FcResultMatch) {
-        dmlprintf(state->mem, "DEBUG: FC_WEIGHT didn't match\n");
-        return 0;
+        result = FcPatternGetInteger (font, FC_WEIGHT, 0, &weight_fc);
+        if (result != FcResultMatch) {
+            dmlprintf(state->mem, "DEBUG: FC_WEIGHT didn't match\n");
+            continue;
+        }
+        break;
     }
 
     /* Gross hack to work around Fontconfig's inability to tell
@@ -459,7 +464,6 @@ int gp_enumerate_fonts_next(void *enum_state, char **fontname, char **path)
     /* return the font path straight out of fontconfig */
     *path = (char*)file_fc;
 
-    state->index ++;
     return 1;
 #else
     return 0;
