ArtifexSoftware/ghostpdl

commit 2a691356aeb538388ecbf0cafdf422bde771bad0

pdfwrite - preserve ToUnicode CMaps with non-standard mappings
Bug #702201 "pdfwrite changes ligature"

The title is incorrect, pdfwrite does not 'change ligature'. The PDF
file uses an ffi ligature glyph from the embedded font, but the
ToUnicode CMap does not map that to U+FB03, it maps it to 3 Unicode
code points; f, f and i.

When we emit a simple font we check its Encoding to see if it needs a
ToUnicode CMap, if it doesn't then we don't emit one. Because all the
glyphs in this font are named, and have standard names, we don't need to
emit a ToUnicode CMap and so we don't (keeps the file size down).

However, that means that PDF consumers will map the ligature glyph to a
ligature ffi when copy/pasting (it has no other effect).

Here we check the Unicode code points as they are added to the internal
record. If any of them is not 2 bytes long, then its not a standard
mapping (simple fonts can only map to a single point). In that case we
note the exception and when we come to write the font, in the code that
checks the Encoding to see if we need to write a ToUnicode, we check
this value, if we've seen a multiple code point in the ToUnicode CMap
then we don't elide it, we embed the one we picked up.

This is not actually a bug IMO, there's nothing wrong with the mapping
as it stands because an ffi glyph in the document really should map to
a single Unicode code point ffi ligature. However, the creator of the
PDF file obviously didn't want that, so we;ll now honour it.

NB this only affects the emission of a ToUnicode CMap, which is only
used for copy/paste/search. The PDF contents are not affected by this
commit.

devices/vector/gdevpdtw.c
@@ -119,6 +119,9 @@ pdf_simple_font_needs_ToUnicode(const pdf_font_resource_t *pdfont)
             In this circumstance, write the ToUnicode map to get a searchable PDF.
         */
         return true;
+    if (!pdfont->u.simple.TwoByteToUnicode)
+        return true;
+
     for (ch = 0; ch < 256; ++ch) {
         pdf_encoding_element_t *pet = &pdfont->u.simple.Encoding[ch];
         gs_glyph glyph = pet->glyph;
@@ -136,8 +139,9 @@ pdf_simple_font_needs_ToUnicode(const pdf_font_resource_t *pdfont)
         if( glyph > GS_C_PDF_MAX_GOOD_GLYPH ||
            !(gs_c_pdf_glyph_type[glyph >> 2] & (mask << (( glyph & 3 )<<1) )))
           return true;
+
     }
-    return false;
+    return true;
 }
 
 /* Write Encoding differencrs. */

devices/vector/gdevpdte.c
@@ -160,6 +160,8 @@ pdf_add_ToUnicode(gx_device_pdf *pdev, gs_font *font, pdf_font_resource_t *pdfon
 
         if (pdfont->cmap_ToUnicode != NULL)
             gs_cmap_ToUnicode_add_pair(pdfont->cmap_ToUnicode, ch, unicode, length);
+        if (length > 2 && pdfont->u.simple.Encoding != NULL)
+            pdfont->u.simple.TwoByteToUnicode = 0;
     }
     if (unicode)
         gs_free_object(pdev->memory, unicode, "temporary Unicode array");

devices/vector/gdevpdtf.c
@@ -572,6 +572,7 @@ font_resource_simple_alloc(gx_device_pdf *pdev, pdf_font_resource_t **ppfres,
     pfres->u.simple.BaseEncoding = -1;
     pfres->u.simple.preferred_encoding_index = -1;
     pfres->u.simple.last_reserved_char = -1;
+    pfres->u.simple.TwoByteToUnicode = 1;
     *ppfres = pfres;
     return 0;
 }

devices/vector/gdevpdtf.h
@@ -253,6 +253,13 @@ struct pdf_font_resource_s {
             gs_point *v; /* [256], glyph origin for WMode 1 */
             int last_reserved_char; /* Except for synthesised Type 3,
                                            which stores such data in LastChar */
+            /* We use this when determining whether we should use an existing ToUnicode
+             * CMap or just use the Encoding, for s aimple font. Even if the Encoding
+             * only uses named glyphs, with names we can understand, the original
+             * ToUnicode may have mapped these in a non-standard way.
+             * See Bug #702201 where the ffi ligature is mapped to 3 code points
+             */
+            int TwoByteToUnicode;
 
             gs_glyph standard_glyph_code_for_notdef;
             union {

commit 3353e8f68c8b4b13db8f712e3d01503d9043df65

pdfwrite - fix previous commit
commit 	2a691356aeb538388ecbf0cafdf422bde771bad0 accidentally left a
debugging hack in place, which would lead to lots of files including
a ToUnicode CMap when that wasn't necessary, fix it here.

devices/vector/gdevpdtw.c
@@ -141,7 +141,7 @@ pdf_simple_font_needs_ToUnicode(const pdf_font_resource_t *pdfont)
           return true;
 
     }
-    return true;
+    return false;
 }
 
 /* Write Encoding differencrs. */