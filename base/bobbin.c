cbb82ba4baaa2a9da11c603d742a58a51c4e50e1
Chris Liddell <chris.liddell@artifex.com>
2020-04-09 18:07:01 +0100

    Update copyright to 2020

--- base/bobbin.c
+++ base/bobbin.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016-2018 Artifex Software, Inc.
+/* Copyright (C) 2016-2020 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
--- toolbin/gitlog2changelog.py
+++ toolbin/gitlog2changelog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright (C) 2001-2019 Artifex Software, Inc.
+# Copyright (C) 2001-2020 Artifex Software, Inc.
 # All Rights Reserved.
 #
 # This software is provided AS-IS with no warranty, either express or
