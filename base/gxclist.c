ArtifexSoftware/ghostpdl

commit 6a3c36f8c5e83448f5d1e5f24ee01c0e80a92877

Fix Bug 702177: VMerrors with some BufferSpace and/or -K limits.
Interestingly, the two examples given in the bug succeed on Windows 64
but don't on linux. The difference is due to an 8 byte difference in the
size of gx_clist_state (the per band writer state). In the case of the
Bug690546.pdf file, the space left in the BufferSpace area after the
logic increased it was only 2,200 bytes on linux, but was 12k bytes on
Windows (it made an extra pass through the automatic "increase space"
loop in gdev_prn_setup_as_command_list. Fix the calculation in
clist_init_states so that the amount of extra space corresponds to the
amount expected by clist command writing (data_bits_size + cmd_largest_size)
rather than the insufficient and arbitrary "100".

Note that although the Bug692057.pdf returned VMerror from cmd_put_list_op, the
'fallback; method of gx_default_strip_tile_rectangle still produces the SAME
raster from psdcmyk16 and does not change the performance or clist file size
Robin's commit cbee0840 fixed this case.

base/gxclist.c
@@ -355,12 +355,13 @@ clist_init_states(gx_device * dev, byte * init_data, uint data_size)
     /* Align to the natural boundary for ARM processors, bug 689600 */
     long alignment = (-(long)init_data) & (sizeof(init_data) - 1);
 
-    /*
-     * The +100 in the next line is bogus, but we don't know what the
-     * real check should be. We're effectively assuring that at least 100
-     * bytes will be available to buffer command operands.
+    /* Leave enough room after states for commands that write a reasonable
+     * amount of data. The cmd_largest_size and the data_bits_size should  be
+     * enough to buffer command operands. The data_bits_size is the level
+     * at which commands should expect to split data across buffers. If this
+     * extra space is a little large, it doesn't really hurt.
      */
-    if (state_size + sizeof(cmd_prefix) + cmd_largest_size + 100 + alignment > data_size)
+    if (state_size + sizeof(cmd_prefix) + cmd_largest_size + data_bits_size + alignment > data_size)
         return_error(gs_error_rangecheck);
     /* The end buffer position is not affected by alignment */
     cdev->cend = init_data + data_size;

commit c184fcbcc23a196a6d6a26648e52d9f3372ac46b

Fix problem introduced by commit 6a3c36f8 with multi-threaded rendering.
That commit changed the allocation requirement in clist_init_states, but didn't
change the corresponding calculation that is supposed to match. Make this a
function so that both will agree going forward.

base/gxclist.c
@@ -342,6 +342,23 @@ clist_init_bands(gx_device * dev, gx_device_memory *bdev, uint data_size,
     return 0;
 }
 
+/* Minimum BufferSpace needed when writing the clist */
+/* This is an exported function because it is used to set up render threads */
+size_t
+clist_minimum_buffer(int nbands) {
+
+    /* Leave enough room after states for commands that write a reasonable
+     * amount of data. The cmd_largest_size and the data_bits_size should  be
+     * enough to buffer command operands. The data_bits_size is the level
+     * at which commands should expect to split data across buffers. If this
+     * extra space is a little large, it doesn't really hurt.
+     */
+    return (nbands * (ulong) sizeof(gx_clist_state) +
+            sizeof(cmd_prefix) +
+            cmd_largest_size +
+            data_bits_size);
+}
+
 /*
  * Initialize the allocation for the band states, which are used only
  * when writing.  Requires: nbands.  Sets: states, cbuf, cend.
@@ -355,13 +372,7 @@ clist_init_states(gx_device * dev, byte * init_data, uint data_size)
     /* Align to the natural boundary for ARM processors, bug 689600 */
     intptr_t alignment = (-(intptr_t)init_data) & (sizeof(init_data) - 1);
 
-    /* Leave enough room after states for commands that write a reasonable
-     * amount of data. The cmd_largest_size and the data_bits_size should  be
-     * enough to buffer command operands. The data_bits_size is the level
-     * at which commands should expect to split data across buffers. If this
-     * extra space is a little large, it doesn't really hurt.
-     */
-    if (state_size + sizeof(cmd_prefix) + cmd_largest_size + data_bits_size + alignment > data_size)
+    if (clist_minimum_buffer(cdev->nbands) > data_size)
         return_error(gs_error_rangecheck);
     /* The end buffer position is not affected by alignment */
     cdev->cend = init_data + data_size;

base/gxclist.h
@@ -548,6 +548,11 @@ clist_enable_multi_thread_render(gx_device *dev);
 void
 clist_teardown_render_threads(gx_device *dev);
 
+/* Minimum BufferSpace needed when writing the clist */
+/* This is an exported function because it is used to set up render threads */
+/* and in clist_init_states to make sure the buffer is large enough */
+size_t clist_minimum_buffer(int nbands);
+
 #ifdef DEBUG
 #define clist_debug_rect clist_debug_rect_imp
 void clist_debug_rect_imp(int x, int y, int width, int height);

base/gxclthrd.c
@@ -64,7 +64,7 @@ setup_device_and_mem_for_thread(gs_memory_t *chunk_base_mem, gx_device *dev, boo
     gs_c_param_list paramlist;
     gs_devn_params *pclist_devn_params;
     gx_device_buf_space_t buf_space;
-    ulong state_size;
+    size_t min_buffer_space;
 
 
     /* Every thread will have a 'chunk allocator' to reduce the interaction
@@ -193,11 +193,12 @@ setup_device_and_mem_for_thread(gs_memory_t *chunk_base_mem, gx_device *dev, boo
     ncdev->space_params.banding_type = BandingAlways;
     code = npdev->printer_procs.buf_procs.size_buf_device
                 (&buf_space, (gx_device *)ncdev, NULL, ncdev->space_params.band.BandHeight, false);
-    /* The 100 is bogus, we are just matching what is in clist_init_states */
-    state_size = cdev->nbands * (ulong) sizeof(gx_clist_state) + sizeof(cmd_prefix) + cmd_largest_size + 100;
+    min_buffer_space = clist_minimum_buffer(cdev->nbands);
     ncdev->space_params.band.BandBufferSpace = buf_space.bits + buf_space.line_ptrs;
-    if (state_size > ncdev->space_params.band.BandBufferSpace)
-        ncdev->space_params.band.BandBufferSpace = state_size;
+    /* Check if the BandBufferSpace is large enough to allow us for clist writing */
+    /* to prevent an error from gdev_prn_allocate_memory which checks that.       */
+    if (min_buffer_space > ncdev->space_params.band.BandBufferSpace)
+        ncdev->space_params.band.BandBufferSpace = min_buffer_space;
     ncdev->space_params.band.tile_cache_size = cdev->page_info.tile_cache_size;	/* must be the same */
     ncdev->space_params.band.BandBufferSpace += cdev->page_info.tile_cache_size;
 