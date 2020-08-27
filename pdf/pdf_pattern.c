ArtifexSoftware/ghostpdl

commit 8199d35eb976d97ec8760c6251ae1802d5dc0402

Improve client memory handling for colour spaces and patterns
Colour spaces have had a client_data member for some time (only used
with Pantone colour replacement) but no means to signal to the client
that a colour space was going to be freed.

In addition, its terribly hard for an interpreter which uses complex
colour spaces (such as Separation and DeviceN, for Patterns see below)
to reliably track when a colour space is freed.

This commit adds a new 'interpreter_data' pointer to hold data allocated
by the interpreter which is associated with a colour space. We also add
an 'interpreter_free_cspace_proc' to be called when a colour space is
about to be freed. This gives the interpreter an opportunity to clean
up the memory it has associated with the colour space.

Similarly pattern instances (as opposed to Pattern colour spaces) have
long had a client_data member, which was previously stored in the
'template' (which is a terrible misnomer) for both type 1 and type 2
patterns. However there was no procedure to notify the client when the
pattern was freed! The PCL interpreter (which was the only one using
this type of pattern) dealt with this by replacing the 'free' routine
in the structure with its own routine which first freed the client data
and then called the original free routine. This seems like a hack.

This commit moves the client_data out of the pattern instance and into
the pattern structure proper. It also adds a 'notfy_free' member which
holds a pointer to a function to be called when a pattern is freed. This
gives the client a reliable opportunity to clean up the client_data.

These changes involve updating the PostScript, PCL and XPS interpreters
to point to the new location of the client_data, as well as similar
changes in the graphics library. pixmap patterns now use the notify_free
functio0n instead of patching over the structure's free routine.

Update pdfi to use the new colour space and Pattern free notifications

pdf/pdf_pattern.c
@@ -101,30 +101,12 @@ pdfi_setpattern_null(pdf_context *ctx, gs_client_color *cc)
 }
 #endif
 
-/*
- * Pattern lifetime management turns out to be more complex than we would ideally like. Although
- * Patterns are reference counted, and contain a client_data pointer, they don't have a gs_notify
- * setup. This means that there's no simple way for us to be informed when a Pattern is released.
- * We could patch up the Pattern finalize() method, replacing it with one of our own which calls
- * the original finalize() but that seems like a really nasty hack.
- * For the time being we put code in pdfi_grestore() to check for Pattern colour spaces being
- * restored away, but we also need to check for Pattern spaces being replaced in the current
- * graphics state. We define 'pdfi' variants of several graphics library colour management
- * functions to 'wrap' these with code to check for replacement of Patterns.
- * This comment is duplicated in pdf_color.c
- */
-int pdfi_pattern_cleanup(pdf_context *ctx, const gs_client_color *pcc)
+void pdfi_pattern_cleanup(gs_memory_t * mem, void *p)
 {
-    gs_pattern1_instance_t *pinst = (gs_pattern1_instance_t *)pcc->pattern;
-    gs_pattern1_template_t *templat;
+    gs_pattern1_instance_t *pinst = (gs_pattern1_instance_t *)p;
     pdf_pattern_context_t *context;
 
-    if (pinst == NULL)
-        return 0;
-    templat = &pinst->templat;
-    if (templat == NULL)
-        return 0;
-    context = (pdf_pattern_context_t *)templat->client_data;
+    context = (pdf_pattern_context_t *)pinst->client_data;
 
     if (context != NULL) {
         pdfi_countdown(context->page_dict);
@@ -132,10 +114,9 @@ int pdfi_pattern_cleanup(pdf_context *ctx, const gs_client_color *pcc)
         if (context->shading)
             pdfi_shading_free(context->ctx, context->shading);
         gs_free_object(context->ctx->memory, context, "Free pattern context");
-        templat->client_data = NULL;
+        pinst->client_data = NULL;
+        pinst->notify_free = NULL;
     }
-
-    return 0;
 }
 
 /* See px_paint_pattern() */
@@ -143,8 +124,7 @@ static int
 pdfi_pattern_paint_stream(pdf_context *ctx, const gs_client_color *pcc)
 {
     gs_pattern1_instance_t *pinst = (gs_pattern1_instance_t *)pcc->pattern;
-    const gs_pattern1_template_t *templat = &pinst->templat;
-    pdf_pattern_context_t *context = (pdf_pattern_context_t *)templat->client_data;
+    pdf_pattern_context_t *context = (pdf_pattern_context_t *)pinst->client_data;
     pdf_dict *page_dict = context->page_dict;
     pdf_dict *pat_dict = context->pat_dict;
     int code = 0;
@@ -160,8 +140,7 @@ static int
 pdfi_pattern_paint(const gs_client_color *pcc, gs_gstate *pgs)
 {
     gs_pattern1_instance_t *pinst = (gs_pattern1_instance_t *)pcc->pattern;
-    const gs_pattern1_template_t *templat = &pinst->templat;
-    pdf_pattern_context_t *context = (pdf_pattern_context_t *)templat->client_data;
+    pdf_pattern_context_t *context = (pdf_pattern_context_t *)pinst->client_data;
     pdf_context *ctx = context->ctx;
     int code = 0;
 
@@ -211,7 +190,7 @@ pdfi_pattern_paint_high_level(const gs_client_color *pcc, gs_gstate *pgs_ignore)
 {
     gs_pattern1_instance_t *pinst = (gs_pattern1_instance_t *)pcc->pattern;
     const gs_pattern1_template_t *templat = &pinst->templat;
-    pdf_pattern_context_t *context = (pdf_pattern_context_t *)templat->client_data;
+    pdf_pattern_context_t *context = (pdf_pattern_context_t *)pinst->client_data;
     pdf_context *ctx = context->ctx;
     gs_gstate *pgs = ctx->pgs;
     gs_matrix m;
@@ -337,7 +316,7 @@ pdfi_pattern_gset(pdf_context *ctx, pdf_dict *page_dict, pdf_dict *ExtGState)
 
 /* Setup the pattern gstate and other context */
 static int
-pdfi_pattern_setup(pdf_context *ctx, gs_pattern_template_t *templat,
+pdfi_pattern_setup(pdf_context *ctx, pdf_pattern_context_t **ppcontext,
                    pdf_dict *page_dict, pdf_dict *pat_dict, pdf_dict *ExtGState)
 {
     int code = 0;
@@ -359,7 +338,7 @@ pdfi_pattern_setup(pdf_context *ctx, gs_pattern_template_t *templat,
     context->shading = NULL;
     pdfi_countup(page_dict);
     pdfi_countup(pat_dict);
-    templat->client_data = context;
+    *ppcontext = context;
 
     return 0;
  errorExit:
@@ -383,6 +362,7 @@ pdfi_setpattern_type1(pdf_context *ctx, pdf_dict *stream_dict, pdf_dict *page_di
     pdf_dict *Resources = NULL;
     pdf_array *Matrix = NULL;
     bool transparency = false;
+    pdf_pattern_context_t *context = NULL;
 
 #if DEBUG_PATTERN
     dbgmprintf(ctx->memory, "PATTERN: Type 1 pattern\n");
@@ -462,7 +442,7 @@ pdfi_setpattern_type1(pdf_context *ctx, pdf_dict *stream_dict, pdf_dict *page_di
     if (code < 0)
         goto exit;
 
-    code = pdfi_pattern_setup(ctx, (gs_pattern_template_t *)&templat, page_dict, pdict, NULL);
+    code = pdfi_pattern_setup(ctx, &context, page_dict, pdict, NULL);
     if (code < 0) {
         (void) pdfi_grestore(ctx);
         goto exit;
@@ -474,10 +454,15 @@ pdfi_setpattern_type1(pdf_context *ctx, pdf_dict *stream_dict, pdf_dict *page_di
         goto exit;
     }
 
+    cc->pattern->client_data = context;
+    cc->pattern->notify_free = pdfi_pattern_cleanup;
+    context = NULL;
+
     code = pdfi_grestore(ctx);
     if (code < 0)
         goto exit;
  exit:
+    pdfi_countdown(context);
     pdfi_countdown(Resources);
     pdfi_countdown(Matrix);
     pdfi_countdown(BBox);
@@ -533,7 +518,7 @@ pdfi_setpattern_type2(pdf_context *ctx, pdf_dict *stream_dict, pdf_dict *page_di
 
     gs_pattern2_init(&templat);
 
-    code = pdfi_pattern_setup(ctx, (gs_pattern_template_t *)&templat, NULL, NULL, ExtGState);
+    code = pdfi_pattern_setup(ctx, &context, NULL, NULL, ExtGState);
     if (code < 0) {
         (void) pdfi_grestore(ctx);
         goto exit;
@@ -546,16 +531,18 @@ pdfi_setpattern_type2(pdf_context *ctx, pdf_dict *stream_dict, pdf_dict *page_di
         goto exit;
     }
 
-    /* We need to call pdfi_pattern_setup (in order to create the ColorSpace) before
-     * we call pdfi_shading_build. So we can't pass the shading dictionary to pdfi_pattern_setup
-     * in the same way as the type 1 pattern passes its persistent data, so we need to do it
-     * ourselves here.
-     */
-    context = (pdf_pattern_context_t *)templat.client_data;
     context->shading = shading;
 
     templat.Shading = shading;
     code = gs_make_pattern(cc, (const gs_pattern_template_t *)&templat, &mat, ctx->pgs, ctx->memory);
+    if (code < 0) {
+        (void) pdfi_grestore(ctx);
+        goto exit;
+    }
+    cc->pattern->client_data = context;
+    context = NULL;
+
+    code = pdfi_pattern_setup(ctx, &context, NULL, NULL, ExtGState);
     if (code < 0) {
         (void) pdfi_grestore(ctx);
         goto exit;
@@ -566,6 +553,7 @@ pdfi_setpattern_type2(pdf_context *ctx, pdf_dict *stream_dict, pdf_dict *page_di
         goto exit;
 
  exit:
+    pdfi_countdown(context);
     pdfi_countdown(Shading);
     pdfi_countdown(Matrix);
     pdfi_countdown(ExtGState);

base/gscspace.c
@@ -98,6 +98,9 @@ gs_cspace_final(const gs_memory_t *cmem, void *vptr)
     gs_color_space *pcs = (gs_color_space *)vptr;
     (void)cmem; /* unused */
 
+    if (pcs->interpreter_free_cspace_proc != NULL) {
+        (*pcs->interpreter_free_cspace_proc) ((gs_memory_t *)cmem, pcs);
+    }
     if (pcs->type->final)
         pcs->type->final(pcs);
     if_debug2m('c', cmem, "[c]cspace final "PRI_INTPTR" %d\n", (intptr_t)pcs, (int)pcs->id);
@@ -124,6 +127,8 @@ gs_cspace_alloc_with_id(gs_memory_t *mem, ulong id,
     pcs->id = id;
     pcs->base_space = NULL;
     pcs->pclient_color_space_data = NULL;
+    pcs->interpreter_data = NULL;
+    pcs->interpreter_free_cspace_proc = NULL;
     pcs->cmm_icc_profile_data = NULL;
     pcs->icc_equivalent = NULL;
     pcs->params.device_n.devn_process_space = NULL;

base/gscspace.h
@@ -290,6 +290,8 @@ typedef struct gs_pattern_params_s {
 #define cs_DeviceRGB_id 3
 #define cs_DeviceCMYK_id 4
 
+typedef void (*gs_cspace_free_proc_t) (gs_memory_t * mem, void *pcs);
+
 /*
  * The colorspace object. For pattern and indexed colorspaces, the
  * base_space refers to the underlying colorspace. For separation,
@@ -304,6 +306,8 @@ struct gs_color_space_s {
     gs_color_space             *base_space;
     gs_color_space             *icc_equivalent;
     client_color_space_data_t  *pclient_color_space_data;
+    void                       *interpreter_data;
+    gs_cspace_free_proc_t      interpreter_free_cspace_proc;
     cmm_profile_t              *cmm_icc_profile_data;
     union {
         gs_device_pixel_params   pixel;

base/gspcolor.c
@@ -77,7 +77,6 @@ gs_pattern_common_init(gs_pattern_template_t * ppat,
     ppat->type = type;
     ppat->PatternType = type->PatternType;
     uid_set_invalid(&ppat->uid);
-    ppat->client_data = 0;	/* for GC */
 }
 
 /* Generic makepattern */
@@ -117,6 +116,8 @@ gs_make_pattern_common(gs_client_color *pcc,
     gs_concat(saved, pmat);
     code = gs_newpath(saved);
     pinst->saved = saved;
+    pinst->client_data = NULL;	/* for GC */
+    pinst->notify_free = NULL;  /* No custom free calllback initially */
     pcc->pattern = pinst;
     pcc->pattern->pattern_id = gs_next_ids(mem, 1);
     return code;
@@ -128,6 +129,9 @@ rc_free_pattern_instance(gs_memory_t * mem, void *pinst_void,
                          client_name_t cname)
 {
     gs_pattern_instance_t *pinst = pinst_void;
+
+    if (pinst->notify_free != NULL)
+        (*pinst->notify_free) ((gs_memory_t *)mem, pinst);
 
     gs_gstate_free(pinst->saved);
     rc_free_struct_only(mem, pinst_void, cname);

base/gspcolor.h
@@ -53,8 +53,7 @@ typedef struct gs_pattern_type_s gs_pattern_type_t;
 #define gs_pattern_template_common\
   const gs_pattern_type_t *type;\
   int PatternType;		/* copied from the type structure */\
-  gs_uid uid;\
-  void *client_data		/* additional data for rendering */
+  gs_uid uid
 
 typedef struct gs_pattern_template_s {
     gs_pattern_template_common;
@@ -63,10 +62,12 @@ typedef struct gs_pattern_template_s {
 /* The descriptor is public for subclassing. */
 extern_st(st_pattern_template);
 #define public_st_pattern_template() /* in gspcolor.c */\
-  gs_public_st_ptrs2(st_pattern_template, gs_pattern_template_t,\
+  gs_public_st_ptrs1(st_pattern_template, gs_pattern_template_t,\
     "gs_pattern_template_t", pattern_template_enum_ptrs,\
-    pattern_template_reloc_ptrs, uid.xvalues, client_data)
+    pattern_template_reloc_ptrs, uid.xvalues)
 #define st_pattern_template_max_ptrs 2
+
+typedef void (*gs_pinst_free_proc_t) (gs_memory_t * mem, void *pinst);
 
 /* Definition of Pattern instances. */
 #define gs_pattern_instance_common\
@@ -74,6 +75,8 @@ extern_st(st_pattern_template);
     /* Following are set by makepattern */\
     const gs_pattern_type_t *type;  /* from template */\
     gs_gstate *saved;\
+    void *client_data;		/* additional data for rendering */\
+    gs_pinst_free_proc_t notify_free;\
     gs_id pattern_id
 struct gs_pattern_instance_s {
     gs_pattern_instance_common;
@@ -82,9 +85,9 @@ struct gs_pattern_instance_s {
 /* The following is public for subclassing. */
 extern_st(st_pattern_instance);
 #define public_st_pattern_instance() /* in gspcolor.c */\
-  gs_public_st_ptrs1(st_pattern_instance, gs_pattern_instance_t,\
+  gs_public_st_ptrs2(st_pattern_instance, gs_pattern_instance_t,\
     "gs_pattern_instance_t", pattern_instance_enum_ptrs,\
-    pattern_instance_reloc_ptrs, saved)
+    pattern_instance_reloc_ptrs, saved, client_data)
 #define st_pattern_instance_max_ptrs 1
 
 /* ---------------- Procedures ---------------- */

base/gsptype1.c
@@ -770,6 +770,15 @@ typedef struct pixmap_info_s {
     void (*free_proc)(gs_memory_t *, void *, client_name_t);
 } pixmap_info;
 
+void *
+gs_get_pattern_client_data(const gs_client_color * pcc)
+{
+    const gs_pattern_instance_t *pinst = pcc->pattern;
+
+    return (pinst == 0 || pinst->type != &gs_pattern1_type ? 0 :
+            (void *)pinst->client_data);
+}
+
 gs_private_st_suffix_add1(st_pixmap_info,
                           pixmap_info,
                           "pixmap info. struct",
@@ -782,26 +791,16 @@ gs_private_st_suffix_add1(st_pixmap_info,
 #define st_pixmap_info_max_ptrs (1 + st_tile_bitmap_max_ptrs)
 
 /*
- *  Free routine for pattern instances created from pixmaps. This overwrites
- *  the free procedure originally stored in the pattern instance, and stores
- *  the pointer to that procedure in the pixmap_info structure. This procedure
- *  will call the original procedure, then free the pixmap_info structure.
+ *  Free routine for pattern instances created from pixmaps.
  *
  *  Note that this routine does NOT release the data in the original pixmap;
  *  that remains the responsibility of the client.
  */
-static void
-free_pixmap_pattern(
-    gs_memory_t *           pmem,
-    void *                  pvpinst,
-    client_name_t           cname
-)
+static void pixmap_free_notify (gs_memory_t * mem, void *vpinst)
 {
-    gs_pattern1_instance_t *pinst = (gs_pattern1_instance_t *)pvpinst;
-    pixmap_info *ppmap = pinst->templat.client_data;
+    gs_pattern1_instance_t *pinst = (gs_pattern1_instance_t *)vpinst;
 
-    ppmap->free_proc(pmem, pvpinst, cname);
-    gs_free_object(pmem, ppmap, cname);
+    gs_free_object(mem, pinst->client_data, "pixmap_free_notify");
 }
 
 /*
@@ -813,7 +812,7 @@ static int
 mask_PaintProc(const gs_client_color * pcolor, gs_gstate * pgs)
 {
     int code;
-    const pixmap_info *ppmap = gs_getpattern(pcolor)->client_data;
+    const pixmap_info *ppmap = (pixmap_info *)gs_get_pattern_client_data(pcolor);
     const gs_depth_bitmap *pbitmap = &(ppmap->bitmap);
     gs_image_enum *pen = gs_image_enum_alloc(gs_gstate_memory(pgs), "mask_PaintProc");
     gs_image1_t mask;
@@ -831,7 +830,7 @@ mask_PaintProc(const gs_client_color * pcolor, gs_gstate * pgs)
 static int
 image_PaintProc(const gs_client_color * pcolor, gs_gstate * pgs)
 {
-    const pixmap_info *ppmap = gs_getpattern(pcolor)->client_data;
+    const pixmap_info *ppmap = gs_get_pattern_client_data(pcolor);
     const gs_depth_bitmap *pbitmap = &(ppmap->bitmap);
     gs_image_enum *pen =
         gs_image_enum_alloc(gs_gstate_memory(pgs), "image_PaintProc");
@@ -946,7 +945,7 @@ int pixmap_high_level_pattern(gs_gstate * pgs)
     gs_color_space *pcs;
     gs_pattern1_instance_t *pinst =
         (gs_pattern1_instance_t *)gs_currentcolor(pgs)->pattern;
-    const pixmap_info *ppmap = ppat->client_data;
+    const pixmap_info *ppmap = (const pixmap_info *)gs_get_pattern_client_data((const gs_client_color *)&pdc->ccolor);
 
     code = gx_pattern_cache_add_dummy_entry(pgs, pinst, pgs->device->color_info.depth);
     if (code < 0)
@@ -1139,7 +1138,6 @@ gs_makepixmappattern(
     pat.XStep = (float)pbitmap->size.x;
     pat.YStep = (float)pbitmap->size.y;
     pat.PaintProc = (mask ? pixmap_remap_mask_pattern : pixmap_remap_image_pattern);
-    pat.client_data = ppmap;
 
     /* set the ctm to be the identity */
     gs_currentmatrix(pgs, &smat);
@@ -1165,9 +1163,8 @@ gs_makepixmappattern(
         if (!mask && (white_index >= (1 << pbitmap->pix_depth)))
             pinst->uses_mask = false;
 
-        /* overwrite the free procedure for the pattern instance */
-        ppmap->free_proc = pinst->rc.free;
-        pinst->rc.free = free_pixmap_pattern;
+        pinst->client_data = ppmap;
+        pinst->notify_free = pixmap_free_notify;
 
         /*
          * Since the PaintProcs don't reference the saved color space or

base/gsptype1.h
@@ -82,6 +82,7 @@ void gs_pattern1_init(gs_pattern1_template_t *);
 int gs_makepattern(gs_client_color *, const gs_client_pattern *,
                    const gs_matrix *, gs_gstate *, gs_memory_t *);
 const gs_client_pattern *gs_getpattern(const gs_client_color *);
+void *gs_get_pattern_client_data(const gs_client_color *);
 
 /* Check device color for Pattern Type 1. */
 bool gx_dc_is_pattern1_color(const gx_device_color *pdevc);

pcl/pxl/pxink.c
@@ -243,7 +243,7 @@ static int
 px_paint_pattern(const gs_client_color * pcc, gs_gstate * pgs)
 {
     const gs_client_pattern *ppat = gs_getpattern(pcc);
-    const px_pattern_t *pattern = ppat->client_data;
+    const px_pattern_t *pattern = (px_pattern_t *)gs_get_pattern_client_data(pcc);
     const byte *dp = pattern->data;
     gs_image_enum *penum;
     gs_image_t image;
@@ -307,7 +307,7 @@ int px_high_level_pattern(gs_gstate * pgs)
     gs_color_space *pcs;
     gs_pattern1_instance_t *pinst =
         (gs_pattern1_instance_t *)gs_currentcolor(pgs)->pattern;
-    const px_pattern_t * pattern = ppat->client_data;
+    const px_pattern_t *pattern = (const px_pattern_t *)gs_get_pattern_client_data(gs_currentcolor(pgs));
 
     code = gx_pattern_cache_add_dummy_entry(pgs, pinst,
         pgs->device->color_info.depth);
@@ -470,7 +470,6 @@ render_pattern(gs_client_color * pcc, const px_pattern_t * pattern,
     templat.XStep = (float)full_width;
     templat.YStep = (float)full_height;
     templat.PaintProc = px_remap_pattern;
-    templat.client_data = (void *)pattern;
     {
         gs_matrix mat;
         gs_point dsize;
@@ -535,6 +534,7 @@ render_pattern(gs_client_color * pcc, const px_pattern_t * pattern,
             gs_setcolorspace(pgs, pcs);
         }
         code = gs_makepattern(pcc, &templat, &mat, pgs, NULL);
+        pcc->pattern->client_data = (void *)pattern;
         gs_grestore(pgs);
         return code;
     }

commit 75aaaef181e28da4c78af47a0c0d3fdc30edfa62

Fix some bugs introduced with pattern context rework
This was crashing on some files in the DEBUG mode and was leaking
memory after I fixed the crash.

Not sure why it wasn't noticed earlier (was broken by commit
8199d35eb976d97ec8760c6251ae1802d5dc0402).

The notify_free routine wasn't being setup, there was a spurious
pdfi_pattern_setup() and spurious pdfi_countdown(context).

pdf/pdf_pattern.c
@@ -540,20 +540,14 @@ pdfi_setpattern_type2(pdf_context *ctx, pdf_dict *stream_dict, pdf_dict *page_di
         goto exit;
     }
     cc->pattern->client_data = context;
+    cc->pattern->notify_free = pdfi_pattern_cleanup;
     context = NULL;
-
-    code = pdfi_pattern_setup(ctx, &context, NULL, NULL, ExtGState);
-    if (code < 0) {
-        (void) pdfi_grestore(ctx);
-        goto exit;
-    }
 
     code = pdfi_grestore(ctx);
     if (code < 0)
         goto exit;
 
  exit:
-    pdfi_countdown(context);
     pdfi_countdown(Shading);
     pdfi_countdown(Matrix);
     pdfi_countdown(ExtGState);