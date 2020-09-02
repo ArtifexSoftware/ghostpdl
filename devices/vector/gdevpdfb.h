bc0403425e55309fbf372a7ebc66037d74e140d8
Ken Sharp <ken.sharp@artifex.com>
2020-09-02 13:50:44 +0100

    pdfwrite - add a new method to detect pdfmark-capable devices
    The existing method to determine if the current device is capable of
    processing pdfmark operations only works in PostScript. Since pdfmark
    is a PostScript operator this isn't unreasonable, but we need a way for
    the pdfi interpreter to know that a device is capable of handling
    pdfmarks, because some information from the current interpreter is
    passed that way (eg DOCINFO).
    
    Add a new parameter "PdfmarkCapable" which can be retrieved via a
    spec_op. Any device capable of handling pdfmarks (via put_param calls)
    must return true for this parameter.
    
    Update the current PDF interpreter to use this instead of the PostScript
    method.
    
    Note: the ps2write device is *not* PdfmarkCapable.

--- devices/vector/gdevpdfb.h
+++ devices/vector/gdevpdfb.h
@@ -283,6 +283,7 @@ const gx_device_pdf PDF_DEVICE_IDENT =
  true,				/* DetectDuplicateImages */
  false,				/* AllowIncrementalCFF */
  !PDF_FOR_OPDFREAD,		/* WantsToUnicode */
+ !PDF_FOR_OPDFREAD,		/* PdfmarkCapable */
  !PDF_FOR_OPDFREAD,		/* WantsPageLabels */
  PDF_FOR_OPDFREAD,		/* AllowPSRepeatFunctions */
  true,				/* IsDistiller (true even for ps2write!) */
