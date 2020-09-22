ArtifexSoftware/ghostpdl

commit cca279886b3bbb4d3af5768880565c9f7b372c08

Fix Bug 702181: SEGV when BufferSpace is at or above -K alloc limit
The issue with this file is that one of the pages is done in page mode because
it was so small, but the 'resize' call when doing page 2 was not checking the
allocation request against the limit, so the malloc would succeed, but the very
next (and all subsequent) allocations would fail. The gdev_prn_allocate would
capture the ecode, but would still call the clist_enable_multi_thread_render
resulting in the SEGV.

Add the check in the gs_heap_resize_object function, and make sure and leave
the 'monitor' after failure, then don't call clist_enable_multi_thread if the
resize failed.

base/gdevprn.c
@@ -520,13 +520,15 @@ gdev_prn_allocate(gx_device *pdev, gdev_prn_space_params *new_space_params,
             if (ecode == 0)
                 ecode = code;
 
-            if ( code >= 0 || (reallocate && pass > 1) )
+            if (code >= 0 || (reallocate && pass > 1))
                 ppdev->procs = gs_clist_device_procs;
-            /*
-             * Now the device is a clist device, we enable multi-threaded rendering.
-             * It will remain enabled, but that doesn't really cause any problems.
-             */
-            clist_enable_multi_thread_render(pdev);
+            if (code > 0) {
+                /*
+                 * Now the device is a clist device, we enable multi-threaded rendering.
+                 * It will remain enabled, but that doesn't really cause any problems.
+                 */
+                clist_enable_multi_thread_render(pdev);
+            }
         } else {
             /* Render entirely in memory. */
             gx_device *bdev = (gx_device *)pmemdev;

base/gsmalloc.c
@@ -279,9 +279,18 @@ gs_heap_resize_object(gs_memory_t * mem, void *obj, size_t new_num_elements,
         return obj;
     if (mmem->monitor)
         gx_monitor_enter(mmem->monitor);	/* Exclusive access */
-    new_ptr = (gs_malloc_block_t *) gs_realloc(ptr, old_size, new_size);
-    if (new_ptr == 0)
+    if (new_size > mmem->limit - sizeof(gs_malloc_block_t)) {
+        /* too large to allocate; also avoids overflow. */
+        if (mmem->monitor)
+            gx_monitor_leave(mmem->monitor);	/* Done with exclusive access */
         return 0;
+    }
+    new_ptr = (gs_malloc_block_t *) gs_realloc(ptr, old_size, new_size);
+    if (new_ptr == 0) {
+        if (mmem->monitor)
+            gx_monitor_leave(mmem->monitor);	/* Done with exclusive access */
+        return 0;
+    }
     if (new_ptr->prev)
         new_ptr->prev->next = new_ptr;
     else

commit 2812a08be06eb6cadabc57fec2c87c949951c5b7

Fix problem with BGPrint and multi-threaded rendering caused by commit cca27988
The unconditional call to enable multi-threaded rendering during set up of the
gx_device_printer as a clist caused the SEGV of bug 702181, but enabling
multi-threaded rendering here isn't correct since it is usually done when we
output the page (gdev_prn_output_page_aux). The problem is that BGPrint would
setup a new clist device to render the page, but needs to enable multi-threaded
rendering for the background clist device. Do this if NumRenderThreads > 0.

base/gdevprn.c
@@ -516,13 +516,6 @@ gdev_prn_allocate(gx_device *pdev, gdev_space_params *new_space_params,
 
             if (code >= 0 || (reallocate && pass > 1))
                 ppdev->procs = gs_clist_device_procs;
-            if (code > 0) {
-                /*
-                 * Now the device is a clist device, we enable multi-threaded rendering.
-                 * It will remain enabled, but that doesn't really cause any problems.
-                 */
-                clist_enable_multi_thread_render(pdev);
-            }
         } else {
             /* Render entirely in memory. */
             gx_device *bdev = (gx_device *)pmemdev;
@@ -1093,6 +1086,12 @@ gdev_prn_output_page_aux(gx_device * pdev, int num_copies, int flush, bool seeka
                 npdev = (gx_device_printer *)ndev;
                 npdev->bg_print_requested = 0;
                 npdev->num_render_threads_requested = ppdev->num_render_threads_requested;
+                /* The bgprint's device was created with normal procs, so multi-threaded */
+                /* rendering was turned off. Re-enable it now if it is needed.           */
+                if (npdev->num_render_threads_requested > 0) {
+                    /* ignore return code - even if it fails, we'll output the page */
+                    (void)clist_enable_multi_thread_render(ndev);
+                }
 
                 /* Now start the thread to print the page */
                 if ((code = gp_thread_start(prn_print_page_in_background,