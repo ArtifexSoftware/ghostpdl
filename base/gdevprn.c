ArtifexSoftware/ghostpdl

commit 8df410c269150bce143d3b8875ac9d60a013d206

Set orig_spec_op for printer class devices so it forwards properly.

base/gdevprn.c
@@ -265,7 +265,7 @@ BACKTRACE(pdev);
 open_c:
     ppdev->buf = base;
     ppdev->buffer_space = space;
-    pclist_dev->common.orig_spec_op = dev_proc(ppdev, dev_spec_op);
+    pclist_dev->common.orig_spec_op = gdev_prn_dev_spec_op;
     clist_init_io_procs(pclist_dev, ppdev->BLS_force_memory);
     clist_init_params(pclist_dev, base, space, target,
                       ppdev->printer_procs.buf_procs,

commit 080a23c44808a93fa27ba2d4dfdf8d8748a8e84a

Fix bug 703198: SEGV's with psdcmykog device and --saved-pages-test
This was a reference of a missing icc_table caused by the band count not
being adjusted during saved-page playback as it was during writing. The
orig_spec_op in the gx_device_printer structure was being set to the
gdev_prn_spec_op, but this did not call the target device's spec_op, so
it did not get the result from gxdso_adjust_bandheight that was needed.

Caused by commit 8df410c26.

Also clean up a left over extern in gxclrect.c (not related to the fix).

base/gdevprn.c
@@ -266,7 +266,7 @@ BACKTRACE(pdev);
 open_c:
     ppdev->buf = base;
     ppdev->buffer_space = space;
-    pclist_dev->common.orig_spec_op = gdev_prn_dev_spec_op;
+    pclist_dev->common.orig_spec_op = gdev_prn_forwarding_dev_spec_op;
     clist_init_io_procs(pclist_dev, ppdev->BLS_force_memory);
     clist_init_params(pclist_dev, base, space, target,
                       ppdev->printer_procs.buf_procs,

base/gxclrect.c
@@ -24,8 +24,6 @@
 #include "gxclpath.h"
 #include "gxdevsop.h"
 
-extern dev_proc_dev_spec_op(gdev_prn_forwarding_dev_spec_op);
-
 /* ---------------- Writing utilities ---------------- */
 
 #define cmd_set_rect(rect)\