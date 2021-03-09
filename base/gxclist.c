ArtifexSoftware/ghostpdl

commit 00e7143ce97a3d983a223c7d69f74f995d8e267d

Pass memory to clist_make_accum_device()
Change function args to propagate the memory type of the graphics
state to clist_make_accum_device to create device with it.

Also, make sure the dev->bandlist_memory (used for freeing 'data') is
allocated in the mem->non_gc_memory instead of the parent device's
(fixes crash in gpdl/pcl that was introduced).

That will mean pdfi and gs/pdfi get non-gc, and gs gets gc.

There was a bug in gs/pdfi where the clist-pattern device was in gc
memory and it was getting freed when pdfi was still using it.

This should fix a segfault in: tests_private/comparefiles/Bug688396.pdf

base/gxclist.c
@@ -1335,12 +1335,11 @@ clist_put_data(const gx_device_clist *cdev, int select, int64_t offset, const by
 }
 
 gx_device_clist *
-clist_make_accum_device(gx_device *target, const char *dname, void *base, int space,
+clist_make_accum_device(gs_memory_t *mem, gx_device *target, const char *dname, void *base, int space,
                         gx_device_buf_procs_t *buf_procs, gx_band_params_t *band_params,
                         bool use_memory_clist, bool uses_transparency,
                         gs_pattern1_instance_t *pinst)
 {
-        gs_memory_t *mem = target->memory;
         gx_device_clist *cdev = gs_alloc_struct(mem, gx_device_clist,
                         &st_device_clist, "clist_make_accum_device");
         gx_device_clist_writer *cwdev = (gx_device_clist_writer *)cdev;

base/gxclist.h
@@ -478,7 +478,7 @@ void clist_update_trans_bbox(gx_device_clist_writer *dev, gs_int_rect *bbox);
 /* Make a clist device for accumulating. Used for pattern-clist as well as */
 /* for pdf14 pages that are too large to be done in page mode.             */
 gx_device_clist *
-clist_make_accum_device(gx_device *target, const char *dname, void *base, int space,
+clist_make_accum_device(gs_memory_t *mem, gx_device *target, const char *dname, void *base, int space,
                         gx_device_buf_procs_t *buf_procs, gx_band_params_t *band_params,
                         bool use_memory_clist, bool uses_transparency,
                         gs_pattern1_instance_t *pinst);

base/gxpcmap.c
@@ -309,7 +309,7 @@ gx_pattern_accum_alloc(gs_memory_t * mem, gs_memory_t * storage_memory,
         gx_device_clist_writer *cwdev;
         const int data_size = 1024*128;
         gx_band_params_t band_params = { 0 };
-        byte *data  = gs_alloc_bytes(tdev->memory->non_gc_memory, data_size, cname);
+        byte *data  = gs_alloc_bytes(mem->non_gc_memory, data_size, cname);
 
         if (data == NULL)
             return 0;
@@ -319,7 +319,7 @@ gx_pattern_accum_alloc(gs_memory_t * mem, gs_memory_t * storage_memory,
         band_params.BandHeight = pinst->size.y;
         band_params.BandBufferSpace = 0;
 
-        cdev = clist_make_accum_device(tdev, "pattern-clist", data, data_size,
+        cdev = clist_make_accum_device(mem, tdev, "pattern-clist", data, data_size,
                                        &buf_procs, &band_params, true, /* use_memory_clist */
                                        pinst->templat.uses_transparency, pinst);
         if (cdev == 0) {

commit 6cd65db61b4285365b930adad85c0ca0c54a7dc4

Fix Bug 703648. Commit 00e7143ce9 caused this to surface by changing allocator.
The clist_make_accum_device previously used the target device's allocator which
was 'system' VM, and commit 00e7143ce9 changed it to use 'local' VM which is
subject to save/restore. The sample file resutled in an extra rc_decrement of
the device on page 4 (from gx_pattern_accum_finalize during the restore).

Fix this by using 'stable_memory' (from the mem allocator passed in) prevents
the restore from affecting the pattern-clist accumulator.

Note the simple command line to reproduce is:
bin/gs -r1200 -sDEVICE=pgmraw -dBufferSpace=16m -o /dev/null \
     Ad_Quark.ps Ad_Quark.ps Ad_Quark.ps
which results in "corrupted double-linked list"

base/gxclist.c
@@ -1378,7 +1378,7 @@ clist_make_accum_device(gs_memory_t *mem, gx_device *target, const char *dname, 
                         bool use_memory_clist, bool uses_transparency,
                         gs_pattern1_instance_t *pinst)
 {
-        gx_device_clist *cdev = gs_alloc_struct(mem, gx_device_clist,
+        gx_device_clist *cdev = gs_alloc_struct(mem->stable_memory, gx_device_clist,
                         &st_device_clist, "clist_make_accum_device");
         gx_device_clist_writer *cwdev = (gx_device_clist_writer *)cdev;
 
@@ -1388,10 +1388,10 @@ clist_make_accum_device(gs_memory_t *mem, gx_device *target, const char *dname, 
         cwdev->params_size = sizeof(gx_device_clist);
         cwdev->static_procs = NULL;
         cwdev->dname = dname;
-        cwdev->memory = mem;
+        cwdev->memory = mem->stable_memory;
         cwdev->stype = &st_device_clist;
         cwdev->stype_is_dynamic = false;
-        rc_init(cwdev, mem, 1);
+        rc_init(cwdev, mem->stable_memory, 1);
         cwdev->retained = true;
         cwdev->is_open = false;
         cwdev->color_info = target->color_info;