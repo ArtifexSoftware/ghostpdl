29e88ec91d42f21b90ce5fe08c3a66e74d7dfe97
Robin Watts <Robin.Watts@artifex.com>
2021-03-15 13:09:13 +0000

    Fix merge error causing CAL SEGVs.
    We were detecting plugins as being old format. This only shows up
    in testing with CAL.

--- lcms2mt/src/cmsxform.c
+++ lcms2mt/src/cmsxform.c
@@ -966,7 +966,7 @@ cmsBool  _cmsRegisterTransformPlugin(cmsContext ContextID, cmsPluginBase* Data)
     if (fl == NULL) return FALSE;
 
     // Check for full xform plug-ins previous to 2.8, we would need an adapter in that case
-    if (Plugin->base.ExpectedVersion < 2080) {
+    if (Plugin->base.ExpectedVersion < 2080-2000) {
 
            fl->OldXform = TRUE;
     }
