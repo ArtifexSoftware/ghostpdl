3775bc48cc4747b1eeccc234d14faa4cc63eb4ee
Ken Sharp <ken.sharp@artifex.com>
2021-03-24 09:52:59 +0000

    For pdfi-inside-Ghostscript, fix the page Group size
    We use the ctx->page.Size values for the initial page Group when a
    page has a Group. We want to use the actual media size of the device,
    so that we cover the actual output.
    
    However, the values passed to the compositor should be in user space,
    so we need to apply the inverse transform of the CTM in order to get
    the correct values.
    
    This appears now to work correctly for rotated and offset media.

--- pdf/pdf_page.c
+++ pdf/pdf_page.c
@@ -191,6 +191,7 @@ static int pdfi_get_media_size(pdf_context *ctx)
     gs_c_param_list list;
     int code;
     gs_param_float_array msa;
+    gs_point point;
 
     code = pdfi_device_check_param(ctx->pgs->device, ".MediaSize", &list);
     if (code < 0) goto exit;
@@ -199,13 +200,13 @@ static int pdfi_get_media_size(pdf_context *ctx)
     code = param_read_float_array((gs_param_list *)&list, ".MediaSize", &msa);
     if (code < 0) goto exit;
 
-    /* TODO: I have no idea if this works right when the page is rotated.
-     * It makes my brain explode :(
-     */
-    ctx->page.Size[0] = 0;
-    ctx->page.Size[1] = 0;
-    ctx->page.Size[2] = msa.data[0];
-    ctx->page.Size[3] = msa.data[1];
+    code = gs_point_transform_inverse(0, 0, &ctm_only(ctx->pgs), &point);
+    ctx->page.Size[0] = point.x;
+    ctx->page.Size[1] = point.y;
+    code = gs_point_transform_inverse(msa.data[0], msa.data[1], &ctm_only(ctx->pgs), &point);
+    ctx->page.Size[2] = point.x;
+    ctx->page.Size[3] = point.y;
+
 
  exit:
     gs_c_param_list_release(&list);
