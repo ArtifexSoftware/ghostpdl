ArtifexSoftware/ghostpdl

commit bdf7cf8b6c7c97531aac14e16b9d2c16775ae8c8

Fix Bug 702034. Missing image to DeviceN devices.
The file from Bug 693300 has a blank image when going to DeviceN devices
such as psdcmyk and tiffsep. The CompatibleOverprint blend mode must be
set before the transparency group is pushed.

Add a .special_op for SupportsDevn to let the PostScript PDF interpreter
detect that the device supports separations.

Make sure the mark fill rect for the knockout case handles
the hybrid case of additive process colors with subtractive spots
and overprint.

And make sure the group that is pushed in gstrans.c for text knockouts
uses compatible overprint if needed.

Ray did the PS parts and Michael did the pdf14dev parts.

base/gdevp14.c
@@ -7122,8 +7122,25 @@ do_mark_fill_rectangle_ko_simple(gx_device *dev, int x, int y, int w, int h,
             }
             /* Complement the results for subtractive color spaces */
             if (additive) {
-                for (k = 0; k < num_chan; ++k)
-                    dst_ptr[k * planestride] = dst[k];
+                if (!overprint) {
+                    for (k = 0; k < num_chan; ++k)
+                        dst_ptr[k * planestride] = dst[k];
+                } else {
+                    /* Hybrid additive with subtractive spots */
+                    /* We may have to do the compatible overprint blending */
+                    if (!buf->isolated && drawn_comps != (((size_t)1 << (size_t)dev->color_info.num_components) - (size_t)1)) {
+                        art_pdf_composite_knockout_8(dst2, src, num_comp,
+                            blend_mode, pdev->blend_procs, pdev);
+                    }
+                    for (k = 0, comps = drawn_comps; k < num_comp; ++k, comps >>= 1) {
+                        if ((comps & 0x1) != 0) {
+                            dst_ptr[k * planestride] = dst[k];
+                        } else {
+                            /* Compatible overprint blend result. */
+                            dst_ptr[k * planestride] = dst2[k];
+                        }
+                    }
+                }
             } else {
                 if (overprint) {
                     /* We may have to do the compatible overprint blending */
@@ -7321,8 +7338,25 @@ do_mark_fill_rectangle_ko_simple16(gx_device *dev, int x, int y, int w, int h,
             }
             /* Complement the results for subtractive color spaces */
             if (additive) {
-                for (k = 0; k < num_chan; ++k)
-                    dst_ptr[k * planestride] = dst[k];
+                if (!overprint) {
+                    for (k = 0; k < num_chan; ++k)
+                        dst_ptr[k * planestride] = dst[k];
+                } else {
+                    /* Hybrid additive with subtractive spots */
+                    /* We may have to do the compatible overprint blending */
+                    if (!buf->isolated && drawn_comps != (((size_t)1 << (size_t)dev->color_info.num_components) - (size_t)1)) {
+                        art_pdf_composite_knockout_16(dst2, src, num_comp,
+                            blend_mode, pdev->blend_procs, pdev);
+                    }
+                    for (k = 0, comps = drawn_comps; k < num_comp; ++k, comps >>= 1) {
+                        if ((comps & 0x1) != 0) {
+                            dst_ptr[k * planestride] = dst[k];
+                        } else {
+                            /* Compatible overprint blend result. */
+                            dst_ptr[k * planestride] = dst2[k];
+                        }
+                    }
+                }
             } else {
                 if (overprint) {
                     /* We may have to do the compatible overprint blending */

Resource/Init/pdf_draw.ps
@@ -2308,6 +2308,12 @@ currentdict /last-ditch-bpc-csp undef
          % code only picks up the current colour space, not the space from the dictionary.
          currentdict /SMask get /Matte known {/CS currentdict /ColorSpace get dup pdfopdict /cs get exec } if
       >> 0 0 1 1
+      gsave 1 1 moveto 0 0 lineto
+      % If we get an error, just emit an empty box
+      { pathbbox } stopped { 0 0 0 0 } if
+      4 array astore grestore
+      /image
+      setup_trans
       .begintransparencygroup
       .currentstrokeconstantalpha .currentfillconstantalpha 3 -1 roll
       1 .setfillconstantalpha 1 .setstrokeconstantalpha
@@ -2409,12 +2415,6 @@ currentdict /last-ditch-bpc-csp undef
       makemaskimage
   } if
                 % Stack: datasource imagemask
-  gsave 1 1 moveto 0 0 lineto
-  % If we get an error, just emit an empty box
-  { pathbbox } stopped { 0 0 0 0 } if
-  4 array astore grestore
-  /image
-  setup_trans
   { currentdict end setsmaskstate //true ValidateDecode { imagemask } }
   { ColorSpace setgcolorspace currentdict end setsmaskstate //false ValidateDecode { image } }
   ifelse
@@ -3008,6 +3008,12 @@ drawopdict begin
     /File PDFsource def
     currentdict makeimagekeys
     OFFlevels length 0 eq {
+      gsave 1 1 moveto 0 0 lineto
+      % If we get an error, just emit an empty box
+      { pathbbox } stopped { 0 0 0 0 } if
+      4 array astore grestore
+      /image
+      setup_trans
       doimage
     } {
       pop Width

Resource/Init/pdf_ops.ps
@@ -695,10 +695,11 @@ end
 {  % Check OP and BM in case we need to push a group
   //OPsaveDstack begin
   //Dpush exec			% push the current OPsaveDstack values into 'previous'
+  /SupportsDevn .special_op
   okOPcs currentcolorspace 0 get dup /Indexed eq {
     pop currentcolorspace 1 get	% use the base space
   } if
-  known {
+  known or {
     1 index /stroke ne { .currentfilloverprint } { .currentstrokeoverprint } ifelse
     % Change BM to CompatibleOverprint if this has overprint true
     dup /ChangeBM exch def

base/gstrans.c
@@ -348,7 +348,11 @@ gx_begin_transparency_group(gs_gstate * pgs, gx_device * pdev,
     tgp.group_opacity = pparams->opacity;
     tgp.group_shape = pparams->shape;
 
-    pgs->blend_mode = pparams->blend_mode;
+    if (tgp.Knockout && tgp.text_group == PDF14_TEXTGROUP_BT_PUSHED &&
+        ((pgs->overprint && pgs->is_fill_color) || (pgs->stroke_overprint && !pgs->is_fill_color)))
+        pgs->blend_mode = BLEND_MODE_CompatibleOverprint;
+    else
+        pgs->blend_mode = pparams->blend_mode;
     bbox = pparams->bbox;
 #ifdef DEBUG
     if (gs_debug_c('v')) {

psi/zdevice.c
@@ -590,6 +590,7 @@ struct spec_op_s {
 spec_op_t spec_op_defs[] = {
     {(char *)"GetDeviceParam", 0},
     {(char *)"EventInfo", 1},
+    {(char *)"SupportsDevn", 2},
 };
 
 /* <any> <any> .... /spec_op name .special_op <any> <any> .....
@@ -684,8 +685,8 @@ zspec_op(i_ctx_t *i_ctx_p)
                 stack_param_list list;
                 dev_param_req_t request;
                 ref rkeys;
-                /* Get a single device parameter, we should be supplied with
-                 * the name of the paramter, as a name object.
+                /* EventInfo we should be supplied with a name object which we
+                 * pass as the event info to the dev_spec_op
                  */
                 check_op(1);
                 if (!r_has_type(op, t_name))
@@ -721,6 +722,19 @@ zspec_op(i_ctx_t *i_ctx_p)
                     } else
                         return_error(code);
                 }
+            }
+            break;
+        case 2:
+            {
+                /* SupportsDevn. Return the boolean from the device */
+
+                code = dev_proc(dev, dev_spec_op)(dev, gxdso_supports_devn, NULL, 0);
+                if (code < 0 && code != gs_error_undefined)
+                    return_error(code);		/* any other error leaves the stack unchanged */
+
+                op = osp;
+                push(1);
+                make_bool(op, code > 0 ? 1 : 0);	/* return true/false */
             }
             break;
         default:

commit 6804eba723c9c9fca76d79b4b20c8893486b8d0d

Fix bug 703848: Missing interior of rectangles (regression in 9.54.0)
This was caused by the commit bdf7cf8b6c. If overprint was true, in the
sections of the 8 and 16 bit do_mark_fill_rectangle_ko_simple that copy
the 'dst' pixel to the plane buffers (dst_ptr), the colors were copied,
but not the alpha.

For the example file, the fill_stroke had the fill end up transparent
and the stroke be visible, since stroke_overprint was false.

base/gdevp14.c
@@ -7795,6 +7795,7 @@ do_mark_fill_rectangle_ko_simple(gx_device *dev, int x, int y, int w, int h,
                             dst_ptr[k * planestride] = dst2[k];
                         }
                     }
+                    dst_ptr[num_comp * planestride] = dst[num_comp];    /* alpha */
                 }
             } else {
                 if (overprint) {
@@ -8011,6 +8012,7 @@ do_mark_fill_rectangle_ko_simple16(gx_device *dev, int x, int y, int w, int h,
                             dst_ptr[k * planestride] = dst2[k];
                         }
                     }
+                    dst_ptr[num_comp * planestride] = dst[num_comp];    /* alpha */
                 }
             } else {
                 if (overprint) {