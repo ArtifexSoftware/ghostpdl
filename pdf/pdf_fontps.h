2187494227097f1b3a9f08e370335ab88d5a297f
Chris Liddell <chris.liddell@artifex.com>
2021-06-03 22:02:04 +0100

    Fix pdfi "ps" stack extension initialisation

--- pdf/pdf_fontps.h
+++ pdf/pdf_fontps.h
@@ -177,7 +177,7 @@ static inline int pdf_ps_stack_push(pdf_ps_ctx_t *s)
        for valid files
      */
     if (s->cur + 1 >= s->toplim - 1) {
-        int i, currsize = s->toplim - s->stack - 1;
+        int i, currsize = s->toplim - s->stack;
         int newsize = currsize + PDF_PS_STACK_GROW_SIZE;
         int newsizebytes = newsize * sizeof(pdf_ps_stack_object_t);
         pdf_ps_stack_object_t *nstack;
@@ -187,13 +187,15 @@ static inline int pdf_ps_stack_push(pdf_ps_ctx_t *s)
             if (nstack != NULL) {
                 for (i = 0; i < PDF_PS_STACK_GUARDS; i++)
                     nstack[i].type = PDF_PS_OBJ_STACK_BOTTOM;
+
                 for (i = 0; i < PDF_PS_STACK_GUARDS; i++)
                     nstack[newsize - PDF_PS_STACK_GUARDS + i].type = PDF_PS_OBJ_STACK_TOP;
 
                 for (i = currsize + PDF_PS_STACK_GUARDS; i < newsize - PDF_PS_STACK_GUARDS; i++) {
                     pdf_ps_make_null(&(nstack[i]));
                 }
-                memcpy(nstack + 1, s->stack + 1, (currsize - 1) * sizeof(pdf_ps_stack_object_t));
+
+                memcpy(nstack + 1, s->stack + 1, (currsize) * sizeof(pdf_ps_stack_object_t));
                 gs_free_object(s->pdfi_ctx->memory, s->stack, "pdf_ps_stack_push(s->stack)");
                 s->stack = nstack;
                 s->cur = s->stack + currsize - 1;
