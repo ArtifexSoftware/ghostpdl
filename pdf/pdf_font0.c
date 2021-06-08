ff69970531ea0ee235d086aa80a99524e010bdb0
Chris Liddell <chris.liddell@artifex.com>
2021-06-08 10:50:50 +0100

    Accept embedded truetype collections

--- pdf/pdf_font0.c
+++ pdf/pdf_font0.c
@@ -77,7 +77,8 @@ static decfont_type pdfi_fonttype_picker(byte *buf, int64_t buflen)
 
     if (buflen >= 4) {
         if (MAKEMAGIC(buf[0], buf[1], buf[2], buf[3]) == MAKEMAGIC(0, 1, 0, 0)
-            || MAKEMAGIC(buf[0], buf[1], buf[2], buf[3]) == MAKEMAGIC('t', 'r', 'u', 'e')) {
+            || MAKEMAGIC(buf[0], buf[1], buf[2], buf[3]) == MAKEMAGIC('t', 'r', 'u', 'e')
+            || MAKEMAGIC(buf[0], buf[1], buf[2], buf[3]) == MAKEMAGIC('t', 't', 'c', 'f')) {
             return decfont_type_cidtype2;
         }
         else if (MAKEMAGIC(buf[0], buf[1], buf[2], buf[3]) == MAKEMAGIC('O', 'T', 'T', 'O')) {
