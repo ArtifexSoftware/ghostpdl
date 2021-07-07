df9bc0c41dcea0a1c04acd05a90db6b686fcad4a
Chris Liddell <chris.liddell@artifex.com>
2021-07-07 14:44:37 +0100

    OTTO font: free font buffer before error out.
    For OTTO fonts, we scan the tables for the CFF data, and use the CFF. If that
    process fails, we drop out with an error, but the early error exit wasn't
    freeing the font data buffer, which it should

--- pdf/pdf_font1C.c
+++ pdf/pdf_font1C.c
@@ -1986,6 +1986,7 @@ pdfi_read_cff_font(pdf_context *ctx, pdf_dict *font_dict, pdf_dict *stream_dict,
             }
         }
         if (toffs == 0 || tlen == 0 || toffs + tlen > fbuflen) {
+            gs_free_object(ctx->memory, pfbuf, "pdfi_read_cff_font(fbuf)");
             return_error(gs_error_invalidfont);
         }
         fbuf += toffs;
