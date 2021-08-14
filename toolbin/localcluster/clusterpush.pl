6239b56a1b0c8f1fdd9381c78993ecd99be987cf
Julian Smith <julian.smith@artifex.com>
2021-08-14 15:05:50 +0100

    toolbin/localcluster/clusterpush.pl: added muwasm.

--- toolbin/localcluster/clusterpush.pl
+++ toolbin/localcluster/clusterpush.pl
@@ -28,6 +28,7 @@ my %products=('abort' =>1,
               'runtests'=>1,
               'extract'=>1,
               'extractmu'=>1,
+              'muwasm'=>1,
               'extractgs'=>1);
 
 my $user;
