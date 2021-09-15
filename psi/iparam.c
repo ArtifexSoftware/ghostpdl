f452256f0ba9a9131f6491afa3637b5c540e01ef
Robin Watts <Robin.Watts@artifex.com>
2021-09-15 14:32:44 +0100

    Fix stack_param_enumerate enumerating past the mark.
    This was causing problems with some files when using NupControl
    when we were copying the param lists, and finding ourselves
    running into hyperspace.
    
    Credit to Ken Sharp for locating and explaining the problem to
    me.

--- psi/iparam.c
+++ psi/iparam.c
@@ -406,11 +406,14 @@ stack_param_enumerate(iparam_list * plist, gs_param_enumerator_t * penum,
     ref *stack_element;
 
     do {
+        if (index >= splist->count*2)
+            return 1;
         stack_element =
             ref_stack_index(splist->pstack, index + 1 + splist->skip);
         if (!stack_element)
             return 1;
-    } while (index += 2, !r_has_type(stack_element, t_name));
+        index += 2;
+    } while (!r_has_type(stack_element, t_name));
     *type = r_type(stack_element);
     code = ref_to_key(stack_element, key, plist);
     penum->intval = index;
