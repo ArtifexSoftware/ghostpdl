ArtifexSoftware/ghostpdl

commit 9f4cf42078f4ce4bc65f1ccf3407d69fbfe476c8

pdfwrite - fix memory leak with patterns and dashed lines
On exiting a pattern accumulation we reset the graphics state to the
gstate in force at the start of the pattern accumulation (any changes
to the graphics state during the course of the pattern belong to the
pattern).

If the pattern altered the line dash parameters without grestoreing them
away then we would end up with an allocated dash pattern which would
leak when we returned the graphics state to it's initial values.

So here, if the dash pattern is not NULL and differs from the initial
graphics state, then we free the object before we overwrite it.

devices/vector/gdevpdfi.c
@@ -2372,6 +2372,8 @@ static int reset_gstate_for_pattern(gx_device_pdf * pdev, gs_gstate *destination
         source->line_params.dot_length_absolute = pdev->vg_initial.line_params.dot_length_absolute;
         destination->line_params.dot_orientation = source->line_params.dot_orientation;
         source->line_params.dot_orientation = pdev->vg_initial.line_params.dot_orientation;
+        if (destination->line_params.dash.pattern != NULL && destination->line_params.dash.pattern != source->line_params.dash.pattern)
+            gs_free_object(pdev->pdf_memory, destination->line_params.dash.pattern, "free dash assigned during pattern accumulation");
         memcpy(&destination->line_params.dash, &source->line_params.dash, sizeof(source->line_params.dash));
         memcpy(&source->line_params.dash, &pdev->vg_initial.line_params.dash, sizeof(source->line_params.dash));
     }

commit 6a24bfe01d84e2d8677a46d78fa697a7fa7f81d2

pdfwrite - Use correct memory allocator (hopefully)
Commit 9f4cf42078f4ce4bc65f1ccf3407d69fbfe476c8 accidentally used the
device memory allocator instead of the gstate memory allocator
to free 'dangling' dash patterns, which caused a seg fault if the two
were not the same (apparently they are not in PCL when run under gpdl)

Apparently it is possible to specify the allocator using gx_set_dash()
but it seems that everywhere uses either the graphics state allocator
or NULL (clist) so using the graphics state allocator to free it should
be OK and in fact if it isn't I suspect other things will fail badly.

devices/vector/gdevpdfi.c
@@ -2373,7 +2373,7 @@ static int reset_gstate_for_pattern(gx_device_pdf * pdev, gs_gstate *destination
         destination->line_params.dot_orientation = source->line_params.dot_orientation;
         source->line_params.dot_orientation = pdev->vg_initial.line_params.dot_orientation;
         if (destination->line_params.dash.pattern != NULL && destination->line_params.dash.pattern != source->line_params.dash.pattern)
-            gs_free_object(pdev->pdf_memory, destination->line_params.dash.pattern, "free dash assigned during pattern accumulation");
+            gs_free_object(destination->memory, destination->line_params.dash.pattern, "free dash assigned during pattern accumulation");
         memcpy(&destination->line_params.dash, &source->line_params.dash, sizeof(source->line_params.dash));
         memcpy(&source->line_params.dash, &pdev->vg_initial.line_params.dash, sizeof(source->line_params.dash));
     }