7fe54b1dbf221864b25ff55eccf1d624c07bb21d
Chris Liddell <chris.liddell@artifex.com>
2021-12-14 08:47:05 +0000

    oss-fuzz 42285: don't free an already freed buffer
    pdfi_close_memory_stream() already frees the buffer, no need to do so
    separately.

--- pdf/pdf_file.c
+++ pdf/pdf_file.c
@@ -1275,7 +1275,6 @@ int pdfi_open_memory_stream_from_filtered_stream(pdf_context *ctx, pdf_stream *s
     code = pdfi_filter(ctx, stream_obj, compressed_stream, &decompressed_stream, false);
     if (code < 0) {
         pdfi_close_memory_stream(ctx, *Buffer, *new_pdf_stream);
-        gs_free_object(ctx->memory, *Buffer, "pdfi_open_memory_stream_from_filtered_stream");
         *Buffer = NULL;
         *new_pdf_stream = NULL;
         return code;
