fabad29820734af831cf4f2ae6c4beb4179d4733
Ken Sharp <ken.sharp@artifex.com>
2021-12-24 11:30:15 +0000

    OSS-fuzz #42908 - limit debug string to available buffer size

--- pdf/pdf_optcontent.c
+++ pdf/pdf_optcontent.c
@@ -302,8 +302,8 @@ pdfi_oc_is_ocg_visible(pdf_context *ctx, pdf_dict *ocdict)
             is_visible = pdfi_oc_check_OCG_usage(ctx, ocdict);
     } else {
         char str[100];
-        memcpy(str, (const char *)type->data, type->length);
-        str[type->length] = '\0';
+        memcpy(str, (const char *)type->data, type->length < 100 ? type->length : 99);
+        str[type->length < 100 ? type->length : 99] = '\0';
         dmprintf1(ctx->memory, "WARNING: OC dict type is %s, expected OCG or OCMD\n", str);
     }
 
