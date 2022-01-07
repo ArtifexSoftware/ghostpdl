0b4c27ca7d92c63ef70205064187ae99617a2f09
Chris Liddell <chris.liddell@artifex.com>
2022-01-07 15:24:34 +0000

    Change order of string bounds checking
    to maybe placate coverity.

--- pdf/pdf_cmap.c
+++ pdf/pdf_cmap.c
@@ -646,10 +646,10 @@ pdf_cmap_open_file(pdf_context *ctx, gs_string *cmap_name, byte **buf, int64_t *
     const char *path_pfx = "CMap/";
     fname[0] = '\0';
 
-    strncat(fname, path_pfx, strlen(path_pfx));
-    if (strlen(fname) + cmap_name->size >= gp_file_name_sizeof)
+    if (strlen(path_pfx) + cmap_name->size >= gp_file_name_sizeof)
         return_error(gs_error_rangecheck);
 
+    strncat(fname, path_pfx, strlen(path_pfx));
     strncat(fname, (char *)cmap_name->data, cmap_name->size);
     code = pdfi_open_resource_file(ctx, (const char *)fname, (const int)strlen(fname), &s);
     if (code >= 0) {
