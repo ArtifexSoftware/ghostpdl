48b28fdf7d78487ebecf4455d671cc711057d37c
Chris Liddell <chris.liddell@artifex.com>
2022-01-20 14:45:24 +0000

    pdfi: Check available buffer space for decoding eexec section
    in Type 1 fonts.
    
    Noticed while trying to investigate oss-fuzz issue 43693

--- pdf/pdf_fontps.c
+++ pdf/pdf_fontps.c
@@ -979,6 +979,10 @@ ps_font_eexec_func(gs_memory_t *mem, pdf_ps_ctx_t *s, byte *buf, byte *bufend)
     stream *strm;
     int c;
 
+    if (bufend < buf) {
+        return_error(gs_error_invalidfont);
+    }
+
     strm = push_eexec_filter(mem, buf, bufend);
     while (1) {
         c = sgetc(strm);
