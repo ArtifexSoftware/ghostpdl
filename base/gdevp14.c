efab3239bc4a02827cafbc89149bb8d982ee8a8e
Michael Vrhel <michael.vrhel@artifex.com>
2022-02-14 16:04:33 -0800

    Softmask object memory leak. Related to Bug 693312
    When the source file has already created a softmask
    and is using another we currently destroy the mask
    stack and create a new one, which was related to
    Bug 693312.  Unfortunately, we were not ensuring
    that the masks in the stack were getting
    released.  See
    
    \tests_private\comparefiles\Bug691803.pdf
    -o output_cmyk.tif -sDEVICE=tiff32nc Bug691803.pdf
    
    for an example memory leak in gpdf with memento build.

--- base/gdevp14.c
+++ base/gdevp14.c
@@ -1893,7 +1893,9 @@ static void pdf14_free_mask_stack(pdf14_ctx *ctx, gs_memory_t *memory)
         pdf14_mask_t *curr_mask = mask_stack;
         pdf14_mask_t *old_mask;
         while (curr_mask != NULL) {
-            rc_decrement(curr_mask->rc_mask, "pdf14_free_mask_stack");
+            /* Force to decrement until free */
+            while (curr_mask->rc_mask != NULL)
+                rc_decrement(curr_mask->rc_mask, "pdf14_free_mask_stack");
             old_mask = curr_mask;
             curr_mask = curr_mask->previous;
             gs_free_object(old_mask->memory, old_mask, "pdf14_free_mask_stack");
