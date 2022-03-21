ArtifexSoftware/ghostpdl

commit 1158b25fe28f9d60c65417712f16a0f9cccac95c

pdfwrite - add controls for optional entries with variable content
Bug #704846 "Optionally omit CreatingTime in PDF output for reproducibility"

Some content in a PDF file is created using the system time, and will
vary depending on the precise time the file is created. This is entirely
correct and we are not prepared to alter this behaviour.

However said content is also often optional, but there was no way to
prevent pdfwrite creating it.

This commit adds three new controls; OmitDateInfo, OmitXMP and OmitID
which direct pdfwrite not to create and include the CreationDate and
ModDate in the /Info dict and Metadata (OmitInfoDate), the file ID in
the trailer dictionary (OmitID) and the XMP Metadata included in the
Catalog dictionary (OmitXMP).

There are, however, other considerations. XMP metadata is required for a
valid PDF/A file, /ID is required for PDF 2.0, and for encrypted files
and CreationDate is required for PDF/X-3.

As a result if you try to set these controls with one of those other
features, it will raise a warning and ignore the control.

devices/vector/gdevpdfp.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2021 Artifex Software, Inc.
+/* Copyright (C) 2001-2022 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
@@ -129,6 +129,9 @@ static const gs_param_item_t pdf_param_items[] = {
     pi("NoOutputFonts", gs_param_type_bool, FlattenFonts),
     pi("WantsPageLabels", gs_param_type_bool, WantsPageLabels),
     pi("UserUnit", gs_param_type_float, UserUnit),
+    pi("OmitInfoDate", gs_param_type_bool, OmitInfoDate),
+    pi("OmitID", gs_param_type_bool, OmitID),
+    pi("OmitXMP", gs_param_type_bool, OmitXMP),
 #undef pi
     gs_param_item_end
 };
@@ -297,6 +300,13 @@ gdev_pdf_get_param(gx_device *dev, char *Param, void *list)
     }
 #endif
 
+    if (strcmp(Param, "OmitInfoDate") == 0)
+        return(param_write_bool(plist, "OmitInfoDate", &pdev->OmitInfoDate));
+    if (strcmp(Param, "OmitXMP") == 0)
+        return(param_write_bool(plist, "OmitXMP", &pdev->OmitXMP));
+    if (strcmp(Param, "OmitID") == 0)
+        return(param_write_bool(plist, "OmitID", &pdev->OmitID));
+
     return gdev_psdf_get_param(dev, Param, list);
 }
 
@@ -380,7 +390,7 @@ gdev_pdf_put_params_impl(gx_device * dev, const gx_device_pdf * save_dev, gs_par
     int ecode, code;
     gx_device_pdf *pdev = (gx_device_pdf *) dev;
     float cl = (float)pdev->CompatibilityLevel;
-    bool locked = pdev->params.LockDistillerParams, ForOPDFRead;
+    bool locked = pdev->params.LockDistillerParams, ForOPDFRead, OmitInfoDate = pdev->OmitInfoDate, OmitID = pdev->OmitID, OmitXMP = pdev->OmitXMP;
     gs_param_name param_name;
 
     pdev->pdf_memory = gs_memory_stable(pdev->memory);
@@ -647,6 +657,25 @@ gdev_pdf_put_params_impl(gx_device * dev, const gx_device_pdf * save_dev, gs_par
 
     if (pdev->is_ps2write && (code = param_read_bool(plist, "ProduceDSC", &pdev->ProduceDSC)) < 0) {
         param_signal_error(plist, param_name, code);
+    }
+
+    if (pdev->OmitInfoDate && pdev->PDFX != 0) {
+        emprintf(pdev->memory, "\nIt is not possible to omit the CreationDate when creating PDF/X\nOmitInfoDate is being ignored.\n");
+        pdev->OmitInfoDate = 0;
+    }
+
+    if (pdev->OmitID && pdev->CompatibilityLevel > 1.7) {
+        emprintf(pdev->memory, "\nIt is not possible to omit the ID array when creating a version 2.0 or greater PDF\nOmitID is being ignored.\n");
+        pdev->OmitID = 0;
+    }
+    if (pdev->OmitID && pdev->OwnerPassword.size != 0) {
+        emprintf(pdev->memory, "\nIt is not possible to omit the ID array when creating an encrypted PDF\nOmitID is being ignored.\n");
+        pdev->OmitID = 0;
+    }
+
+    if (pdev->OmitXMP && pdev->PDFA != 0) {
+        emprintf(pdev->memory, "\nIt is not possible to omit the XMP metadta when creating a PDF/A\nOmitXMP is being ignored.\n");
+        pdev->OmitXMP = 0;
     }
 
     /* PDFA and PDFX are stored in the page device dictionary and therefore

devices/vector/gdevpdf.c
@@ -425,6 +425,7 @@ pdf_initialize_ids(gx_device_pdf * pdev)
      * date and time, rather than (for example) %%CreationDate from the
      * PostScript file.  We think this is wrong, but we do the same.
      */
+    if (!pdev->OmitInfoDate)
     {
         struct tm tms;
         time_t t;
@@ -466,6 +467,7 @@ pdf_initialize_ids(gx_device_pdf * pdev)
 static int
 pdf_compute_fileID(gx_device_pdf * pdev)
 {
+
     /* We compute a file identifier when beginning a document
        to allow its usage with PDF encryption. Due to that,
        in contradiction to the Adobe recommendation, our
@@ -1906,7 +1908,11 @@ static int pdf_linearise(gx_device_pdf *pdev, pdf_linearisation_t *linear_params
      * +1 for the linearisation dict and +1 for the primary hint stream.
      */
     linear_params->FirsttrailerOffset = gp_ftell(linear_params->Lin_File.file);
-    gs_snprintf(LDict, sizeof(LDict), "\ntrailer\n<</Size %ld/Info %d 0 R/Root %d 0 R/ID[%s%s]/Prev %d>>\nstartxref\r\n0\n%%%%EOF\n        \n",
+    if (pdev->OmitID)
+        gs_snprintf(LDict, sizeof(LDict), "\ntrailer\n<</Size %ld/Info %d 0 R/Root %d 0 R/Prev %d>>\nstartxref\r\n0\n%%%%EOF\n        \n",
+        linear_params->LastResource + 3, pdev->ResourceUsage[linear_params->Info_id].NewObjectNumber, pdev->ResourceUsage[linear_params->Catalog_id].NewObjectNumber, 0);
+    else
+        gs_snprintf(LDict, sizeof(LDict), "\ntrailer\n<</Size %ld/Info %d 0 R/Root %d 0 R/ID[%s%s]/Prev %d>>\nstartxref\r\n0\n%%%%EOF\n        \n",
         linear_params->LastResource + 3, pdev->ResourceUsage[linear_params->Info_id].NewObjectNumber, pdev->ResourceUsage[linear_params->Catalog_id].NewObjectNumber, fileID, fileID, 0);
     gp_fwrite(LDict, strlen(LDict), 1, linear_params->Lin_File.file);
 
@@ -2442,7 +2448,11 @@ static int pdf_linearise(gx_device_pdf *pdev, pdf_linearisation_t *linear_params
     if (code != 0)
         return_error(gs_error_ioerror);
 
-    gs_snprintf(LDict, sizeof(LDict), "\ntrailer\n<</Size %ld/Info %d 0 R/Root %d 0 R/ID[%s%s]/Prev %"PRId64">>\nstartxref\r\n0\n%%%%EOF\n",
+    if (pdev->OmitID)
+        gs_snprintf(LDict, sizeof(LDict), "\ntrailer\n<</Size %ld/Info %d 0 R/Root %d 0 R/Prev %"PRId64">>\nstartxref\r\n0\n%%%%EOF\n",
+        linear_params->LastResource + 3, pdev->ResourceUsage[linear_params->Info_id].NewObjectNumber, pdev->ResourceUsage[linear_params->Catalog_id].NewObjectNumber, mainxref);
+    else
+        gs_snprintf(LDict, sizeof(LDict), "\ntrailer\n<</Size %ld/Info %d 0 R/Root %d 0 R/ID[%s%s]/Prev %"PRId64">>\nstartxref\r\n0\n%%%%EOF\n",
         linear_params->LastResource + 3, pdev->ResourceUsage[linear_params->Info_id].NewObjectNumber, pdev->ResourceUsage[linear_params->Catalog_id].NewObjectNumber, fileID, fileID, mainxref);
     gp_fwrite(LDict, strlen(LDict), 1, linear_params->sfile);
 
@@ -2871,10 +2881,12 @@ pdf_close(gx_device * dev)
             COS_WRITE_OBJECT(pdev->PageLabels, pdev, resourceLabels);
         }
 
-        /* Write the document metadata. */
-        code1 = pdf_document_metadata(pdev);
-        if (code >= 0)
-            code = code1;
+        if (!pdev->OmitXMP) {
+            /* Write the document metadata. */
+            code1 = pdf_document_metadata(pdev);
+            if (code >= 0)
+                code = code1;
+        }
 
         /* Write the Catalog. */
 
@@ -3136,10 +3148,12 @@ pdf_close(gx_device * dev)
             stream_puts(s, "trailer\n");
             pprintld3(s, "<< /Size %ld /Root %ld 0 R /Info %ld 0 R\n",
                   pdev->next_id, Catalog_id, Info_id);
-            stream_puts(s, "/ID [");
-            psdf_write_string(pdev->strm, pdev->fileID, sizeof(pdev->fileID), 0);
-            psdf_write_string(pdev->strm, pdev->fileID, sizeof(pdev->fileID), 0);
-            stream_puts(s, "]\n");
+            if (!pdev->OmitID) {
+                stream_puts(s, "/ID [");
+                psdf_write_string(pdev->strm, pdev->fileID, sizeof(pdev->fileID), 0);
+                psdf_write_string(pdev->strm, pdev->fileID, sizeof(pdev->fileID), 0);
+                stream_puts(s, "]\n");
+            }
             if (pdev->OwnerPassword.size > 0) {
                 pprintld1(s, "/Encrypt %ld 0 R ", Encrypt_id);
             }

devices/vector/gdevpdfb.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2021 Artifex Software, Inc.
+/* Copyright (C) 2001-2022 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
@@ -237,7 +237,10 @@ const gx_device_pdf PDF_DEVICE_IDENT =
  0,                     /* OCR_char_code */
  0,                     /* OCR_glyph */
  NULL,                  /* ocr_glyphs */
- 0                   /* initial_pattern_state */
+ 0,                     /* initial_pattern_state */
+ false,                 /* OmitInfoDate */
+ false,                 /* OmitXMP */
+ false                  /* OmitID */
 };
 
 #else

devices/vector/gdevpdfe.c
@@ -696,21 +696,23 @@ pdf_write_document_metadata(gx_device_pdf *pdev, const byte digest[6])
             pdf_xml_attribute_name(s, "xmlns:xmp");
             pdf_xml_attribute_value(s, "http://ns.adobe.com/xap/1.0/");
             pdf_xml_tag_end(s);
-            {
-                pdf_xml_tag_open_beg(s, "xmp:ModifyDate");
-                pdf_xml_tag_end(s);
-                mod_date_time[mod_date_time_len] = 0x00;
-                pdf_xml_copy(s, mod_date_time);
-                pdf_xml_tag_close(s, "xmp:ModifyDate");
-                pdf_xml_newline(s);
-            }
-            {
-                pdf_xml_tag_open_beg(s, "xmp:CreateDate");
-                pdf_xml_tag_end(s);
-                cre_date_time[cre_date_time_len] = 0x00;
-                pdf_xml_copy(s, cre_date_time);
-                pdf_xml_tag_close(s, "xmp:CreateDate");
-                pdf_xml_newline(s);
+            if (!pdev->OmitInfoDate) {
+                {
+                    pdf_xml_tag_open_beg(s, "xmp:ModifyDate");
+                    pdf_xml_tag_end(s);
+                    mod_date_time[mod_date_time_len] = 0x00;
+                    pdf_xml_copy(s, mod_date_time);
+                    pdf_xml_tag_close(s, "xmp:ModifyDate");
+                    pdf_xml_newline(s);
+                }
+                {
+                    pdf_xml_tag_open_beg(s, "xmp:CreateDate");
+                    pdf_xml_tag_end(s);
+                    cre_date_time[cre_date_time_len] = 0x00;
+                    pdf_xml_copy(s, cre_date_time);
+                    pdf_xml_tag_close(s, "xmp:CreateDate");
+                    pdf_xml_newline(s);
+                }
             }
             {
                 pdf_xml_tag_open_beg(s, "xmp:CreatorTool");

devices/vector/gdevpdfx.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2021 Artifex Software, Inc.
+/* Copyright (C) 2001-2022 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
@@ -946,7 +946,7 @@ struct gx_device_pdf_s {
                                      * anything in the image processing routines.
                                      */
     float UserUnit;
-    pdf_OCR_usage UseOCR;                     /* Never, AsNeeded or Always */
+    pdf_OCR_usage UseOCR;           /* Never, AsNeeded or Always */
     gs_text_enum_t* OCRSaved;       /* Saved state of the text enumerator before rendering glyph bitmaps for later OCR */
     pdf_OCR_stage OCRStage;         /* Used to control a (sort of) state machine when using OCR to get a Unicode value for a glyph */
     int *OCRUnicode;                /* Used to pass back the Unicode value from the OCR engine to the text processing */
@@ -954,6 +954,9 @@ struct gx_device_pdf_s {
     gs_glyph OCR_glyph;             /* Passes the current glyph code from text processing to the image processing code when rendering glyph bitmaps for OCR */
     ocr_glyph_t *ocr_glyphs;        /* Records bitmaps and other data from text processing when doing OCR */
     gs_gstate **initial_pattern_states;
+    bool OmitInfoDate;              /* If true, do not emit CreationDate and ModDate in the Infor dictionary and XMP Metadata (must not be true for PDF/X support) */
+    bool OmitXMP;                   /* If true, do not emit an XMP /Metadata block and do not reference it from the Catalog (must not be true for PDF/A output) */
+    bool OmitID;                    /* If true, do not emit a /ID array in the trailer dicionary (must not be true for encrypted files or PDF 2.0) */
 };
 
 #define is_in_page(pdev)\

doc/VectorDevices.htm
@@ -798,14 +798,42 @@ usually result in a slightly larger PDF file.</dd>
     instead applied by the interpreter.
     <p>
     This control also requires the PDF interpreter to pass the marked content to the pdfwrite device, this is only done with the
-    new (C-based) PDF interpreter. THe old (PostScript-based) interpreter does not support this feature and will not pass marked
+    new (C-based) PDF interpreter. The old (PostScript-based) interpreter does not support this feature and will not pass marked
     content to the pdfwrite device.
     </p>
     </dd>
 </dl>
 
-The following options are useful for creating PDF 1.2 files:
+<d1>
+    <dt><code>-dOmitInfoDate</code><em>boolean</em></dt>
+    <dd>Under some conditions the CreationDate and ModDate in the /Info dictionary are optional and can be omitted. They are required
+    when producing PDF/X output however. This control will allow the user to omit the /CreationDate and /ModDate entries in the
+    Info dictionary (and the corresponding information in the XMP metadata, if present). If you try to set this control when
+    writing PDF/X output, the device will give a warning and ignore this control.
+    </dd>
+</d1>
+<p></p>
+
+<d1>
+    <dt><code>-dOmitID</code><em>boolean</em></dt>
+    <dd>Under some conditions the /ID array trailer dictionary is optional and can be omitted. It is required
+    when producing PDF 2.0, or encrypted PDFs however. This control will allow the user to omit the /ID entry in the
+    trailer dictionary. If you try to set this control when writing PDF 2.0 or encrypted PDF output, the device will give a warning and ignore this control.
+    </dd>
+</d1>
+<p></p>
+
+<d1>
+    <dt><code>-dOmitXMP</code><em>boolean</em></dt>
+    <dd>Under some conditions the XMP /Metadata entry in the Catalog dictionary is optional and can be omitted. It is required
+    when producing PDF/A output however. This control will allow the user to omit the /Metadata entry in the
+    Catalog dictionary. If you try to set this control when writing PDF/A output, the device will give a warning and ignore this control.
+    </dd>
+</d1>
+<p></p>
+
 <p>
+The following options are useful for creating PDF 1.2 files:
 
 <dl>
 

commit eee28843b33ac5dbf0e03c289e1d9f7d035dc316

pdfwrite - Remove unused variables
Commit 1158b25fe28f9d60c65417712f16a0f9cccac95c accidentally left
some unused variables in gdevpdfp.c

devices/vector/gdevpdfp.c
@@ -390,7 +390,7 @@ gdev_pdf_put_params_impl(gx_device * dev, const gx_device_pdf * save_dev, gs_par
     int ecode, code;
     gx_device_pdf *pdev = (gx_device_pdf *) dev;
     float cl = (float)pdev->CompatibilityLevel;
-    bool locked = pdev->params.LockDistillerParams, ForOPDFRead, OmitInfoDate = pdev->OmitInfoDate, OmitID = pdev->OmitID, OmitXMP = pdev->OmitXMP;
+    bool locked = pdev->params.LockDistillerParams, ForOPDFRead;
     gs_param_name param_name;
 
     pdev->pdf_memory = gs_memory_stable(pdev->memory);