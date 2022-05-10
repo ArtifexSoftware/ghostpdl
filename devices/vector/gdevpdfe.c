5c15fadf8a22622b11408673a6aa81d2098445f3
Chris Liddell <chris.liddell@artifex.com>
2022-05-10 18:06:41 +0100

    Fix a compiler warning - pass accurate length to strncpy

--- devices/vector/gdevpdfe.c
+++ devices/vector/gdevpdfe.c
@@ -568,7 +568,7 @@ pdf_make_uuid(const byte node[6], uint64_t uuid_time, ulong time_seq, char *buf,
     writehex(&p, node[4], 1);
     writehex(&p, node[5], 1);
     *p = 0;
-    strncpy(buf, b, buf_length);
+    strncpy(buf, b, strlen(b) + 1);
 }
 
 static int
