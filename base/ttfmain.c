361febcf961baa853f34909cd3925ffa7f90e7ea
Chris Liddell <chris.liddell@artifex.com>
2022-09-01 10:10:55 +0100

    oss-fuzz 50847: Bounds check points indices in ttfOutliner__BuildGlyphOutlineAux()

--- base/ttfmain.c
+++ base/ttfmain.c
@@ -650,8 +650,15 @@ retry:
                 e->m.tx = Scale_X( &exec->metrics, e->arg1 ) << 10;
                 e->m.ty = Scale_Y( &exec->metrics, e->arg2 ) << 10;
             } else {
-                e->m.tx = (pts->org_x[e->arg1] - pts->org_x[gOutline->pointCount + e->arg2]) << 10;
-                e->m.ty = (pts->org_y[e->arg1] - pts->org_y[gOutline->pointCount + e->arg2]) << 10;
+                if (e->arg1 < 0 || e->arg1 > pts->n_points
+                 || (gOutline->pointCount + e->arg2) < 0 || (gOutline->pointCount + e->arg2) > pts->n_points) {
+                    error = fBadFontData;
+                    goto ex;
+                }
+                else {
+                    e->m.tx = (pts->org_x[e->arg1] - pts->org_x[gOutline->pointCount + e->arg2]) << 10;
+                    e->m.ty = (pts->org_y[e->arg1] - pts->org_y[gOutline->pointCount + e->arg2]) << 10;
+                }
             }
             MoveGlyphOutline(pts, nPointsStored, &out, &e->m);
             for (j = nContoursStored; j < out.contourCount + nContoursStored; j++)
