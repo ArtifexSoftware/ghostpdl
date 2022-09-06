8d30023842e59a9af26844e6172f8413ff51d7fb
Chris Liddell <chris.liddell@artifex.com>
2022-09-06 15:10:19 +0100

    oss-fuzz 51045: Bounds check available points in Ins_ISECT()

--- base/ttinterp.c
+++ base/ttinterp.c
@@ -4068,7 +4068,8 @@ static int nInstrCount=0;
     if ( BOUNDS( b0, CUR.zp0.n_points ) ||
          BOUNDS( b1, CUR.zp0.n_points ) ||
          BOUNDS( a0, CUR.zp1.n_points ) ||
-         BOUNDS( a1, CUR.zp1.n_points ) )
+         BOUNDS( a1, CUR.zp1.n_points ) ||
+         BOUNDS( point, CUR.zp2.n_points) )
     {
       CUR.error = TT_Err_Invalid_Reference;
       return;
