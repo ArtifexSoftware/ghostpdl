9d47a470aaaddcff09aa444e993aafe8e5e2b455
Ken Sharp <ken.sharp@artifex.com>
2022-11-10 14:11:26 +0000

    GhostPDF - simplify InkList annotation appearance generation
    OSS-fuzz 53179
    
    The fuzzed file has a bad initial co-ordinate:
    
    /InkList [[124.666666666 66676769.3333333333334 124.66666666666667 768.6666666666666
    
    The way we draw InkList annotations, this causes us to try and draw a
    Bezier curve and because the second co-ordinate is unaffected this
    becomes a truly huge curve segment, too long to cope with. So we
    subdivide it and try again, still too big, subdivide and try again....
    
    We end up subdividing many times, producing many smaller curves which,
    later, will turn out to be off the page and not rendered. But before we
    can get to that point we spend an awful lot of time chopping up curves.
    
    As long as we use curve segments there really isn't anything we can do
    about that, but.... There is no reason we have to use curve segments
    rather than straight lines. The spec says
    
    "When drawn, the points are connected by straight lines or curves in
    an implementation-dependent way"
    
    and points us to implementation note 93 in Appendix H (1.7 spec) which
    says:
    
    "Acrobat viewers always use straight lines to connect the points along
    each path."
    
    In fact, looking at the display in Acrobat X, I don't believe that
    statement is true, but it allows us the option of doing this. If we do
    that instead of using curves we don't have to subdivide them and so we
    can run the file *much* more quickly.

--- pdf/pdf_annot.c
+++ pdf/pdf_annot.c
@@ -1706,10 +1706,7 @@ static int pdfi_annot_draw_InkList(pdf_context *ctx, pdf_dict *annot, pdf_array 
     pdf_array *points = NULL;
     int i;
     int num_points;
-    double x0, y0, x1, y1, x2, y2, x3, y3, xc1, yc1, xc2, yc2, xc3, yc3;
-    double len1, len2, len3, k1, k2, xm1, ym1, xm2, ym2;
-    double ctrl1_x, ctrl1_y, ctrl2_x, ctrl2_y;
-    const double smooth_value = 1; /* from 0..1 range */
+    double x1, y1, x2, y2;
 
     for (i=0; i<pdfi_array_size(InkList); i++) {
         int j;
@@ -1731,68 +1728,12 @@ static int pdfi_annot_draw_InkList(pdf_context *ctx, pdf_dict *annot, pdf_array 
         if (num_points == 2)
             goto stroke;
 
-        code = pdfi_array_get_number(ctx, points, 2, &x2);
-        if (code < 0) goto exit;
-        code = pdfi_array_get_number(ctx, points, 3, &y2);
-        if (code < 0) goto exit;
-        if (num_points == 4) {
-            code = gs_lineto(ctx->pgs, x2, y2);
-            if (code < 0)
-                goto exit;
-            goto stroke;
-        }
-
-        x0 = 2*x1 - x2;
-        y0 = 2*y1 - y2;
-
-        for (j = 4; j <= num_points; j += 2) {
-            if (j < num_points) {
-                code = pdfi_array_get_number(ctx, points, j, &x3);
-                if (code < 0) goto exit;
-                code = pdfi_array_get_number(ctx, points, j+1, &y3);
-                if (code < 0) goto exit;
-            } else {
-                x3 = 2*x2 - x1;
-                y3 = 2*y2 - y1;
-            }
-
-            xc1 = (x0 + x1) / 2.0;
-            yc1 = (y0 + y1) / 2.0;
-            xc2 = (x1 + x2) / 2.0;
-            yc2 = (y1 + y2) / 2.0;
-            xc3 = (x2 + x3) / 2.0;
-            yc3 = (y2 + y3) / 2.0;
-
-            len1 = hypot(x1 - x0, y1 - y0);
-            len2 = hypot(x2 - x1, y2 - y1);
-            len3 = hypot(x3 - x2, y3 - y2);
-
-            if ((len1 + len2) == 0)
-                k1 = 0;
-            else
-                k1 = len1 / (len1 + len2);
-            if ((len2 + len3) == 0)
-                k2 = 0;
-            else
-                k2 = len2 / (len2 + len3);
-
-            xm1 = xc1 + (xc2 - xc1) * k1;
-            ym1 = yc1 + (yc2 - yc1) * k1;
-
-            xm2 = xc2 + (xc3 - xc2) * k2;
-            ym2 = yc2 + (yc3 - yc2) * k2;
-
-            ctrl1_x = xm1 + (xc2 - xm1) * smooth_value + x1 - xm1;
-            ctrl1_y = ym1 + (yc2 - ym1) * smooth_value + y1 - ym1;
-
-            ctrl2_x = xm2 + (xc2 - xm2) * smooth_value + x2 - xm2;
-            ctrl2_y = ym2 + (yc2 - ym2) * smooth_value + y2 - ym2;
-
-            code = gs_curveto(ctx->pgs, ctrl1_x, ctrl1_y, ctrl2_x, ctrl2_y, x2, y2);
+        for (j = 2; j < num_points; j += 2) {
+            code = pdfi_array_get_number(ctx, points, j, &x2);
             if (code < 0) goto exit;
-
-            x0 = x1, x1 = x2, x2 = x3;
-            y0 = y1, y1 = y2, y2 = y3;
+            code = pdfi_array_get_number(ctx, points, j+1, &y2);
+            if (code < 0) goto exit;
+            code = gs_lineto(ctx->pgs, x2, y2);
         }
 
     stroke:
