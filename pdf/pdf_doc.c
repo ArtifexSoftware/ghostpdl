1f69e67da9cd2b985558232874a9941e5c301b76
Ken Sharp <ken.sharp@artifex.com>
2022-12-17 11:14:15 +0000

    GhostPDF - improve Resource finding.
    The code was exiting early if we received an undefined error from
    dictionary retrieval, which meant it wasn't falling back as far as it
    should.
    
    Also the error about inheriting Resources from an earlier stream was
    simply wrong, because it was triggered on an error. The noted test
    file 'tests_private/comparefiles/Bug692783.pdf' does not inherit in
    that fashion, the Resources are genuinely undefined in the file.
    
    These changes mean we fall back further and give an error if we
    completely fail to find a named Resource.

--- pdf/pdf_doc.c
+++ pdf/pdf_doc.c
@@ -1044,7 +1044,8 @@ int pdfi_find_resource(pdf_context *ctx, unsigned char *Type, pdf_name *name,
 
         if (code > 0) {
             code = pdfi_dict_get_no_store_R_key(ctx, typedict, name, o);
-            goto exit;
+            if (code != gs_error_undefined)
+                goto exit;
         }
     }
 
@@ -1058,7 +1059,8 @@ int pdfi_find_resource(pdf_context *ctx, unsigned char *Type, pdf_name *name,
 
         if (code > 0) {
             code = pdfi_dict_get_no_store_R_key(ctx, typedict, name, o);
-            goto exit;
+            if (code != gs_error_undefined)
+                goto exit;
         }
     }
 
@@ -1075,9 +1077,10 @@ int pdfi_find_resource(pdf_context *ctx, unsigned char *Type, pdf_name *name,
                 goto exit;
             if (code > 0) {
                 code = pdfi_dict_get_no_store_R_key(ctx, typedict, name, o);
-                /* Triggered by: tests_private/comparefiles/Bug692783.pdf */
-                pdfi_set_error(ctx, 0, NULL, E_PDF_INHERITED_STREAM_RESOURCE, "pdfi_find_resource", (char *)"Couldn't find named resource in supplied dictionary, or Parents, or Pages, matching name located in earlier stream Resource");
-                goto exit;
+                if (code == 0) {
+                    pdfi_set_error(ctx, 0, NULL, E_PDF_INHERITED_STREAM_RESOURCE, "pdfi_find_resource", (char *)"Couldn't find named resource in supplied dictionary, or Parents, or Pages, matching name located in earlier stream Resource");
+                    goto exit;
+                }
             }
             pdfi_countdown(typedict);
             typedict = NULL;
