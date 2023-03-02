843de19f9afa1b7a9c393aa5152d7819a9602af2
Chris Liddell <chris.liddell@artifex.com>
2023-03-02 09:05:27 +0000

    Product family, date docs version for 10.01.0 RC 1

--- base/gscdefs.h
+++ base/gscdefs.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2022 Artifex Software, Inc.
+/* Copyright (C) 2001-2023 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
@@ -46,7 +46,7 @@
 
 #ifndef GS_PRODUCT
 #  define GS_PRODUCT\
-        GS_PRODUCTFAMILY " GIT PRERELEASE"
+        GS_PRODUCTFAMILY " RELEASE CANDIDATE 1"
 #endif
 
 /* Prototypes for configuration definitions in gconfig.c. */
