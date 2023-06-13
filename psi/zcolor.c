8aaf185be774107d315f4d5a43aff0e4c50f6092
Chris Liddell <chris.liddell@artifex.com>
2023-06-13 14:28:24 +0100

    Bug 706770: Fix reference counting when evaluating transfer functions
    Internal transfer function objects are one of the cases where (with Postscript)
    we use both reference counting and garbage collection to manage the life spans
    of object instances.
    
    During evaluation we store a counted reference to the map on the exec stack,
    which we decremented the ref count, before popping the objects off the stack
    on successful completion of the evaluation.
    
    In this case, we encounter a typecheck error part way through the evaluation,
    meaning we'd have decremented the reference count, but *not* popped the
    reference off the exec stack.
    
    To maintain proper Postscript error handling, we don't want to pop the objects
    in the event on an error, but we also don't want to retain a reference no
    longer reference counted. So, in that event, make that one entry on the stack
    null (since it's an internal object, that won't adversely affect error
    reporting).
    
    Thanks for Alan Goodman for the fuzz testing which uncovered this bug.

--- psi/zcolor.c
+++ psi/zcolor.c
@@ -747,28 +747,35 @@ zcolor_remap_one(
 static int
 zcolor_remap_one_store(i_ctx_t *i_ctx_p, double min_value)
 {
-    int i;
+    int i, code = 0;
     gx_transfer_map *pmap = r_ptr(esp, gx_transfer_map);
 
-    rc_decrement_only(pmap, "zcolor_remap_one_store");
-    if (ref_stack_count(&o_stack) < transfer_map_size)
-        return_error(gs_error_stackunderflow);
+    if (ref_stack_count(&o_stack) < transfer_map_size) {
+        code = gs_note_error(gs_error_stackunderflow);
+        goto error;
+    }
     for (i = 0; i < transfer_map_size; i++) {
         double v;
-        int code =
-            real_param(ref_stack_index(&o_stack, transfer_map_size - 1 - i),
-                       &v);
 
-        if (code < 0)
-            return code;
+        code = real_param(ref_stack_index(&o_stack, transfer_map_size - 1 - i), &v);
+        if (code < 0) {
+            goto error;
+        }
+
         pmap->values[i] =
             (v < min_value ? float2frac(min_value) :
              v >= 1.0 ? frac_1 :
              float2frac(v));
     }
+    rc_decrement_only(pmap, "zcolor_remap_one_store");
     ref_stack_pop(&o_stack, transfer_map_size);
     esp--;			/* pop pointer to transfer map */
     return o_pop_estack;
+
+error:
+    rc_decrement_only(pmap, "zcolor_remap_one_store");
+    make_null(esp);
+    return code;
 }
 int
 zcolor_remap_one_finish(i_ctx_t *i_ctx_p)
