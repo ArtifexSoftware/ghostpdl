1abe199b9e6da3487db3eb488c8f852f583412b7
Ken Sharp <Ken.Sharp@artifex.com>
2023-12-24 15:26:43 +0000

    pdfwrite - fix a reference count
    Bug #707433 "Converting PS to PDF causes seg fault"
    
    This bug was introduced in this commit
    d07b9485b2bb3a70552aee9407fcff8fa52698df
    
    Unfortunately Nancy didn't give any information on how to reproduce the
    memory leak. The countdown causes us to count down a reference to a
    colour space which we have not counted up, which (much later!) results
    in it being freed when there is still a reference to it, and eventually
    results in a seg fault at a still later point when we try to use the
    (now invalid) reference.
    
    The reporter claims this works with 9.55 but if so, it is entirely by
    coincidence, as the change predates that release (and 9.55 crashes for
    me).
    
    With no way to investigate the original commit, I'm effectively
    reverting it, as a memory leak is less serious than a seg fault.

--- devices/vector/gdevpdfi.c
+++ devices/vector/gdevpdfi.c
@@ -1427,7 +1427,6 @@ pdf_begin_typed_image(gx_device_pdf *pdev, const gs_gstate * pgs,
             code = make_device_color_space(pdev, pdev->pcm_color_info_index, &pcs_device);
             if (code < 0)
                 goto fail_and_fallback;
-            rc_decrement(image[0].pixel.ColorSpace, "pdf_begin_typed_image(pixel.ColorSpace)");
             image[0].pixel.ColorSpace = pcs_device;
             image[0].pixel.BitsPerComponent = 8;
             code = pdf_color_space_named(pdev, pgs, &cs_value, &pranges, pcs_device, names,
