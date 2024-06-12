ArtifexSoftware/ghostpdl

commit 949be69c7039afa848095acbd6d1cb772ed8510c

pdfwrite - limit co-ordinates for PDF/A output
Bug #703896 "Regression: some PDF/A conversions from PostScript have invalid float range"

Commit 3156b0aa8ac4e9ef440c169799658da249eb41c9 removed a long-standing
'kludge' which limited the co-ordinate values we could use to the range
permitted by Acrobat 4; +/- 16384.

Given that the piece of software in question had been obselete for 15
years, this seemed reasonable, but it turns out that, buried in the
PDF/A-1 spec is a small note; 6.1.12 'Implementation limits' which
specifies (rather horrifyingly) that a conforming file must not violate
the limitations of Acrobat 5. Not the PDF 1.4 specifications, but the
actual limits of that particular piece of software.....

So, here we reinstate the code to work around the limitations; but
we use the limits of Acrobat 5 (+/- 32767) rather than Acrobat 4. We
also use the production of PDF/A-1 (specifically version 1, version 2 is
not subject to these limits) rather than the production of PDF 1.4 to
trigger the implementation.

In the places where we clamp a value rather than scaling it, we consult
the PDFACompatibilityLevel to see what to do.

As the original commit noted; I'm far from certain that this covers all
the places where we might possibly emit co-ordinates outside the
range of the architectural limitations of Acrobat 5, but it does restore
the functionality so that it as least no worse than it was before the
commit to remove it.

devices/vector/gdevpdfd.c
@@ -181,6 +181,47 @@ pdf_dorect(gx_device_vector * vdev, fixed x0, fixed y0, fixed x1, fixed y1,
         ymin -= d;
         ymax += d;
     }
+    if (pdev->PDFA == 1) {
+        /* Check values, and decide how to proceed based on PDFACompatibilitylevel */
+        if (x0 < xmin || y0 < ymin || x1 - x0 > xmax || y1 - y0 >ymax) {
+            switch(pdev->PDFACompatibilityPolicy) {
+                case 0:
+                    emprintf(pdev->memory,
+                         "Required co-ordinate outside valid range for PDF/A-1, reverting to normal PDF output.\n");
+                    pdev->AbortPDFAX = true;
+                    pdev->PDFA = 0;
+                    break;
+                case 1:
+                    emprintf(pdev->memory,
+                         "Required co-ordinate outside valid range for PDF/A-1, clamping to valid range, output may be incorrect.\n");
+                    /*
+                     * Clamp coordinates to avoid tripping over Acrobat Reader's limit
+                     * of 32K on user coordinate values, which was adopted by the PDF/A-1 spec.
+                     */
+                    if (x0 < xmin)
+                        x0 = xmin;
+
+                    if (y0 < ymin)
+                        y0 = ymin;
+
+                    /* We used to clamp x1 and y1 here, but actually, because this is a rectangle
+                     * we don't need to do that, we need to clamp the *difference* between x0,x1
+                     * and y0,y1 to keep it inside the Acrobat 4 or 5 limits.
+                     */
+                    if (x1 - x0 > xmax)
+                        x1 = x0 + xmax;
+                    if (y1 - y0 > ymax)
+                        y1 = y0 + ymax;
+                    break;
+                default:
+                case 2:
+                    emprintf(pdev->memory,
+                         "Required co-ordinate outside valid range for PDF/A-1, aborting.\n");
+                    return_error(gs_error_limitcheck);
+                    break;
+            }
+        }
+    }
     return psdf_dorect(vdev, x0, y0, x1, y1, type);
 }
 
@@ -777,6 +818,38 @@ pdf_put_clip_path(gx_device_pdf * pdev, const gx_clip_path * pcpath)
     pdev->clip_path_id = new_id;
     return pdf_remember_clip_path(pdev,
             (pdev->clip_path_id == pdev->no_clip_path_id ? NULL : pcpath));
+}
+
+/*
+ * Compute the scaling to ensure that user coordinates for a path are within
+ * PDF/A-1 valid range.  Return true if scaling was needed.  In this case, the
+ * CTM will be multiplied by *pscale, and all coordinates will be divided by
+ * *pscale.
+ */
+static bool
+make_rect_scaling(const gx_device_pdf *pdev, const gs_fixed_rect *bbox,
+                  double prescale, double *pscale)
+{
+    double bmin, bmax;
+
+    if (pdev->PDFA != 1) {
+        *pscale = 1;
+        return false;
+    }
+
+    bmin = min(bbox->p.x / pdev->scale.x, bbox->p.y / pdev->scale.y) * prescale;
+    bmax = max(bbox->q.x / pdev->scale.x, bbox->q.y / pdev->scale.y) * prescale;
+    if (bmin <= int2fixed(-MAX_USER_COORD) ||
+        bmax > int2fixed(MAX_USER_COORD)
+        ) {
+        /* Rescale the path. */
+        *pscale = max(bmin / int2fixed(-MAX_USER_COORD),
+                      bmax / int2fixed(MAX_USER_COORD));
+        return true;
+    } else {
+        *pscale = 1;
+        return false;
+    }
 }
 
 /*
@@ -1622,6 +1695,8 @@ gdev_pdf_fill_path(gx_device * dev, const gs_gstate * pgs, gx_path * ppath,
         return code;
     {
         stream *s = pdev->strm;
+        double scale;
+        gs_matrix smat, *psmat = NULL;
         gs_path_enum cenum;
         gdev_vector_dopath_state_t state;
 
@@ -1634,11 +1709,19 @@ gdev_pdf_fill_path(gx_device * dev, const gs_gstate * pgs, gx_path * ppath,
             pprintg1(s, "%g i\n", params->flatness);
             pdev->state.flatness = params->flatness;
         }
+        if (make_rect_scaling(pdev, &box1, 1.0, &scale)) {
+            gs_make_scaling(pdev->scale.x * scale, pdev->scale.y * scale,
+                            &smat);
+            pdf_put_matrix(pdev, "q ", &smat, "cm\n");
+            psmat = &smat;
+        }
         code = pdf_write_path(pdev, (gs_path_enum *)&cenum, &state, (gx_path *)ppath, 0, gx_path_type_fill | gx_path_type_optimize, NULL);
         if (code < 0)
             return code;
 
         stream_puts(s, (params->rule < 0 ? "f\n" : "f*\n"));
+        if (psmat != NULL)
+            stream_puts(pdev->strm, "Q\n");
     }
     return 0;
 }
@@ -1652,9 +1735,10 @@ gdev_pdf_stroke_path(gx_device * dev, const gs_gstate * pgs,
     gx_device_pdf *pdev = (gx_device_pdf *) dev;
     stream *s;
     int code;
-    double scale;
+    double scale, path_scale;
     bool set_ctm;
     gs_matrix mat;
+    double prescale = 1;
     gs_fixed_rect bbox;
     gs_path_enum cenum;
     gdev_vector_dopath_state_t state;
@@ -1740,6 +1824,20 @@ gdev_pdf_stroke_path(gx_device * dev, const gs_gstate * pgs,
         scale = fabs(pgs->ctm.xx + pgs->ctm.xy + pgs->ctm.yx + pgs->ctm.yy) /* Using the non-zero coeff. */
                 / sqrt(2); /* Empirically from Adobe. */
     }
+    if (set_ctm && pdev->PDFA == 1) {
+        /*
+         * We want a scaling factor that will bring the largest reasonable
+         * user coordinate within bounds.  We choose a factor based on the
+         * minor axis of the transformation.  Thanks to Raph Levien for
+         * the following formula.
+         */
+        double a = mat.xx, b = mat.xy, c = mat.yx, d = mat.yy;
+        double u = fabs(a * d - b * c);
+        double v = a * a + b * b + c * c + d * d;
+        double minor = (sqrt(v + 2 * u) - sqrt(v - 2 * u)) * 0.5;
+
+        prescale = (minor == 0 || minor > 1 ? 1 : 1 / minor);
+    }
     gx_path_bbox(ppath, &bbox);
     {
         /* Check whether a painting appears inside the clipping box.
@@ -1771,6 +1869,15 @@ gdev_pdf_stroke_path(gx_device * dev, const gs_gstate * pgs,
         rect_intersect(stroke_bbox, clip_box);
         if (stroke_bbox.q.x < stroke_bbox.p.x || stroke_bbox.q.y < stroke_bbox.p.y)
             return 0;
+    }
+    if (make_rect_scaling(pdev, &bbox, prescale, &path_scale)) {
+        scale /= path_scale;
+        if (set_ctm)
+            gs_matrix_scale(&mat, path_scale, path_scale, &mat);
+        else {
+            gs_make_scaling(path_scale, path_scale, &mat);
+            set_ctm = true;
+        }
     }
     code = gdev_vector_prepare_stroke((gx_device_vector *)pdev, pgs, params,
                                       pdcolor, scale);
@@ -1838,7 +1945,8 @@ gdev_pdf_fill_stroke_path(gx_device *dev, const gs_gstate *pgs, gx_path *ppath,
     } else {
         bool set_ctm;
         gs_matrix mat;
-        double scale;
+        double scale, path_scale;
+        double prescale = 1;
         gs_fixed_rect bbox;
         gs_path_enum cenum;
         gdev_vector_dopath_state_t state;
@@ -1901,6 +2009,20 @@ gdev_pdf_fill_stroke_path(gx_device *dev, const gs_gstate *pgs, gx_path *ppath,
             scale = fabs(pgs->ctm.xx + pgs->ctm.xy + pgs->ctm.yx + pgs->ctm.yy) /* Using the non-zero coeff. */
                     / sqrt(2); /* Empirically from Adobe. */
         }
+        if (pdev->PDFA == 1 && set_ctm) {
+            /*
+             * We want a scaling factor that will bring the largest reasonable
+             * user coordinate within bounds.  We choose a factor based on the
+             * minor axis of the transformation.  Thanks to Raph Levien for
+             * the following formula.
+             */
+            double a = mat.xx, b = mat.xy, c = mat.yx, d = mat.yy;
+            double u = fabs(a * d - b * c);
+            double v = a * a + b * b + c * c + d * d;
+            double minor = (sqrt(v + 2 * u) - sqrt(v - 2 * u)) * 0.5;
+
+            prescale = (minor == 0 || minor > 1 ? 1 : 1 / minor);
+        }
         gx_path_bbox(ppath, &bbox);
         {
             /* Check whether a painting appears inside the clipping box.
@@ -1932,6 +2054,15 @@ gdev_pdf_fill_stroke_path(gx_device *dev, const gs_gstate *pgs, gx_path *ppath,
             rect_intersect(stroke_bbox, clip_box);
             if (stroke_bbox.q.x < stroke_bbox.p.x || stroke_bbox.q.y < stroke_bbox.p.y)
                 return 0;
+        }
+        if (make_rect_scaling(pdev, &bbox, prescale, &path_scale)) {
+            scale /= path_scale;
+            if (set_ctm)
+                gs_matrix_scale(&mat, path_scale, path_scale, &mat);
+            else {
+                gs_make_scaling(path_scale, path_scale, &mat);
+                set_ctm = true;
+            }
         }
 
         code = pdf_setfillcolor((gx_device_vector *)pdev, pgs, pdcolor_fill);
@@ -1987,6 +2118,8 @@ gdev_pdf_fill_rectangle_hl_color(gx_device *dev, const gs_fixed_rect *rect,
     int code;
     gs_fixed_rect box1 = *rect, box = box1;
     gx_device_pdf *pdev = (gx_device_pdf *) dev;
+    double scale;
+    gs_matrix smat, *psmat = NULL;
     const bool convert_to_image = (pdev->CompatibilityLevel <= 1.2 &&
             gx_dc_is_pattern2_color(pdcolor));
 
@@ -2005,9 +2138,16 @@ gdev_pdf_fill_rectangle_hl_color(gx_device *dev, const gs_fixed_rect *rect,
             rect_intersect(box1, box);
         if (box1.p.x > box1.q.x || box1.p.y > box1.q.y)
             return 0;		/* outside the clipping path */
+        if (make_rect_scaling(pdev, &box1, 1.0, &scale)) {
+            gs_make_scaling(pdev->scale.x * scale, pdev->scale.y * scale, &smat);
+            pdf_put_matrix(pdev, "q ", &smat, "cm\n");
+            psmat = &smat;
+        }
         pprintg4(pdev->strm, "%g %g %g %g re f\n",
-                fixed2float(box1.p.x), fixed2float(box1.p.y),
-                fixed2float(box1.q.x - box1.p.x) , fixed2float(box1.q.y - box1.p.y));
+                fixed2float(box1.p.x) / scale, fixed2float(box1.p.y) / scale,
+                fixed2float(box1.q.x - box1.p.x) / scale, fixed2float(box1.q.y - box1.p.y) / scale);
+        if (psmat != NULL)
+            stream_puts(pdev->strm, "Q\n");
         if (pdev->Eps2Write) {
             gs_rect *Box;
 

devices/vector/gdevpdfx.h
@@ -45,7 +45,13 @@
  * Acrobat 4 is now more than 20 years old, lets just drop support for it. The
  * PDF specification never had this limit, just Adobe's software.
  */
-/* #define MAX_USER_COORD 16300 */
+/* 31/05/2021 However, it transpires that the PDF/A-1 specification adopted the
+ * implementation limits of Acrobat 5 as part of the specification. So we need
+ * to handle that, but we don't need to limit ourselves to the more restrictive
+ * Acrobat 4 limit of 16384 ( MAX_USER_COORD 16300), we can use 32767. THis is only
+ * used when creating PDF/A-1 files.
+ */
+#define MAX_USER_COORD 32700
 
 /* ---------------- Statically allocated sizes ---------------- */
 /* These should really be dynamic.... */

devices/vector/gdevpdts.c
@@ -67,7 +67,7 @@ gs_private_st_ptrs2(st_pdf_text_state, pdf_text_state_t,  "pdf_text_state_t",
  * pdf_append_chars.)  Requires pts->buffer.count_chars > 0.
  */
 static int
-append_text_move(pdf_text_state_t *pts, double dw)
+append_text_move(gx_device_pdf *pdev, pdf_text_state_t *pts, double dw)
 {
     int count = pts->buffer.count_moves;
     int pos = pts->buffer.count_chars;
@@ -81,6 +81,10 @@ append_text_move(pdf_text_state_t *pts, double dw)
     rounded = floor(dw + 0.5);
     if (fabs(dw - rounded) < 0.001)
         dw = rounded;
+    if (pdev->PDFA == 1 && dw < -MAX_USER_COORD) {
+        /* PDF/A-1 limit on co-ordinates */
+        return -1;
+    }
     if (dw != 0) {
         if (count == MAX_TEXT_BUFFER_MOVES)
             return -1;
@@ -168,11 +172,47 @@ add_text_delta_move(gx_device_pdf *pdev, const gs_matrix *pmat)
          * for the xhow and once for the Width override. Otherwise, we do
          * want to use TJ as it makes for smaller files.
          */
-        if (pts->can_use_TJ && dnotw == 0 && pts->buffer.count_chars > 0) {
+        if (pts->can_use_TJ && dnotw == 0 && pts->buffer.count_chars > 0 &&
+            /*
+             * Acrobat Reader limits the magnitude of user-space
+             * coordinates.  Also, AR apparently doesn't handle large
+             * positive movement values (negative X displacements), even
+             * though the PDF Reference says this bug was fixed in AR3.
+             *
+             * Old revisions used the upper threshold 1000 for tdw,
+             * but it appears too big when a font sets a too big
+             * character width in setcachedevice. Particularly this happens
+             * with a Type 3 font generated by Aldus Freehand 4.0
+             * to represent a texture - see bug #687051.
+             * The problem is that when the Widths is multiplied
+             * to the font size, the viewer represents the result
+             * with insufficient fraction bits to represent the precise width.
+             * We work around that problem here restricting tdw
+             * with a smaller threshold 990. Our intention is to
+             * disable Tj when the real glyph width appears smaller
+             * than 1% of the width specified in setcachedevice.
+             * A Td instruction will be generated instead.
+             * Note that the value 990 is arbitrary and may need a
+             * further adjustment.
+             */
+             /* Revised the above. It seems unreasonable to use a fixed
+              * value which is not based on the point size, when the problem is
+              * caused by a large point size being multiplied by the width. The
+              * original fix also caused bitmap fonts (from PCL and other sources)
+              * to fail to use kerning, as these fonts are scaled to 1 point and
+              * therefore use large kerning values. Instead we check the kerned value
+              * multiplied by the point size of the font.
+              */
+            (((tdw >= -MAX_USER_COORD && (tdw * pts->in.size) < MAX_USER_COORD) || pdev->PDFA != 1))
+            ) {
             /* Use TJ. */
             int code;
 
-            code = append_text_move(pts, tdw);
+            if (tdw < MAX_USER_COORD || pdev->PDFA != 1)
+                code = append_text_move(pdev, pts, tdw);
+            else
+                return -1;
+
             if (code >= 0)
                 goto finish;
         }

commit 81d1fa3c928511c6e1a9828c70abfe34f280fcd3

pdfwrite - fix PDF/A co-ordinate clamping
Bug #707811 " Unreadable document due to font with GS 10"

The problem was introduced with commit
949be69c7039afa848095acbd6d1cb772ed8510c which was itself a fix for
commit 3156b0aa8ac4e9ef440c169799658da249eb41c9

Both of those commits relate to limiting the co-ordinate space we use;
originally limited to +/-16384, to work with Acrobat 4, which we
removed, then reinstated for PDF/A only, but with a limit of 32768
because the PDF/A specification pickles the limitations of Acrobat 5
into the spec.

There was a small error in one of the checks; we pass in a rectangular
bounding box of a path to see if we need to scale it. The checking
arithmetic was using the bbox values verbatim, but in fact these are
fixed precision 24.8 values, not plain floating point.

This resulted in us scaling paths 1/256th the maximum size.

The example file has a type 3 font with a FontMatrix scaling the font
co-ordinate space by 1000, this was enough to trigger the rescaling
when it should not have, resulting in the output being incorrectly
scaled.

devices/vector/gdevpdfd.c
@@ -840,8 +840,8 @@ make_rect_scaling(const gx_device_pdf *pdev, const gs_fixed_rect *bbox,
         return false;
     }
 
-    bmin = min(bbox->p.x / pdev->scale.x, bbox->p.y / pdev->scale.y) * prescale;
-    bmax = max(bbox->q.x / pdev->scale.x, bbox->q.y / pdev->scale.y) * prescale;
+    bmin = min(fixed2float(bbox->p.x) / pdev->scale.x, fixed2float(bbox->p.y) / pdev->scale.y) * prescale;
+    bmax = max(fixed2float(bbox->q.x) / pdev->scale.x, fixed2float(bbox->q.y) / pdev->scale.y) * prescale;
     if (bmin <= int2fixed(-MAX_USER_COORD) ||
         bmax > int2fixed(MAX_USER_COORD)
         ) {