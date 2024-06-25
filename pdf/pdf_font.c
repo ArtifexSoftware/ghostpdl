04618d7220fd2db89c966da9a416b8618cba898f
Ken Sharp <Ken.Sharp@artifex.com>
2024-06-25 08:11:51 +0100

    Coverity ID #427571 - fix overwrite of variable
    'code' wasn't being checked.

--- pdf/pdf_font.c
+++ pdf/pdf_font.c
@@ -1089,7 +1089,7 @@ int pdfi_load_font(pdf_context *ctx, pdf_dict *stream_dict, pdf_dict *page_dict,
             if (fontfile != NULL) {
                 code = pdfi_stream_to_buffer(ctx, (pdf_stream *) fontfile, &fbuf, &fbuflen);
                 pdfi_countdown(fontfile);
-                if (fbuflen == 0) {
+                if (code < 0 || fbuflen == 0) {
                     char obj[129];
                     pdfi_print_cstring(ctx, "**** Warning: cannot process embedded stream for font object ");
                     gs_snprintf(obj, 128, "%d %d\n", (int)font_dict->object_num, (int)font_dict->generation_num);
