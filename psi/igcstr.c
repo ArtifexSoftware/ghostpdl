aae327387727bc7f2ee540dfa749acef112c8093
Chris Liddell <chris.liddell@artifex.com>
2024-07-02 14:37:24 +0100

    Coverity 427587: Add some bit masking to avoid overflow warnings

--- psi/igcstr.c
+++ psi/igcstr.c
@@ -66,7 +66,7 @@ gc_mark_string(const byte * ptr, uint size, bool set, const clump_t * cp)
     uint offset = ptr - cp->sbase;
     bword *bp = (bword *) (cp->smark + ((offset & -bword_bits) >> 3));
     uint bn = offset & (bword_bits - 1);
-    bword m = bword_1s << bn;
+    bword m = (bword_1s << bn) & bword_1s;
     uint left = size;
     bword marks = 0;
 
@@ -84,7 +84,7 @@ gc_mark_string(const byte * ptr, uint size, bool set, const clump_t * cp)
         }
         if (left) {
             bword_swap_bytes(m);
-            m -= m << left;
+            m = (m - (m << left)) & bword_1s;
             bword_swap_bytes(m);
             marks |= ~*bp & m;
             *bp |= m;
@@ -105,7 +105,7 @@ gc_mark_string(const byte * ptr, uint size, bool set, const clump_t * cp)
         }
         if (left) {
             bword_swap_bytes(m);
-            m -= m << left;
+            m = (m - (m << left)) & bword_1s;
             bword_swap_bytes(m);
             *bp &= ~m;
         }
