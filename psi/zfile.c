ea69a1388245ad959d31c272b5ba66d40cebba2c
Zdenek Hutyra <zhutyra@centrum.cz>
2024-07-23 11:48:39 +0100

    PostScript interpreter - fix buffer length check
    Bug 707895
    
    See bug report for details.
    
    CVE-2024-46956

--- psi/zfile.c
+++ psi/zfile.c
@@ -443,7 +443,7 @@ file_continue(i_ctx_t *i_ctx_p)
         if (code == ~(uint) 0) {    /* all done */
             esp -= 6;               /* pop proc, pfen, scratch, devlen, iodev , mark */
             return o_pop_estack;
-        } else if (code > len) {      /* overran string */
+        } else if (code > len - devlen) {      /* overran string */
             return_error(gs_error_rangecheck);
         }
         else if (iodev != iodev_default(imemory)
