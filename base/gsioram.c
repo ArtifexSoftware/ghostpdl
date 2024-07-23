f6ffb675946cd2581735aae976751df1f10bb3ca
Zdenek Hutyra <zhutyra@centrum.cz>
2024-07-23 11:45:27 +0100

    Ram device - don't enumerate non-GC'ed object
    Bug 707896 "Crash in garbage collection for ramfs file enumerator"
    
    Listing a pointer to a non-GC'ed object for enumeration by the garbage
    collector leads to a crash (unsurprisingly).

--- base/gsioram.c
+++ base/gsioram.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2023 Artifex Software, Inc.
+/* Copyright (C) 2001-2024 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
@@ -86,8 +86,8 @@ typedef struct gsram_enum_s {
     gs_memory_t *memory;
 } gsram_enum;
 
-gs_private_st_ptrs3(st_gsram_enum, struct gsram_enum_s, "gsram_enum",
-    gsram_enum_enum_ptrs, gsram_enum_reloc_ptrs, pattern, e, memory);
+gs_private_st_ptrs2(st_gsram_enum, struct gsram_enum_s, "gsram_enum",
+    gsram_enum_enum_ptrs, gsram_enum_reloc_ptrs, pattern, memory);
 
 /* could make this runtime configurable later.  It doesn't allocate
    all the blocks in one go so it's not critical */
