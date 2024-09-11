ArtifexSoftware/ghostpdl

commit f33ebdd329c424d5389d9e1aaaf84197d4e0b6fa

pdfwrite - improver preservation of clip text rendering modes
This arises from a customer file, but we already knew the code was less
than optimal, and there were comments describing why.

Because of the way the old (PostScript) PDF interpreter worked, when we
drew text with a text rendering mode which involved clipping (4, 5, 6
and 7) we would write the text and then separately write a path and
clip to that path. This results in larger, slower, output and the
path is written at the resolution of the device. If pdfwrite was run at,
say, 72 dpi, then small text would end up creating a path which was
unusable at higher resolution.

With the new PDF interpreter we can revisit that, and this commit does
so.

The pdfwrite device doesn't write text until it needs to, which allows
it to often write a more compact version, and it also executes a q
(gsave) and Q (grestore) round it. We can't do that with text involving
a clip, as the grestore woould restore away the clip.

This commit alters the PDF interpreter so that it sends a spec_op to the
device, if it is preserving text rendering modes, when we switch to a
mode which involves a clip. The itnerpreter remembers this has been done
and when we restore back to a state where the text rednering mode does
not involve a clip it again sends a spec_op to the device to inform it
that the clip should be removed.

Note that the spec specifically forbids switching from a clipping mode
to a non-clipping mode.

In the pdfwrite device we set a flag whenever we get a spec_op telling
us that we have switched to a clipping mode. At that point we flush any
accumulated text and do a gsave. From then on we do not gsave/grestore
around text operations. When we are informed by the interpreter that
we have restored back to the point where the text mode is non-clipping
we again flush any accumulated text, and issue a grestore to match the
gsave.

This results in a few files in the test suite producing smaller output
but no actual differences.

devices/vector/gdevpdfi.c
@@ -3010,6 +3010,48 @@ gdev_pdf_dev_spec_op(gx_device *pdev1, int dev_spec_op, void *data, int size)
                 pdev->PendingOC = *object;
             }
             break;
+        case gxdso_hilevel_text_clip:
+            if (data == 0) {
+                /* We are exiting a text render mode 'clip' by grestoring back to
+                 *  a time when the clip wasn't active.
+                 * First, check if we have a clip (this should always be true).
+                 */
+                if (pdev->vgstack[pdev->vgstack_depth].clipped_text_pending) {
+                    /* Get back to the content stream. This will (amongst other things) flush
+                     * any pending text.
+                     */
+                    code = pdf_open_page(pdev, PDF_IN_STREAM);
+                    if (code < 0)
+                        return code;
+                    /* Reset the pending state */
+                    pdev->vgstack[pdev->vgstack_depth].clipped_text_pending = 0;
+                    /* Restore to our saved state */
+                    code = pdf_restore_viewer_state(pdev, pdev->strm);
+                    if (code < 0)
+                        return code;
+                    pdf_reset_text(pdev);	/* because of Q */
+                }
+            } else {
+                /* We are starting text in a clip mode
+                 * First make sure we aren't already in a clip mode (this shuld never be true)
+                 */
+                if (!pdev->vgstack[pdev->vgstack_depth].clipped_text_pending) {
+                    /* Return to the content stream, this will (amongst other things) flush
+                     * any pending text.
+                     */
+                    code = pdf_open_page(pdev, PDF_IN_STREAM);
+                    if (code < 0)
+                        return code;
+                    /* Save the current graphics state (or at least that bit which we track) so
+                     * that we can put it back later.
+                     */
+                    code = pdf_save_viewer_state(pdev, pdev->strm);
+                    if (code < 0)
+                        return code;
+                    pdev->vgstack[pdev->vgstack_depth].clipped_text_pending = 1;
+                }
+            }
+            break;
         case gxdso_get_dev_param:
             {
                 int code;

devices/vector/gdevpdfx.h
@@ -515,6 +515,7 @@ typedef struct pdf_viewer_state_s {
     float *dash_pattern;
     uint dash_pattern_size;
     gs_id soft_mask_id;
+    bool clipped_text_pending;
 } pdf_viewer_state;
 
 /*

base/gxdevsop.h
@@ -527,6 +527,12 @@ enum {
      */
     gxdso_is_sep_supporting_additive_device,
 
+    /* Communicate the start of text with a 'clip' render mode, and the grestore
+     * which restores it away. We send data of 1 for the start of text in a clipping
+     * mode and 0 when we grestore back to a non-clip mode.
+     */
+    gxdso_hilevel_text_clip,
+
     /* Add new gxdso_ keys above this. */
     gxdso_pattern__LAST
 };

devices/vector/gdevpdfg.c
@@ -41,6 +41,8 @@
 #include "gsccolor.h"
 #include "gxcdevn.h"
 #include "gscie.h"
+#include "gdevpdtx.h"
+#include "gdevpdts.h"
 
 /* ------ Exported by gdevpdfc.c for gdevpdfg.c ------ */
 int pdf_make_sampled_base_space_function(gx_device_pdf *pdev, gs_function_t **pfn,
@@ -96,6 +98,10 @@ pdf_save_viewer_state(gx_device_pdf *pdev, stream *s)
     pdev->vgstack[i].line_params = pdev->state.line_params;
     pdev->vgstack[i].line_params.dash.pattern = 0; /* Use pdev->dash_pattern instead. */
     pdev->vgstack[i].soft_mask_id = pdev->state.soft_mask_id; /* Use pdev->dash_pattern instead. */
+    if (i > 0)
+        pdev->vgstack[i].clipped_text_pending = pdev->vgstack[i - 1].clipped_text_pending;
+    else
+        pdev->vgstack[i].clipped_text_pending = 0;
     if (pdev->dash_pattern) {
         if (pdev->vgstack[i].dash_pattern)
             gs_free_object(pdev->memory->non_gc_memory, pdev->vgstack[i].dash_pattern, "free gstate copy dash");

devices/vector/gdevpdfu.c
@@ -45,6 +45,9 @@
 #include "gs_mgl_e.h"
 #include "gs_mro_e.h"
 
+#include "gdevpdtx.h"
+#include "gdevpdts.h"
+
 extern single_glyph_list_t SingleGlyphList[];
 
     /* Define the size of internal stream buffers. */
@@ -1159,6 +1162,16 @@ stream_to_text(gx_device_pdf * pdev)
 {
     int code;
 
+    /* This is to handle clipped text rendering modes. If we have started
+     * a text clipping operation (signalled via a spec_op) then we do
+     * *NOT* want to save the viewer state as that will write a q/Q round the text
+     * which will discard the clip.
+     */
+    if (!pdev->vgstack[pdev->vgstack_depth].clipped_text_pending) {
+        code = pdf_save_viewer_state(pdev, pdev->strm);
+        if (code < 0)
+            return 0;
+    }
     /*
      * Bizarrely enough, Acrobat Reader cares how the final font size is
      * obtained -- the CTM (cm), text matrix (Tm), and font size (Tf)
@@ -1167,9 +1180,6 @@ stream_to_text(gx_device_pdf * pdev)
      * anti-alias characters.  Therefore, we have to temporarily patch
      * the CTM so that the scale factors are unity.  What a nuisance!
      */
-    code = pdf_save_viewer_state(pdev, pdev->strm);
-    if (code < 0)
-        return 0;
     pprintg2(pdev->strm, "%g 0 0 %g 0 0 cm BT\n",
              pdev->HWResolution[0] / 72.0, pdev->HWResolution[1] / 72.0);
     pdev->procsets |= Text;
@@ -1191,10 +1201,21 @@ text_to_stream(gx_device_pdf * pdev)
     int code;
 
     stream_puts(pdev->strm, "ET\n");
-    code = pdf_restore_viewer_state(pdev, pdev->strm);
-    if (code < 0)
-        return code;
-    pdf_reset_text(pdev);	/* because of Q */
+    /* This is to handle clipped text rendering modes. If we are in
+     * a text clipping operation (signalledd via a spec_op) then we do
+     * *NOT* want to restore the viewer state as that will write a q/Q round the text
+     * which will discard the clip. However, we *do* have to undo the 'cm'
+     * operation which is done for Acrobat (see stream_to_text above).
+     */
+    if (pdev->vgstack[pdev->vgstack_depth].clipped_text_pending)
+        pprintg2(pdev->strm, "%g 0 0 %g 0 0 cm\n",
+             72.0 / pdev->HWResolution[0], 72.0 / pdev->HWResolution[1]);
+    else {
+        code = pdf_restore_viewer_state(pdev, pdev->strm);
+        if (code < 0)
+            return code;
+        pdf_reset_text(pdev);	/* because of Q */
+    }
     return PDF_IN_STREAM;
 }
 /* Exit stream context. */

pdf/pdf_gstate.c
@@ -46,6 +46,7 @@
 #include "gscolor3.h"       /* For gs_setsmoothness() */
 #include "gzpath.h"
 #include "gspenum.h"
+#include "gxdevsop.h"               /* For special ops */
 
 static const char *blend_mode_names[] = {
     GS_BLEND_MODE_NAMES, 0
@@ -222,7 +223,20 @@ int pdfi_op_Q(pdf_context *ctx)
             return code;
     }
 
-    return pdfi_grestore(ctx);
+    code = pdfi_grestore(ctx);
+
+    /* Special hackery for pdfwrite. If we have started doing text with a clip render mode
+     * and the output is pdfwrite, and the current (restored gstate does not have a text clipping
+     * render mode, then we need to tell pdfwrite that it must emit a 'Q'.
+     */
+    if (code >= 0 && ctx->device_state.preserve_tr_mode && ctx->text.TextClip && gs_currenttextrenderingmode(ctx->pgs) < 4) {
+        gx_device *dev = gs_currentdevice_inline(ctx->pgs);
+
+        ctx->text.TextClip = 0;
+        dev_proc(dev, dev_spec_op)(dev, gxdso_hilevel_text_clip, 0, 1);
+    }
+
+    return code;
 }
 
 /* We want pdfi_grestore() so we can track and warn of "too many Qs"

pdf/pdf_text.c
@@ -32,6 +32,7 @@
 #include "gdevbbox.h"
 #include "gspaint.h"        /* For gs_fill() and friends */
 #include "gscoord.h"        /* For gs_setmatrix() */
+#include "gxdevsop.h"               /* For special ops */
 
 static int pdfi_set_TL(pdf_context *ctx, double TL);
 
@@ -55,25 +56,10 @@ int pdfi_BT(pdf_context *ctx)
     if (code < 0)
         return code;
 
-    /* In theory we should not perform the clip (for text rendering modes involving a clip)
-     * when preserving the text rendering mode. However the pdfwrite device requires us to do
-     * so. The reason is that it wraps all text operations in a sequence like 'q BT...ET Q'
-     * and the grestore obviously restores away the clip before any following operation can use it.
-     * The reason pdfwrite does this is historical; originally the PDF graphics state was not
-     * part of the graphics library graphics state, so the only information available to
-     * the device was the CTM, after the text rendering matrix and text line matrix had been
-     * applied. Obviously that had to be undone at the end of the text to avoid polluting
-     * the CTM for following operations.
-     * Now that we track the Trm and Tlm in the graphics state it would be possible to
-     * modify pdfwrite so that it emits those instead of modifying the CTM, which would avoid
-     * the q/Q pair round the text, which would mean we could do away with the separate
-     * clip for text rendering. However this would mean modifying both the pdfwrite device
-     * and the existing PDF interpreter, which would be awkward to do. I will open an
-     * enhancement bug for this, but won't begin work on it until we have completely
-     * deprecated and removed the old PDF interpreter.
-     * In the meantime we have to persist with this kludge.
+    /* We should not perform the clip (for text rendering modes involving a clip)
+     * when preserving the text rendering mode.
      */
-    if (gs_currenttextrenderingmode(ctx->pgs) >= 4 && ctx->text.BlockDepth == 0 /* && !ctx->device_state.preserve_tr_mode*/) {
+    if (gs_currenttextrenderingmode(ctx->pgs) >= 4 && ctx->text.BlockDepth == 0 && !ctx->device_state.preserve_tr_mode) {
         /* Whenever we are doing a 'clip' text rendering mode we need to
          * accumulate a path until we reach ET, and then we need to turn that
          * path into a clip and apply it (along with any existing clip). But
@@ -116,32 +102,34 @@ static int do_ET(pdf_context *ctx)
      */
 
     /* See the note on text rendering modes with clip in pdfi_BT() above */
-    if (ctx->text.BlockDepth == 0 && gs_currenttextrenderingmode(ctx->pgs) >= 4 /*&& !ctx->device_state.preserve_tr_mode*/) {
+    if (ctx->text.BlockDepth == 0 && gs_currenttextrenderingmode(ctx->pgs) >= 4) {
         gs_point initial_point;
 
-        ctx->text.TextClip = false;
-        /* Capture the current position */
-        code = gs_currentpoint(ctx->pgs, &initial_point);
-        if (code >= 0) {
-            gs_point adjust;
+        if  (!ctx->device_state.preserve_tr_mode) {
+            ctx->text.TextClip = false;
+            /* Capture the current position */
+            code = gs_currentpoint(ctx->pgs, &initial_point);
+            if (code >= 0) {
+                gs_point adjust;
 
-            gs_currentfilladjust(ctx->pgs, &adjust);
-            code = gs_setfilladjust(ctx->pgs, (double)0.0, (double)0.0);
-            if (code < 0)
-                return code;
+                gs_currentfilladjust(ctx->pgs, &adjust);
+                code = gs_setfilladjust(ctx->pgs, (double)0.0, (double)0.0);
+                if (code < 0)
+                    return code;
 
-            code = gs_clip(ctx->pgs);
-            if (code >= 0)
-                copy = gx_cpath_alloc_shared(ctx->pgs->clip_path, ctx->memory, "save clip path");
+                code = gs_clip(ctx->pgs);
+                if (code >= 0)
+                    copy = gx_cpath_alloc_shared(ctx->pgs->clip_path, ctx->memory, "save clip path");
 
-            code = gs_setfilladjust(ctx->pgs, adjust.x, adjust.y);
-            if (code < 0)
-                return code;
+                code = gs_setfilladjust(ctx->pgs, adjust.x, adjust.y);
+                if (code < 0)
+                    return code;
 
-            pdfi_grestore(ctx);
-            if (copy != NULL)
-                (void)gx_cpath_assign_free(ctx->pgs->clip_path, copy);
-            code = gs_moveto(ctx->pgs, initial_point.x, initial_point.y);
+                pdfi_grestore(ctx);
+                if (copy != NULL)
+                    (void)gx_cpath_assign_free(ctx->pgs->clip_path, copy);
+                code = gs_moveto(ctx->pgs, initial_point.x, initial_point.y);
+            }
         }
     }
     if (ctx->page.has_transparency && gs_currenttextknockout(ctx->pgs))
@@ -805,19 +793,19 @@ static int pdfi_show_Tr_preserve(pdf_context *ctx, gs_text_params_t *text)
         gs_swapcolors_quick(ctx->pgs);
     }
 
-    code = pdfi_show_simple(ctx, text);
-    if (code < 0)
-        return code;
-
-    /* See the comment in pdfi_BT() aboe regarding  text rendering modes and clipping.
-     * NB regardless of the device, we never apply clipping modes to text in a type 3 font.
+    /* If we've switched to aemitting text with a 'clip' rendering mode then we
+     * tell pdfwrite that here, so that it can emit a 'q', which it can later
+     * (see pdfi_op_Q) restore to with a Q. It's the only way to get the clipping
+     * right.
      */
-    if (Trmode >= 4 && current_font->pdfi_font_type != e_pdf_font_type3) {
-        text->operation &= ~TEXT_DO_DRAW;
+    if (Trmode >= 4 && current_font->pdfi_font_type != e_pdf_font_type3 && ctx->text.TextClip == 0) {
+        gx_device *dev = gs_currentdevice_inline(ctx->pgs);
 
-        gs_moveto(ctx->pgs, initial_point.x, initial_point.y);
-        code = pdfi_show_Tr_7(ctx, text);
-    }
+        ctx->text.TextClip = true;
+        dev_proc(dev, dev_spec_op)(dev, gxdso_hilevel_text_clip, 1, 1);
+  }
+
+    code = pdfi_show_simple(ctx, text);
     return code;
 }
 
@@ -1396,24 +1384,18 @@ int pdfi_Tr(pdf_context *ctx)
     if (mode < 0 || mode > 7)
         return_error(gs_error_rangecheck);
 
-/* See comment regarding text rendering modes involving clip in pdfi_BT() above.
- * The commented out code here will be needed when we enhance pdfwrite so that
- * we don't need to do the clip separately.
- */
-/*
+    /* Detect attempts to switch from a clipping mode to a non-clipping
+     * mode, this is defined as invalid in the spec. (We don't warn if we haven't yet
+     * drawn any text in the clipping mode).
+     */
+    if (gs_currenttextrenderingmode(ctx->pgs) > 3 && mode < 4 && ctx->text.BlockDepth != 0 && ctx->text.TextClip)
+        pdfi_set_warning(ctx, 0, NULL, W_PDF_BADTRSWITCH, "pdfi_Tr", NULL);
+
     if (ctx->device_state.preserve_tr_mode) {
         gs_settextrenderingmode(ctx->pgs, mode);
     } else
-*/
     {
         gs_point initial_point;
-
-        /* Detect attempts to switch from a clipping mode to a non-clipping
-         * mode, this is defined as invalid in the spec. (We don't warn if we haven't yet
-         * drawn any text in the clipping mode).
-         */
-        if (gs_currenttextrenderingmode(ctx->pgs) > 3 && mode < 4 && ctx->text.BlockDepth != 0 && ctx->text.TextClip)
-            pdfi_set_warning(ctx, 0, NULL, W_PDF_BADTRSWITCH, "pdfi_Tr", NULL);
 
         if (gs_currenttextrenderingmode(ctx->pgs) < 4 && mode >= 4 && ctx->text.BlockDepth != 0) {
             /* If we are switching from a non-clip text rendering mode to a

commit cc6d078f0901fcb2dd96047ed6333661c1816c33

pdfwrite devide - more work on 'clip' text rendering modes
Bug #708025 "incorrect clipping with text rendering mode 7 and additional clips in side gsaved states"

The problem is caused by commit f33ebdd329c424d5389d9e1aaaf84197d4e0b6fa
which was an enhancement (for the same customer) to deal with text
rendering modes involving clips as text, instead of writing out a long
path representing the shape of the text. Partly because when using
pdfwrite at low resolution (72 dpi) the resulting path is 'blocky' as it
is created at the resolution of the device, eg 72 dpi.

I hadn't appreciated that images, and possibly some other places, would
write out a clip, and if there was one active could end up doing a
grestore to get rid of it, before executing the operation. This leads to
the text clipping being elided in the output.

The fix is more complicated unfortunately. We capture the current
'bottom' of the saved gstate stack, then we update that bottom to point
at the state we have just created.

The 'put_clip' functions look at the difference between the current
depth of the saved gstate stack and the bottom before deciding whether
to emit a clip or a 'Q' to undo an existing clip. By altering the
'bottom' we can prevent them trying to grestore away the clip from the
text.

When we're done with the gstate holding the text clip we restore the
'bottom' value, and then restore back until we reach that point, which
removes the text clip.

devices/vector/gdevpdfb.h
@@ -162,6 +162,7 @@ const gx_device_pdf PDF_DEVICE_IDENT =
  {0, 0},			/* char_width */
  0,				/* clip_path */
  0,             /* clipped_text_pending */
+ 0,             /* saved_vgstack_depth_for_textclip */
  0,                             /* PageLabels */
  -1,                            /* PageLabels_current_page */
  0,                             /* PageLabels_current_label */

devices/vector/gdevpdfi.c
@@ -3062,10 +3062,19 @@ gdev_pdf_dev_spec_op(gx_device *pdev1, int dev_spec_op, void *data, int size)
                     /* Reset the pending state */
                     pdev->clipped_text_pending = 0;
                     /* Restore to our saved state */
-                    if (pdev->vgstack_depth)
+
+                    /* The saved state in this case is the dpeth of the saved gstate stack at the time we
+                     * started the text clipping.
+                     */
+                    if (pdev->vgstack_bottom)
+                        pdev->vgstack_bottom = pdev->saved_vgstack_depth_for_textclip;
+
+                    while (pdev->vgstack_depth > pdev->vgstack_bottom) {
                         code = pdf_restore_viewer_state(pdev, pdev->strm);
-                    if (code < 0)
-                        return code;
+                        if (code < 0)
+                            return code;
+                    }
+
                     pdf_reset_text(pdev);	/* because of Q */
                 }
             } else {
@@ -3086,6 +3095,19 @@ gdev_pdf_dev_spec_op(gx_device *pdev1, int dev_spec_op, void *data, int size)
                     if (code < 0)
                         return code;
                     pdev->clipped_text_pending = 1;
+
+                    /* Save the current 'bottom' of the saved gstate stack, we need to
+                     * restore back to this state when we exit the graphics state
+                     * with a text rendering mode involving a clip.
+                     */
+                    pdev->saved_vgstack_depth_for_textclip = pdev->vgstack_bottom;
+                    /* And push the bottom of the stack up until it is where we are now.
+                     * This is because clip paths, images, and possibly other constructs
+                     * will emit a clip path if the 'depth - bottom' is not zero, to create
+                     * a clip path. We want to make sure that it doesn't try to restore back
+                     * to a point before we established the text clip.
+                     */
+                    pdev->vgstack_bottom = pdev->vgstack_depth;
                 }
             }
             break;

devices/vector/gdevpdfx.h
@@ -795,6 +795,7 @@ struct gx_device_pdf_s {
 
     /* Used for preserving text rendering modes with clip. */
     bool clipped_text_pending;
+    int saved_vgstack_depth_for_textclip;
 
     /*
      * Page labels.

devices/vector/gdevpdtt.c
@@ -510,6 +510,16 @@ pdf_prepare_text_drawing(gx_device_pdf *const pdev, gs_text_enum_t *pte)
             code = pdf_put_clip_path(pdev, pcpath);
             if (code < 0)
                 return code;
+
+            /* If we have text in a clipping mode we need to update the saved 'bottom'
+             * of the saved gstate stack. This is the point we will use for potentially
+             * writing out any additional clip path, or clips used by images. We *don't*
+             * update the 'saved bottom' (saved_vgstack_depth_for_textclip) because that's
+             * what we restore back to after we grestore back to a time when the text
+             * rendering mode didn't involve clip.
+             */
+            if (pdev->clipped_text_pending)
+                pdev->vgstack_bottom = pdev->vgstack_depth;
         }
 
         /* For ps2write output, and for any 'type 3' font we need to write both the stroke and fill colours