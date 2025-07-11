e97eabe828b9f653547c123c4d86da88ba06ad5e
Robin Watts <Robin.Watts@artifex.com>
2025-07-11 11:50:34 +0100

    Fix another int * int overflow buffer calculation.
    Thanks to Stefan Leitermann here!

--- base/gdevprn.c
+++ base/gdevprn.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2024 Artifex Software, Inc.
+/* Copyright (C) 2001-2025 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
@@ -1553,7 +1553,7 @@ gx_default_setup_buf_device(gx_device *bdev, byte *buffer, int raster,
         mdev->foreign_line_pointers = false;
     }
     mdev->height = full_height;
-    code = gdev_mem_set_line_ptrs(mdev, buffer + raster * y, raster,
+    code = gdev_mem_set_line_ptrs(mdev, buffer + (intptr_t)raster * (intptr_t)y, raster,
                                   ptrs, setup_height);
     mdev->height = setup_height;
     bdev->height = setup_height; /* do here in case mdev == bdev */
