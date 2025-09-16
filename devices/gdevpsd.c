ccea8478247ca6ddea3302d993635403f9e5ceb5
Robin Watts <Robin.Watts@artifex.com>
2025-09-16 18:10:18 +0100

    Fix psd devices use of downscaler.
    We were counting the number of colors that we needed to output,
    and initialising the downscaler to that number of planes. This
    works fine for PDF files, but can go wrong for postscript ones,
    especially where tags are involved.
    
    For postscript files, we might only have CMYK + Tags, but the
    color setup will be that we have 16 colors, with tags in position
    15.
    
    When we call the downscaler to 'getbits' it would only fetch the
    first 5 colors, and hence the tags value would be garbage.
    
    Instead fetch all the planes it can give us, and we'll pick the
    correct ones to output.

--- devices/gdevpsd.c
+++ devices/gdevpsd.c
@@ -1760,7 +1760,7 @@ psd_write_image_data(psd_write_ctx *xc, gx_device_printer *pdev)
         return_error(gs_error_VMerror);
 
     code = gx_downscaler_init_planar(&ds, (gx_device *)pdev,
-                                     bpc, bpc, num_comp,
+                                     bpc, bpc, pdev->color_info.num_components,
                                      &psd_dev->downscale,
                                      &params);
     if (code < 0)
