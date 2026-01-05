ArtifexSoftware/ghostpdl

commit 82e72fd6eeb43e850d3c473272558b3c22796da7

Contrib devices - fix image fallback handling
Bug 708865 "Null pointer dereference when using the lips4v output device"

The problem here is, simply, that the 'fallback' handling in the lips4v
device simply doesn't work. At all. It looks likely that it never has,
it certainly hasn't worked since the introduction of type 3 and 4 images
in PostScript language level 3.

Hardly surprising since the copyright date of the device is 1999, the
same year level 3 was published.

There is code that purports to handle type 3 and 4 images, by falling
back and letting the graphics library handle them, then catching the
data at a lower level (copy_mono and copy_color), but the type 3 image
code requires a 'planes_wanted' in the image enumerator (which the device
doesn't provide) and the code didn't copy the relevant 'common' portion
of the fallback enumerator to its own custom enumerator, and that is
used by the interpreter, which falls in a heap because the data isn't
initialised.

So this is actually fairly simple. After creating the fallback
enumerator (which we store in the vector device's 'default_info') we copy
the common portion from that into the custom enumerator. That means the
interpreter now has sensible data to work with.

We create 'planes_wanted' and 'flush' methods for the custom enumerator
'procs'. These simply pass on the call to the fallback enumerator, the
'image_plane_data' and 'end_image' methods already do this. This means
that the graphics library now has the required methods available.

This did require a small change in the vector device implementation, so
that we don't try and use a non-existent 'bbox_info'.

Desk-checking it looks very much like the Open Printing device has
exactly the same problem, as does the 'escv' device(s). So updated
those similarly.

This cures the crash in the lips4v device, but I have no way to
determine if the output is reasonable. The same for the escv and
opvp devices.

contrib/opvp/gdevopvp.c
@@ -321,6 +321,8 @@ static  dev_proc_fill_rectangle(opvp_fill_rectangle);
 static  dev_proc_begin_typed_image(opvp_begin_typed_image);
 static  image_enum_proc_plane_data(opvp_image_plane_data);
 static  image_enum_proc_end_image(opvp_image_end_image);
+static  image_enum_proc_flush(opvp_image_flush);
+static  image_enum_proc_planes_wanted(opvp_image_planes_wanted);
 
 gs_public_st_suffix_add0_final(
     st_device_opvp,
@@ -1415,7 +1417,9 @@ static  const
 gx_image_enum_procs_t opvp_image_enum_procs =
 {
     opvp_image_plane_data,
-    opvp_image_end_image
+    opvp_image_end_image,
+    opvp_image_flush,
+    opvp_image_planes_wanted
 };
 typedef enum _FastImageSupportMode {
     FastImageDisable,
@@ -4807,6 +4811,28 @@ opvp_image_end_image(gx_image_enum_common_t *info, bool draw_last)
     }
 
     return gdev_vector_end_image(vdev, vinfo, draw_last, vdev->white);
+}
+
+static int
+opvp_image_flush(gx_image_enum_common_t* info)
+{
+    gdev_vector_image_enum_t* pie = (gdev_vector_image_enum_t*)info;
+    gx_device* saved_dev = pie->dev;
+    int code = 0;
+
+    code = pie->default_info->procs->flush(pie->default_info);
+    return code;
+}
+
+static bool
+opvp_image_planes_wanted(const gx_image_enum_common_t* info, byte* wanted)
+{
+    gdev_vector_image_enum_t* pie = (gdev_vector_image_enum_t*)info;
+    if (pie->default_info->procs->planes_wanted)
+        return pie->default_info->procs->planes_wanted(pie->default_info, wanted);
+    else
+        memset(wanted, 0xff, info->num_planes);
+    return true;
 }
 
 /* ----- vector driver procs ----- */

base/gdevvec.c
@@ -956,7 +956,7 @@ gdev_vector_end_image(gx_device_vector * vdev,
         }
         code = 1;
     }
-    if (vdev->bbox_device) {
+    if (vdev->bbox_device && pie->bbox_info) {
         int bcode = gx_image_end(pie->bbox_info, draw_last);
 
         if (bcode < 0)

contrib/eplaser/gdevescv.c
@@ -2382,10 +2382,14 @@ escv_fill_mask(gx_device * dev,
 
 static image_enum_proc_plane_data(escv_image_plane_data);
 static image_enum_proc_end_image(escv_image_end_image);
+static int escv_image_flush(gx_image_enum_common_t* info);
+static bool escv_image_planes_wanted(const gx_image_enum_common_t* info, byte* wanted);
+
 static const gx_image_enum_procs_t escv_image_enum_procs =
   {
-    escv_image_plane_data, escv_image_end_image
-  };
+    escv_image_plane_data, escv_image_end_image,
+    escv_image_flush, escv_image_planes_wanted
+};
 
 /* Start processing an image. */
 static int
@@ -2463,9 +2467,14 @@ escv_begin_typed_image(gx_device               *dev,
   }
   if (!can_do) {
 fallback:
-    return gx_default_begin_typed_image(dev, pgs, pmat, pic, prect,
-                                        pdcolor, pcpath, mem,
-                                        &pie->default_info);
+      code = gx_default_begin_typed_image(dev, pgs, pmat, pic, prect,
+          pdcolor, pcpath, mem,
+          &pie->default_info);
+      if (code >= 0) {
+          memcpy(pie, pie->default_info, sizeof(gx_image_enum_common_t));
+          pie->procs = &escv_image_enum_procs;
+      }
+      return code;
   }
 
   if (pim->ImageMask || (pim->BitsPerComponent == 1 && num_components == 1)) {
@@ -2833,6 +2842,27 @@ escv_image_end_image(gx_image_enum_common_t * info, bool draw_last)
 
   code = gdev_vector_end_image(vdev, (gdev_vector_image_enum_t *) pie, draw_last, pdev->white);
   return code;
+}
+static int
+escv_image_flush(gx_image_enum_common_t* info)
+{
+    gdev_vector_image_enum_t* pie = (gdev_vector_image_enum_t*)info;
+    gx_device* saved_dev = pie->dev;
+    int code = 0;
+
+    code = pie->default_info->procs->flush(pie->default_info);
+    return code;
+}
+
+static bool
+escv_image_planes_wanted(const gx_image_enum_common_t* info, byte* wanted)
+{
+    gdev_vector_image_enum_t* pie = (gdev_vector_image_enum_t*)info;
+    if (pie->default_info->procs->planes_wanted)
+        return pie->default_info->procs->planes_wanted(pie->default_info, wanted);
+    else
+        memset(wanted, 0xff, info->num_planes);
+    return true;
 }
 
 static int escv_write_begin(gx_device *dev, int bits, int x, int y, int sw, int sh, int dw, int dh, int roll)

contrib/lips4/gdevl4v.c
@@ -2153,8 +2153,12 @@ lips4v_fill_mask(gx_device * dev,
 
 static image_enum_proc_plane_data(lips4v_image_plane_data);
 static image_enum_proc_end_image(lips4v_image_end_image);
+static int lips4v_image_flush(gx_image_enum_common_t* info);
+static bool lips4v_image_planes_wanted(const gx_image_enum_common_t* info, byte* wanted);
+
 static const gx_image_enum_procs_t lips4v_image_enum_procs = {
-    lips4v_image_plane_data, lips4v_image_end_image
+    lips4v_image_plane_data, lips4v_image_end_image,
+    lips4v_image_flush, lips4v_image_planes_wanted
 };
 
 /* Start processing an image. */
@@ -2237,9 +2241,14 @@ lips4v_begin_typed_image(gx_device               *dev,
     }
     if (!can_do) {
 fallback:
-        return gx_default_begin_typed_image(dev, pgs, pmat, pic, prect,
+        code = gx_default_begin_typed_image(dev, pgs, pmat, pic, prect,
                                             pdcolor, pcpath, mem,
                                             &pie->default_info);
+        if (code >= 0) {
+            memcpy(pie, pie->default_info , sizeof(gx_image_enum_common_t));
+            pie->procs = &lips4v_image_enum_procs;
+        }
+        return code;
     }
     else if (index == gs_color_space_index_DeviceGray) {
         gx_drawing_color dcolor;
@@ -2456,4 +2465,25 @@ lips4v_image_end_image(gx_image_enum_common_t * info, bool draw_last)
     code = gdev_vector_end_image(vdev, (gdev_vector_image_enum_t *) pie,
                                  draw_last, pdev->white);
     return code;
+}
+static int
+lips4v_image_flush(gx_image_enum_common_t* info)
+{
+    gdev_vector_image_enum_t* pie = (gdev_vector_image_enum_t*)info;
+    gx_device* saved_dev = pie->dev;
+    int code = 0;
+
+    code = pie->default_info->procs->flush(pie->default_info);
+    return code;
+}
+
+static bool
+lips4v_image_planes_wanted(const gx_image_enum_common_t* info, byte* wanted)
+{
+    gdev_vector_image_enum_t* pie = (gdev_vector_image_enum_t*)info;
+    if (pie->default_info->procs->planes_wanted)
+        return pie->default_info->procs->planes_wanted(pie->default_info, wanted);
+    else
+        memset(wanted, 0xff, info->num_planes);
+    return true;
 }

commit 92e0676b3cb873d4c239a2e34f2e54672493f827

Fix opvp device
Bug # 709006 "opvp driver crashes in git master"

As noted by the reporter, this was an accidental error introduced in
82e72fd6eeb43e850d3c473272558b3c22796da7 which was a fix for a different
device. The reason the opvp device was changed was because it used the
same code structure, and was vulnerable to the same bug.

Unfortunately the opvp device is not quite the same as a normal
rendering device, being based off the 'vector device' template. This
means it stores two 'child' enumerators, one of which is normally NULL.

Because we were not chekcing for that (the default_info member of an
imaeg enumerator cannot normally be NULL), we could end up trying to
dereference a NULL pointer.

Fixed here by checking to see which enumerator we should be using.

contrib/opvp/gdevopvp.c
@@ -4819,7 +4819,10 @@ opvp_image_flush(gx_image_enum_common_t* info)
     gdev_vector_image_enum_t* pie = (gdev_vector_image_enum_t*)info;
     int code = 0;
 
-    code = pie->default_info->procs->flush(pie->default_info);
+    if (pie->default_info && pie->default_info->procs->flush != NULL)
+        code = pie->default_info->procs->flush(pie->default_info);
+    if (pie->bbox_info && pie->bbox_info->procs->flush != NULL)
+        code = pie->bbox_info->procs->flush(pie->bbox_info);
     return code;
 }
 
@@ -4827,10 +4830,13 @@ static bool
 opvp_image_planes_wanted(const gx_image_enum_common_t* info, byte* wanted)
 {
     gdev_vector_image_enum_t* pie = (gdev_vector_image_enum_t*)info;
-    if (pie->default_info->procs->planes_wanted)
+    if (pie->default_info && pie->default_info->procs->planes_wanted)
         return pie->default_info->procs->planes_wanted(pie->default_info, wanted);
-    else
+    else {
+        if (pie->bbox_info && pie->bbox_info->procs->planes_wanted)
+            return pie->bbox_info->procs->planes_wanted(pie->bbox_info, wanted);
         memset(wanted, 0xff, info->num_planes);
+    }
     return true;
 }
 