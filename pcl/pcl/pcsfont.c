310e1274edf5253520f4edb158d81d6b7c0e6c2d
Chris Liddell <chris.liddell@artifex.com>
2026-03-19 16:06:32 +0000

    Bug 709192(1032): Cope with deleting softfonts with synonyms
    Patch from 89db43bb057f42f5380b15769f8a86a178a878aa reapplied due to mistake
    copying newer changes to the source file.

--- pcl/pcl/pcsfont.c
+++ pcl/pcl/pcsfont.c
@@ -174,6 +174,14 @@ pcl_font_control(pcl_args_t * pargs, pcl_state_t * pcs)
                     code = pcl_delete_soft_font(pcs, key.data, key.size, value);
                     if (code < 0)
                         return code;
+                    /* Since deleting the softfont also deletes any "synonyms" in
+                       the dictionary, it means the dictionary contents can change
+                       under our feet.
+                       The simplest solution is to start the enumeration with
+                       a clean slate. This isn't used often enough for the
+                       efficiency to be a worry.
+                     */
+                    pl_dict_enum_stack_begin(&pcs->soft_fonts, &denum, false);
                 }
             break;
         case 2:
