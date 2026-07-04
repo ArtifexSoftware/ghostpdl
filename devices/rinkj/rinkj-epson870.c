f9776ade0de87dca9262f1274a0b836cf76ffd80
Ken Sharp <Ken.Sharp@artifex.com>
2026-07-04 09:19:14 +0100

    Coverity ID #504618
    Silence a trivial Coverity warning.

--- devices/rinkj/rinkj-epson870.c
+++ devices/rinkj/rinkj-epson870.c
@@ -118,8 +118,7 @@ rinkj_escp_set (RinkjDevice *self, const char *config)
           z->bps = atoi (val);
           if (z->bps < 1 || z->bps > 16) {
               free (key);
-              if (val)
-                free (val);
+              free (val);
               z->bps = 1;
               return_error(gs_error_configurationerror);
           }
